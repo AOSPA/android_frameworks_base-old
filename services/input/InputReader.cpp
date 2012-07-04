746c68c89a957c22c45584e358d76e2878f83e70
Volker Braun <vbraun.name@gmail.com>
2012-07-04 22:29:13 +0100

    Disable touch while using the stylus (Galaxy Note)
    Android 4.0 cannot form multitouch events from multiple input
    devices. So it is not possible to report one touchpoint from the
    stylus position and, at the same time, another touchpoint from a
    finger touch. Instead, when a new input device starts up the currently
    active input is cancelled. This is highly undesirable while writing
    with the pen.
    
    The easiest solution is to ignore non-stylus touch events while the
    stylus is within range (hovering) of the touchscreen. For example,
    N-trig digitizers implement this in hardware. But wacom digitizers do
    report pen data simultaneously with touch data. This patch disables
    (non-stylus) touch input within 50ms of stylus data. On my Galaxy
    Note this is necessary to make stylus input usable.
    
    Change-Id: I9f458d85d8bf83d6cf03b467d46ce066fd55b7e0
    
    Conflicts:
    	services/input/InputReader.cpp
    	services/input/InputReader.h

--- services/input/InputReader.cpp
+++ services/input/InputReader.cpp
@@ -5410,6 +5410,7 @@ void TouchInputMapper::dispatchPointerStylus(nsecs_t when, uint32_t policyFlags)
         mPointerSimple.currentProperties.id = 0;
         mPointerSimple.currentProperties.toolType =
                 mCurrentCookedPointerData.pointerProperties[index].toolType;
+        mLastStylusTime = when;
     } else {
         down = false;
         hovering = false;
@@ -5490,6 +5491,11 @@ void TouchInputMapper::dispatchPointerSimple(nsecs_t when, uint32_t policyFlags,
         } else if (!down && !hovering && (mPointerSimple.down || mPointerSimple.hovering)) {
             mPointerController->fade(PointerControllerInterface::TRANSITION_GRADUAL);
         }
+    }
+
+    if (rejectPalm(when)) {     // stylus is currently active
+        mPointerSimple.reset();
+        return;
     }
 
     if (mPointerSimple.down && !down) {
@@ -5609,6 +5615,9 @@ void TouchInputMapper::dispatchMotion(nsecs_t when, uint32_t policyFlags, uint32
         const PointerProperties* properties, const PointerCoords* coords,
         const uint32_t* idToIndex, BitSet32 idBits,
         int32_t changedId, float xPrecision, float yPrecision, nsecs_t downTime) {
+
+    if (rejectPalm(when)) return;
+
     PointerCoords pointerCoords[MAX_POINTERS];
     PointerProperties pointerProperties[MAX_POINTERS];
     uint32_t pointerCount = 0;
