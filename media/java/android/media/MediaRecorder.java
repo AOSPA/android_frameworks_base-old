1051dfd4fb26314aa71cdd7027697f544ef2ec74
Surajit Podder <spodder@codeaurora.org>
2013-08-07 14:18:11 +0530

    frameworks/base: Update timelapse quality check
    Updated timelapse quality check to include WQVGA
    
    Change-Id: Ic2ef9ad78cbbc2daa14ff5e95ff3cfb6e10a7529

--- media/java/android/media/MediaRecorder.java
+++ media/java/android/media/MediaRecorder.java
@@ -360,7 +360,7 @@ public class MediaRecorder
         setVideoEncodingBitRate(profile.videoBitRate);
         setVideoEncoder(profile.videoCodec);
         if (profile.quality >= CamcorderProfile.QUALITY_TIME_LAPSE_LOW &&
-             profile.quality <= CamcorderProfile.QUALITY_TIME_LAPSE_QVGA) {
+             profile.quality <= CamcorderProfile.QUALITY_TIME_LAPSE_WQVGA) {
             // Nothing needs to be done. Call to setCaptureRate() enables
             // time lapse video recording.
         } else {
