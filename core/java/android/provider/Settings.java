3eb976dfbe9a40faab785d08dea89ae424f75dc3
maxwen <max.weninger@gmail.com>
2013-10-23 02:22:11 +0200

    [1/2] Base: Expandable volume panel and tweaks
    * Ditch aosp volume panel and use expandable as default, hardcode it.
    * Add VOLUME_LINK_NOTIFICATION option to allow adjusting the separation of notification and ringer volumes (default=1).
    * Add VOLUME_ADJUST_SOUNDS_ENABLED option to allow disabling the volume adjust sound (default=1).
    
    PS1: Signed-off-by: Ricky Salinas <rsalinas1@mail.usf.edu>
    
    PS2: Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Change-Id: I94e0f944f76ee9751663de8ac40d99a342884259

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1961,6 +1961,36 @@ public final class Settings {
             "notifications_use_ring_volume";
 
         /**
+         * Volume Overlay Mode, This is behaviour of the volume overlay panel
+         * Defaults to 1 - which is expandable
+         * @hide
+         */
+        public static final String MODE_VOLUME_OVERLAY = "mode_volume_overlay";
+
+        /** @hide */
+        public static final int VOLUME_OVERLAY_SINGLE = 0;
+        /** @hide */
+        public static final int VOLUME_OVERLAY_EXPANDABLE = 1;
+        /** @hide */
+        public static final int VOLUME_OVERLAY_EXPANDED = 2;
+        /** @hide */
+        public static final int VOLUME_OVERLAY_NONE = 3;
+
+        /**
+         * Volume Adjust Sounds Enable, This is the noise made when using volume hard buttons
+         * Defaults to 1 - sounds enabled
+         * @hide
+         */
+        public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
+
+        /**
+         * Boolean value whether to link ringtone and notification volumes
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Whether silent mode should allow vibration feedback. This is used
          * internally in AudioService and the Sound settings activity to
          * coordinate decoupling of vibrate and silent modes. This setting
