f57233dda04f8ddaf89aafbf631360f0b0fbfec3
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-10-30 19:58:01 +0000

    MiniThumbFile: Fix image/bitmap retrieval
    Change I66005dd69b0a5c8f4353bd7a8225d163a654fd2d changed the insertion
    and creation indexing mechanism, but the retrieval method was still
    using the old sparse one. Fixit
    
    Fixes android.provider.cts.MediaStore_Images_MediaTest#testInsertImageWithBitmap,
    android.provider.cts.MediaStore_Images_MediaTest#testInsertImageWithImagePath,
    android.provider.cts.MediaStore_Images_ThumbnailsTest#testQueryExternalMiniThumbnails,
    and android.provider.cts.MediaStore_Video_ThumbnailsTest#testGetThumbnail
    
    Change-Id: I41feea79fa304635f3908055fb95a8c85718a97f

--- services/java/com/android/server/power/DisplayPowerController.java
+++ services/java/com/android/server/power/DisplayPowerController.java
@@ -378,8 +378,6 @@ final class DisplayPowerController {
     private boolean mTwilightChanged;
     private boolean mAutoBrightnessSettingsChanged;
 
-    private Context mContext;
-
     private KeyguardServiceWrapper mKeyguardService;
 
     private final ServiceConnection mKeyguardConnection = new ServiceConnection() {
@@ -413,7 +411,6 @@ final class DisplayPowerController {
         mDisplayBlanker = displayBlanker;
         mCallbacks = callbacks;
         mCallbackHandler = callbackHandler;
-        mContext = context;
 
         mLights = lights;
         mTwilight = twilight;
