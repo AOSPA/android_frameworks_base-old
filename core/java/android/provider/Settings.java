0f3e8db769abdbaa1bcc9ce20b3dc78dc4a5946e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 04:04:13 +0000

    Swap volume buttons when the screen is rotated (1/2)
    See: http://review.cyanogenmod.org/#/c/14530/
    
    Patch Set 8: Invert the orientation interpretations on tablets
    
    Change-Id: I201febc2827e6fbd347cb0e3f8d3347ad1bc487b
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	media/java/android/media/AudioManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2749,6 +2749,15 @@ public final class Settings {
          */
         public static final String QUICK_SETTINGS_TILES = "quick_settings_tiles";
 
+       /**
+         * Swap volume buttons when the screen is rotated
+         * 0 - Disabled
+         * 1 - Enabled (screen is rotated by 90 or 180 degrees: phone, hybrid)
+         * 2 - Enabled (screen is rotated by 180 or 270 degrees: tablet)
+         * @hide
+         */
+        public static final String SWAP_VOLUME_KEYS_ON_ROTATION = "swap_volume_keys_on_rotation";
+
         /**
          * Setting to show the battery percentage text
          * @hide
