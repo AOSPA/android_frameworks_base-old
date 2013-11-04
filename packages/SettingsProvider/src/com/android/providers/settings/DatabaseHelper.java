d91fcfdf2147054dd7a54eba32d2ff07a1a6acde
Adnan Begovic <adnan@cyngn.com>
2013-11-04 00:58:57 +0000

    Implement linked volumes and add upgrade path.
    Change-Id: Ibbeff656fcaaa4a2ff79de530b48b216df271b21

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -70,7 +70,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // database gets upgraded properly. At a minimum, please confirm that 'upgradeVersion'
     // is properly propagated through your change.  Not doing so will result in a loss of user
     // settings.
-    private static final int DATABASE_VERSION = 113;
+    private static final int DATABASE_VERSION = 116;
 
     private Context mContext;
     private int mUserHandle;
@@ -1825,6 +1825,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 if (stmt != null) stmt.close();
             }
             upgradeVersion = 113;
+        }
+
+        /************* The following are CM-12.0 changes ************/
+
+        if (upgradeVersion < 116) {
+            moveSettingsToNewTable(db, TABLE_SYSTEM, TABLE_SECURE,
+                    new String[] { Settings.Secure.VOLUME_LINK_NOTIFICATION }, true);
+            upgradeVersion = 116;
         }
 
         // *** Remember to update DATABASE_VERSION above!
