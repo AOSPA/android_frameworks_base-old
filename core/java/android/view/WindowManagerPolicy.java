6fe7430996307948197459db6b5af95d97cf79d9
Andre Saddler <dresadd09691@gmail.com>
2013-11-09 11:32:19 -0500

    Advanced PowerMenu
    rebased by Ayysir <dresadd09691@gmail.com>
    ===
    
    [*] screenshot and fastboot
    <author> <drcmda>
    
    [*]Remove safemode reboot option
    
    Duplicated option, holding shutdown option is aosp way
    <author> <D4rKn3sSyS>
    
    [*]New reboot dialog
    
    Nice icons and more options. Not hardcoded, used typedarray.
    Credits to FNV for the original commit and safe mode drawable
    <author> <D4rKn3sSyS>
    
    [*]PowerMenu reboot
    
    Based on Cm implmentation original author unknown
    Change-Id: If478247694f2b4ae7ff4f02daeb0b4a655181751
    <author> <aaronpoweruser>

--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
  *
+ * This code has been modified. Portions copyright (C) 2013, ParanoidAndroid Project.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -417,6 +419,8 @@ public interface WindowManagerPolicy {
 
         public void shutdown(boolean confirm);
         public void rebootSafeMode(boolean confirm);
+        public void reboot(boolean confirm);
+        public void reboot(String reason, boolean confirm);
 
         /**
          * Return the window manager lock needed to correctly call "Lw" methods.
