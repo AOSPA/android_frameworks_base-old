91a90e39270aca5dd793501b2110473ef65df637
Dave Daynard <nardholio@gmail.com>
2013-11-19 18:22:45 -0500

    keyboard: re-add code to detect lid state
    This was lost in the cm-11 merge. Without this, a change in lid state
    isn't generated to light up the keyboard backlight
    
    Change-Id: Ief35cebcb62da13afe1ae7531d69ab58e196be9e

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1567,6 +1567,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
+    private boolean isBuiltInKeyboardVisible() {
+        return mHaveBuiltInKeyboard && !isHidden(mLidKeyboardAccessibility);
+    }
+
     /** {@inheritDoc} */
     @Override
     public void adjustConfigurationLw(Configuration config, int keyboardPresence,
@@ -5192,6 +5196,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void applyLidSwitchState() {
+        mPowerManager.setKeyboardVisibility(isBuiltInKeyboardVisible());
+
         if (mLidState == LID_CLOSED && mLidControlsSleep) {
             mPowerManager.goToSleep(SystemClock.uptimeMillis());
         }
