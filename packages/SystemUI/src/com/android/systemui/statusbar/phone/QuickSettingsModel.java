2bd3b00ddf70646df205c6348735f50125b91054
Carlo Savignano <stevewatersy@gmail.com>
2013-12-07 11:37:32 +0100

    Quicksettings addons.
    * Add lightbulb tile.
      Phone: Click to toggle flash on/off, long click to open the app.
      Tablet (No flash): Click to open the app and make screen white with 100% of brightness.
      Added boolean to check for camera flash availability.
      Thanks Lo Huot for the drawables.
    * Quick pull down from right side of status bar.
    * Long click action for user tile (opens sync settings).
    * Long click action for rotation tile (opens accessibility settings).
    * General cleanup.
    
    Change-Id: I84a90d64cae466b2461be4164ff89b17fa920b84
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
@@ -27,6 +27,7 @@ import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.drawable.Drawable;
+import android.hardware.Camera;
 import android.media.MediaRouter;
 import android.media.MediaRouter.RouteInfo;
 import android.net.ConnectivityManager;
@@ -275,6 +276,10 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
     private RefreshCallback mLocationCallback;
     private State mLocationState = new State();
 
+    private QuickSettingsTileView mLightbulbTile;
+    private RefreshCallback mLightbulbCallback;
+    private State mLightbulbState = new State();
+
     private QuickSettingsTileView mImeTile;
     private RefreshCallback mImeCallback = null;
     private State mImeState = new State();
@@ -345,6 +350,7 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
         refreshRotationLockTile();
         refreshRssiTile();
         refreshLocationTile();
+        refreshLightbulbTile();
     }
 
     // Settings
@@ -637,6 +643,44 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
         mLocationState.label = label;
         mLocationState.iconId = locationIconId;
         mLocationCallback.refreshView(mLocationTile, mLocationState);
+    }
+
+    // Lightbulb
+    boolean deviceHasCameraFlash() {
+        Camera camera = Camera.open();
+        if (camera == null) {
+            return false;
+        }
+
+        Camera.Parameters parameters = camera.getParameters();
+
+        if (parameters.getFlashMode() == null) {
+            camera.release();
+            return false;
+        }
+
+        List<String> supportedFlashModes = parameters.getSupportedFlashModes();
+        if (supportedFlashModes == null
+                || supportedFlashModes.isEmpty() || supportedFlashModes.size() == 1
+                && supportedFlashModes.get(0).equals(Camera.Parameters.FLASH_MODE_OFF)) {
+            camera.release();
+            return false;
+        }
+
+        camera.release();
+        return true;
+    }
+
+    void addLightbulbTile(QuickSettingsTileView view, RefreshCallback cb) {
+        mLightbulbTile = view;
+        mLightbulbCallback = cb;
+        refreshLightbulbTile();
+    }
+
+    void refreshLightbulbTile() {
+        Resources r = mContext.getResources();
+        mLightbulbState.label = r.getString(R.string.quick_settings_lightbulb_label);
+        mLightbulbCallback.refreshView(mLightbulbTile, mLightbulbState);
     }
 
     // Bug report
