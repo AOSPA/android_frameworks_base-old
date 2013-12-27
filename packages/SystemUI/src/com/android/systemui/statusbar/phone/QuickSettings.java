7ee7ec06a2dfeab13f5e7544d7b0ceec4bae65a5
BigBrother1984 <stevewatersy@gmail.com>
2013-12-27 17:53:06 +0100

    Quicksettings: Sleep tile.
    * Click goes to sleep.
    * Long click opens display settings.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Change-Id: I3eeae514535e9edff72f233e18d8f8dfaadf3517

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -44,7 +44,9 @@ import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Handler;
+import android.os.PowerManager;
 import android.os.RemoteException;
+import android.os.SystemClock;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.AlarmClock;
@@ -109,7 +111,8 @@ class QuickSettings {
         IMMERSIVE,
         LTE,
         MOBILENETWORK,
-        LIGHTBULB
+        LIGHTBULB,
+        SLEEP
     }
 
     public static final String NO_TILES = "NO_TILES";
@@ -958,6 +961,31 @@ class QuickSettings {
 
                     parent.addView(lightbulbTile);
                     if(addMissing) lightbulbTile.setVisibility(View.GONE);
+                } else if(Tile.SLEEP.toString().equals(tile.toString())) { // Sleep
+                    final PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+
+                    final QuickSettingsBasicTile sleepTile
+                    = new QuickSettingsBasicTile(mContext);
+                    sleepTile.setTileId(Tile.SLEEP);
+                    sleepTile.setImageResource(R.drawable.ic_qs_sleep);
+                    sleepTile.setTextResource(R.string.quick_settings_sleep_label);
+                    sleepTile.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            pm.goToSleep(SystemClock.uptimeMillis());
+                        }
+                    });
+
+                    sleepTile.setOnLongClickListener(new View.OnLongClickListener() {
+                        @Override
+                        public boolean onLongClick(View v) {
+                            collapsePanels();
+                            startSettingsActivity(android.provider.Settings.ACTION_DISPLAY_SETTINGS);
+                            return true;
+                        }
+                    });
+                    parent.addView(sleepTile);
+                    if(addMissing) sleepTile.setVisibility(View.GONE);
                 }
             }
         }
