6e26d95f324f6c4f24dd7f7bd75731ce5f8eb871
Anu6is <curtis.mayers@gmail.com>
2013-12-28 17:44:53 -0600

    [2/3] Frameworks: Increasing Ringtone
          Option to set the incoming call ringtone volume to increase
          in pre-defined (user selected) increments.
    
    Linked commits:
          Settings   - https://gerrit.paranoidandroid.co/#/c/447/
          Telephony  - https://gerrit.paranoidandroid.co/#/c/451/
    
    Change-Id: I6314108033089c68c3bd6f716508f40fe863f630

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2208,6 +2208,27 @@ public final class Settings {
         public static final String VIBRATE_WHEN_RINGING = "vibrate_when_ringing";
 
         /**
+         * Whether the phone ringtone should be played in an increasing manner
+         * 
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+
+        /**
+         * Increase ringtone volume each ringing interval
+         * 
+         * @hide
+         */
+        public static final String INCREASING_RING_INTERVAL = "increasing_ring_interval";
+
+        /**
+         * Volume level to start at for increasing ringtone
+         * 
+         * @hide
+         */
+        public static final String INCREASING_RING_MIN_VOLUME = "increasing_ring_min_volume";
+
+        /**
          * Whether the audible DTMF tones are played by the dialer when dialing. The value is
          * boolean (1 or 0).
          */
@@ -2593,6 +2614,7 @@ public final class Settings {
             SIP_RECEIVE_CALLS,
             POINTER_SPEED,
             VIBRATE_WHEN_RINGING,
+            INCREASING_RING,
             RINGTONE,
             NOTIFICATION_SOUND
         };
