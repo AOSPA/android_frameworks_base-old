591f85a532a56c814863847307460b255b2afeb4
Muhammed Nazim <mnm9994u@gmail.com>
2014-01-13 03:06:31 +0100

    [3/3] frameworks/base: National Data Roaming
    Credits: CyanogenMod
    
    Linked commits:
            opt/telephony: https://gerrit.paranoidandroid.co/#/c/3655/
            services/Telephony: https://gerrit.paranoidandroid.co/#/c/3656/
    
    Change-Id: I0280d516067e1ef50f7321355cbee42009657d7e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2262,6 +2262,12 @@ public final class Settings {
         public static final String TTY_MODE = "tty_mode";
 
         /**
+         * Whether national data roaming should be used.
+         * @hide
+         */
+        public static final String MVNO_ROAMING = "mvno_roaming";
+
+        /**
          * Whether the sounds effects (key clicks, lid open ...) are enabled. The value is
          * boolean (1 or 0).
          */
