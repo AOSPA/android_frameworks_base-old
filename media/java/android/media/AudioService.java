27f75b9eaa109b442500be3f66fe61a06b630441
Anu6is <curtis.mayers@gmail.com>
2014-02-19 06:28:12 -0600

    Disable safe headset volume warning
    In some countries, regulations require that a warning be displayed
    when the media volume is being raised above recommended level while
    using a headset. As this is regulation we don't simply remove the
    warning, but instead give the user the option to disable it.
    
    In this implementation, the user is presented with the warning at
    least once, at which point they are given the option to disable it
    completely (via on-the-spot preference).
    
    PS1: initial commit
    PS2: update to reflect changes to https://gerrit.paranoidandroid.co/#/c/3702/
    PS3: add new drawable (thanks Arz....finally)
    
    Change-Id: Ib29ddb7f0ed5b1d45ed56d13bd6f7e14d005da79

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -722,6 +722,7 @@ public class AudioService extends IAudioService.Stub {
 
             updateRingerModeAffectedStreams();
             readDockAudioSettings(cr);
+            updateManualSafeMediaVolume();
         }
 
         mLinkNotificationWithVolume = Settings.System.getIntForUser(cr,
@@ -3722,6 +3723,8 @@ public class AudioService extends IAudioService.Stub {
                 Settings.Global.DOCK_AUDIO_MEDIA_ENABLED), false, this);
             mContentResolver.registerContentObserver(Settings.System.getUriFor(
                 Settings.System.VOLUME_LINK_NOTIFICATION), false, this);
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.SAFE_HEADSET_VOLUME), false, this);
         }
 
         @Override
@@ -3740,6 +3743,7 @@ public class AudioService extends IAudioService.Stub {
                     setRingerModeInt(getRingerMode(), false);
                 }
                 readDockAudioSettings(mContentResolver);
+                updateManualSafeMediaVolume();
 
                 mLinkNotificationWithVolume = Settings.System.getIntForUser(mContentResolver,
                         Settings.System.VOLUME_LINK_NOTIFICATION, 1, UserHandle.USER_CURRENT) == 1;
@@ -4522,6 +4526,8 @@ public class AudioService extends IAudioService.Stub {
     // mSafeMediaVolumeDevices lists the devices for which safe media volume is enforced,
     private final int mSafeMediaVolumeDevices = AudioSystem.DEVICE_OUT_WIRED_HEADSET |
                                                 AudioSystem.DEVICE_OUT_WIRED_HEADPHONE;
+    // mManualSafeMediaVolume overrides the built-in safe media volume
+    private boolean mManualSafeMediaVolume;
     // mMusicActiveMs is the cumulative time of music activity since safe volume was disabled.
     // When this time reaches UNSAFE_VOLUME_MUSIC_ACTIVE_MS_MAX, the safe media volume is re-enabled
     // automatically. mMusicActiveMs is rounded to a multiple of MUSIC_ACTIVE_POLL_PERIOD_MS.
@@ -4553,6 +4559,11 @@ public class AudioService extends IAudioService.Stub {
     }
 
     private void enforceSafeMediaVolume() {
+        // return if safe volume has been manually turned off
+        if (!mManualSafeMediaVolume) {
+            return;
+        }
+
         VolumeStreamState streamState = mStreamStates[AudioSystem.STREAM_MUSIC];
         int devices = mSafeMediaVolumeDevices;
         int i = 0;
@@ -4582,7 +4593,7 @@ public class AudioService extends IAudioService.Stub {
             if ((mSafeMediaVolumeState == SAFE_MEDIA_VOLUME_ACTIVE) &&
                     (mStreamVolumeAlias[streamType] == AudioSystem.STREAM_MUSIC) &&
                     ((device & mSafeMediaVolumeDevices) != 0) &&
-                    (index > mSafeMediaVolumeIndex)) {
+                    (index > mSafeMediaVolumeIndex) && mManualSafeMediaVolume) {
                 return false;
             }
             return true;
@@ -4654,5 +4665,12 @@ public class AudioService extends IAudioService.Stub {
         if (status != 0) {
             Log.w(TAG, "AudioFlinger informed of device's low RAM attribute; status " + status);
         }
+    }
+
+    private void updateManualSafeMediaVolume() {
+        int safeMediaVolumeEnable = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SAFE_HEADSET_VOLUME, 0, UserHandle.USER_CURRENT);
+        mManualSafeMediaVolume = (safeMediaVolumeEnable != 1);
+        setSafeMediaVolumeEnabled(mManualSafeMediaVolume);
     }
 }
