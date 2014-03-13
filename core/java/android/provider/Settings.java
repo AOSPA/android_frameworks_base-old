6b192e0bf0eb50d1a90dc4bce13653bc3e70bfdd
Joe Wooster <jeddington66@gmail.com>
2014-03-13 21:07:11 +0000

    Framework: Allow choice of screen-off animation (1/2)
    Adds a choice of alternative screen-off animations:
    
    - Fade out
    - Electron beam (default)
    - Scale down
    
    Change-Id: I8ec9bfc6248a1223037fd252b0c853db7994afe6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2588,6 +2588,12 @@ public final class Settings {
         public static final String LOCKSCREEN_MAXIMIZE_WIDGETS = "lockscreen_maximize_widgets";
 
         /**
+         * Defines the screen-off animation to display
+         * @hide
+         */
+        public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
