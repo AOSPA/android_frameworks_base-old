73e32d9422c7790a8f9a95f93e224ea6f73ac79f
kevdliu <kevdliu@gmail.com>
2014-03-14 17:43:24 -0400

    Immersive: Fix black bar in some games
    Some games (FIFA14, Virtual Tennis, Rebuild etc.) show a black bar where the navigation bar used to be even in full immersive mode.
    This fixes that.
    
    Change-Id: Id7f8959da0496d95beaa21a567ef0f756b00a82d

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1583,7 +1583,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mHasNavigationBar) {
             // For a basic navigation bar, when we are in landscape mode we place
             // the navigation bar to the side.
-            if (mNavigationBarCanMove && fullWidth > fullHeight) {
+            if (!immersiveModeHidesNavigationBar() && mNavigationBarCanMove && fullWidth > fullHeight) {
                 return fullWidth - mNavigationBarWidthForRotation[rotation];
             }
         }
@@ -1594,7 +1594,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mHasNavigationBar) {
             // For a basic navigation bar, when we are in portrait mode we place
             // the navigation bar to the bottom.
-            if (!mNavigationBarCanMove || fullWidth < fullHeight) {
+            if (!immersiveModeHidesNavigationBar() && !mNavigationBarCanMove || fullWidth < fullHeight) {
                 return fullHeight - mNavigationBarHeightForRotation[rotation];
             }
         }
