4dd4bc791beefff7812ac408b11fb2365295b5fd
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-03-17 16:22:50 +0000

    Keyguard: Fix configuration change observer
    Change I9e5dfdb12df522dc7dd50c26f35aba26b1bbe44e made recreations
    only happen when a stored copy of the previous configuration differs
    from the current one. But "configuration = otherConfiguration" creates
    a reference to the original object, not a copy, so newConfig.diff(mLastConfiguration)
    always returned zero. Fixit by creating an actual deep copy of the
    Configuration object.
    
    Change-Id: I236932dfc0e121dd83212882b4fe71306c8a3fab

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -269,7 +269,7 @@ public class KeyguardViewManager {
         public ViewManagerHost(Context context) {
             super(context);
             setBackground(mBackgroundDrawable);
-            mLastConfiguration = context.getResources().getConfiguration();
+            mLastConfiguration = new Configuration(context.getResources().getConfiguration());
         }
 
         public void setCustomBackground(Drawable d) {
@@ -323,7 +323,7 @@ public class KeyguardViewManager {
             } else {
                 if (DEBUG) Log.v(TAG, "onConfigurationChanged: view not visible");
             }
-            mLastConfiguration = newConfig;
+            mLastConfiguration = new Configuration(newConfig);
         }
 
         @Override
