d63e63009213a5eb354dcf628f7589fe5941cb1e
gerard <gerar.f87@gmail.com>
2014-03-18 18:27:24 +0100

    Add clear all recents button to in-the-spot user preference
    Change-Id: I53c7d5f881542d2644aac8cc09e49ae1c5543bcb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2569,6 +2569,12 @@ public final class Settings {
          */
         public static final String QUICK_SETTINGS_QUICK_PULL_DOWN = "quick_settings_quick_pull_down";
 
+         /**
+         * Navbar recents clear all option
+         * @hide
+         */
+        public static final String NAVBAR_RECENTS_CLEAR_ALL = "navbar_recents_clear_all";
+
         /**
          * Safe headset volume warning option
          * @hide
@@ -2663,7 +2669,8 @@ public final class Settings {
          */
         public static final String[] SETTINGS_TO_RESET = {
             QUICK_SETTINGS_QUICK_PULL_DOWN,
-            SAFE_HEADSET_VOLUME
+            SAFE_HEADSET_VOLUME,
+            NAVBAR_RECENTS_CLEAR_ALL
         };
 
         // Settings moved to Settings.Secure
