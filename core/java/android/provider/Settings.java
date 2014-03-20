1585d23b173aca549f61046b0e3156c58563df42
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-20 15:08:59 +0100

    FW: more improve Active display [1/2]
    1. fix http://jira.omnirom.org/browse/OMNI-651
    2. fix http://jira.omnirom.org/browse/OMNI-641
    3. add http://jira.omnirom.org/browse/OMNI-652
    4. Add option Double tap to sleep every where
    5. Add option to show wallpaper blur behind active display
    6. Allow to show numbers of notification on Handle (only show if notification count is enabled in bar settings)
    7. minor improvements
    8. add battery circle around handle (only show if battery circle around lockscreen handle enabled, see security settings)
    9. fix http://jira.omnirom.org/browse/OMNI-665
    10. fix whitespace
    
    Change-Id: Ia9d38d0278718a5820c6d35aa8ca178e2ac476d5
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2740,6 +2740,25 @@ public final class Settings {
         /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
+         * Whether to showing wallpaper behind active display.
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_WALLPAPER = "active_display_wallpaper";
+
+        /**
+         * Whether to not showing active display when there is annoying notifications.
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_ANNOYING = "active_display_annoying";
+
+        /**
+         * double tap every where to sleep on active display.
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_DOUBLE_TAP = "active_display_double_tap";
+
+        /**
+         * Stores values for custom lockscreen targets
          * @hide
          */
         @Deprecated
