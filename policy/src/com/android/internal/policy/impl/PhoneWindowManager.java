edf64e11d125afc0004058f08a8094aa525097be
BigBrother1984 <stevewatersy@gmail.com>
2014-03-28 02:32:08 +0100

    PIE: Fix random showing of navbar when swiping from top.
    We need to condition here aswell to be sure top swipe doesn't trigger up
    navbar at all even if we set its height to 0 is an useless call.
    
    Addressing JIRA AOSPA-151 issue.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4445,7 +4445,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     return;
                 }
                 if (sb) mStatusBarController.showTransient();
-                if (nb) mNavigationBarController.showTransient();
+                if (nb && !immersiveModeImplementsPie()) mNavigationBarController.showTransient();
                 mImmersiveModeConfirmation.confirmCurrentPrompt();
                 updateSystemUiVisibilityLw();
             }
