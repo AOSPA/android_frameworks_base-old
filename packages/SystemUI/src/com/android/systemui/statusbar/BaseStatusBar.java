2da6cf94af5db5807767b720dcfaf80e7c62f7f3
zodex <mimirower45@wp.pl>
2014-04-16 10:59:51 +0200

    STATUS_BAR_NOTIF_COUNT

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -219,6 +219,8 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected ActiveDisplayView mActiveDisplayView;
 
+    private boolean mShowNotificationCounts;
+
     public IStatusBarService getStatusBarService() {
         return mBarService;
     }
@@ -368,6 +370,9 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         mLocale = mContext.getResources().getConfiguration().locale;
         mLayoutDirection = TextUtils.getLayoutDirectionFromLocale(mLocale);
+
+        mShowNotificationCounts = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_NOTIF_COUNT, 0) == 1;
 
         mStatusBarContainer = new FrameLayout(mContext);
 
