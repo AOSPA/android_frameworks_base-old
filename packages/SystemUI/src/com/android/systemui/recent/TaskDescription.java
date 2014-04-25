2ae37265683f6baa648270e5e821ec43bed2543e
Yamil Ghazi <yghazi@gmail.com>
2014-04-25 07:30:15 +0200

    Long press recents to switch to last app
    Change-Id: I8c52a7b40d4229d523bccc1c9246673b82f5b48b

--- packages/SystemUI/src/com/android/systemui/recent/TaskDescription.java
+++ packages/SystemUI/src/com/android/systemui/recent/TaskDescription.java
@@ -56,6 +56,10 @@ public final class TaskDescription {
         packageName = null;
     }
 
+    public Intent getIntent() {
+        return intent;
+    }
+
     public void setLoaded(boolean loaded) {
         mLoaded = loaded;
     }
