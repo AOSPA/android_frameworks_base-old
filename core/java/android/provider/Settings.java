09c7019ac9ab176b9414ede581b7cd4402fd543b
Ricky Salinas <rsalinas1@mail.usf.edu>
2014-04-29 02:59:10 -0400

    Allow disabling the IME selector notification (1/2)
    Credit CM
    
    Signed-off-by: Ricky Salinas <rsalinas1@mail.usf.edu>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2606,6 +2606,12 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /**
+         * Whether to show the IME switcher in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
+
+        /**
          * Quick settings quick pull down option
          * @hide
          */
