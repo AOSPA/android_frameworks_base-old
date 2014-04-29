488cbcf505aa4c0a7e1006cf219b82f20ad56d50
zodex <mimirower45@wp.pl>
2014-04-29 17:29:54 +0200

    Revert "Floating Window: Welcome back"
    This reverts commit 42636e54d70fdd9809aeea7c68c73cfdf1993974.
    
    Conflicts:
    	services/java/com/android/server/NotificationManagerService.java

--- packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
@@ -56,7 +56,6 @@ public class RecentsActivity extends Activity {
     private static boolean mShowing;
     private IntentFilter mIntentFilter;
     private boolean mForeground;
-    protected boolean mBackPressed;
 
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
@@ -174,12 +173,7 @@ public class RecentsActivity extends Activity {
 
     @Override
     public void onBackPressed() {
-        mBackPressed = true;
-        try {
-            dismissAndGoBack();
-        } finally {
-            mBackPressed = false;
-        }
+        dismissAndGoBack();
     }
 
     public void dismissAndGoHome() {
@@ -203,7 +197,6 @@ public class RecentsActivity extends Activity {
                             ActivityManager.RECENT_IGNORE_UNAVAILABLE);
             if (recentTasks.size() > 1 &&
                     mRecentsPanel.simulateClick(recentTasks.get(1).persistentId)) {
-                finish();
                 // recents panel will take care of calling show(false) through simulateClick
                 return;
             }
