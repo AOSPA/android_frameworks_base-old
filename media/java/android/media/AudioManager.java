08b78930a4a8c88f7d2dba9d22513adc376d6c2b
Andre Saddler <dresadd09691@gmail.com>
2014-05-17 15:49:53 -0400

    [WIP] Quiet Hours (1/3)
    Basic Import from CM's Quiet Hours implement
    
    //TODO
    -- Implement QH Tile (https://github.com/CyanogenMod/android_frameworks_base/commit/656aef4ef346d5bc52cf1252c4ff1c8f9583d432)
    
    -- Add 4 state (https://github.com/CyanogenMod/android_frameworks_base/commit/23b08bc9edd650e75d53733fbf7e4eec25f87656)
    
    additions:
    Quiet Hours: Allow disabling phone ringer (1/3)
         - includes Services/Telephony & Settings parts
    @Jubakuba authored on Dec 8, 2013
    
    QuietHours: disable volume change tone during quiet hours
    * Its quiet time, you're trying to be quiet, why would the volume
      change make noise when you're being quiet?
    * Turn this off when system sounds are disabled during quiet hours
    @invisiblek authored on Dec 9, 2013
    
    P7:
    - Keep ArrayMap in NotificationManagerService class
    - 1/3 disable phone ringer (SlimRoms)
    
    P8:
    - QuietHours: disable volume change tone during quiet hours
    
    P12:
    -  add missing void observe function
    
    Signed-off-by: ayysir <dresadd09691@gmail.com>
    Change-Id: Ic7671acee2582600e725630b34bf262d3c08eb01

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -41,6 +41,8 @@ import android.view.VolumePanel;
 
 import java.util.HashMap;
 
+import com.android.internal.util.cm.QuietHoursUtils;
+
 /**
  * AudioManager provides access to volume and ringer mode control.
  * <p>
@@ -1739,6 +1741,10 @@ public class AudioManager {
      */
     public void  playSoundEffect(int effectType) {
         if (effectType < 0 || effectType >= NUM_SOUND_EFFECTS) {
+            return;
+        }
+
+        if (QuietHoursUtils.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM)) {
             return;
         }
 
