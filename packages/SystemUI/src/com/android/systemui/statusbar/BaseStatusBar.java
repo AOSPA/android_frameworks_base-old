46a539061badadd3e806317fe8faed9b560260c8
oreiche <oliver.reiche@gmail.com>
2014-05-23 17:31:24 +0200

    SystemUI: Swipe to float for notifications and recents
    I was not happy with long press on notifications and recents
    to open apps in floating mode. The long press duration kinda
    breaks the UI interaction flow, which prevented me from
    actually using floating windows. Therefore, I've created a
    private modification, which enables floating mode for
    notifications and recents by swiping to the left. This
    greatly improves usability without strongly deviating from
    stock UI experience, in strict accordance with PA's
    philosophy. Changes are surgically implemented to keep the
    probability of merge conflicts rather low. The code was
    successfully tested on mako, hammerhead, tilapia, and manta.
    Let the team discuss and decide whether or not this is a
    worthy extension to PA.
    
    Changes are split into two commits (PT1 & PT2).
    
    PT2:
    - Swipe recents for floating window:
    Recents ScrollViews set separate trigger directions on
    SwipeHelper (LEFT for vertical and DOWN for horizontal).
    Directions are chosen as the opposite of the 'natural'
    swipe-to-dismiss direction performed by the clear-all
    button. onChildTriggered() will open the app in floating
    window mode. To confirm this behavior for horizontal and
    vertical recents views, separate settings confirmation
    dialogs are shown with appropriate PA-style graphics (Gimp
    sources on request).
    - Swipe status bar notification for floating window:
    Analogous to recents, here the NotificationRowLayout sets
    the trigger direction to LEFT. It also got its own
    confirmation dialog together with new hint graphics. After a
    child is triggered, the app is started in floating window
    mode by performing a click causing the notification to
    disappear (exactly as it would anyway, if the app would have
    been opened in 'normal' full-screen mode).
    
    Conflicts:
    	packages/SystemUI/res/values-de/pa_strings.xml
    	packages/SystemUI/src/com/android/systemui/recent/RecentsHorizontalScrollView.java
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    	packages/SystemUI/src/com/android/systemui/recent/RecentsVerticalScrollView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    PS6: Rebase with hover changes and blacklist interface.
    PS7: Fix build & NPE
    
    Change-Id: Ib32e2c88033b1c143bd8dc8fa7394b002060d526

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -641,9 +641,9 @@ public abstract class BaseStatusBar extends SystemUI implements
                 null, UserHandle.CURRENT);
     }
 
-    private void launchFloating(PendingIntent pIntent) {
+    private void launchFloating(PendingIntent pIntent, boolean allowed) {
         Intent overlay = new Intent();
-        overlay.addFlags(Intent.FLAG_FLOATING_WINDOW | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        if (allowed) overlay.addFlags(Intent.FLAG_FLOATING_WINDOW | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         try {
             ActivityManagerNative.getDefault().resumeAppSwitches();
             ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
@@ -694,7 +694,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                                     animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE);
                                     Toast.makeText(mContext, text, duration).show();
                                 } else {
-                                    launchFloating(contentIntent);
+                                    launchFloating(contentIntent, true);
                                     animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE);
                                 }
                             } else {
@@ -1151,7 +1151,11 @@ public abstract class BaseStatusBar extends SystemUI implements
                 // System is dead
             }
 
-            if (mPendingIntent != null) {
+            if (mPile.launchNextNotificationFloating()) {
+                if (mPendingIntent != null) {
+                    launchFloating(mPendingIntent, allowed);
+                }
+            } else if (mPendingIntent != null) {
                 int[] pos = new int[2];
                 v.getLocationOnScreen(pos);
                 Intent overlay = new Intent();
