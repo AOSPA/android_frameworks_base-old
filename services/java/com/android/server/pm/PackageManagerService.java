85e282c823be5ca748efbd52184c84b262b96b37
houst0nn <houstonn@gmail.com>
2014-06-03 19:45:05 -0400

    fw/base: themes: update
    Themes: Update last postFinish call
    * Previous patchset changed postFinish function signature.
    Change-Id: Ibe07435e274054c7203504bc4b0877b4df006893
    
    Make theme change broadcast a protected broadcast.
    Change-Id: Ic24c840685d18d133dc73ff2637c041c4775f197
    
    Make theme change broadcast a protected broadcast.
    Change-Id: Ic24c840685d18d133dc73ff2637c041c4775f197
    
    WallpaperCropper: do not display null bitmaps
    It is possible that themes can include a corrupted bitmap,
    and the lockscreen will try and display a NULL image resulting in an
    empty thumbnail and applying it will cause the app to crash.
    Change-Id: Ie0770486a9d58050a5f296a219d844a39937421e
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Cleanup app->{theme1,theme2...} mapping on uninstall
    Change-Id: I24b43e8817db1927d5b43fcf7ad3340269d046ea
    
    Themes: Delete icon resources during uninstall
    Change-Id: Icf247642f9d246c8826f3394f873e4b434ad2b3f
    
    Themes: remove unused code
    Change-Id: I5ee8de962c40ddc5fcfbbc0332a9e389223e716c
    
    Themes: Handle apps using "original-package" tag
    Before this patch the themes code assumed the pkgName from PackageInfo was equal to the
    manifest value. This is not the case however for upgraded system apps that have gone
    through a pkg-name change. For example, Trebuchet was "com.android.launcher3" and now
    it is "com.cyanogenmod.trebuchet". If the device had wiped data then PM would use the new
    pkg-name, but if the system apps were being upgraded then PM would use the old name which is
    now different from the manifest name.
    
    Change-Id: Ie7eb1b4e17899d1055535dc890d4f7a7bca9a98a
    
    Themes: Add '|' delimited list of components to broadcast
    Change-Id: I742dcb0f502b6089dee584a92a3792016f2a9799
    
    Fix resource leaks
    Change-Id: I3a201cfd924e467d605351782bd06874c2af725e
    
    Revert "Reduce memory leaks by removing all views."
    This reverts commit 8df12e5.
    
    Signed-off-by: houst0nn <houstonn@gmail.com>

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -3629,34 +3629,6 @@ public class PackageManagerService extends IPackageManager.Stub {
 
         return finalList;
     }
-
-    private boolean createIdmapsForPackageLI(PackageParser.Package pkg) {
-        HashMap<String, PackageParser.Package> overlays = mOverlays.get(pkg.packageName);
-        final String pkgName = pkg.packageName;
-        if (overlays == null) {
-            Log.w(TAG, "Unable to create idmap for " + pkgName + ": no overlay packages");
-            return false;
-        }
-        for (PackageParser.Package opkg : overlays.values()) {
-            for(String overlayTarget : opkg.mOverlayTargets) {
-                if (overlayTarget.equals(pkgName)) {
-                    try {
-                        if (opkg.mIsLegacyThemeApk) {
-                            generateIdmapForLegacyTheme(pkgName, opkg);
-                        } else {
-                            generateIdmap(pkgName, opkg);
-                        }
-                    } catch (Exception e) {
-                        Log.w(TAG, "Unable to create idmap for " + pkgName
-                                + ": no overlay packages", e);
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-
     private boolean createIdmapForPackagePairLI(PackageParser.Package pkg,
             PackageParser.Package opkg, String redirectionsPath) {
         if (DEBUG_PACKAGE_SCANNING) Log.d(TAG, "Generating idmaps between " + pkg.packageName + ":" + opkg.packageName);
@@ -5425,7 +5397,16 @@ public class PackageManagerService extends IPackageManager.Stub {
             return;
         }
 
-        compileResourcesIfNeeded(targetPkg.packageName, themePkg);
+
+        // Always use the manifest's pkgName when compiling resources
+        // the member value of "packageName" is dependent on whether this was a clean install
+        // or an upgrade w/  If the app is an upgrade then the original package name is used.
+        // because libandroidfw uses the manifests's pkgName during idmap creation we must
+        // be consistent here and use the same name, otherwise idmap will look in the wrong place
+        // for the resource table.
+        String pkgName = targetPkg.mRealPackage != null ?
+                targetPkg.mRealPackage : targetPkg.packageName;
+        compileResourcesIfNeeded(pkgName, themePkg);
         generateIdmap(targetPkg.packageName, themePkg);
     }
 
@@ -5570,6 +5551,10 @@ public class PackageManagerService extends IPackageManager.Stub {
                 map.remove(opkg.packageName);
             }
 
+            if (map.isEmpty()) {
+                mOverlays.remove(target);
+            }
+
             PackageParser.Package targetPkg = mPackages.get(target);
             if (targetPkg != null) {
                 String idmapPath = getIdmapPath(targetPkg, opkg);
@@ -5580,6 +5565,10 @@ public class PackageManagerService extends IPackageManager.Stub {
             String resPath = ThemeUtils.getResDir(target, opkg);
             recursiveDelete(new File(resPath));
         }
+
+        // Cleanup icons
+        String iconResources = ThemeUtils.getIconPackDir(opkg.packageName);
+        recursiveDelete(new File(iconResources));
     }
 
     private void uninstallThemeForApp(PackageParser.Package appPkg) {
@@ -5590,6 +5579,7 @@ public class PackageManagerService extends IPackageManager.Stub {
            String idmapPath = getIdmapPath(appPkg, opkg);
            new File(idmapPath).delete();
         }
+        mOverlays.remove(appPkg.packageName);
     }
 
     private void recursiveDelete(File f) {
