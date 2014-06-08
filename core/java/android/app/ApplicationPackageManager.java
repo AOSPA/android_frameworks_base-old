890ebb54bc63117511b81ba379bbc509759f0033
zodex <mimirower45@wp.pl>
2014-06-08 18:28:22 +0200

    Revert "Merge "Fix theme provider crashing for non-primary user." into kitkat"
    This reverts commit 7e7b338190a983ecafe6fc2d14cd5e540ee620cd.

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -449,17 +449,14 @@ final class ApplicationPackageManager extends PackageManager {
         }
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public List<PackageInfo> getInstalledThemePackages() {
-        // Returns a list of theme APKs.
-        ArrayList<PackageInfo> finalList = new ArrayList<PackageInfo>();
-        List<PackageInfo> installedPackagesList = getInstalledPackages(0);
-        for (PackageInfo pi : installedPackagesList) {
-            if (pi != null && pi.isThemeApk) {
-                finalList.add(pi);
-            }
+        try {
+            return mPM.getInstalledThemePackages();
+        } catch (RemoteException e) {
+            throw new RuntimeException("Package manager has died", e);
         }
-        return finalList;
     }
 
     @SuppressWarnings("unchecked")
--- core/java/android/content/pm/IPackageManager.aidl
+++ core/java/android/content/pm/IPackageManager.aidl
@@ -145,6 +145,8 @@ interface IPackageManager {
     ParceledListSlice getPackagesHoldingPermissions(in String[] permissions,
             int flags, int userId);
 
+    List<PackageInfo> getInstalledThemePackages();
+
     /**
      * This implements getInstalledApplications via a "last returned row"
      * mechanism that is not exposed in the API. This is to get around the IPC
