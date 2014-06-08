7c1d358310527dfa99f75729b6f10c848c287bb2
zodex <mimirower45@wp.pl>
2014-06-08 18:27:38 +0200

    Revert "Merge "ResourcesManager: Fix theme insertion at boot" into kitkat"
    This reverts commit 33b730c496ea90fe61b863bbf477e5608bfdc2bf.

--- core/java/android/app/ResourcesManager.java
+++ core/java/android/app/ResourcesManager.java
@@ -189,7 +189,6 @@ public class ResourcesManager {
         //}
 
         AssetManager assets = new AssetManager();
-        assets.setThemeSupport(compatInfo.isThemeable);
         if (assets.addAssetPath(resDir) == 0) {
             return null;
         }
@@ -210,18 +209,6 @@ public class ResourcesManager {
         } else {
             config = getConfiguration();
         }
-
-        /* Attach theme information to the resulting AssetManager when appropriate. */
-        if (compatInfo.isThemeable && config != null) {
-            if (config.customTheme == null) {
-                config.customTheme = CustomTheme.getBootTheme();
-            }
-
-            if (!TextUtils.isEmpty(config.customTheme.getThemePackageName())) {
-                attachThemeAssets(assets, config.customTheme);
-            }
-        }
-
         r = new Resources(assets, dm, config, compatInfo, token);
         if (false) {
             Slog.i(TAG, "Created app resources " + resDir + " " + r + ": "
