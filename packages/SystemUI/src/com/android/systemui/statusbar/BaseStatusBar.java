2bfd2dc7b6a4806e2b85a7eb793d5e2cc8216866
cr3pt <cr3pt@users.noreply.github.com>
2014-06-20 23:07:33 +0200

    Update BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -282,7 +282,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     private ContentObserver mSettingsObserver = new ContentObserver(mHandler) {
-    }
+    };
 
     public SearchPanelView getSearchPanelView() {
         return mSearchPanelView;
