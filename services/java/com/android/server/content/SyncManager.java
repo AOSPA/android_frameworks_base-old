fb1062f7fcc1d375b24b61dcc3462d7e6a2b6077
Valter Strods <valters.strods@gmail.com>
2014-07-04 14:56:36 +0300

    Fix possible NPE in SyncHandler.onBootCompleted()
    If onBootCompleted() was called a second time, it would have throw a
    NullPointerException since the queue object would have already been
    set to null by the previous call. This commit just makes such an
    object behave as an empty queue instead of throwing the exception.
    
    Change-Id: I7ef82ba1e2504ceb0a2c11694d32f0588db7d0af

--- services/java/com/android/server/content/SyncManager.java
+++ services/java/com/android/server/content/SyncManager.java
@@ -1795,10 +1795,12 @@ public class SyncManager {
             doDatabaseCleanup();
             synchronized(this) {
                 // Dispatch any stashed messages.
-                for (Message message : mBootQueue) {
-                    sendMessage(message);
+                if (mBootQueue != null) {
+                    for (Message message : mBootQueue) {
+                        sendMessage(message);
+                    }
+                    mBootQueue = null;
                 }
-                mBootQueue = null;
                 mBootCompleted = true;
             }
         }
