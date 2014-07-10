387b1f63f8fd424944af27626c32d3ee7523d48a
rovo89 <android@robv.de>
2014-07-10 21:25:11 +0200

    Restore lazy loading of LoadedApk resources
    This was broken in 8fff544ec349593cfa61ae2f5ec59419fff666b6. Since that change,
    resources are always initialized in getPackageInfo(), even if they haven't been
    loaded before (mResources == null).
    
    It shouldn't be necessary in *any* case to preload the resources at this point.
    Anything using the resources should call LoadedApk.getResources(), which will
    initialize them just when needed. Setting mResources to null resets the cache
    and will force a reload the next time the resources are requested.
    
    Change-Id: I9bc9986297478e99752b1e2b9bd04dab3dcc9656

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -1670,10 +1670,9 @@ public final class ActivityThread {
                             new WeakReference<LoadedApk>(packageInfo));
                 }
             }
-            if (packageInfo.mResources == null
-                    || !packageInfo.mResources.getAssets().isUpToDate()) {
+            if (packageInfo.mResources != null
+                    && !packageInfo.mResources.getAssets().isUpToDate()) {
                 packageInfo.mResources = null;
-                packageInfo.getResources(this);
             }
             return packageInfo;
         }
