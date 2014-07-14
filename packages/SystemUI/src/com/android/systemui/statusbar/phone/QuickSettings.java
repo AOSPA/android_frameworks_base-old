74f2191e393aa2af760582ac2004b05557714b7e
Evan Anderson <evan@thelinuxman.us>
2014-07-14 15:58:42 -0500

    QuickSettings: Refresh RSSI tile on tap
    When tapping the RSSI tile to disable mobile data, the indicator
    would not show immediately because the view is not updated until
    the next signal refresh.  Refreshing on tap makes more since so
    that the user sees instant feedback.
    
    Change-Id: I70d3e08e9a4af4f5d764d8c90cbaf944f4f25139
    Signed-off-by: Evan Anderson <evan@thelinuxman.us>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -571,6 +571,7 @@ class QuickSettings {
                             public void onClick(View v) {
                                 boolean currentState = mConnectivityManager.getMobileDataEnabled();
                                 mConnectivityManager.setMobileDataEnabled(!currentState);
+                                mModel.refreshRssiTile();
                             }
                         });
                         rssiTile.setFrontOnLongClickListener(new View.OnLongClickListener() {
