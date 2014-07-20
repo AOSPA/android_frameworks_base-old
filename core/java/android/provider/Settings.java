3df49ffbe623826f3ad7a65d05da5e0cffd9e5a5
Danny Baumann <dannybaumann@web.de>
2014-07-20 12:51:05 +0200

    Make incoming call screen style configurable (1/3)
    [(3/3) is located in services/telephony ccmc squash commit]
    
    Change-Id: I2183b2db5f4e6603f57df70520a9b7da50a651b8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3075,6 +3075,18 @@ public final class Settings {
         public static final String T9_SEARCH_INPUT_LOCALE = "t9_search_input_locale";
 
         /**
+         * The style of the incoming call screen.
+         * Default is {@link INCOMING_CALL_STYLE_FULLSCREEN_PHOTO}.
+         * @hide
+         */
+        public static final String INCOMING_CALL_STYLE = "incoming_call_style";
+
+        /** @hide */
+        public static final int INCOMING_CALL_STYLE_CLASSIC = 0;
+        /** @hide */
+        public static final int INCOMING_CALL_STYLE_FULLSCREEN_PHOTO = 1;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -3142,7 +3154,8 @@ public final class Settings {
             VIBRATE_WHEN_RINGING,
             INCREASING_RING,
             RINGTONE,
-            NOTIFICATION_SOUND
+            NOTIFICATION_SOUND,
+            INCOMING_CALL_STYLE,
         };
 
         /**
