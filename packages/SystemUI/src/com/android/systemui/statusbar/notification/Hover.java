bb0c7c09ae3256bbf744f359cca651c8cb8112d9
BigBrother1984 <stevewatersy@gmail.com>
2014-08-07 15:43:09 +0200

    HOVER: A few improvements
    - Fix touch outside handling
    - Better handling logic for touch inputs
    - Code style
    - Tiny cleanup
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java
@@ -229,7 +229,6 @@ public class Hover {
                 }
                 mShowing = false;
                 mHiding = false;
-                setTouchOutside(false);
                 setAnimatingVisibility(false);
                 clearHandlerCallbacks();
                 clearNotificationList();
@@ -855,7 +854,8 @@ public class Hover {
     public void clearForegroundAppNotifications() {
         for (int i = 0; i < mNotificationList.size(); i++) {
             if (mNotificationList.get(i).getContent().getPackageName()
-                    .equals(mNotificationHelper.getForegroundPackageName())) {
+                    .equals(mNotificationHelper.getForegroundPackageName())
+                            && i > 0) {
                 mNotificationList.remove(i);
             }
         }
