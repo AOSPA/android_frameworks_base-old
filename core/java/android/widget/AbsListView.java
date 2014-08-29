f043a3df76a72884a95cd8005dd630966aefe797
zodex <mimirower45@wp.pl>
2014-08-29 11:03:26 +0200

    FIX

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -2290,6 +2290,9 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         final View scrapView = mRecycler.getScrapView(position);
         final View child = mAdapter.getView(position, scrapView, this);
         if (scrapView != null) {
+            if (mIsScrolling && !mIsWidget) {
+                child = setAnimation(child);
+            }
             if (child != scrapView) {
                 // Failed to re-bind the data, return scrap to the heap.
                 mRecycler.addScrapView(scrapView, position);
