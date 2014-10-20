f6b6bc362678b3d2caae4d715207ab96325e6d41
Valter Strods <valterstrods@aospa.co>
2014-10-20 22:42:03 +0300

    Implement Dynamic System Bars [1/2]
    This commit implements DSB. Simple as that.
    
    All the changes:
    - frameworks/base (I3bc7102b2139fd1fa3de22aac096bd2a75bde878)
    - packages/apps/Settings (Id2b105dfb3faf3cad74d0fa0dc33d01f6b71a853)
    
    Change-Id: I3bc7102b2139fd1fa3de22aac096bd2a75bde878

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2709,6 +2709,50 @@ public final class Settings {
         public static final String PEEK_STATE = "peek_state";
 
         /**
+         * Whether the status bar is set to be colored dynamically
+         * 0 = disabled (default)
+         * 1 = enabled
+         * @hide
+         */
+        public static final String DYNAMIC_STATUS_BAR_STATE = "dynamic_status_bar_state";
+
+        /**
+         * Whether the navigation bar is set to be colored dynamically
+         * 0 = disabled (default)
+         * 1 = enabled
+         * @hide
+         */
+        public static final String DYNAMIC_NAVIGATION_BAR_STATE = "dynamic_navigation_bar_state";
+
+        /**
+         * Whether the dynamic system bars are set to have a gradient overlay
+         * 0 = disabled (default)
+         * 1 = enabled
+         * @hide
+         */
+        public static final String DYNAMIC_SYSTEM_BARS_GRADIENT_STATE =
+                "dynamic_system_bars_gradient_state";
+
+        /**
+         * Whether the dynamic status bar is set to have a (darkening) filter overlay
+         * 0 = disabled (default)
+         * 1 = enabled
+         * @hide
+         */
+        public static final String DYNAMIC_STATUS_BAR_FILTER_STATE =
+                "dynamic_status_bar_filter_state";
+
+        /**
+         * The frequency of dynamic status bar updates
+         * less than 0 = update once each -N seconds
+         * 0 = update once each second
+         * more than 0 = update N times each second
+         * default value: 2
+         * @hide
+         */
+        public static final String EXPERIMENTAL_DSB_FREQUENCY = "experimental_dsb_frequency";
+
+        /**
          * Whether incomming call UI stays in background
          *
          * @hide
