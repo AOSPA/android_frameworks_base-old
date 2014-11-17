e25d4d7c4595ae74fdbae8f82bceebcd81dedc43
BigBrother1984 <carlosavignano@aospa.co>
2014-11-17 13:22:42 +0100

    SystemUI: Allow disabling adjusting volume sounds [1/2]
    * Option stored in Settings->Sounds & notification->Other settings.
    
    Signed-off-by: BigBrother1984 <carlosavignano@aospa.co>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2446,6 +2446,12 @@ public final class Settings {
         public static final String HAPTIC_FEEDBACK_ENABLED = "haptic_feedback_enabled";
 
         /**
+         * Whether the volume slider adjust sound is enabled. The value is
+         * boolean (1 or 0).
+         */
+        public static final String VOLUME_ADJUST_SOUND_ENABLED = "volume_adjust_sound_enabled";
+
+        /**
          * @deprecated Each application that shows web suggestions should have its own
          * setting for this.
          */
@@ -2688,6 +2694,7 @@ public final class Settings {
             TTY_MODE,
             SOUND_EFFECTS_ENABLED,
             HAPTIC_FEEDBACK_ENABLED,
+            VOLUME_ADJUST_SOUND_ENABLED,
             POWER_SOUNDS_ENABLED,       // moved to global
             DOCK_SOUNDS_ENABLED,        // moved to global
             LOCKSCREEN_SOUNDS_ENABLED,
@@ -2710,6 +2717,7 @@ public final class Settings {
         public static final String[] CLONE_TO_MANAGED_PROFILE = {
             DATE_FORMAT,
             HAPTIC_FEEDBACK_ENABLED,
+            VOLUME_ADJUST_SOUND_ENABLED,
             SOUND_EFFECTS_ENABLED,
             TEXT_SHOW_PASSWORD,
             TIME_12_24
--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -51,6 +51,7 @@ import android.os.Debug;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Vibrator;
+import android.provider.Settings;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.KeyEvent;
@@ -1227,6 +1228,11 @@ public class VolumePanel extends Handler implements DemoMode {
     }
 
     protected void onPlaySound(int streamType, int flags) {
+        // Just return if we disabled slider sound while adjusting volume
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                 Settings.System.VOLUME_ADJUST_SOUND_ENABLED, 1) == 0) {
+             return;
+        }
 
         if (hasMessages(MSG_STOP_SOUNDS)) {
             removeMessages(MSG_STOP_SOUNDS);
