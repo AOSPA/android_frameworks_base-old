9637ad46f7ed34145a0e578340ce7c1ac18a6d01
Danesh M <daneshm90@gmail.com>
2014-11-27 14:17:19 +0100

    BatteryMeterDrawable: add battery styles
     - Portrait
     - Circle
    
    Change-Id: Icfc14d989eebde9014538291c24aa76d328df234

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7174,6 +7174,15 @@ public final class Settings {
         public static final String SRGB_ENABLED = "srgb_enabled";
 
         /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 1: Display the battery as a circle
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -7276,7 +7285,8 @@ public final class Settings {
             SCREENSAVER_ACTIVATE_ON_DOCK,
             SCREENSAVER_ACTIVATE_ON_SLEEP,
             WIFI_DISCONNECT_DELAY_DURATION,
-            WIFI_SOFTAP_ACS_ENABLED
+            WIFI_SOFTAP_ACS_ENABLED,
+            STATUS_BAR_BATTERY_STYLE
         };
 
         /** @hide */
