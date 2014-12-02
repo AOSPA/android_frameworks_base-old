04680ff07a8af7e9a1cc36fe657a03c820067712
Lars Greiss <kufikugel@googlemail.com>
2014-12-02 14:16:17 +0100

    SystemUI: Do not allow to access quick settings on secure lockscreen
    We have a lot security guys as user due of using the phones as business phones...that
    said a lot complains go into the direction of google how it is possible to have
    access to quick settings on a secure lockscreen which is absolutely understandable
    due that you are able to control Wifi BT AirplaneMode data etc.
    
    This patch takes care on this problem and controls this behaviour.
    
    ATTENTION: Due that this is really an UX derp for an average user this feature is
    enabled by default now.
    
    /-/
    
    Changes apported to original patch and reflected in the edited commit message above:
    * This has been integrated and is hardcoded, cause this is the behaviour it should have.
      Thanks Larss for this.
    
    Signed-off-by: @BigBrother1984 <carlosavignano@aospa.co>
    Change-Id: Ib437802ae8687dc8e99122a40e285bdd2df3e04c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -37,6 +37,7 @@ import android.view.animation.Interpolator;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardStatusView;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSPanel;
@@ -167,8 +168,11 @@ public class NotificationPanelView extends PanelView implements
     private boolean mQsTouchAboveFalsingThreshold;
     private int mQsFalsingThreshold;
 
+    private LockPatternUtils mLockPatternUtils;
+
     public NotificationPanelView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mLockPatternUtils = new LockPatternUtils(mContext);
     }
 
     public void setStatusBar(PhoneStatusBar bar) {
@@ -587,9 +591,12 @@ public class NotificationPanelView extends PanelView implements
         if (mOnlyAffordanceInThisMotion) {
             return true;
         }
+
+        boolean isQSEventBlocked = mLockPatternUtils.isSecure() && mKeyguardShowing;
+
         if (event.getActionMasked() == MotionEvent.ACTION_DOWN && getExpandedFraction() == 1f
                 && mStatusBar.getBarState() != StatusBarState.KEYGUARD && !mQsExpanded
-                && mQsExpansionEnabled) {
+                && mQsExpansionEnabled && !isQSEventBlocked) {
 
             // Down in the empty area while fully expanded - go to QS.
             mQsTracking = true;
@@ -599,7 +606,7 @@ public class NotificationPanelView extends PanelView implements
             mInitialTouchY = event.getX();
             mInitialTouchX = event.getY();
         }
-        if (mExpandedHeight != 0) {
+        if (mExpandedHeight != 0 && !isQSEventBlocked) {
             handleQsDown(event);
         }
         if (!mTwoFingerQsExpand && mQsTracking) {
@@ -618,7 +625,8 @@ public class NotificationPanelView extends PanelView implements
         }
         if (mTwoFingerQsExpandPossible && event.getActionMasked() == MotionEvent.ACTION_POINTER_DOWN
                 && event.getPointerCount() == 2
-                && event.getY(event.getActionIndex()) < mStatusBarMinHeight) {
+                && event.getY(event.getActionIndex()) < mStatusBarMinHeight
+                && !isQSEventBlocked) {
             mTwoFingerQsExpand = true;
             requestPanelHeightUpdate();
 
