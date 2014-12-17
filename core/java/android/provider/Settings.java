b0eee025a37b112348fa7d022d43ace68b7f88d8
Austin T. Conn <atc3030dev@gmail.com>
2014-12-17 13:28:00 -0500

    base: Forward Port Volume Long Press Skip Tracks (1/2)
    Bring Forward Volume Rocker Track Change
    
    Requires Settings Change
    (packages/apps/Settings)
    
    Change-Id: I70b00e08fe48707110725dd1daa56b1e48d15a03
    
    Fix long press volume buttons skip tracks
    
    Without that patch, the volume changes up or down prior
    to skipping tracks because the sendvolumekeyevent doesn't
    have the mayChangeVolume condition. Also, that part is
    useless cause the mUseTvRouting condition is taken care
    of correctly later.
    
    Change-Id: I88757315f135f013e7692861d46b3a07fbfc07d5
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3621,6 +3621,12 @@ public final class Settings {
         public static final String BUTTON_BRIGHTNESS_ENABLED = "button_brightness_enabled";
 
         /**
+         * Whether or not volume button music controls should be enabled to seek media tracks
+         * @hide
+         */
+        public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
