c1047344b4ee41e7fd230eb2a7a548bab1d7a160
Shashi Shekar Shankar <ssbang@codeaurora.org>
2015-01-05 17:40:39 +0530

    Performance: Memory Optimizations.
    The following optimizations are squashed in this change.
    
    1. Enable Aggressive trim settings.
    
    This change will enable aggressive trim settings for targets
    up to 1GB. The change can be turned on/off from system properties.
    By default, the properties are set for targets up to 1GB
    
    Changes spread in to : ActivityManagerConstants.java
    
    2. Propagate B-services to higher adj.
    
    Depending on the inactivity of a service, move the B-services to
    highest adj 906. Under memory pressure, these services will
    be killed first ahead of cached apps which results in better
    concurrency numbers with bg apps. Inactivity time and minumum
    no of services to be maintained are configurable as system properties.
    Improves concurrency.
    
    Changes spread in to : ActivityManagerService.java
    
    3. Postpone service restart during app launch.
    
    In the android framework, when the service gets killed,
    it will be rescheduled. Postpone the service restart if
    the app is in process of startup. By Default this feature
    is disabled. It can be enabled from build.prop of target.
    
    Changes spread in to : ActiveServices.java
    
    4. Performance: Make Cached apps limit configurable.
    
    Set it via a target specific property
    file. Allows flexibility to set the limit
    based on device config.
    
    Normally, setting a smaller value for
    low-end device helps with performance.
    
    Changes spread in to : ActivityManagerConstants.java
    
    5. am: Add new flag to detect activity "launching" state
    
    Use this flag to determine service re-schdelue delay and
    exclude persistent services from delay.
    
    CRs-Fixed: 783020 792122 879756 911145 2111356
    
    Squash of following change-IDs:
        Change-Id: I233dddbff07e7ec1fe2ee96402fe1d411903beb5
        Change-Id: Ied6cfcc3d11951f32f18de680b0e3483db8e163e
        Change-Id: Ia86edf027e20df2c7aa6f34e3aa293f7a8e6f1fa
        Change-Id: I90a733d7134d86efffadf6815ce3d2944dd09419
        Change-Id: Id38d536ff375b57d69f5ca73265f62e85740fc04
    
    Change-Id: I3b2110743a6e0dd1379aa3b7703b3a897db3f6e6

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -2326,6 +2326,7 @@ class ActivityStack<T extends StackWindowController> extends ConfigurationContai
         mStackSupervisor.mGoingToSleepActivities.remove(next);
         next.sleeping = false;
         mStackSupervisor.mActivitiesWaitingForVisibleActivity.remove(next);
+        next.launching = true;
 
         if (DEBUG_SWITCH) Slog.v(TAG_SWITCH, "Resuming " + next);
 
@@ -3382,6 +3383,7 @@ class ActivityStack<T extends StackWindowController> extends ConfigurationContai
 
     final void stopActivityLocked(ActivityRecord r) {
         if (DEBUG_SWITCH) Slog.d(TAG_SWITCH, "Stopping: " + r);
+        r.launching = false;
         if ((r.intent.getFlags()&Intent.FLAG_ACTIVITY_NO_HISTORY) != 0
                 || (r.info.flags&ActivityInfo.FLAG_NO_HISTORY) != 0) {
             if (!r.finishing) {
