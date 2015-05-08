eac4c755d09aa83b420d9776ac6fa94b01aed5e6
rogersb11 <brettrogesr11@gmail.com>
2015-05-08 02:06:29 +0200

    QS: Pie Control tile
    PS2: New pref name
    PS3: Use get/putIntForUser
    
    Change-Id: I1904913adfc08d197a9ba4a802b9ebfd2f4f09ac
    Signed-off-by: rogersb11 <brettrogesr11@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.PieControlTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
@@ -264,6 +265,7 @@ public class QSTileHost implements QSTile.Host {
         else if (tileSpec.equals(CastTile.SPEC)) return new CastTile(this);
         else if (tileSpec.equals(HotspotTile.SPEC)) return new HotspotTile(this);
         else if (tileSpec.equals(ImmersiveTile.SPEC)) return new ImmersiveTile(this);
+        else if (tileSpec.equals(PieControlTile.SPEC)) return new PieControlTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
