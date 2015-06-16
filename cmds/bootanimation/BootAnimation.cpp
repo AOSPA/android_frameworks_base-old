83b1920f773b3a95d2be7a02c3d367ceb4945010
Sai Kiran Korwar <skirank@nvidia.com>
2015-06-16 17:13:35 +0530

    bootanimation: Call eglReleaseThread in threadLoop
    Add eglReleaseThread call in the threadLoop function to
    ensure that the thread properly cleans up its resources
    before exiting.
    
    Change-Id: I90dff47370e7acaecc046c43f0bb274214a29f77
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -387,6 +387,7 @@ bool BootAnimation::threadLoop()
     mFlingerSurface.clear();
     mFlingerSurfaceControl.clear();
     eglTerminate(mDisplay);
+    eglReleaseThread();
     IPCThreadState::self()->stopProcess();
     return r;
 }
