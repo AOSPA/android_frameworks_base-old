jejecule/android_frameworks_base-1

base: Allow QS tile reordering
This commit makes tiles resond to a long-press. That starts a drag
operation on them. Dropping the tile places it somewhere. The anims
are fairly simple - a nice fade for the tile being picked up/dropped
and some generic sliding action for the other tiles. The reordering
works so that all tiles retail their positions relative to each other
and simply fill up all the free space.

Change-Id: If846b5a8920702b936c405478d8b873379528f2e

packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -34,6 +34,7 @@ import com.android.systemui.statusbar.policy.NetworkController.NetworkSignalChan
 
 /** Quick settings tile: Cellular **/
 public class CellularTile extends QSTile<QSTile.SignalState> {
+    public static final String SPEC = "cell";
     private static final Intent CELLULAR_SETTINGS = new Intent().setComponent(new ComponentName(
             "com.android.settings", "com.android.settings.Settings$DataUsageSummaryActivity"));
 
@@ -41,7 +42,7 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
     private final CellularDetailAdapter mDetailAdapter;
 
     public CellularTile(Host host) {
-        super(host);
+        super(host, SPEC);
         mController = host.getNetworkController();
         mDetailAdapter = new CellularDetailAdapter();
     }
@@ -71,7 +72,17 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
     }
 
     @Override
-    protected void handleClick() {
+    protected void handleToggleClick() {
+        if (mController.isMobileDataSupported()) {
+            mController.setMobileDataEnabled(!mController.isMobileDataEnabled());
+        } else {
+            // We have nothing to toggle; just give the user the Settings app.
+            mHost.startSettingsActivity(CELLULAR_SETTINGS);
+        }
+    }
+
+    @Override
+    protected void handleDetailClick() {
         if (mController.isMobileDataSupported()) {
             showDetail(true);
         } else {

AOSPA/android_frameworks_base-old

packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -35,6 +35,7 @@ import com.android.systemui.statusbar.policy.NetworkController.NetworkSignalChan
 
 /** Quick settings tile: Cellular **/
 public class CellularTile extends QSTile<QSTile.SignalState> {
+    public static final String SPEC = "cell";
     private static final Intent CELLULAR_SETTINGS = new Intent().setComponent(new ComponentName(
             "com.android.settings", "com.android.settings.Settings$DataUsageSummaryActivity"));
 
@@ -43,7 +44,7 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
     private final CellularDetailAdapter mDetailAdapter;
 
     public CellularTile(Host host) {
-        super(host);
+        super(host, SPEC);
         mController = host.getNetworkController();
         mDataController = mController.getMobileDataController();
         mDetailAdapter = new CellularDetailAdapter();
@@ -74,7 +75,17 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
     }
 
     @Override
-    protected void handleClick() {
+    protected void handleToggleClick() {
+        if (mDataController.isMobileDataSupported()) {
+            mDataController.setMobileDataEnabled(!mDataController.isMobileDataEnabled());
+        } else {
+            // We have nothing to toggle; just give the user the Settings app.
+            mHost.startSettingsActivity(CELLULAR_SETTINGS);
+        }
+    }
+
+    @Override
+    protected void handleDetailClick() {
         if (mDataController.isMobileDataSupported()) {
             showDetail(true);
         } else {