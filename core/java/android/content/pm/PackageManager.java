8e0d27d7aef67995d4a52952f4e29ba4b7dd0444
Evan Anderson <evananderson@aospa.co>
2015-07-04 17:22:44 -0500

    PackageManager: Themes constants should not be SystemApi.
    Not sure how these got here.  CM never had these designated as SystemApi in CM12
    
    Change-Id: Ia2a5a48cf30eed4139130f7aa7eb612c1df29932
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- core/java/android/content/pm/PackageManager.java
+++ core/java/android/content/pm/PackageManager.java
@@ -777,7 +777,6 @@ public abstract class PackageManager {
      * if the system failed to install the theme because aapt could not compile the app
      * @hide
      */
-    @SystemApi
     public static final int INSTALL_FAILED_THEME_AAPT_ERROR = -400;
 
     /**
@@ -788,7 +787,6 @@ public abstract class PackageManager {
      * apps.
      * @hide
      */
-    @SystemApi
     public static final int INSTALL_FAILED_THEME_IDMAP_ERROR = -401;
 
     /**
@@ -799,7 +797,6 @@ public abstract class PackageManager {
      * apps.
      * @hide
      */
-    @SystemApi
     public static final int INSTALL_FAILED_THEME_UNKNOWN_ERROR = -402;
 
     /**
