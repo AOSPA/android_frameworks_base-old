66f349ea1167db03fd36572479d036b76873b3ec
Jake Weinstein <xboxlover360@gmail.com>
2015-10-21 02:59:44 +0000

    base: revert boost framework since it depends on proprietary stuff
    * We don't have QPerformance.jar
    
    Revert "Perf: Adding hooks for IO prefetcher into framework"
    
    This reverts commit 96e75fcafbeb77427ff4407e80a5d05bf92ec732.
    
    Revert "BoostFramework to enchance performance during critical scenarios"
    
    This reverts commit 0c3a49a56f827e16d038542b0a81c0e257787995.
    
    Change-Id: Id9ed44a476881066626f26365bf50378b3e1224d

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -66,7 +66,6 @@ import android.service.voice.IVoiceInteractionSession;
 import android.util.EventLog;
 import android.util.Slog;
 import android.view.Display;
-import android.util.BoostFramework;
 import com.android.internal.app.ActivityTrigger;
 
 import java.io.FileDescriptor;
@@ -153,10 +152,6 @@ final class ActivityStack {
     final WindowManagerService mWindowManager;
     private final RecentTasks mRecentTasks;
 
-    public BoostFramework mPerf = null;
-    public boolean mIsAnimationBoostEnabled = false;
-    public int aBoostTimeOut = 0;
-    public int aBoostParamVal[];
     /**
      * The back history of all previous (and possibly still
      * running) activities.  It contains #TaskRecord objects.
@@ -370,14 +365,6 @@ final class ActivityStack {
         mCurrentUser = mService.mCurrentUserId;
         mRecentTasks = recentTasks;
         mOverrideConfig = Configuration.EMPTY;
-        mIsAnimationBoostEnabled = mService.mContext.getResources().getBoolean(
-                   com.android.internal.R.bool.config_enablePerfBoostForAnimation);
-        if (mIsAnimationBoostEnabled) {
-           aBoostTimeOut = mService.mContext.getResources().getInteger(
-                   com.android.internal.R.integer.animationboost_timeout_param);
-           aBoostParamVal = mService.mContext.getResources().getIntArray(
-                   com.android.internal.R.array.animationboost_param_value);
-        }
     }
 
     boolean okToShowLocked(ActivityRecord r) {
@@ -1821,9 +1808,6 @@ final class ActivityStack {
         // that the previous one will be hidden soon.  This way it can know
         // to ignore it when computing the desired screen orientation.
         boolean anim = true;
-        if (mIsAnimationBoostEnabled == true && mPerf == null) {
-            mPerf = new BoostFramework();
-        }
         if (prev != null) {
             if (prev.finishing) {
                 if (DEBUG_TRANSITION) Slog.v(TAG_TRANSITION,
@@ -1835,9 +1819,6 @@ final class ActivityStack {
                     mWindowManager.prepareAppTransition(prev.task == next.task
                             ? AppTransition.TRANSIT_ACTIVITY_CLOSE
                             : AppTransition.TRANSIT_TASK_CLOSE, false);
-                    if(prev.task != next.task && mPerf != null) {
-                       mPerf.perfLockAcquire(aBoostTimeOut, aBoostParamVal);
-                    }
                 }
                 mWindowManager.setAppWillBeHidden(prev.appToken);
                 mWindowManager.setAppVisibility(prev.appToken, false);
@@ -1853,9 +1834,6 @@ final class ActivityStack {
                             : next.mLaunchTaskBehind
                                     ? AppTransition.TRANSIT_TASK_OPEN_BEHIND
                                     : AppTransition.TRANSIT_TASK_OPEN, false);
-                    if(prev.task != next.task && mPerf != null) {
-                        mPerf.perfLockAcquire(aBoostTimeOut, aBoostParamVal);
-                    }
                 }
             }
             if (false) {
