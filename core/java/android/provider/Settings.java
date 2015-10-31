3569144e622a94ba8385398511e4fb1678af0189
Evan Anderson <evananderson@aospa.co>
2015-10-31 16:50:03 -0400

    GlobalActions: Bring back advanced reboot menu
    * Expose worth AOSP hidden items.
    * Add Reboot item:
      - Show another dialog to chose from normal, recovery
      and bootloader (fastboot) mode. Needs to be activated
      in developer settings.
      - When on secure lockscreen and/or advanced actions are not
      enabled it will just reboot your device.
    * Add back screenshot item.
    * Cleanup code.
    * New drawables by @arzbhatia
    
    credits to @BigBrother1984 for the original implement
    
    Change-Id: I4eb29d8c963d582b053d51a8d79ddb299b55694e
    Signed-off-by: Evan Anderson <evananderson@aospa.co>
    
    Improve global actions
    
    Change-Id: I33966ceec81bff59411ec4b7fa24fac5a49f81c1
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Evan Anderson <evananderson@aospa.co>
    
    base: Remove old global actions pngs welcoming vectors
    
    * Android needs to fetch vectors xmls now, and they will scale on all devices.
    
    Change-Id: I20994068d80610c7b246c12e1ab7977afa634b30
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    
    Fix invalid formatting in drawables
    
    Change-Id: I44e943ad2bd68b390577b420d8ff2c0e26b84ec4
    
    base: Add overlaiable style for global action item text views
    
    Change-Id: I93104ec3e3ece5dc5f2c351d56bb419a8684959d
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Evan Anderson <evananderson@aospa.co>
    
    base: Rewrite global actions airplane mode integration
    
    * Fix a bunch of bugs.
    
    Change-Id: I54b58e0c984578202bd39b5ea05d862fdc3b33c6
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Evan Anderson <evananderson@aospa.co>
    
    base: Avoid race condition when showing global actions dialog
    
    Change-Id: Ie4000e74aedf321a637a720227b7536aba7042ea
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    
    base: Consistency within gloabal actions dialogs
    
    * Confirm power off action, set proper title and message for reboot action.
    
    Change-Id: I7b604c2f6ac43e14a9138d6b5170b7a2b7336b74
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    
    Update globalaction drawables
    
    Change-Id: Ia544c4063ffd477bfc3561a8d58d36f2a66716c0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6241,12 +6241,19 @@ public final class Settings {
         public static final String MANAGED_PROFILE_CONTACT_REMOTE_SEARCH =
                 "managed_profile_contact_remote_search";
 
-	/**
+        /**
+         * Whether to include options in power menu for rebooting into recovery and bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Settings to reset on user request. They will fall back to their default value (0).
          *
          * @hide
          */
         public static final String[] SETTINGS_TO_RESET = {
+            ADVANCED_REBOOT
         };
 
         /**
@@ -6322,7 +6329,8 @@ public final class Settings {
             PREFERRED_TTY_MODE,
             ENHANCED_VOICE_PRIVACY_ENABLED,
             TTY_MODE_ENABLED,
-            INCALL_POWER_BUTTON_BEHAVIOR
+            INCALL_POWER_BUTTON_BEHAVIOR,
+            ADVANCED_REBOOT
         };
 
         /**
