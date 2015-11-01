9be56f845b7c83a560b049f55e570c3973ceae44
Carlo Savignano <carlosavignano@aospa.co>
2015-11-01 22:31:30 +0800

    base: Dynamic navigation bar toggling
    Change-Id: I55da79fe8da83cf839cc91c1257ea91d2d632a40
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: thecrazyskull <anaskarbila@aospa.co>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10111,6 +10111,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean needsNavigationBar() {
+        return mPolicy.needsNavigationBar();
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
