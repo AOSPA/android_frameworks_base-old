b221bd1cafa2f08350c42b47fcb4772b166c6d2f
Peter Schiller <peterschiller@google.com>
2015-11-11 12:54:14 -0800

    Adjusted animation timing
    Change-Id: I7f001abe0c97167ef9bdf83b3371b0875fd1c59f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -50,6 +50,8 @@ public abstract class PanelView extends FrameLayout {
     public static final boolean DEBUG = PanelBar.DEBUG;
     public static final String TAG = PanelView.class.getSimpleName();
 
+    public static final float SPEED_UP_FACTOR_CLICKED = 1.7f;
+
     private final void logf(String fmt, Object... args) {
         Log.v(TAG, (mViewName != null ? (mViewName + ": ") : "") + String.format(fmt, args));
     }
@@ -1069,6 +1071,13 @@ public abstract class PanelView extends FrameLayout {
         }
         return onMiddleClicked();
     }
+
+    protected final Runnable mEmptySpaceClickedRunnable = new Runnable() {
+        @Override
+        public void run() {
+            collapse(false /* delayed */, SPEED_UP_FACTOR_CLICKED /* speedUpFactor */);
+        }
+    };
 
     protected final Runnable mPostCollapseRunnable = new Runnable() {
         @Override
