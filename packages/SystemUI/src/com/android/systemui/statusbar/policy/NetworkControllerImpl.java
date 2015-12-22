93d9213165467f50f65557f632f4909aa7473025
Fang Yunong <yunong@codeaurora.org>
2015-12-22 11:31:51 +0800

    Frameworks: Customize network name and indicator on status bar
    Support following carrier specific customizations:
    1. Add operator name (i.e network name) into left top of status bar.
    This function is controlled by config_showOperatorNameInStatusBar and
    also configurable to users via Settings-->Display-->Operator name
    
    2. Show both roaming and network icons
    Some regional carriers requires the status bar display the Roaming
    icon "R" and Network Icon "3G/4G" and so on, when the phone Roaming
    and data connection is on.
    
    3. Show network capacity instead of current RAT in network indicator
    For some carrier, the network indicator should display the network
    capacity (i.e highest RAT) of a given cell.
    
    CRs-Fixed: 945412
    
    Change-Id: I5603965b0303f3e1a0ec8fb1897ef6a93c45d116

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -39,6 +39,8 @@ import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.MathUtils;
+import android.view.View;
+import android.widget.TextView;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.telephony.PhoneConstants;
@@ -115,6 +117,8 @@ public class NetworkControllerImpl extends BroadcastReceiver
 
     // The current user ID.
     private int mCurrentUserId;
+
+    private TextView mNetWorkNameLabelView;
 
     private OnSubscriptionsChangedListener mSubscriptionListener;
 
@@ -603,6 +607,39 @@ public class NetworkControllerImpl extends BroadcastReceiver
         }
         mWifiSignalController.updateConnectivity(mConnectedTransports, mValidatedTransports);
         mEthernetSignalController.updateConnectivity(mConnectedTransports, mValidatedTransports);
+    }
+
+    private void setTextViewVisibility(TextView v) {
+        String networkName = getMobileDataNetworkName();
+        if (networkName.equals(mContext.getString(
+                com.android.internal.R.string.lockscreen_carrier_default))
+                || networkName.equals(mContext.getString(
+                com.android.internal.R.string.emergency_calls_only))) {
+            v.setVisibility(View.GONE);
+        } else {
+            v.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void addNetworkLabelView(TextView v) {
+        if (v != null) {
+            mNetWorkNameLabelView = v;
+            mNetWorkNameLabelView.setText(getMobileDataNetworkName());
+            setTextViewVisibility(mNetWorkNameLabelView);
+        }
+    }
+
+    public void removeNetworkLabelView() {
+        if (mNetWorkNameLabelView != null) {
+            mNetWorkNameLabelView = null;
+        }
+    }
+
+    public void updateNetworkLabelView() {
+        if (mNetWorkNameLabelView != null) {
+            mNetWorkNameLabelView.setText(getMobileDataNetworkName());
+            setTextViewVisibility(mNetWorkNameLabelView);
+        }
     }
 
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
