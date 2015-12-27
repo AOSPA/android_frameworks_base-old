f6eace39fb93e9922dd328b03a8ea8b018eecc0e
Carlo Savignano <carlosavignano@aospa.co>
2015-12-27 15:46:44 +0100

    Improve global actions
    Change-Id: I33966ceec81bff59411ec4b7fa24fac5a49f81c1
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -34,6 +34,7 @@ import android.content.pm.ThemeUtils;
 import android.content.pm.UserInfo;
 import android.database.ContentObserver;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff.Mode;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.provider.Settings;
@@ -203,6 +204,9 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             attrs.setTitle("GlobalActions");
             mDialog.getWindow().setAttributes(attrs);
             mDialog.show();
+            // int dialogWidth = mContext.getResources().getDimensionPixelSize(R.dimen.global_actions_dialog_width);
+            // int dialogHeight = mContext.getResources().getDimensionPixelSize(R.dimen.global_actions_dialog_height);
+            // mDialog.getWindow().setLayout(dialogWidth /*width*/, dialogHeight /*height*/);
             mDialog.getWindow().getDecorView().setSystemUiVisibility(View.STATUS_BAR_DISABLE_EXPAND);
         }
     }
@@ -296,10 +300,6 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                             Settings.Global.BUGREPORT_IN_POWER_MENU, 0) != 0 && isCurrentUserOwner()) {
                         mItems.add(getBugReportAction());
                     }
-                } else if (GLOBAL_ACTION_KEY_SILENT.equals(actionKey)) {
-                    if (mShowSilentToggle) {
-                        mItems.add(mSilentModeAction);
-                    }
                 } else if (GLOBAL_ACTION_KEY_USERS.equals(actionKey)) {
                     if (SystemProperties.getBoolean("fw.power_user_switcher", false)) {
                         addUsersToMenu(mItems);
@@ -312,6 +312,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                     mItems.add(getVoiceAssistAction());
                 } else if (GLOBAL_ACTION_KEY_ASSIST.equals(actionKey)) {
                     mItems.add(getAssistAction());
+                } else if (GLOBAL_ACTION_KEY_SILENT.equals(actionKey)) {
+                    if (mShowSilentToggle) {
+                        mItems.add(mSilentModeAction);
+                    }
                 } else {
                     Log.e(TAG, "Invalid global action key " + actionKey);
                 }
@@ -325,6 +329,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         params.mAdapter = mAdapter;
         params.mOnClickListener = this;
         params.mForceInverseBackground = true;
+        // params.mViewSpacingLeft = 0;
+        // params.mViewSpacingTop = 0;
+        // params.mViewSpacingRight = 0;
+        // params.mViewSpacingBottom = 0;
 
         GlobalActionsDialog dialog = new GlobalActionsDialog(getUiContext(), params);
         dialog.setCanceledOnTouchOutside(false); // Handled by the custom class.
@@ -352,7 +360,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private final class PowerAction extends SinglePressAction implements LongPressAction {
         private PowerAction() {
-            super(com.android.internal.R.drawable.ic_lock_power_off,
+            super(R.drawable.ic_lock_power_off,
                 R.string.global_action_power_off);
         }
 
@@ -385,8 +393,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private final class RebootAction extends SinglePressAction {
         private RebootAction() {
-            super(com.android.internal.R.drawable.ic_lock_power_reboot,
-                    R.string.global_action_reboot);
+            super(R.drawable.ic_lock_power_reboot,
+                R.string.global_action_reboot);
         }
 
         @Override
@@ -418,8 +426,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private final class ScreenShotAction extends SinglePressAction {
         private ScreenShotAction(){
-            super(com.android.internal.R.drawable.ic_lock_screenshot,
-                    R.string.global_action_screenshot);
+            super(R.drawable.ic_lock_screenshot,
+                R.string.global_action_screenshot);
         }
 
         public void onPress() {
@@ -1216,6 +1224,64 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             int index = (Integer) v.getTag();
             mAudioManager.setRingerMode(indexToRingerMode(index));
             mHandler.sendEmptyMessageDelayed(MESSAGE_DISMISS, DIALOG_DISMISS_DELAY);
+        }
+    }
+
+    private static class Separator implements Action {
+
+        @Override
+        public CharSequence getLabelForAccessibility(Context context) {
+            return null;
+        }
+
+        public View create(Context context, View convertView, ViewGroup parent,
+                LayoutInflater inflater) {
+            View v = inflater.inflate(R.layout.global_actions_separator, parent, false);
+            return v;
+        }
+
+        public void onPress() {
+        }
+
+        public boolean showDuringKeyguard() {
+            return true;
+        }
+
+        public boolean showBeforeProvisioning() {
+            return false;
+        }
+
+        public boolean isEnabled() {
+            return false;
+        }
+    }
+
+    private static class Spacer implements Action {
+
+        @Override
+        public CharSequence getLabelForAccessibility(Context context) {
+            return null;
+        }
+
+        public View create(Context context, View convertView, ViewGroup parent,
+                LayoutInflater inflater) {
+            View v = inflater.inflate(R.layout.global_actions_spacer, parent, false);
+            return v;
+        }
+
+        public void onPress() {
+        }
+
+        public boolean showDuringKeyguard() {
+            return true;
+        }
+
+        public boolean showBeforeProvisioning() {
+            return false;
+        }
+
+        public boolean isEnabled() {
+            return false;
         }
     }
 
