074920adcb66eb32c67c42e5ffd9a6fb019e2849
Valters Strods <valterstrods@aospa.co>
2016-01-14 19:59:54 +0200

    SystemUI: Clean up the QS panel in general
    This commit includes a bit of everything:
    
    - The QS add button in the status bar header now turns more
      and generally feels a bit more dynamic.
    - The QS panel's hidden tiles list now displays an informational
      message at the top about what's going on, in place of
      the brightness bar.
    - The tiles' click handlers have been cleaned up a bit;
      nothing really noteworthy in that regard, but still.
    
    Change-Id: Ia0561876281754b57e701fe00f65815ec489a609

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QsAddButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QsAddButton.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright 2016 ParanoidAndroid Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -39,7 +39,7 @@ public class QsAddButton extends AlphaOptimizedImageButton {
         mAnimator = ObjectAnimator.ofFloat(this, View.ROTATION, getRotation(), deg);
         mAnimator.setDuration(350);
         mAnimator.setInterpolator(AnimationUtils.loadInterpolator(mContext,
-                android.R.interpolator.linear));
+                android.R.interpolator.overshoot));
         mAnimator.start();
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -696,7 +696,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                 : mMultiUserSwitch.getLeft() - mQsAddButton.getLeft();
         target.signalClusterAlpha = mSignalClusterDetached ? 0f : 1f;
         target.settingsRotation = !mExpanded ? 90f : 0f;
-        target.qsAddRotation = !mExpanded ? 90f : mQsShowingHidden ? 45f : 0f;
+        target.qsAddRotation = !mExpanded ? 90f : mQsShowingHidden ? -135f : 0f;
     }
 
     private float getAlphaForVisibility(View v) {
@@ -942,7 +942,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             if (mQsShowingHidden == isShowingHidden) return;
             mQsShowingHidden = isShowingHidden;
 
-            final float degRotation = isShowingHidden ? 45f : 0f;
+            final float degRotation = isShowingHidden ? -135f : 0f;
             mExpandedValues.qsAddRotation = degRotation;
             if (mExpanded) {
                 mQsAddButton.rotate(degRotation);
