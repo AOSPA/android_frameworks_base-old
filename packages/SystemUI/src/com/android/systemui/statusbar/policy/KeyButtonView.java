a684b532947d2aa216f49b6f1b8a696313081e82
Dohyun Lee <leedhyun11@gmail.com>
2016-01-21 13:46:21 +0900

    KeyButtonView: use a static instance of AudioManager since we don't need a separate one for each KeyButtonView instance
    Change-Id: Iae6a6e722fb8a73d7453c28c435da9440018f511

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -53,9 +53,15 @@ public class KeyButtonView extends ImageView {
     private int mCode;
     private int mTouchSlop;
     private boolean mSupportsLongpress = true;
-    private AudioManager mAudioManager;
     private boolean mGestureAborted;
     private boolean mLongClicked;
+
+    static AudioManager mAudioManager;
+    static AudioManager getAudioManager(Context context) {
+		if (mAudioManager == null)
+		    mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+		return mAudioManager;
+	}
 
     private final Runnable mCheckLongPress = new Runnable() {
         public void run() {
@@ -98,7 +104,7 @@ public class KeyButtonView extends ImageView {
 
         setClickable(true);
         mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
-        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        mAudioManager = getAudioManager(context);
         setBackground(new KeyButtonRipple(context, this));
     }
 
