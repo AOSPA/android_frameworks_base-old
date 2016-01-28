746ffd6ea28f6563e8ccc75e1a0a1c6290e436bc
Thecrazyskull <anaskarbila@gmail.com>
2016-01-28 21:35:10 +0100

    ShutDownThread: allow theming safe reboot dialog
    Change-Id: Ib855e2ec0aaad909f4a41d5c08412c33d2acb859

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -184,7 +184,7 @@ public final class ShutdownThread extends Thread {
                     }
                 });
             } else {
-                sConfirmDialog = new AlertDialog.Builder(context)
+                sConfirmDialog = new AlertDialog.Builder(uiContext)
                         .setTitle(mRebootSafeMode
                                 ? com.android.internal.R.string.reboot_safemode_title
                                 : com.android.internal.R.string.power_off)
