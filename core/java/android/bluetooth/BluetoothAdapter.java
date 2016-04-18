ab3a41a2559c9a860ea55069ef3a9f2e09ff6ac4
Gurpreet Ghai <gghai@codeaurora.org>
2016-04-18 19:05:28 +0530

    BT: Set persiste Bluetooth State during enable timeout case
    Use case: When enable timeout occurs during S&S testing and
    Bluetooth state is moved from Turning On to Off.
    
    Steps: Changes related to enable timeout case, to test the
    changes, S&S testing can be done.
    
    Failure: Changes done to handle Bluetooth enable timeout
    case when Bluetooth state changes from Turning On to Off.
    
    Root cause: Setting Bluetooth Persist state to handle the
    enable timeout case so that Bluetooth doesnot self enable
    after enable timeout case is handled.
    
    Fix: Setting the Bluetooth Persist State to Off when enable
    timeout occurs.
    
    Change-Id: I17b3b05020eb7469666e6d8c88c86df7764b4024
    CRs-Fixed: 1006444

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -2064,13 +2064,14 @@ public final class BluetoothAdapter {
             }
 
             public void onBluetoothServiceDown() {
-                if (VDBG) Log.d(TAG, "onBluetoothServiceDown: " + mService);
+                Log.d(TAG, "onBluetoothServiceDown: " + mService);
                 synchronized (mManagerCallback) {
                     mService = null;
                     if (mLeScanClients != null) mLeScanClients.clear();
                     if (sBluetoothLeAdvertiser != null) sBluetoothLeAdvertiser.cleanup();
                     if (sBluetoothLeScanner != null) sBluetoothLeScanner.cleanup();
                     synchronized (mProxyServiceStateCallbacks) {
+                        Log.d(TAG, "onBluetoothServiceDown: Sending callbacks to " + mProxyServiceStateCallbacks.size() + " clients");
                         for (IBluetoothManagerCallback cb : mProxyServiceStateCallbacks ){
                             try {
                                 if (cb != null) {
@@ -2082,6 +2083,7 @@ public final class BluetoothAdapter {
                         }
                     }
                 }
+                Log.d(TAG, "onBluetoothServiceDown: Finished sending callbacks to registered clients");
             }
 
             public void onBrEdrDown() {
