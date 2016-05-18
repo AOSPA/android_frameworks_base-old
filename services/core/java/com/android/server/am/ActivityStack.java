b04fe3bb6ac889e9a2d54ee3c484de3a3cea305c
Shasidhar Ganiga <sganig@codeaurora.org>
2016-05-18 16:12:08 +0530

    Fix for Recents not launching on menu press when date is set in past
    When date is set in the past, menu key press does not launch the
    recent apps screen. The issue occurs because the RunningTaskInfo
    list returned from framework is sorted based on the lastActiveTime.
    For a focussed app/activity task this is changed to the current
    system time in milliseconds. When the date is changed to a past date,
    and we try to get the RunningTaskInfo list from framework, it results
    in lastActiveTime being set in the past for settings task and recents
    having a higher lastActiveTime. This causes the system app to believe
    recents is the current top task and not launch the recents activity.
    
    Issue-Id: http://code.google.com/p/android/issues/detail?id=210214
    Change-Id: I9021471f7b2b0eb192bdafa75ffa9e57e0c6ac25
    Signed-off-by: Shasidhar Ganiga <sganig@codeaurora.org>

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -4928,7 +4928,13 @@ final class ActivityStack {
             if (focusedStack && topTask) {
                 // Give the latest time to ensure foreground task can be sorted
                 // at the first, because lastActiveTime of creating task is 0.
-                ci.lastActiveTime = System.currentTimeMillis();
+                long currentTime = System.currentTimeMillis();
+                // only update lastActiveTime if the currentTime is greater.
+                // The current time becomes lesser if the system date is changed to past.
+                // This FIXes the recents app not launching issue.
+                if(ci.lastActiveTime < currentTime) {
+                    ci.lastActiveTime = currentTime;
+                }
                 topTask = false;
             }
 
