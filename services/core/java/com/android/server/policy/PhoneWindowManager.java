6ef62f5cfd69e7b48e426dcbc13f5b719fea3e42
Ícaro Hoff <icarohoff@gmail.com>
2016-07-10 15:02:27 -0300

    base: correct the sensor wakeup behavior for KEY_WAKEUP
    Devices that use KEY_WAKEUP might have problems when the sensor 'wakeUp'
    API isn't set to "true" by default.
    This behavior makes the sensor avoid screen wake-up if the input key is
    KEY_WAKEUP, otherwise it would still wake the device regardless of
    sensor blocked or not.
    KEY_POWER will always wake the device.
    This is a good behavior and was tested on the LG G2 and Moto X.
    
    Change-Id: I2257b39ab8279046cc736b4acb511bc349ee396b
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6083,7 +6083,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (isValidGlobalKey(keyCode)
                 && mGlobalKeyManager.shouldHandleGlobalKey(keyCode, event)) {
             if (isWakeKey) {
-                wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY");
+                wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY", true);
             }
             return result;
         }
@@ -6328,7 +6328,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         if (isWakeKey) {
-            wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY");
+            wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY",
+                event.getKeyCode() == KeyEvent.KEYCODE_WAKEUP /* check prox only on wake key*/);
         }
 
         return result;
