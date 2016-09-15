b5f755594dad6768741e05a9eacfd2dbabfbb5f5
sigv <valterjansons@aospa.co>
2016-09-15 08:47:44 -0400

    SystemUI: add quick settings pull down with one finger
    Contributors:
    romanbb
    oreiche
    signal-v
    parthipanramesh
    thecrazyskull
    
    NotificationPanelView: update for new OTS change
    
    Change-Id: Ie0fa4d90484e8bcab9b928e4b4db6d76a0eb5423
    
    SystemUI: fix qs pulldown on certain themes
    
    Some themes define the peek height as 0, we should respect that and
    allow the QS pulldown to still work.
    
    Ticket: CYNGNOS-2756
    
    Change-Id: I0404974a23ef41f4689c38ab86e3f09c4bb150ab
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: fix multi touch gestures and extra jank
    
    The dejank utils were getting a little ahead of themselves: in between
    the panel height being set to 0 (could happen often), and the user
    actively trying to expand the panel, the next layout traversal from the
    dejank utils would try to collapse the panel, causing bad states.
    
    Also in the qs height animator updater, be careful to only actively
    update the end height when the user did not just peak.
    
    Finally, and most importantly, we need to disable motion event splitting
    in our view that holds our notification panel logic so we can get multi
    touch gestures in those views properly.
    
    Ticket: CYNGNOS-2756
    Change-Id: I2cda440d4e0548d3477c6fad88f7dac1cb59d9db
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Quick pull-down: Do not override on keyguard
    
    If keyguard is showing, we should not override and intercept and
    do all that magic. The quick settings will be what gets pulled
    down first anyway in that case. Also, our current implementation
    messes with swipes on keyguard itself if quick pull-down is
    enabled, which is a "feature" we do not want.
    
    Change-Id: I8735887ffab6f2c5f23612b6fd04ed8d18270203
    (cherry picked from commit 3257ac38f1f0606fb9c451572062d1988deb43d8)
    
    Quick pull-down: Clean up the touch interception
    
    This commit cleans up shouldQuickSettingsIntercept logics,
    preferably once and for all.
    
    Change-Id: I5e4eedfcc7c5cb872f6f2b2e55c653cc90a8f25d
    
    Quick pull-down: Add logical magic for intercept
    
    Interception code is a weird part of the codebase. Sometimes it
    works, more often than not, it doesn't. This is to fix an issue
    which made the notifications not respond to touch.
    
    Additionally:
    This reverts commit 94763e398b2056de0f7ac1dc692ad75ab8a43a4c.
    
    Change-Id: I0abec16b866bb93d667e932bfdfa2b0c7715c539
    
    Fix: Quickpulldown expand using notification view header
    
    At the moment you can't pull down the quick settings panel if quickpulldown is activated.
    
    Change-Id: Ifbb28aae3d1728488b4850c259135c8bb2d04d39
    
    OnTheSpot: Follow up for QS quick pull-down
    
    This commit fixes building errors caused by the recent change in
    OnTheSpot frameworks by switching to the new methods.
    
    Change-Id: Iacd95a901d60a0a41fe1300696a004a701c84159
    
    SystemUI: Ignore collapsed state for quick pull down
    
    Change-Id: Ibd1cd3a46983f881b9eba20825df58345db9f668
    
    OnTheSpot: Make QS quick pull-down optional
    
    We are AOSP with adjustments and tweaks. Our goal is to have a UX
    that is like AOSP, if the user does not want to experience the
    tweaks. No big options screens, just little tweaks.
    
    This commit allows the user to choose to skip the Quick Settings quick
    pull-down feature that provides a region on the side of the status bar
    from which the Quick Settings panel can be expanded right away,
    skipping the notifications listing screen.
    
    Change-Id: I98e0bd66249d7d9b6c11611a7d4dab2766d1034d
    
    SystemUI: Reduce activation area a bit of quick pulldown
    
    Change-Id: I06b3a50982930491c6fd4e70ceda3b1ceee28f87
    
    SystemUI: add quick settings pull down with one finger
    
    Logic is currently set to use 1/3 of right hand side of the statusbar to
    trigger a quick settings quick pull down, quickly.
    
    Change-Id: I3f536496f724f07d2419a5b2db5fa8f8261609e7
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Change-Id: Id6b8ea8fdfedd649513753e0d6e3a52257f2a9b5
    Signed-off-by: thecrazyskull <anaskarbila@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -95,6 +95,7 @@ public class PhoneStatusBarView extends PanelBar {
     @Override
     public void onPanelPeeked() {
         super.onPanelPeeked();
+        removePendingHideExpandedRunnables();
         mBar.makeExpandedVisible(false);
     }
 
