7182844d1b3a91784bfa13fdd4d8a4518e2ac44f
Carlo Savignano <carlosavignano@aospa.co>
2016-09-17 04:17:40 -0400

    input: hardware buttons lights integration
    * Enable and improve partial AOSP disabled button brightness hooks.
    * Implement user-timeout/doze/dream/awake/asleep policy.
    * Addresses NOUGAT-14
    
    Change-Id: Ie1fc9d1ff98e1782bdeb3afd03b03b762f39e5fc
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3603,6 +3603,24 @@ public final class Settings {
         public static final String NAVIGATION_BAR_ENABLED = "navigation_bar_enabled";
 
         /**
+         * Value for button brightness.
+         * This is an integer value in a range between 0 and 255.
+         *      0 = off
+         *    255 = default
+         *    255 = max
+         * @hide
+         */
+        public static final String BUTTON_BRIGHTNESS = "button_brightness";
+
+        /**
+         * Whether user can enable/disable button brightness.
+         * <p>
+         * Type: int (0 for false, 1 for true)
+         * @hide
+         */
+        public static final String BUTTON_BRIGHTNESS_ENABLED = "button_brightness_enabled";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -3758,6 +3776,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(EGG_MODE);
             // New private settings.
             PRIVATE_SETTINGS.add(NAVIGATION_BAR_ENABLED);
+            PRIVATE_SETTINGS.add(BUTTON_BRIGHTNESS);
+            PRIVATE_SETTINGS.add(BUTTON_BRIGHTNESS_ENABLED);
         }
 
         /**
