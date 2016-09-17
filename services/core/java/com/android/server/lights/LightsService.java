86fd0a7c51c2bd677e20067880f0da6a64ecaa6c
Carlo Savignano <carlosavignano@aospa.co>
2016-09-17 04:17:40 -0400

    Buttons: Hardware buttons lights integration.
    * Enable and improve partial AOSP disabled button brightness hooks.
    * Implement user-timeout/doze/dream/awake/asleep policy.
    
    Change-Id: Ie1fc9d1ff98e1782bdeb3afd03b03b762f39e5fc
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- services/core/java/com/android/server/lights/LightsService.java
+++ services/core/java/com/android/server/lights/LightsService.java
@@ -58,12 +58,22 @@ public class LightsService extends SystemService {
         }
 
         @Override
+        public int getBrightness() {
+            return mBrightness;
+        }
+
+        @Override
         public void setBrightness(int brightness) {
             setBrightness(brightness, BRIGHTNESS_MODE_USER);
         }
 
         @Override
         public void setBrightness(int brightness, int brightnessMode) {
+            if (brightness < 0) {
+                // Avoid setting invalid brightness.
+                brightness = 0;
+            }
+
             synchronized (this) {
                 // LOW_PERSISTENCE cannot be manually set
                 if (brightnessMode == BRIGHTNESS_MODE_LOW_PERSISTENCE) {
@@ -90,6 +100,7 @@ public class LightsService extends SystemService {
                     int color = brightness & 0x000000ff;
                     color = 0xff000000 | (color << 16) | (color << 8) | color;
                     setLightLocked(color, LIGHT_FLASH_NONE, 0, 0, brightnessMode);
+                    mBrightness = brightness;
                 }
             }
         }
@@ -195,6 +206,7 @@ public class LightsService extends SystemService {
         private int mMode;
         private int mOnMS;
         private int mOffMS;
+        private int mBrightness;
         private boolean mFlashing;
         private int mBrightnessMode;
         private int mLastBrightnessMode;
