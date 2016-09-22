56cad3ef3bd6bc7270d3fc38ace93b3c693f6c1d
Alex Naidis <alex.naidis@linux.com>
2016-09-22 20:26:11 +0200

    base: remove support for power off Alarm
    This feature is currently causing a constaint I/O load.
    It doesn't seem like there would be a possibility to avoid it.
    -> Globally remove the frameworks support because of noticed issues
       and the overall non usefulness of the feature.
    
    It is suggested that the feature will be disabled in the
    kernels too.
    
    Reverted commits:
    
    Revert "Add support for power off alarm"
    
    This reverts commit 6a4dfa59ebd17206b596e56d9a794fce75c36b2d.
    
    Revert "Alarm: Add one more RTC alarm type for poweroff alarm."
    
    This reverts commit ed29480f3ce6176ee363cedcb8f4ae2fa51f3e5d.
    
    Change-Id: I6f83d6115a2f6cfc25afc01c670e37270f5dcd23
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -533,9 +533,6 @@ public final class ActivityManagerService extends ActivityManagerNative
     private static final String INTENT_REMOTE_BUGREPORT_FINISHED =
             "android.intent.action.REMOTE_BUGREPORT_FINISHED";
 
-    private static final String ACTION_POWER_OFF_ALARM =
-            "org.codeaurora.alarm.action.POWER_OFF_ALARM";
-
     // Delay to disable app launch boost
     static final int APP_BOOST_MESSAGE_DELAY = 3000;
     // Lower delay than APP_BOOST_MESSAGE_DELAY to disable the boost
@@ -3979,15 +3976,6 @@ public final class ActivityManagerService extends ActivityManagerNative
             intent.addCategory(Intent.CATEGORY_HOME);
         }
         return intent;
-    }
-
-    /**
-     * If system is power off alarm boot mode, we need to start alarm UI.
-     */
-    void sendAlarmBroadcast() {
-        Intent intent = new Intent(ACTION_POWER_OFF_ALARM);
-        intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
-        mContext.sendBroadcastAsUser(intent, UserHandle.CURRENT);
     }
 
     boolean startHomeActivityLocked(int userId, String reason) {
@@ -13307,12 +13295,6 @@ public final class ActivityManagerService extends ActivityManagerNative
                 }
             }
             startHomeActivityLocked(currentUserId, "systemReady");
-
-            // start the power off alarm by boot mode
-            boolean isAlarmBoot = SystemProperties.getBoolean("ro.alarm_boot", false);
-            if (isAlarmBoot) {
-                sendAlarmBroadcast();
-            }
 
             try {
                 if (AppGlobals.getPackageManager().hasSystemUidErrors()) {
