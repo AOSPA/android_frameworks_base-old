a31ee0d4854fa78edd4a74146a02d4682270e614
thecrazyskull <anaskarbila@aospa.co>
2016-09-30 15:56:48 -0400

    DND: Add alert slider policy and initial design
    * Observer for alert slider if present.
    * Volume panel adjustements when alert slider present.
    * Dynamic quick settings tile to switch between complete silent and alarms only
    * Ability to swap order of alert slider.
    
    * Contributors:
    @Thecrazyskull
    @Carlosavignano
    
    Change-Id: I965527e5bdf0c4f64bedf80f557e6103cf583e08
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.external.TileLifecycleManager;
 import com.android.systemui.qs.external.TileServices;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.AlertSliderTile;
 import com.android.systemui.qs.tiles.BatteryTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
@@ -444,6 +445,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("user")) return new UserTile(this);
         else if (tileSpec.equals("battery")) return new BatteryTile(this);
         else if (tileSpec.equals("saver")) return new DataSaverTile(this);
+        else if (tileSpec.equals("alertslider")) return new AlertSliderTile(this);
         else if (tileSpec.equals(NightModeTile.NIGHT_MODE_SPEC))
             return new NightModeTile(this);
         // Intent tiles.
