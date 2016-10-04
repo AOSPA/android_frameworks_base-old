ec00afab929035fcbe7bc08db5ea7627cc2d5c0d
Shasidhar Ganiga <sganig@codeaurora.org>
2016-10-04 14:33:56 +0530

    Fix for wallpaper flicker on bootup on some targets
    We see wallpaper flicker in Android N on some devices. Issue
    happens due to wallpaper target keyguard Scrim being destroyed
    and new wallpaper target statusbar coming up. This creates a
    surface with width & height larger than the device supported
    width and height. This causes a momentary jank in UI during
    bootup.
    
    Change-Id: I9afe5b0a5e6d6c3da0e6bbcdde183861d5f3376d
    Issue-Id: 224329
    Signed-off-by: Shasidhar Ganiga <sganig@codeaurora.org>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/ImageWallpaper.java
+++ packages/SystemUI/src/com/android/systemui/ImageWallpaper.java
@@ -500,9 +500,6 @@ public class ImageWallpaper extends WallpaperService {
                     }
                     updateSurfaceSize(getSurfaceHolder(), getDefaultDisplayInfo(),
                             false /* forDraw */);
-                    if (mNeedsDrawAfterLoadingWallpaper) {
-                        drawFrame();
-                    }
 
                     mLoader = null;
                     mNeedsDrawAfterLoadingWallpaper = false;
