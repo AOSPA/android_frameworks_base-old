e0770b2f813f19e8606babc0bc5c4317aeb8a3bc
Carlo Savignano <carlosavignano@aospa.co>
2016-10-23 18:41:27 +0800

    PocketWIP
    Change-Id: Ifcf59c172f55cce95a72411820723696ef2dd9ae

---
+++ core/java/android/pocket/PocketManager.java
@@ -0,0 +1,66 @@
+package android.pocket;
+
+import android.content.Context;
+import android.os.RemoteException;
+import android.util.Log;
+import android.util.Slog;
+
+public class PocketManager {
+
+    private static final String TAG = PocketManager.class.getSimpleName();
+
+    private Context mContext;
+    private IPocketService mService;
+
+    public interface PocketCallback {
+        void onPocketStateChanged(boolean isDeviceInPocket);
+    }
+
+    public PocketManager(Context context, IPocketService service) {
+        mContext = context;
+        mService = service;
+        if (mService == null) {
+            Slog.v(TAG, "PocketService was null");
+        }
+    }
+
+    public void addCallback(PocketCallback callback) {
+        if (mService != null) try {
+            mService.addCallback(callback);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in addCallback: ", e);
+            if (callback != null) {
+                callback.onPocketStateChanged(false);
+            }
+        }
+    }
+
+    public void removeCallback(PocketCallback callback) {
+        if (mService != null) try {
+            mService.removeCallback(callback);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in addCallback: ", e);
+            if (callback != null) {
+                callback.onPocketStateChanged(false);
+            }
+        }
+    }
+
+    public void onDeviceInteractiveChanged(boolean interactive) {
+        if (mService != null) try {
+            mService.onDeviceInteractiveChanged(interactive);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in addCallback: ", e);
+        }
+    }
+
+    public boolean isDeviceInPocket() {
+        if (mService != null) try {
+            return mService.isDeviceInPocket();
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in addCallback: ", e);
+        }
+        return false;
+    }
+
+}
