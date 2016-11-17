10133d1db1cdb862fb53f88944bdf589982dc708
Ivaylo Asenov <ivaylo.asenov@linaro.org>
2016-11-17 11:46:56 +0200

    Move preloading classes to SystemServer.java and start it in background.
    Add delay to StartEntropyMixer and PrintManagerService in SystemServer.java.
    Move endIcuCachePinning() from ZygoteInit.java to SystemServer.java and start it in background.
    
    Change-Id: I3b6ce3976e45aae3224c060a0f0aae7ded6ea314
    Signed-off-by: Ivaylo Asenov <ivaylo.asenov@linaro.org>

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -194,7 +194,7 @@ public class ZygoteInit {
         beginIcuCachePinning();
         Trace.traceEnd(Trace.TRACE_TAG_DALVIK);
         Trace.traceBegin(Trace.TRACE_TAG_DALVIK, "PreloadClasses");
-        preloadClasses();
+        //preloadClasses(); //moved to SystemServer and started in background
         Trace.traceEnd(Trace.TRACE_TAG_DALVIK);
         Trace.traceBegin(Trace.TRACE_TAG_DALVIK, "PreloadResources");
         preloadResources();
@@ -207,7 +207,7 @@ public class ZygoteInit {
         // Ask the WebViewFactory to do any initialization that must run in the zygote process,
         // for memory sharing purposes.
         WebViewFactory.prepareWebViewInZygote();
-        endIcuCachePinning();
+        //endIcuCachePinning(); //moved to SystemServer and started in background
         warmUpJcaProviders();
         Log.d(TAG, "end preload");
     }
