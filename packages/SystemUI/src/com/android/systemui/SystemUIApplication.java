c5be886c011a60c20ecd80a958d66f356bdad95b
thecrazyskull <anaskarbila@aospa.co>
2016-12-20 05:43:32 -0500

    Introduce color engine.
    Color engine is a system which allows changing the primary and accent colors of the device theme on-the-fly.
    
    Colors and design by @arzbhatia
    
    The following primary colors are supported:
    * White (android default)
    * Dark
    * Pixel
    
    The following accent colors are supported:
    * Blue
    * Red
    * Teal/Default
    * Orange
    * Green
    * Yellow
    * Purple
    * Pink
    * Cyan
    
    Change-Id: I69347616e2e61e805647c2e1a3f3c4578f42847f
    Signed-off-by: @Thecrazyskull <anaskarbila@aospa.co>

--- packages/SystemUI/src/com/android/systemui/SystemUIApplication.java
+++ packages/SystemUI/src/com/android/systemui/SystemUIApplication.java
@@ -17,6 +17,8 @@
 package com.android.systemui;
 
 import android.app.Application;
+import android.app.IThemeCallback;
+import android.app.ThemeManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -25,6 +27,7 @@ import android.content.res.Configuration;
 import android.os.Process;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.Log;
 
 import com.android.systemui.stackdivider.Divider;
@@ -39,6 +42,8 @@ public class SystemUIApplication extends Application {
 
     private static final String TAG = "SystemUIService";
     private static final boolean DEBUG = false;
+
+    private ThemeManager mThemeManager;
 
     /**
      * The classes of the stuff to start.
@@ -111,6 +116,11 @@ public class SystemUIApplication extends Application {
             // components which require the SystemUI component to be initialized per-user, we
             // start those components now for the current non-system user.
             startServicesIfNeeded(SERVICES_PER_USER);
+        }
+
+        mThemeManager = (ThemeManager) getSystemService(Context.THEME_SERVICE);
+        if (mThemeManager != null) {
+            mThemeManager.addCallback(mThemeCallback);
         }
     }
 
@@ -197,4 +207,28 @@ public class SystemUIApplication extends Application {
     public SystemUI[] getServices() {
         return mServices;
     }
+
+    private final IThemeCallback mThemeCallback = new IThemeCallback.Stub() {
+
+        @Override
+        public void onThemeChanged(int themeMode, int color) {
+            onCallbackAdded(themeMode, color);
+        }
+
+        @Override
+        public void onCallbackAdded(int themeMode, int color) {
+            final boolean defaultTheme = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.THEME_ACCENT_COLOR, 0) == 0;
+            final boolean pixelTheme = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.THEME_PRIMARY_COLOR, 0) == 2;
+            if (themeMode == 0 && defaultTheme) {
+                getTheme().applyStyle(R.style.systemui_theme, true);
+            } else {
+                getTheme().applyStyle(color, true);
+            }
+            if (pixelTheme) {
+                getTheme().applyStyle(R.style.systemui_pixel_theme, true);
+            }
+        }
+    };
 }
