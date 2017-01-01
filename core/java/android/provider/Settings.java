810c31c965441a64c3ad8ef67c5d6ff92607601c
ezio84 <brabus84@gmail.com>
2016-12-31 22:50:55 -0500

    Night Light brightness mode options [1/2]
    thanks also AOSPA guys for suggestions
    
    Change-Id: I12b4f4d0893ad85dcf5c7213fd42f8a6ba567767

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6872,6 +6872,16 @@ public final class Settings {
         public static final String SYSTEM_DESIGN_FLAGS = "system_design_flags";
 
         /**
+         * Whether to set a lower brightness level when enabling night mode
+         * 0: Disabled
+         * 1: Set the brightness to a very low value
+         * 2: Set the brightness to a low value
+         * 3: Set the brightness to a medium value
+         * @hide
+         */
+        public static final String NIGHT_BRIGHTNESS_VALUE = "night_brightness_value";
+
+        /**
          * Used to store sRGB state
          * @hide
          */
--- services/core/java/com/android/server/display/NightDisplayService.java
+++ services/core/java/com/android/server/display/NightDisplayService.java
@@ -31,10 +31,14 @@ import android.content.IntentFilter;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.opengl.Matrix;
+import android.os.AsyncTask;
 import android.os.Handler;
+import android.os.IPowerManager;
 import android.os.Looper;
 import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.provider.Settings.Secure;
 import android.service.vr.IVrManager;
 import android.service.vr.IVrStateCallbacks;
@@ -118,10 +122,19 @@ public final class NightDisplayService extends SystemService
     private ContentObserver mUserSetupObserver;
     private boolean mBootCompleted;
 
+    private IPowerManager mPm;
     private NightDisplayController mController;
     private ValueAnimator mColorMatrixAnimator;
     private Boolean mIsActivated;
     private AutoMode mAutoMode;
+
+    //Night Mode custom brightness
+    private boolean mIsAdaptiveBrightness;
+    private float mNightCustomAdaptiveBrightness;
+    private float mPreviousUserAdaptiveBrightness;
+    private int mNightModeBrightnessLevel;
+    private int mNightCustomManualBrightness;
+    private int mPreviousUserManualBrightness;
 
     public NightDisplayService(Context context) {
         super(context);
@@ -145,6 +158,7 @@ public final class NightDisplayService extends SystemService
                     Slog.e(TAG, "Failed to register VR mode state listener: " + e);
                 }
             }
+            mPm = IPowerManager.Stub.asInterface(ServiceManager.getService("power"));
         } else if (phase == PHASE_BOOT_COMPLETED) {
             mBootCompleted = true;
 
@@ -263,6 +277,8 @@ public final class NightDisplayService extends SystemService
         if (mIsActivated == null || mIsActivated != activated) {
             Slog.i(TAG, activated ? "Turning on night display" : "Turning off night display");
 
+            boolean isReboot = mIsActivated == null;
+
             if (mAutoMode != null) {
                 mAutoMode.onActivated(activated);
             }
@@ -317,6 +333,82 @@ public final class NightDisplayService extends SystemService
                 }
             });
             mColorMatrixAnimator.start();
+            if (!isReboot) {
+                setBrightness(mIsActivated);
+            }
+        }
+    }
+
+    private void setBrightness(boolean activated) {
+        if (activated) {
+            updateBrightnessModeValues();
+        }
+        if (mNightModeBrightnessLevel == 0 || mPm == null) {
+            return;
+        }
+        final ContentResolver cr = getContext().getContentResolver();
+        try {
+            if (mIsAdaptiveBrightness) {
+                mPm.setTemporaryScreenAutoBrightnessAdjustmentSettingOverride(mNightCustomAdaptiveBrightness);
+                if (activated) {
+                    Settings.System.putFloatForUser(cr,
+                            Settings.System.SCREEN_AUTO_BRIGHTNESS_ADJ, mNightCustomAdaptiveBrightness,
+                            mCurrentUser);
+                } else {
+                    Settings.System.putFloatForUser(cr,
+                            Settings.System.SCREEN_AUTO_BRIGHTNESS_ADJ, mPreviousUserAdaptiveBrightness,
+                            mCurrentUser);
+                }
+            } else {
+                mPm.setTemporaryScreenBrightnessSettingOverride(mNightCustomManualBrightness);
+                if (activated) {
+                    Settings.System.putIntForUser(cr,
+                            Settings.System.SCREEN_BRIGHTNESS, mNightCustomManualBrightness,
+                            mCurrentUser);
+                } else {
+                    Settings.System.putIntForUser(cr,
+                            Settings.System.SCREEN_BRIGHTNESS, mPreviousUserManualBrightness,
+                            mCurrentUser);
+                }
+            }
+        } catch (RemoteException e) {
+            Slog.w(TAG, "Setting Brightness failed: " + e);
+        }
+    }
+
+    public void updateBrightnessModeValues() {
+        final ContentResolver cr = getContext().getContentResolver();
+        //store current brightness user values to be able to restore them lately
+        mPreviousUserAdaptiveBrightness = Settings.System.getFloatForUser(cr,
+                Settings.System.SCREEN_AUTO_BRIGHTNESS_ADJ, 0,
+                mCurrentUser);
+        mPreviousUserManualBrightness = Settings.System.getIntForUser(cr,
+                Settings.System.SCREEN_BRIGHTNESS, 0,
+                mCurrentUser);
+        //check the current brightness mode and the wanted brightness level on night mode to set
+        int currentBrightnessMode = Settings.System.getIntForUser(cr,
+                Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+                mCurrentUser);
+        mIsAdaptiveBrightness = currentBrightnessMode != Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
+        mNightModeBrightnessLevel = Settings.Secure.getIntForUser(cr,
+                Settings.Secure.NIGHT_BRIGHTNESS_VALUE, 0,
+                mCurrentUser);
+        switch (mNightModeBrightnessLevel) {
+            case 1: //lower
+                mNightCustomAdaptiveBrightness = -1f;
+                mNightCustomManualBrightness = 0;
+                break;
+            case 2: //low
+                mNightCustomAdaptiveBrightness = -0.33f;
+                mNightCustomManualBrightness = 40;
+                break;
+            case 3: //medium
+                mNightCustomAdaptiveBrightness = 0f;
+                mNightCustomManualBrightness = 100;
+                break;
+            default: //disabled
+                break;
         }
     }
 
