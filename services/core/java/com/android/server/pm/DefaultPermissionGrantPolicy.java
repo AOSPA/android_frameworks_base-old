30ed36823049dcc0149442b37aa5b54be248a963
Alex Naidis <alex.naidis@linux.com>
2017-01-03 18:24:57 +0100

    DefaultPermissionGrantPolicy: Pass correct arguments
    After parsing the xml files in the "default-permissions"
    system, we are passing the arguments in the wrong order.
    
    The method we are targeting is:
    private void grantRuntimePermissionsLPw(PackageParser.Package pkg, Set<String> permissions,
                boolean systemFixed, boolean isDefaultPhoneOrSms, int userId) {
    
    However we are passing our argument for "systemFixed"
    to "isDefaultPhoneOrSms" and the other way around too.
    
    This patch fixes it by swapping the order of these
    two arguments.
    
    Change-Id: I3604a5d6045aadcd91b6dd3f6f1b47b561403b23
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -1013,8 +1013,8 @@ final class DefaultPermissionGrantPolicy {
                         permissions.clear();
                     }
                     permissions.add(permissionGrant.name);
-                    grantRuntimePermissionsLPw(pkg, permissions, false,
-                            permissionGrant.fixed, userId);
+                    grantRuntimePermissionsLPw(pkg, permissions,
+                            permissionGrant.fixed, false, userId);
                 }
             }
         }
