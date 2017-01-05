704f3c645a99bbc4fcc1dbcc12ca35c27eb2c984
Carlo Savignano <carlosavignano@aospa.co>
2017-01-05 13:11:56 +0100

    SystemUI: support swapping navigation bar keys order
    * Forward-ported to Oreo
    * Use new API from SystemUI tuner that allows inflate
      custom layout on the fly.
    
    TODO> Allow SystemUI Tuner to override this when enabled and changes
          are made after enabling it. When disabling the tuner then use
          default arrangement and apply swap only.
    
    Change-Id: Ib1993140eb1bf7dc0017d75015516db95ce70d9a
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: joshuous <joshuous@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3974,6 +3974,14 @@ public final class Settings {
          */
 
         /**
+         * Whether user can swap navigation lateral keys.
+         * <p>
+         * Type: int (0 for false, 1 for true)
+         * @hide
+         */
+        public static final String SWAP_NAVIGATION_KEYS = "swap_navigation_keys";
+
+        /**
          * Action to perform when the home key is long-pressed.
          * (Default can be configured via config_longPressOnHardwareHomeBehavior)
          * 0 - Nothing
