f09d5338e2404982814ddf0d73aea2a7ec639528
Ivaylo Asenov <ivaylo.asenov@linaro.org>
2017-01-10 13:33:25 +0200

    Boot time: Stop bootanim earlier.
    Stop boot animation without waiting all existing windows have been drawn.
    
    Test results from hikey board with Nougat:
                TOTAL_BOOT_TIME is 0.55s less
    
    Test results from other boards with Nougat:
                TOTAL_BOOT_TIME is from 0.43s to 1.06s less
    
    Change-Id: I6344474f530b6cb7584892063ab6c83f871716c7
    Signed-off-by: Ivaylo Asenov <ivaylo.asenov@linaro.org>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6095,10 +6095,6 @@ public class WindowManagerService extends IWindowManager.Stub
                 return;
             }
 
-            // Don't enable the screen until all existing windows have been drawn.
-            if (!mForceDisplayEnabled && checkWaitingForWindowsLocked()) {
-                return;
-            }
 
             if (!mBootAnimationStopped) {
                 // Do this one time.
@@ -6119,7 +6115,13 @@ public class WindowManagerService extends IWindowManager.Stub
                 mBootAnimationStopped = true;
             }
 
-            if (!mForceDisplayEnabled && !checkBootAnimationCompleteLocked()) {
+
+	   // Don't enable the screen until all existing windows have been drawn.
+            if (!mForceDisplayEnabled && checkWaitingForWindowsLocked()) {
+                return;
+            }
+
+	   if (!mForceDisplayEnabled && !checkBootAnimationCompleteLocked()) {
                 if (DEBUG_BOOT) Slog.i(TAG_WM, "performEnableScreen: Waiting for anim complete");
                 return;
             }
