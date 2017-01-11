05e9b621331254f9c81a1f67b026a58f643861ba
Carlo Savignano <carlosavignano@aospa.co>
2017-01-11 01:30:01 +0100

    policy: refact navigation bar configs and defaults
    * Add an internal navigation bar default to use with
      our system settings NAVIGATION_BAR_ENABLED.
      Policy methods needsNavigationBar() and hasNavigationBar()
      now force-follow this as well.
    * Override system QCOM prop when our system setting changes
      which will un-break third party apps like Snapchat which
      check this property to set SystemUI flags for system bars.
    
    Change-Id: If9017d1c8afc0f2f85257477be48543e8d466a42
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -771,6 +771,7 @@ public final class PowerManagerService extends SystemService
 
     private void updateSettingsLocked() {
         final ContentResolver resolver = mContext.getContentResolver();
+        final Resources resources = mContext.getResources();
 
         mDreamsEnabledSetting = (Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.SCREENSAVER_ENABLED,
@@ -856,8 +857,11 @@ public final class PowerManagerService extends SystemService
             mButtonBrightnessEnabled = buttonBrightnessEnabled;
         }
 
+        final boolean defaultToNavigationBar = resources
+                .getBoolean(com.android.internal.R.bool.config_defaultToNavigationBar);
         final boolean navBarEnabled = Settings.System.getIntForUser(resolver,
-                Settings.System.NAVIGATION_BAR_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
+                Settings.System.NAVIGATION_BAR_ENABLED, defaultToNavigationBar ? 1 : 0,
+                        UserHandle.USER_CURRENT) != 0;
         mButtonBrightnessEnabled &= !navBarEnabled;
 
         mDirty |= DIRTY_SETTINGS;
