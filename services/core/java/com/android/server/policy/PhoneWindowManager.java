649c098d85bc83f4467ab6d12148114dea1867d8
Carlo Savignano <carlosavignano@aospa.co>
2017-01-11 01:30:01 +0100

    policy: refact navigation bar configs and defaults
    * Forward-ported to Oreo
    * Add an internal navigation bar default to use with
      our system settings NAVIGATION_BAR_ENABLED.
      Policy methods needsNavigationBar() and hasNavigationBar()
      now force-follow this as well.
    * Override system QCOM prop when our system setting changes
      which will un-break third party apps like Snapchat which
      check this property to set SystemUI flags for system bars.
    
    Change-Id: If9017d1c8afc0f2f85257477be48543e8d466a42
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: joshuous <joshuous@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2505,6 +2505,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mDisplay = display;
 
         final Resources res = mContext.getResources();
+        final ContentResolver resolver = mContext.getContentResolver();
         int shortSize, longSize;
         if (width > height) {
             shortSize = height;
@@ -2540,6 +2541,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mNavigationBarCanMove = width != height && shortSizeDp < 600;
 
         mHasNavigationBar = res.getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+
+        // Override prop based on our internal config. carlosavignano@aospa.co
+        // TODO> Create a setting helper to centralize navigation bar settings.
+        final boolean defaultToNavigationBar = res
+                .getBoolean(com.android.internal.R.bool.config_defaultToNavigationBar);
+        mNavBarEnabled = Settings.System.getIntForUser(resolver,
+                Settings.System.NAVIGATION_BAR_ENABLED, defaultToNavigationBar ? 1 : 0,
+                        UserHandle.USER_CURRENT) == 1;
+        SystemProperties.set("qemu.hw.mainkeys", mNavBarEnabled ? "0" : "1");
 
         // Allow a system property to override this. Used by the emulator.
         // See also hasNavigationBar().
@@ -2605,6 +2615,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     public void updateSettings() {
         ContentResolver resolver = mContext.getContentResolver();
+        Resources resources = mContext.getResources();
         boolean updateRotation = false;
         synchronized (mLock) {
             mEndcallBehavior = Settings.System.getIntForUser(resolver,
@@ -2629,10 +2640,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 updateWakeGestureListenerLp();
             }
 
+
+            final boolean defaultToNavigationBar = resources
+                    .getBoolean(com.android.internal.R.bool.config_defaultToNavigationBar);
+
             final boolean navBarEnabled = Settings.System.getIntForUser(resolver,
-                    Settings.System.NAVIGATION_BAR_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+                    Settings.System.NAVIGATION_BAR_ENABLED, defaultToNavigationBar ? 1 : 0,
+                            UserHandle.USER_CURRENT) == 1;
             if (navBarEnabled != mNavBarEnabled) {
                 mNavBarEnabled = navBarEnabled;
+                SystemProperties.set("qemu.hw.mainkeys", mNavBarEnabled ? "0" : "1");
             }
 
             readConfigurationDependentBehaviors();
