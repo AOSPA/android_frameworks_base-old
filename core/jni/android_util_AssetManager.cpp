ee23633f34dfdcd2be9e76ff565564a914165445
utk-spartan <utkarsh.spartan@gmail.com>
2017-02-25 18:22:32 +0530

    OMS-7N : frameworks_base : Fix compilation error
            Cause:
            merge of following commit:
                    OMS7-N: Integrate OverlayManagerService into framework [3/11]
                    https://gerrit.aospa.co/#/c/1882/
    
            This may potentially break following patch:
                    Support AOSP service to scan carrier resources
                    android_frameworks_base/commit/44764ccde343d64233fa8c96881af3f76c595cd7
    
    Change-Id: Iad1d9b85e7229f5c053dabfae8bad170c182befd

--- core/jni/android_util_AssetManager.cpp
+++ core/jni/android_util_AssetManager.cpp
@@ -1999,20 +1999,6 @@ static jintArray android_content_AssetManager_getStyleAttributes(JNIEnv* env, jo
 
 static void android_content_AssetManager_init(JNIEnv* env, jobject clazz, jboolean isSystem)
 {
-    if (isSystem) {
-        // Load frameworks-res.apk's overlay through regionalization environment
-        if (Environment::isSupported()) {
-            Environment* environment = new Environment();
-            if (environment != NULL) {
-                const char* overlay_dir = environment->getOverlayDir();
-                if (overlay_dir != NULL && strcmp(overlay_dir, "") != 0) {
-                    ALOGD("Regionalization - getOverlayDir:%s", overlay_dir);
-                    verifySystemIdmaps(overlay_dir);
-                }
-                delete environment;
-            }
-        }
-    }
     AssetManager* am = new AssetManager();
     if (am == NULL) {
         jniThrowException(env, "java/lang/OutOfMemoryError", "");
