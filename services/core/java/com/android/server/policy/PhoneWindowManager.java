0300ca1d1b87f7e1e360ec6d8e0cfc6af2f9d71b
Alex Naidis <alex.naidis@linux.com>
2017-03-03 22:18:47 +0100

    [WIP] Introduce Keypress Boost
    Introduce a new boost which boosts for a
    fine tuned amount of time on certain key events.
    
    Change-Id: Ie192475a8fa5cbf43b0cd9913f6207bb9c3c0d3d
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -108,6 +108,7 @@ import android.service.dreams.IDreamManager;
 import android.speech.RecognizerIntent;
 import android.telecom.TelecomManager;
 import android.service.gesture.EdgeGestureManager;
+import android.util.BoostFramework;
 import android.util.DisplayMetrics;
 import android.util.EventLog;
 import android.util.Log;
@@ -415,6 +416,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     Handler mHandler;
     WindowState mLastInputMethodWindow = null;
     WindowState mLastInputMethodTargetWindow = null;
+
+    private BoostFramework mPerf = null;
+    private boolean lIsPerfBoostEnabled;
+    private int[] mBoostParamVal;
 
     // FIXME This state is shared between the input reader and handler thread.
     // Technically it's broken and buggy but it has been like this for many years
@@ -1640,6 +1645,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
         mAppOpsManager = (AppOpsManager) mContext.getSystemService(Context.APP_OPS_SERVICE);
         mHasFeatureWatch = mContext.getPackageManager().hasSystemFeature(FEATURE_WATCH);
+
+
+        // Initialise Keypress Boost
+        lIsPerfBoostEnabled = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_enableKeypressBoost);
+        mBoostParamVal = context.getResources().getIntArray(
+                com.android.internal.R.array.keypressboost_param_value);
+        if (lIsPerfBoostEnabled) {
+            mPerf = new BoostFramework();
+        }
 
         // Init display burn-in protection
         boolean burnInProtectionEnabled = context.getResources().getBoolean(
@@ -3801,6 +3816,37 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         return mSearchManager;
     }
 
+    private void dispatchKeypressBoost(int keyCode, boolean interactive) {
+        int mBoostDuration = 0;
+
+        // Calculate the duration of the boost
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_UNKNOWN:
+                return;
+            case KeyEvent.KEYCODE_HOME:
+            case KeyEvent.KEYCODE_BACK:
+            case KeyEvent.KEYCODE_SOFT_LEFT:
+            case KeyEvent.KEYCODE_SOFT_RIGHT:
+                if (interactive)
+                    mBoostDuration = 350;
+                break;
+            case KeyEvent.KEYCODE_POWER:
+            case KeyEvent.KEYCODE_CAMERA:
+                mBoostDuration = 750;
+                break;
+            case KeyEvent.KEYCODE_CLEAR:
+            case KeyEvent.KEYCODE_MEDIA_PLAY:
+                mBoostDuration = 500;
+                break;
+        }
+
+        // Dispatch the boost
+        if (mBoostDuration != 0) {
+            Slog.i(TAG, "Dispatching Keypress boost for " + mBoostDuration + " ms.");
+            mPerf.perfLockAcquire(mBoostDuration, mBoostParamVal);
+        }
+    }
+
     private void preloadRecentApps() {
         mPreloadedRecentApps = true;
         StatusBarManagerInternal statusbar = getStatusBarManagerInternal();
@@ -5793,6 +5839,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             Log.d(TAG, "interceptKeyTq keycode=" + keyCode
                     + " interactive=" + interactive + " keyguardActive=" + keyguardActive
                     + " policyFlags=" + Integer.toHexString(policyFlags));
+        }
+
+        // Intercept the Keypress event for Keypress boost
+        if (lIsPerfBoostEnabled) {
+            dispatchKeypressBoost(keyCode, interactive);
         }
 
         // Basic policy based on interactive state.
