ea744b1fabc302729b3262a81ea4dd8316ba8f2a
Carlo Savignano <carlosavignano@aospa.co>
2017-03-29 15:09:00 +0100

    Add gestures control [3/4]
    * Database policy.
    * KeyHandler for gestures inputs.
    * Input policy integration.
    * Supported ALL QCOM chipsets synaptic gestures.
    * Add support for new "S" gesture
    
    Ticket: NOUGAT-17
    
    Jake's notes from Quartz:
    Squashed the following commits:
    KeyHandler: Fix case when KEY_WAKEUP keycode is used for gestures
    PhoneWindowManager: Check if keyhandler is null before using
    
    Change-Id: I47a61cf054b56d601a4a332359daabb3c666309f
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Willi Ye <williye97@gmail.com>
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>
    [TheStrix: Forward-port to Pie]

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3758,7 +3758,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             pm.wakeUp(SystemClock.uptimeMillis(), PowerManager.WAKE_REASON_CAMERA_LAUNCH,
                     "com.android.systemui:CAMERA_GESTURE");
         }
-        vibrateForCameraGesture();
+        if (source != StatusBarManager.CAMERA_LAUNCH_SOURCE_SCREEN_GESTURE) {
+            vibrateForCameraGesture();
+        }
 
         if (source == StatusBarManager.CAMERA_LAUNCH_SOURCE_POWER_DOUBLE_TAP) {
             Log.v(TAG, "Camera launch");
