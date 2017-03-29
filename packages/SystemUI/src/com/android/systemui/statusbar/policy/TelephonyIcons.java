d0f2dda517a4a85f023a10f1f66db825bade8c54
mqi <mqi@codeaurora.org>
2017-03-29 15:46:17 +0800

    SystemUI: Hide X Mark for QS Signal
    Hide X Mark for QS Signal to align with the status bar signal display.
    
    Change-Id: I5e93344a016787ad35ae43218ad3298e14ea5c40
    CRs-Fixed: 2024995

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/TelephonyIcons.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/TelephonyIcons.java
@@ -53,11 +53,11 @@ class TelephonyIcons {
     static final int QS_TELEPHONY_NO_NETWORK = R.drawable.ic_qs_signal_no_signal;
 
     static final int[][] QS_TELEPHONY_SIGNAL_STRENGTH = {
-        { R.drawable.ic_qs_signal_0,
-          R.drawable.ic_qs_signal_1,
-          R.drawable.ic_qs_signal_2,
-          R.drawable.ic_qs_signal_3,
-          R.drawable.ic_qs_signal_4 },
+        { R.drawable.ic_qs_signal_full_0,
+          R.drawable.ic_qs_signal_full_1,
+          R.drawable.ic_qs_signal_full_2,
+          R.drawable.ic_qs_signal_full_3,
+          R.drawable.ic_qs_signal_full_4 },
         { R.drawable.ic_qs_signal_full_0,
           R.drawable.ic_qs_signal_full_1,
           R.drawable.ic_qs_signal_full_2,
