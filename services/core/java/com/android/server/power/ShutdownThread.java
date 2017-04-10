0908d901fe1e565d2dc72d3cafa9b8599033bcc9
Carlo Savignano <carlosavignano@aospa.co>
2017-04-10 10:40:25 +0200

    policy: fix ShutdownThread reboot dialog labelling logic
    * We don't check for SHUTDOWN_USER_REQUESTED since it is assumed
      this reason is used for all not-handled cases, mReboot will
      judge whether is a shutdown or a reboot.
    
    Change-Id: I21b9740c1788afc04bc1f663e397afaab7a18bba
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -364,7 +364,11 @@ public final class ShutdownThread extends Thread {
             pd.setTitle(context.getText(com.android.internal.R.string.global_action_restart));
             pd.setMessage(context.getText(com.android.internal.R.string.reboot_progress));
             pd.setIndeterminate(true);
-        } else if (PowerManager.SHUTDOWN_USER_REQUESTED.equals(mReason)) {
+        } else if (PowerManager.REBOOT_BOOTLOADER.equals(mReason)) {
+            pd.setTitle(context.getText(com.android.internal.R.string.global_action_restart));
+            pd.setMessage(context.getText(com.android.internal.R.string.reboot_progress));
+            pd.setIndeterminate(true);
+        } else { // PowerManager.SHUTDOWN_USER_REQUESTED
             if (mReboot) {
                 pd.setTitle(context.getText(com.android.internal.R.string.global_action_restart));
                 pd.setMessage(context.getText(com.android.internal.R.string.reboot_progress));
@@ -372,10 +376,6 @@ public final class ShutdownThread extends Thread {
                 pd.setTitle(context.getText(com.android.internal.R.string.power_off));
                 pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
             }
-            pd.setIndeterminate(true);
-        } else if (PowerManager.REBOOT_BOOTLOADER.equals(mReason)) {
-            pd.setTitle(context.getText(com.android.internal.R.string.global_action_restart));
-            pd.setMessage(context.getText(com.android.internal.R.string.reboot_progress));
             pd.setIndeterminate(true);
         }
         pd.setCancelable(false);
