0bba63a622d0e7f0410d69c890df471d8ad1bae7
Anas Karbila <anaskarbila@gmail.com>
2017-04-17 19:29:44 +0900

    services: Use pocket mode detection for gestures proximity wake
    Change-Id: I229bb03b7b500d39287b37c6d3d61adca9709ada

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -844,6 +844,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
             if (wasDeviceInPocket != mIsDeviceInPocket) {
                 handleDevicePocketStateChanged();
+                if (mKeyHandler != null) {
+                    mKeyHandler.setIsInPocket(mIsDeviceInPocket);
+                }
             }
         }
 
