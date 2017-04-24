78fda4ddabbc5e2eb0faaac8adf038b6f4d3ceb6
Alex Naidis <alex.naidis@linux.com>
2017-04-24 22:00:05 +0200

    Revert "Creating only a single activity trigger module"
    This is the main incompatibility in ActivityTrigger,
    this needs to be reverted.
    
    This reverts commit 81dd8c094fb641e05b8f13fbeb00b3557e3ef842.
    
    Change-Id: Iafaf11d25f0f09b2204a178aba012ee215a3e0c3
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -577,11 +577,6 @@ public final class ActivityManagerService extends ActivityManagerNative
     private int lBoost_v2_TimeOut = 0;
     private int lBoost_v2_ParamVal[];
 
-    /*define misc. activty trigger function*/
-    static final int START_PROCESS = 1;
-    static final int NETWORK_OPTS = 2;
-    static final int ANIMATION_SCALE = 3;
-
     /** All system services */
     SystemServiceManager mSystemServiceManager;
 
@@ -3053,11 +3048,11 @@ public final class ActivityManagerService extends ActivityManagerNative
                 /* netType: 0 for Mobile, 1 for WIFI*/
                 int netType = netInfo.getType();
                 if (mActivityTrigger != null) {
-                    mActivityTrigger.activityMiscTrigger(NETWORK_OPTS, packageName, netType, flag);
+                    mActivityTrigger.networkOptsCheck(flag, netType, packageName);
                 }
             } else {
                 if (mActivityTrigger != null) {
-                    mActivityTrigger.activityMiscTrigger(NETWORK_OPTS, packageName, ConnectivityManager.TYPE_NONE, flag);
+                    mActivityTrigger.networkOptsCheck(flag, ConnectivityManager.TYPE_NONE, packageName);
                 }
             }
         }
@@ -4022,7 +4017,7 @@ public final class ActivityManagerService extends ActivityManagerNative
             }
             checkTime(startTime, "startProcess: done updating pids map");
             if ("activity".equals(hostingType) || "service".equals(hostingType)) {
-                mActivityTrigger.activityMiscTrigger(START_PROCESS, app.processName, startResult.pid, 0);
+                mActivityTrigger.activityStartProcessTrigger(app.processName, startResult.pid);
             }
         } catch (RuntimeException e) {
             Slog.e(TAG, "Failure starting process " + app.processName, e);
