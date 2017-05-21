c77fc4502cf7927d6f61658fdccafb4c5f1a061e
Park Ju Hyung <qkrwngud825@gmail.com>
2017-05-21 16:05:33 +0900

    PowerProfile: allow reading from a file instead of resource
    Unified builds may require reading different power profile for each variants.
    
    Allow reading power profile from a file instead of resource by querying
    ro.power_profile prop.
    
    Device maintainers can opt-in by using the init extension.
    
    Change-Id: I346bb3e5a71edfe7570af495b73ffd854042367f
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    (cherry picked from commit fdedb9e883c7112678ba06ccc7205634b05c8404)

--- core/java/com/android/internal/os/PowerProfile.java
+++ core/java/com/android/internal/os/PowerProfile.java
@@ -20,6 +20,8 @@ package com.android.internal.os;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
+import android.os.SystemProperties;
+import android.util.Xml;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.XmlUtils;
@@ -27,6 +29,10 @@ import com.android.internal.util.XmlUtils;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
+import libcore.io.IoUtils;
+
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -243,15 +249,28 @@ public class PowerProfile {
     }
 
     private void readPowerValuesFromXml(Context context, boolean forTest) {
-        final int id = forTest ? com.android.internal.R.xml.power_profile_test :
-                com.android.internal.R.xml.power_profile;
+        final String profilePath = SystemProperties.get("ro.vendor.power_profile",
+                                           "/system/vendor/etc/power_profile.xml");
         final Resources resources = context.getResources();
-        XmlResourceParser parser = resources.getXml(id);
+        XmlResourceParser resParser = null;
+        XmlPullParser parser = null;
         boolean parsingArray = false;
         ArrayList<Double> array = new ArrayList<>();
         String arrayName = null;
+        FileReader profileReader = null;
 
         try {
+            if (new File(profilePath).exists()) {
+                profileReader = new FileReader(profilePath);
+                parser = Xml.newPullParser();
+                parser.setInput(profileReader);
+            } else {
+                int id = forTest ? com.android.internal.R.xml.power_profile_test :
+                    com.android.internal.R.xml.power_profile;
+                resParser = resources.getXml(id);
+                parser = resParser;
+            }
+
             XmlUtils.beginDocument(parser, TAG_DEVICE);
 
             while (true) {
@@ -295,7 +314,10 @@ public class PowerProfile {
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
-            parser.close();
+            if (resParser != null) {
+                resParser.close();
+            }
+            IoUtils.closeQuietly(profileReader);
         }
 
         // Now collect other config variables.
