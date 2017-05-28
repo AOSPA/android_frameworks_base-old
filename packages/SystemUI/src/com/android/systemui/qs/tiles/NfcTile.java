8e5187bbf3d2557651854cdebaaf449cdc6207b2
Jake Weinstein <xboxlover360@gmail.com>
2017-05-28 16:27:18 -0400

    base: Make our copyright consistent again
    Change-Id: I3f4ee0956959971578ef916f44198fff41c169ed

--- packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016, The Android Open Source Project
- * Contributed by the Paranoid Android Project
+ * Contributed by Paranoid Android
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
