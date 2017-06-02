66d0a8c00c2582a662469035eda6c37b7237c102
Jake Weinstein <xboxlover360@gmail.com>
2017-06-01 23:37:01 -0400

    NetworkMonitor: fall back to Chinese Google URL
    * Chinese users cannot access the global Google page without
      a VPN which causes issues with Captive Portal.  All users
      can connect to the Chinese page and as it is a fallback,
      it won't be used too often anyway.
    
    * Thank you to aaronpoweruser for his guidance here.
    
    Change-Id: I21f3c02bfb042d21dc495d704f5a2e6afc16392e

--- services/core/java/com/android/server/connectivity/NetworkMonitor.java
+++ services/core/java/com/android/server/connectivity/NetworkMonitor.java
@@ -89,7 +89,7 @@ public class NetworkMonitor extends StateMachine {
     private static final String DEFAULT_HTTPS_URL     = "https://www.google.com/generate_204";
     private static final String DEFAULT_HTTP_URL      =
             "http://connectivitycheck.gstatic.com/generate_204";
-    private static final String DEFAULT_FALLBACK_URL  = "http://www.google.com/gen_204";
+    private static final String DEFAULT_FALLBACK_URL  = "http://www.google.cn/gen_204";
     private static final String DEFAULT_USER_AGENT    = "Mozilla/5.0 (X11; Linux x86_64) "
                                                       + "AppleWebKit/537.36 (KHTML, like Gecko) "
                                                       + "Chrome/52.0.2743.82 Safari/537.36";
