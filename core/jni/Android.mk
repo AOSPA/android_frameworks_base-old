96d30ce74db16a7ccf49fbcc52bf3a454871d665
Caleb Xu <calebcenter@live.com>
2017-06-14 11:17:13 -0400

    core/jni: Use sdclang lto for building libandroid_runtime
    Change-Id: Ieb4cb1f613feecbde83046d764842fb7904b866f
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -286,6 +286,7 @@ LOCAL_CFLAGS += -Wall -Werror -Wno-error=deprecated-declarations -Wunused -Wunre
 # -Wno-c++11-extensions: Clang warns about Skia using the C++11 override keyword, but this project
 #                        is not being compiled with that level. Remove once this has changed.
 LOCAL_CLANG_CFLAGS += -Wno-c++11-extensions
+LOCAL_SDCLANG_LTO := true
 
 include $(BUILD_SHARED_LIBRARY)
 
