c52199e8b8376cf1015b2f8149f0870cae686f55
zfu <zfu@codeaurora.org>
2017-07-12 11:14:33 +0800

    Expose perfLock  handle to JAVA context.
    This API change has no impact for current BoostFramework solution, modifications are as below:
    1.When user calls perfLockAcquire/perfHint, if SUCCEED, the return value is unique handle,which is > 0, if FAILED, the return
    value is -1.
    2.One new API is added.
    public int perfLockReleaseHandler(int handle), handle is the unique value, and user can use this to release perflock
    in different contexts.
    
    Change-Id: I48a2513c524c1fa69609cc405940a44590243fa7
    CRs-Fixed: 2017684

--- core/java/android/util/BoostFramework.java
+++ core/java/android/util/BoostFramework.java
@@ -51,6 +51,7 @@ public class BoostFramework {
     private static Method mAcquireFunc = null;
     private static Method mPerfHintFunc = null;
     private static Method mReleaseFunc = null;
+    private static Method mReleaseHandlerFunc = null;
     private static Constructor<Class> mConstructor = null;
 
 /** @hide */
@@ -99,6 +100,9 @@ public class BoostFramework {
                     argClasses = new Class[] {};
                     mReleaseFunc =  perfClass.getDeclaredMethod("perfLockRelease", argClasses);
 
+                    argClasses = new Class[] {int.class};
+                    mReleaseHandlerFunc =  perfClass.getDeclaredMethod("perfLockReleaseHandler", argClasses);
+
                     mIsLoaded = true;
                 }
                 catch(Exception e) {
@@ -134,6 +138,18 @@ public class BoostFramework {
         int ret = -1;
         try {
             Object retVal = mReleaseFunc.invoke(mPerf);
+            ret = (int)retVal;
+        } catch(Exception e) {
+            Log.e(TAG,"Exception " + e);
+        }
+        return ret;
+    }
+
+/** @hide */
+    public int perfLockReleaseHandler(int handle) {
+        int ret = -1;
+        try {
+            Object retVal = mReleaseHandlerFunc.invoke(mPerf, handle);
             ret = (int)retVal;
         } catch(Exception e) {
             Log.e(TAG,"Exception " + e);
