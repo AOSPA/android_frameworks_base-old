2690b562f5bd4c3df900e04c56d876a2140ee56b
Park Ju Hyung <qkrwngud825@gmail.com>
2017-07-20 18:04:37 +0900

    [DO NOT MERGE] AppOps: extend OP_RUN_IN_BACKGROUND to also control wakelocks and wakeups
    Wakeup appops code were taken from LineageOS' Privacy Guard implementation
    by Sam Mortimer <sam@mortimer.me.uk>.
    
    Change-Id: I410b2fc6da9d493b98838df7dc3b7aa577348a50
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- services/core/java/com/android/server/AlarmManagerService.java
+++ services/core/java/com/android/server/AlarmManagerService.java
@@ -1748,6 +1748,24 @@ class AlarmManagerService extends SystemService {
         } else {
             maxElapsed = triggerElapsed + windowLength;
         }
+
+        if (operation != null &&
+            operation.getCreatorUid() >= Process.FIRST_APPLICATION_UID &&
+                (type == AlarmManager.RTC_WAKEUP
+                        || type == AlarmManager.ELAPSED_REALTIME_WAKEUP)
+                && mAppOps.checkOpNoThrow(AppOpsManager.OP_RUN_IN_BACKGROUND,
+                        operation.getCreatorUid(),
+                        operation.getCreatorPackage())
+                == AppOpsManager.MODE_IGNORED) {
+
+            if (type == AlarmManager.RTC_WAKEUP) {
+                type = AlarmManager.RTC;
+            } else {
+                type = AlarmManager.ELAPSED_REALTIME;
+            }
+
+        }
+
         synchronized (mLock) {
             if (DEBUG_BATCH) {
                 Slog.v(TAG, "set(" + operation + ") : type=" + type
