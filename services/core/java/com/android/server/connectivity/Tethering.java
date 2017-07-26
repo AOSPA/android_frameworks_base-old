c966ad906f01c0c667d8692caabc55bfe968ebb0
Lior David <liord@codeaurora.org>
2017-07-26 17:19:44 +0300

    Tethering: remove TETHERING_WIGIG state machine on interface down
    The wigig state machine relies on a TETHERING_STATE_CHANGED broadcast
    that is sent when the tethering state machine is first created, during
    interface up. Currently the tethering state machine is not removed
    on interface down except for TETHERING_BLUETOOTH, and as a result
    wigig tethering only works the first time SoftAP is started.
    In order to fix this, remove the tethering state machine on interface
    down for TETHERING_WIGIG as well.
    
    Change-Id: I708b90e77c109d56ff30b12d032a263e88e62abb

--- services/core/java/com/android/server/connectivity/Tethering.java
+++ services/core/java/com/android/server/connectivity/Tethering.java
@@ -247,7 +247,8 @@ public class Tethering extends BaseNetworkObserver implements IControlsTethering
             if (up) {
                 maybeTrackNewInterfaceLocked(iface);
             } else {
-                if (ifaceNameToType(iface) == ConnectivityManager.TETHERING_BLUETOOTH) {
+                if (ifaceNameToType(iface) == ConnectivityManager.TETHERING_BLUETOOTH ||
+                    ifaceNameToType(iface) == ConnectivityManager.TETHERING_WIGIG) {
                     stopTrackingInterfaceLocked(iface);
                 } else {
                     // Ignore usb0 down after enabling RNDIS.
