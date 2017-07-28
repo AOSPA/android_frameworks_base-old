eba2e8128fbdc9966efd814269610a273d0980cc
Ivan Iskandar <ivan@prjkt.io>
2017-07-28 04:56:45 +0200

    base: Introduce SubstratumService
    Change-Id: I8c9f331ca7e4c68c35ea13bed587775ee4abeb30
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>
    Signed-off-by: Nicholas Chum <nicholas@prjkt.io>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- Android.mk
+++ Android.mk
@@ -179,6 +179,7 @@ LOCAL_SRC_FILES += \
 	core/java/android/content/pm/IPinItemRequest.aidl \
 	core/java/android/content/pm/IShortcutService.aidl \
 	core/java/android/content/pm/permission/IRuntimePermissionPresenter.aidl \
+	core/java/android/content/substratum/ISubstratumService.aidl \
 	core/java/android/database/IContentObserver.aidl \
 	../av/camera/aidl/android/hardware/ICameraService.aidl \
 	../av/camera/aidl/android/hardware/ICameraServiceListener.aidl \
@@ -407,6 +408,7 @@ LOCAL_SRC_FILES += \
 	core/java/com/android/internal/os/IShellCallback.aidl \
 	core/java/com/android/internal/statusbar/IStatusBar.aidl \
 	core/java/com/android/internal/statusbar/IStatusBarService.aidl \
+	core/java/com/android/internal/substratum/ISubstratumHelperService.aidl \
 	core/java/com/android/internal/textservice/ISpellCheckerService.aidl \
 	core/java/com/android/internal/textservice/ISpellCheckerServiceCallback.aidl \
 	core/java/com/android/internal/textservice/ISpellCheckerSession.aidl \
--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7123,6 +7123,13 @@ public final class Settings {
         public static final String QS_AUTO_ADDED_TILES = "qs_auto_tiles";
 
         /**
+         * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
