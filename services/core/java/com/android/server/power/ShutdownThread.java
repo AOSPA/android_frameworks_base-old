570bed4ac2004c8e3c63a7291300138390349685
zljing <zljing@codeaurora.org>
2017-08-02 15:10:40 +0800

    framework: shorten wait time for shutdown time optimization
    Default 500ms delay is waitting shutdown actions done in while cycle.
    Shutdown actions include broadcast,nfc,radio,wifi and mountservice.
    The whole actions are ordered serial operations. This delay is too
    long for broadcast/nfc/radio/wifi/mountservice shutdown. Decrease the
    delay will add mActionDoneSync lock time and while cycle times, so this
    delay also can't be too short. Set half will have 0.5~0.7s benefit for
    shutdown time, total time from 2.4s to 1.7~1.9s, which is comparable
    with ref device.
    
    CRs-Fixed: 2023578
    
    Change-Id: Ieb34034652c3cba1c41eadc00500d1f211c2a009

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -70,7 +70,7 @@ import java.nio.charset.StandardCharsets;
 public final class ShutdownThread extends Thread {
     // constants
     private static final String TAG = "ShutdownThread";
-    private static final int PHONE_STATE_POLL_SLEEP_MSEC = 500;
+    private static final int PHONE_STATE_POLL_SLEEP_MSEC = 250;
     // maximum time we wait for the shutdown broadcast before going on.
     private static final int MAX_BROADCAST_TIME = 10*1000;
     //maximun time we wait for the mountservice before going on.
