1f47f25525dcd8012539be453ee1d6b63e0fdc77
Rituj Beniwal <ritujbeniwal@gmail.com>
2017-08-25 22:03:00 +0530

    PocketJudge: Improvements
    * Do not animate the pocketLock view. It is hardly noticeable. Fixes a race condition as well when you animate and toggle the sensors super fast. The system server crashes.
    * Automatically turn off the screen when showing the message after 2.5 secs.
    * Fix phone going to sleep when the phone wakes up but not due to the Power button(like a call, alarm, etc.) and pocket msg is showing. Very annoying when a call comes and the phone is facing downwards.
    * Fix glitches when the screen is showing and navigation bar is enabled.
    * Major battery improvements. Now the view is not updated or animated or even attached to the window while in pocket and screen turned off.
    * Stop listening to the PocketState as soon as the user unlocks the device in any form.
    * May have missed something.
    
    Change-Id: If10d27eb676603c68fa63c0ad5e6ed4350bc8b23

--- services/core/java/com/android/server/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/fingerprint/FingerprintService.java
@@ -44,6 +44,7 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.pocket.PocketManager;
 import android.util.Slog;
 
 import com.android.internal.logging.MetricsLogger;
@@ -121,6 +122,8 @@ public class FingerprintService extends SystemService implements IBinder.DeathRe
     private ClientMonitor mCurrentClient;
     private ClientMonitor mPendingClient;
     private PerformanceStats mPerformanceStats;
+    
+    private PocketManager mPocketManager;
 
     // Normal fingerprint authentications are tracked by mPerformanceMap.
     private HashMap<Integer, PerformanceStats> mPerformanceMap
@@ -200,6 +203,15 @@ public class FingerprintService extends SystemService implements IBinder.DeathRe
         mDaemon = null;
         mCurrentUserId = UserHandle.USER_CURRENT;
         handleError(mHalDeviceId, FingerprintManager.FINGERPRINT_ERROR_HW_UNAVAILABLE);
+    }
+    
+    @Override
+    public void onBootPhase(int phase) {
+        switch(phase) {
+            case PHASE_SYSTEM_SERVICES_READY:
+                mPocketManager = (PocketManager) mContext.getSystemService(Context.POCKET_SERVICE);
+                break;
+        }
     }
 
     public IFingerprintDaemon getFingerprintDaemon() {
@@ -707,6 +719,7 @@ public class FingerprintService extends SystemService implements IBinder.DeathRe
             mHandler.post(new Runnable() {
                 @Override
                 public void run() {
+					if(mPocketManager != null) mPocketManager.setListeningExternal(false);
                     handleAuthenticated(deviceId, fingerId, groupId);
                 }
             });
@@ -814,32 +827,32 @@ public class FingerprintService extends SystemService implements IBinder.DeathRe
             final int callingUserId = UserHandle.getCallingUserId();
             final int pid = Binder.getCallingPid();
             final boolean restricted = isRestricted();
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    if (!canUseFingerprint(opPackageName, true /* foregroundOnly */,
-                            callingUid, pid)) {
-                        if (DEBUG) Slog.v(TAG, "authenticate(): reject " + opPackageName);
-                        return;
-                    }
+			mHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					if (!canUseFingerprint(opPackageName, true /* foregroundOnly */,
+							callingUid, pid)) {
+						if (DEBUG) Slog.v(TAG, "authenticate(): reject " + opPackageName);
+						return;
+					}
 
-                    MetricsLogger.histogram(mContext, "fingerprint_token", opId != 0L ? 1 : 0);
+					MetricsLogger.histogram(mContext, "fingerprint_token", opId != 0L ? 1 : 0);
 
-                    // Get performance stats object for this user.
-                    HashMap<Integer, PerformanceStats> pmap
-                            = (opId == 0) ? mPerformanceMap : mCryptoPerformanceMap;
-                    PerformanceStats stats = pmap.get(mCurrentUserId);
-                    if (stats == null) {
-                        stats = new PerformanceStats();
-                        pmap.put(mCurrentUserId, stats);
-                    }
-                    mPerformanceStats = stats;
+					// Get performance stats object for this user.
+					HashMap<Integer, PerformanceStats> pmap
+							= (opId == 0) ? mPerformanceMap : mCryptoPerformanceMap;
+					PerformanceStats stats = pmap.get(mCurrentUserId);
+					if (stats == null) {
+						stats = new PerformanceStats();
+						pmap.put(mCurrentUserId, stats);
+					}
+					mPerformanceStats = stats;
 
-                    startAuthentication(token, opId, callingUserId, groupId, receiver,
-                            flags, restricted, opPackageName);
-                }
-            });
-        }
+					startAuthentication(token, opId, callingUserId, groupId, receiver,
+							flags, restricted, opPackageName);
+				}
+			});
+		}
 
         @Override // Binder call
         public void cancelAuthentication(final IBinder token, final String opPackageName) {
