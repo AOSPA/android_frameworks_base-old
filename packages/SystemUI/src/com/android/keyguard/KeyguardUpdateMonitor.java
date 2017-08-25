13769bffb1151bb75c8b1643cecdeca7446a622b
Alexander Martinz <alex@amartinz.at>
2017-08-25 12:01:11 +0200

    BatteryService: Add support for oem fast charger detection
    Based on AOSPA fw/base 0af31949c69f7c306b49ecf2c25579300973fdc9
      - https://github.com/AOSPA/android_frameworks_base/commit/0af31949c69f7c306b49ecf2c25579300973fdc9
    
    Allows to indicate whether a device is charged using a
    proprietary oem fast charge solution which writes status
    to sysfs.
    [nift4: Some devices turn off fast charging when display is on,
    which is why even though AOSP detects the fast charger, the user won't
    actually get to see it without this commit.]
    
    The oem fast charge detection tries to be as generic as possible and is
    configured via overlays.
    
    nift4:
    - port to 13
    - support using custom value for path, don't hardcode 1
    - big cleanup
    
    Change-Id: Ia91a9c3bb20967c5aac18c24ac7c2be10a0b8bf1
    Signed-off-by: Alexander Martinz <alex@amartinz.at>
    Signed-off-by: BladeRunner-A2C <john.smith@unused.email>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2565,7 +2565,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
         mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, /* level= */ 100, /* plugged= */
-                0, CHARGING_POLICY_DEFAULT, /* maxChargingWattage= */0, /* present= */true);
+                0, CHARGING_POLICY_DEFAULT, /* maxChargingWattage= */0, /* present= */true, false);
 
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();
@@ -4058,6 +4058,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
         // change in the incompatible charger
         if (!old.incompatibleCharger.equals(current.incompatibleCharger)) {
+            return true;
+        }
+
+        // change in oem fast charging while plugged in
+        if (nowPluggedIn && current.oemFastCharging != old.oemFastCharging) {
             return true;
         }
 
