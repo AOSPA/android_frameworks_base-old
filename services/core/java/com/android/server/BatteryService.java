ecdec4bc611579ceb88567f8bf902d1a8e78ca01
Sam Mortimer <sam@mortimer.me.uk>
2017-09-04 18:56:52 +0200

    base: Allow to disable battery light [1/2]
    [jhenrique09: Fix battery light toggle on multiple users]
    
    Change-Id: Ia4a1bb45aacf3a25839e9873a7a8d2e131fbc0ea
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -184,6 +184,10 @@ public final class BatteryService extends SystemService {
 
     private Led mLed;
 
+    // Battery light customization
+    private boolean mBatteryLightEnabled;
+    private boolean mHasIntrusiveBatteryLed;
+
     private boolean mSentLowBatteryBroadcast = false;
 
     private ActivityManagerInternal mActivityManagerInternal;
@@ -233,6 +237,9 @@ public final class BatteryService extends SystemService {
             invalidChargerObserver.startObserving(
                     "DEVPATH=/devices/virtual/switch/invalid_charger");
         }
+        mHasIntrusiveBatteryLed = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_intrusiveBatteryLed);
+        mBatteryLightEnabled = mHasIntrusiveBatteryLed;
     }
 
     @Override
@@ -265,6 +272,39 @@ public final class BatteryService extends SystemService {
                         false, obs, UserHandle.USER_ALL);
                 updateBatteryWarningLevelLocked();
             }
+        } else if (phase == PHASE_BOOT_COMPLETED) {
+            SettingsObserver mObserver = new SettingsObserver(new Handler());
+            mObserver.observe();
+        }
+    }
+
+    private synchronized void updateLed() {
+        mLed.updateLightsLocked();
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.Global.getUriFor(
+                    Settings.Global.BATTERY_LIGHT_ENABLED),
+                    false, this, UserHandle.USER_ALL);
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+        public void update() {
+            ContentResolver resolver = mContext.getContentResolver();
+            mBatteryLightEnabled = Settings.Global.getInt(resolver,
+                    Settings.Global.BATTERY_LIGHT_ENABLED, mHasIntrusiveBatteryLed ? 1 : 0) == 1;
+            updateLed();
         }
     }
 
@@ -1106,7 +1146,9 @@ public final class BatteryService extends SystemService {
             }
             final int level = mHealthInfo.batteryLevel;
             final int status = mHealthInfo.batteryStatus;
-            if (level < mLowBatteryWarningLevel) {
+            if (!mBatteryLightEnabled) {
+                mBatteryLight.turnOff();
+            } else if (level < mLowBatteryWarningLevel) {
                 if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
                     // Solid red when battery is charging
                     mBatteryLight.setColor(mBatteryLowARGB);
