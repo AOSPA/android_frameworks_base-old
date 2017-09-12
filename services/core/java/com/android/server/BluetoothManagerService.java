7cdbd4cadf12aff97a057782ab9cf13d997e60b0
Bandari Ramesh <ramban@codeaurora.org>
2017-09-12 17:44:27 +0530

    BT: Clearing BLE app count while processing factory reset.
    Change-Id: Iabf223c3c956eb82e02b80f989b97f041ebc6304
    CRs-Fixed: 2108924

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -1345,6 +1345,20 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
         return mName;
     }
 
+    public boolean factoryReset() {
+        try {
+            if (mBluetooth != null) {
+                // Clear registered LE apps to force shut-off
+                clearBleApps();
+                return mBluetooth.factoryReset();
+            }
+        } catch (RemoteException e) {
+            Slog.e(TAG, "factoryReset(): Unable to do factoryReset.", e);
+            return false;
+        }
+        return true;
+    }
+
     private class BluetoothServiceConnection implements ServiceConnection {
         public void onServiceConnected(ComponentName componentName, IBinder service) {
             String name = componentName.getClassName();
