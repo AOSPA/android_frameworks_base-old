bce512c197e124d13b58f489ea10a27a99265d1c
Niraj kumar Mishra <nmishr@codeaurora.org>
2017-10-06 17:42:30 +0530

    BT:Crash occured due to null pointer exception.
    Reason: Trying to access remote device
    object (mobjectdevice)which have been set to NULL before.
    
    Fix:Avoid handling state changes for invalid devices(NULL)
    
    Change-Id: If21cf7a3059a7942dcfa5b2cbe568810fa4676a9
    CRs-Fixed: 2121814

--- packages/SettingsLib/src/com/android/settingslib/bluetooth/LocalBluetoothProfileManager.java
+++ packages/SettingsLib/src/com/android/settingslib/bluetooth/LocalBluetoothProfileManager.java
@@ -306,6 +306,11 @@ public class LocalBluetoothProfileManager {
         }
 
         public void onReceive(Context context, Intent intent, BluetoothDevice device) {
+            if (device == null) {
+                Log.w(TAG, "StateChangedHandler receives state-change for invalid device");
+                return;
+            }
+
             CachedBluetoothDevice cachedDevice = mDeviceManager.findDevice(device);
             if (cachedDevice == null) {
                 Log.w(TAG, "StateChangedHandler found new device: " + device);
