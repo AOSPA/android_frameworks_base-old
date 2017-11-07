2dac9e5aa43b9788d7cf78a8c7755bcd7d9eb34c
Prabhanjan Kandula <pkandula@codeaurora.org>
2017-11-07 12:32:36 +0530

    frameworks/base: Add HDMI hotplug handling
    Listen on uevent for custom external display hotplug
    till an alternative for switch node is finalized.
    
    CRs-Fixed: 2299180
    Change-Id: I9abe3cf9115ea4547329e36e5e7985728acc04f4

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -995,6 +995,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     };
 
+    private UEventObserver mExtEventObserver = new UEventObserver() {
+        @Override
+        public void onUEvent(UEventObserver.UEvent event) {
+            if (event.get("status") != null) {
+                setHdmiPlugged("connected".equals(event.get("status")));
+            }
+        }
+    };
+
     class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {
             super(handler);
@@ -6065,6 +6074,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     void setHdmiPlugged(boolean plugged) {
         if (mHdmiPlugged != plugged) {
+            Slog.v(TAG, "Broadcasting Intent for HDMI HPD :"+plugged);
             mHdmiPlugged = plugged;
             updateRotation(true, true);
             Intent intent = new Intent(ACTION_HDMI_PLUGGED);
@@ -6085,6 +6095,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     void initializeHdmiStateInternal() {
         boolean plugged = false;
+        mExtEventObserver.startObserving("mdss_mdp/drm/card");
         // watch for HDMI plug messages if the hdmi switch exists
         if (new File("/sys/devices/virtual/switch/hdmi/state").exists()) {
             mHDMIObserver.startObserving("DEVPATH=/devices/virtual/switch/hdmi");
