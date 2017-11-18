9d03aa64634065b5d7b85de86cf80bd5c289e8cd
Evan Anderson <evananderson@aospa.co>
2017-11-17 21:45:51 -0600

    core: Build: hide date field
    * Followup commit to https://gerrit.aospa.co/c/4211/, which added the
    DATE field. This is file is part of the public API, and as such any
    additions must be annotated with the @hide designation.
    
    Change-Id: I5414b47481fe217af9b832706a70e22f93573034
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -56,7 +56,10 @@ public class Build {
     /** The name of the underlying board, like "goldfish". */
     public static final String BOARD = getString("ro.product.board");
 
-    /** The build date */
+    /**
+     * The build date
+     * @hide
+     */
     public static final String DATE = getString("ro.build.date");
 
     /**
