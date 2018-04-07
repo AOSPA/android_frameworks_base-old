a0132b67bd62fd58c954afa84abcd500f4fc076b
Carlo Savignano <carlosavignano@aospa.co>
2018-04-07 08:55:37 +0000

    input: support dynamic navigation bar toggling
    * Forward-ported to Oreo
    * Add system setting to dynamically switch navigation bar on/off.
    
    Change-Id: I55da79fe8da83cf839cc91c1257ea91d2d632a40
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: joshuous <joshuous@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6420,6 +6420,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean needsNavigationBar() {
+        return mPolicy.needsNavigationBar();
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
