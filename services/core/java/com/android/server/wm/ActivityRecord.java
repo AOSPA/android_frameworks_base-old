42b738d37bf943af35218aa0331e776df7c4a8dc
Jesse Chan <jc@lineageos.org>
2018-05-12 10:44:00 +0000

    Add an option to force pre-O apps to use full screen aspect ratio
    When an app target pre-O releases, the default max aspect ratio
    is 1.86:1 which leads to ugly black areas on devices that have
    screens with higher aspect ratio (for example Galaxy S8/S9).
    
    This change adds an option to allow users to change default
    aspect ratio for pre-O apps to 2.1 which would fit recent devices.
    
    Wight554:
    Simplify the code, we don't need the Settings for that, just bool
    
    @jhenrique09 : Port to android 10
    
    Change-Id: I88035fb079bad9a66b36fe5e861017af568b8f4c

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -186,6 +186,7 @@ import android.os.Bundle;
 import android.os.Debug;
 import android.os.IBinder;
 import android.os.Message;
+import android.content.res.Resources;
 import android.os.PersistableBundle;
 import android.os.Process;
 import android.os.RemoteException;
@@ -413,6 +414,9 @@ public final class ActivityRecord extends ConfigurationContainer {
     private boolean mShowWhenLocked;
     private boolean mInheritShownWhenLocked;
     private boolean mTurnScreenOn;
+    // Full screen aspect ratio
+    private final float mFullScreenAspectRatio = Resources.getSystem().getFloat(
+                    com.android.internal.R.dimen.config_screenAspectRatio);
 
     /**
      * Current sequencing integer of the configuration, for skipping old activity configurations.
@@ -3090,7 +3094,9 @@ public final class ActivityRecord extends ConfigurationContainer {
     // TODO(b/36505427): Consider moving this method and similar ones to ConfigurationContainer.
     private void computeBounds(Rect outBounds, Rect containingAppBounds) {
         outBounds.setEmpty();
-        final float maxAspectRatio = info.maxAspectRatio;
+        final boolean higherAspectRatio = Resources.getSystem().getBoolean(
+                com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+        final float maxAspectRatio = higherAspectRatio ? mFullScreenAspectRatio : info.maxAspectRatio;
         final ActivityStack stack = getActivityStack();
         final float minAspectRatio = info.minAspectRatio;
 
