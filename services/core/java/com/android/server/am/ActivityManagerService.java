075a9892e80d6d06b4e71bca785d1c7f1e317001
Aaditya Vohra <vohraa@codeaurora.org>
2018-06-28 20:26:44 +0530

    Revert "ContentProvider: avoid client waiting forever if server is killed".
    This reverts commit 33df5617aeb09f7257929e92492584b211aae5a3.
    
    Change-Id: Ibe5f6b5b6df37cc39a1253041c58b60b51a13365

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -12487,16 +12487,11 @@ public class ActivityManagerService extends IActivityManager.Stub
                 }
 
                 ComponentName comp = new ComponentName(cpi.packageName, cpi.name);
-                boolean inLaunching = false;
-                for (int i = 0; i < mLaunchingProviders.size(); i++) {
-                    if (mLaunchingProviders.get(i).name.equals(comp)
-                            && mLaunchingProviders.get(i).uid == cpi.applicationInfo.uid) {
-                        inLaunching = true;
-                        cpr = mLaunchingProviders.get(i);
-                        break;
-                    }
-                }
-                if (!inLaunching) {
+                checkTime(startTime, "getContentProviderImpl: before getProviderByClass");
+                cpr = mProviderMap.getProviderByClass(comp, userId);
+                checkTime(startTime, "getContentProviderImpl: after getProviderByClass");
+                final boolean firstClass = cpr == null;
+                if (firstClass) {
                     final long ident = Binder.clearCallingIdentity();
 
                     // If permissions need a review before any of the app components can run,
@@ -12614,7 +12609,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
                 // Make sure the provider is published (the same provider class
                 // may be published under multiple names).
-                if (!inLaunching) {
+                if (firstClass) {
                     mProviderMap.putProviderByClass(comp, cpr);
                 }
 
