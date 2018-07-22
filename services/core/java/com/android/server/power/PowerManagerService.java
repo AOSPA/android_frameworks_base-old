e762bb6a02c745c9799f5bb24966849f09caf46b
Alex Naidis <alex.naidis@paranoidandroid.co>
2018-07-22 19:03:33 -0400

    base: Don't send interaction hint for unnecessary events
    This fixes the occasional interaction hint spam.
    
    The following codes get classified as touch event:
    MOTION_EVENT_ACTION_DOWN
    MOTION_EVENT_ACTION_MOVE
    MOTION_EVENT_ACTION_UP
    MOTION_EVENT_ACTION_POINTER_DOWN
    MOTION_EVENT_ACTION_POINTER_UP
    MOTION_EVENT_ACTION_CANCEL
    MOTION_EVENT_ACTION_OUTSIDE
    
    For all these codes on the touchscreen device, the
    in-kernel cpu-boost driver already triggers a boost.
    
    Triggering one here as well adds latency to the direct
    touch processing path.
    
    Avoid that for touch events.
    
    The "other" events are mostly brightness changes and other
    events that happen often and don't need boosting.
    
    Change-Id: I78b3b6c8a889e9a3b22ebb475f02d5c30a71bdc7

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -2067,7 +2067,9 @@ public final class PowerManagerService extends SystemService
 
         Trace.traceBegin(Trace.TRACE_TAG_POWER, "userActivity");
         try {
-            if (eventTime > mLastInteractivePowerHintTime) {
+            if (eventTime > mLastInteractivePowerHintTime &&
+                    event != PowerManager.USER_ACTIVITY_EVENT_OTHER &&
+                    event != PowerManager.USER_ACTIVITY_EVENT_TOUCH) {
                 setPowerBoostInternal(Boost.INTERACTION, 0);
                 mLastInteractivePowerHintTime = eventTime;
             }
