2df34badb52cb1e300aecdecbda65f980e04de96
Cong Jiajia <jcong@codeaurora.org>
2018-07-30 15:04:51 +0800

    MediaRecorder: add more quality checks for time-lapse recording
    CRs-Fixed: 2226734
    
    Change-Id: I3110a23a983795c8ca466d5785e6b9ef6adee3cf

--- media/java/android/media/MediaRecorder.java
+++ media/java/android/media/MediaRecorder.java
@@ -623,8 +623,9 @@ public class MediaRecorder implements AudioRouting,
         setVideoSize(profile.videoFrameWidth, profile.videoFrameHeight);
         setVideoEncodingBitRate(profile.videoBitRate);
         setVideoEncoder(profile.videoCodec);
-        if (profile.quality >= CamcorderProfile.QUALITY_TIME_LAPSE_LOW &&
-             profile.quality <= CamcorderProfile.QUALITY_TIME_LAPSE_QVGA) {
+        if ((profile.quality >= CamcorderProfile.QUALITY_TIME_LAPSE_LOW &&
+             profile.quality <= CamcorderProfile.QUALITY_TIME_LAPSE_2K) ||
+             profile.quality <= CamcorderProfile.QUALITY_TIME_LAPSE_8KUHD) {
             // Nothing needs to be done. Call to setCaptureRate() enables
             // time lapse video recording.
         } else {
