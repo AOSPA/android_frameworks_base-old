2a7fb306f6fe97ab1ece66b3d6488b51f534430c
LuK1337 <priv.luk@gmail.com>
2018-12-09 18:39:35 +0530

    SystemUI: Allow overlaying max notification icons
    * I believe there are devices in this world that can fit
      more than 4 notification icons.
    
    Change-Id: Id0f3593c78a29765927c5001456ce582db3ce6b8
    Signed-off-by: Abhay Ruparel <abhayruparel2000@gmail.com>
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
@@ -128,9 +128,9 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
         }
     }.setDuration(CONTENT_FADE_DURATION);
 
-    private static final int MAX_VISIBLE_ICONS_WHEN_DARK = 5;
-    public static final int MAX_STATIC_ICONS = 4;
-    private static final int MAX_DOTS = 1;
+    public int MAX_VISIBLE_ICONS_WHEN_DARK = 5;
+    public int MAX_STATIC_ICONS = 4;
+    final int MAX_DOTS = 1;
 
     private boolean mIsStaticLayout = true;
     private final HashMap<View, IconState> mIconStates = new HashMap<>();
@@ -172,6 +172,8 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
         mDotPadding = getResources().getDimensionPixelSize(R.dimen.overflow_icon_dot_padding);
         mStaticDotRadius = getResources().getDimensionPixelSize(R.dimen.overflow_dot_radius);
         mStaticDotDiameter = 2 * mStaticDotRadius;
+        MAX_VISIBLE_ICONS_WHEN_DARK = getResources().getInteger(R.integer.config_maxVisibleNotificationIconsWhenDark);
+        MAX_STATIC_ICONS = getResources().getInteger(R.integer.config_maxVisibleNotificationIcons);
     }
 
     @Override
