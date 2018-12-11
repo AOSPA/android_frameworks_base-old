3d5ce724ba0f154a36c06fb9f8060795b3b39f55
ezio84 <brabus84@gmail.com>
2018-12-11 20:44:13 +0100

    Reload assets on overlay change: code improvements
    don't do some of the refresh stuff that now on P is already triggered
    by onThemeChanged()
    this also fixes some colors after a theme change
    
    also remove useless mEntryManager.updateNotifications() that was calling mPresenter.updateNotificationViews()
    (so just StatusBar.updateNotificationViews()).
    
    
    Change-Id: I906192487761405a554a97e3d97cd36102bf5189

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationEntryManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationEntryManager.java
@@ -927,6 +927,10 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
         updateNotifications();
     }
 
+    public void onOverlayChanged() {
+        updateNotificationsOnDensityOrFontScaleChanged();
+    }
+
     protected boolean shouldPeek(NotificationData.Entry entry) {
         return shouldPeek(entry, entry.notification);
     }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1210,35 +1210,11 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     @Override
     public void onOverlayChanged() {
-        reevaluateStyles();
-
-        // Clock and bottom icons
-        mNotificationPanel.onThemeChanged();
-        // The status bar on the keyguard is a special layout.
-        if (mKeyguardStatusBar != null) mKeyguardStatusBar.onThemeChanged();
-        // Recreate Indication controller because internal references changed
-        mKeyguardIndicationController =
-                SystemUIFactory.getInstance().createKeyguardIndicationController(mContext,
-                        mStatusBarWindow.findViewById(R.id.keyguard_indication_area),
-                        mNotificationPanel.getLockIcon());
-        mNotificationPanel.setKeyguardIndicationController(mKeyguardIndicationController);
-        mKeyguardIndicationController
-                .setStatusBarKeyguardViewManager(mStatusBarKeyguardViewManager);
-        mKeyguardIndicationController.setVisible(mState == StatusBarState.KEYGUARD);
-        mKeyguardIndicationController.setDozing(mDozing);
-        if (mStatusBarKeyguardViewManager != null) {
-            mStatusBarKeyguardViewManager.onThemeChanged();
-        }
-        if (mAmbientIndicationContainer instanceof AutoReinflateContainer) {
-            ((AutoReinflateContainer) mAmbientIndicationContainer).inflateLayout();
-        }
-
-        mEntryManager.updateNotificationsOnDensityOrFontScaleChanged();
         updateNotificationViews();
         mStackScroller.onOverlayChanged();
         mNotificationShelf.onOverlayChanged();
         Dependency.get(DarkIconDispatcher.class).onOverlayChanged(mContext);
-        mEntryManager.updateNotifications();
+        mEntryManager.onOverlayChanged();
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onOverlayChanged();
         }
