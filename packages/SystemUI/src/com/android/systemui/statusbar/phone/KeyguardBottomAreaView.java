c10ff2fb79be3153209b895e9a20f2978629e4d9
Yingren Wang <yingrenw@codeaurora.org>
2018-12-26 17:30:01 +0800

    SystemUI: Fix an issue where the EMERGENCY button clicks no response
    After screen is off and received message, the EMERGENCY button click no
    response. At this moment device is in doze state, the EMERGENCY button
    click only can start Emergency Call activity but doesn't unlock keyguard.
    So the EMERGENCY button click looks like no response.
    
    Hide EMERGENCY button in doze state.
    
    Change-Id: Id508a2098b87d1fdd4dc5abec6e7780703685c4e
    CRs-Fixed: 2372363

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -63,7 +63,7 @@ import android.widget.TextView;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.widget.LockPatternUtils;
-import com.android.keyguard.EmergencyButton;
+import com.android.keyguard.EmergencyCarrierArea;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.systemui.EventLogTags;
@@ -119,7 +119,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private static final int DOZE_ANIMATION_STAGGER_DELAY = 48;
     private static final int DOZE_ANIMATION_ELEMENT_DURATION = 250;
 
-    private EmergencyButton mEmergencyButton;
+    private EmergencyCarrierArea mEmergencyCarrierArea;
     private KeyguardAffordanceView mRightAffordanceView;
     private KeyguardAffordanceView mLeftAffordanceView;
     private LockIcon mLockIcon;
@@ -235,7 +235,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         super.onFinishInflate();
         mLockPatternUtils = new LockPatternUtils(mContext);
         mPreviewContainer = findViewById(R.id.preview_container);
-        mEmergencyButton = (EmergencyButton) findViewById(R.id.emergency_call_button);
+        mEmergencyCarrierArea = (EmergencyCarrierArea) findViewById(R.id.keyguard_selector_fade_container);
         mOverlayContainer = findViewById(R.id.overlay_container);
         mRightAffordanceView = findViewById(R.id.camera_button);
         mLeftAffordanceView = findViewById(R.id.left_button);
@@ -255,7 +255,6 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         mLockIcon.setScreenOn(updateMonitor.isScreenOn());
         mLockIcon.setDeviceInteractive(updateMonitor.isDeviceInteractive());
         mLockIcon.update();
-        updateEmergencyButton();
         setClipChildren(false);
         setClipToPadding(false);
         mPreviewInflater = new PreviewInflater(mContext, new LockPatternUtils(mContext));
@@ -353,7 +352,6 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         lp.height = getResources().getDimensionPixelSize(R.dimen.keyguard_affordance_height);
         mLeftAffordanceView.setLayoutParams(lp);
         updateLeftAffordanceIcon();
-        updateEmergencyButton();
     }
 
     private void updateRightAffordanceIcon() {
@@ -834,9 +832,11 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         if (dozing) {
             mLockIcon.setVisibility(INVISIBLE);
             mOverlayContainer.setVisibility(INVISIBLE);
+            mEmergencyCarrierArea.setVisibility(INVISIBLE);
         } else {
             mLockIcon.setVisibility(VISIBLE);
             mOverlayContainer.setVisibility(VISIBLE);
+            mEmergencyCarrierArea.setVisibility(VISIBLE);
             if (animate) {
                 startFinishDozeAnimation();
             }
@@ -916,12 +916,6 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
                     KeyguardUpdateMonitor.getCurrentUser());
             boolean secure = mLockPatternUtils.isSecure(KeyguardUpdateMonitor.getCurrentUser());
             return (secure && !canSkipBouncer) ? SECURE_CAMERA_INTENT : INSECURE_CAMERA_INTENT;
-        }
-    }
-
-    private void updateEmergencyButton() {
-        if (mEmergencyButton != null) {
-            mEmergencyButton.updateEmergencyCallButton();
         }
     }
 
