5500ce3440dbcb7894d8e90d00594109f3a88ce5
TheStrix <parthbhatia98@gmail.com>
2019-01-05 19:38:25 +0530

    base: Implement battery styles (1/2)
    Original commits:
     Author: TheStrix <parthbhatia98@gmail.com>
     Date: Sat Jan 5 19:27:53 2019 +0530
     PA: Battery circle & bolt
     BatteryMeterDrawable: Add ParanoidAndroid customizations
      - Modify to PA circle battery icon
      - Add charging bolt icon
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Fri Dec 14 19:10:33 2018 +0100
     Battery icon style: improve text/hidden switch [1/2]
        if the set style is text or hidden, don't allow
        percentage option (inside/outside icon) to break it
        also cleanup unused code
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Wed Nov 21 22:31:39 2018 +0100
     Battery icon: force show pct in QS header and when charging/powersave
        like aosp already wanted to do (QuickStatusBarHeader setForceShowPercent
        was being overridden by the tuner observer added by custom battery
        commits to BatteryMeterView).
    
        This is also good for users that don't want the percentage in the
        statusbar but still want to be able to check it in the QS panel.
    
        PS 3: Fix percentage not showing in qs/keyguard after screen off / on
        and force it only when hidden icon style or hidden percentage option
        are enabled
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Thu Nov 15 23:15:09 2018 +0100
     Battery icon: show outside percentage on charging
        for circle and portrait, the bolt overrides inside icon percentage
        same for power save
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Wed Feb 14 00:24:18 2018 +0100
     Text battery style: show a ⚡when charging
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Wed Nov 14 22:50:09 2018 +0100
     Battery icon: show pct on the right of the icon like stock
        and set the correct relative padding for the percentage
    
     Author: ezio84 <brabus84@gmail.com>
     Date: Wed Nov 21 21:31:02 2018 +0100
     Battery icon: circle styles fixes
      fix critical level exclamation mark position and size
      (to quickly test the issue just set mCriticalLevel to 100 in
      BatteryMeterDrawableBase)
    
      improve power saving "+" size so it doesn't look like a hospital symbol
    
     Author: Nicolas Dhouailly <ndhouailly@gmail.com>
     Date: Sun Nov 11 18:04:32 2018 +0100
     Battery styles : Fix icon portrait level
      * The icon portrait mode was always displaying full level. Lets fix it.
      * Clean up code a little bit.
    
     Author: Alex Cruz <alex@dirtyunicorns.com>
     Date: Wed Nov 7 12:28:39 2018 -0600
     Clean up battery styles
      - Remove code for landscape battery. Won't be used any time soon.
    
     Author: Simon Davie <nexx@nexxdesign.co.uk>
     Date: Mon Oct 24 02:12:18 2011 +0100
     BatteryMeterDrawable: Add hidden option
      * Remove the tuner call for icon blacklisting
        since it is no longer needed
    
     Author: Danesh M <daneshm90@gmail.com>
     Date: Thu Nov 27 14:17:19 2014 +0100
     BatteryMeterDrawable: Add text style
    
     Author: Danesh M <daneshm90@gmail.com>
     Date: Thu Nov 27 14:17:19 2014 +0100
     BatteryMeterDrawable: Add battery styles
      - Portrait
      - Landscape
      - Circle
      - Battery percent views (Inside/Ouside/Hidden)
    
    Change-Id: I7af333d4b8bede5643660a3e4888950e178b525f
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -18,6 +18,7 @@ package com.android.systemui.statusbar;
 
 import static android.app.StatusBarManager.DISABLE2_SYSTEM_ICONS;
 import static android.app.StatusBarManager.DISABLE_NONE;
+import static android.provider.Settings.Secure.STATUS_BAR_BATTERY_STYLE;
 
 import android.annotation.DrawableRes;
 import android.content.Context;
@@ -38,6 +39,7 @@ import android.view.accessibility.AccessibilityEvent;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.android.settingslib.graph.BatteryMeterDrawableBase;
 import com.android.settingslib.graph.SignalDrawable;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
@@ -72,6 +74,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
     private final NetworkController mNetworkController;
     private final SecurityController mSecurityController;
 
+    private boolean mBatteryVisible = false;
     private boolean mVpnVisible = false;
     private int mVpnIconId = 0;
     private int mLastVpnIconId = -1;
@@ -165,7 +168,11 @@ public class SignalClusterView extends LinearLayout implements NetworkController
 
     @Override
     public void onTuningChanged(String key, String newValue) {
-        if (!StatusBarIconController.ICON_BLACKLIST.equals(key)) {
+        if (STATUS_BAR_BATTERY_STYLE.equals(key)) {
+            final int style = newValue == null ?
+                BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT : Integer.parseInt(newValue);
+            mBatteryVisible = style != BatteryMeterDrawableBase.BATTERY_STYLE_HIDDEN;
+        } else if (!StatusBarIconController.ICON_BLACKLIST.equals(key)) {
             return;
         }
         ArraySet<String> blockList = StatusBarIconController.getIconBlacklist(newValue);
@@ -235,7 +242,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
         int endPadding = mMobileSignalGroup.getChildCount() > 0 ? mMobileSignalGroupEndPadding : 0;
         mMobileSignalGroup.setPaddingRelative(0, 0, endPadding, 0);
 
-        Dependency.get(TunerService.class).addTunable(this, StatusBarIconController.ICON_BLACKLIST);
+        Dependency.get(TunerService.class).addTunable(this, StatusBarIconController.ICON_BLACKLIST, STATUS_BAR_BATTERY_STYLE);
 
         apply();
         applyIconTint();
@@ -563,7 +570,8 @@ public class SignalClusterView extends LinearLayout implements NetworkController
 
         boolean anythingVisible = mWifiVisible || mIsAirplaneMode
                 || anyMobileVisible || mVpnVisible || mEthernetVisible;
-        setPaddingRelative(0, 0, anythingVisible ? mEndPadding : mEndPaddingNothingVisible, 0);
+        int endPadding = mBatteryVisible ? (anythingVisible ? mEndPadding : mEndPaddingNothingVisible) : 0;
+        setPaddingRelative(0, 0, endPadding, 0);
     }
 
     /**
