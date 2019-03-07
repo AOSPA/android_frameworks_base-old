66e2dd1b27e87524a87b1d8d5e25b290379d709c
HrX03 <dn.bianco03@gmail.com>
2019-03-07 17:50:40 +0000

    SystemUI: Redesign volume dialog
    Inspiration from MiUI, iOS and Dil3mm4
    
    Change-Id: I8bddda04c83c995bec82e9493017db69bd460b34
    Co-Authored-By: Andrew Fluck <andrew@aospa.co>
    Co-Authored-By: rituj <ritujbeniwal@gmail.com>
    Co-Authored-By: Jyotiraditya <jyotiraditya@aospa.co>
    Co-Authored-By: Akash Srivastava <akashsrivastava@aospa.co>
    Co-Authored-By: 00day0 <therandomuser11@gmail.com>
    Co-authored-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    base: Redo expanded volume panel for 10.x
    
    [ @HrX03 | @AgentFabulous - POSP ]
    - Google nuked expanded volume panel in pie. Redo the current
      implementation to bring this back from the past and dejank it.
    - Back to Android 8.x functionality!
    
    Change-Id: Ie4931a4ae09483ba737a74fc32ed0a1f6acf105d
    Co-authored-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Arian <arian.kulmer@web.de>
    
    SystemUI: VolumePanel: Version 1.1
    
    * Add support for unlinked notification volume
    * Fix progress bar inconsistencies when switching ringer modes
    * Some minor animation tweaks
    
    Change-Id: I31f87aacb7fbb445dd3b03ceb5064263e5af4836
    
    VolumeDialogImpl: fix ConcurrentModificationException
    
    SDB: it's happening to only a few users and let make those fc free
    
    msg: java.util.ConcurrentModificationException
    stacktrace: java.util.ConcurrentModificationException
    	at java.util.ArrayList$Itr.next(ArrayList.java:860)
    	at com.android.systemui.volume.VolumeDialogImpl.updateMediaOutputViewH(VolumeDialogImpl.java:905)
    	at com.android.systemui.volume.VolumeDialogImpl.access$4800(VolumeDialogImpl.java:120)
    	at com.android.systemui.volume.VolumeDialogImpl$H.handleMessage(VolumeDialogImpl.java:1912)
    	at android.os.Handler.dispatchMessage(Handler.java:106)
    	at android.os.Looper.loop(Looper.java:223)
    	at android.app.ActivityThread.main(ActivityThread.java:7657)
    	at java.lang.reflect.Method.invoke(Native Method)
    	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:594)
    	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:947)
    
    VolumePluginManager: Fix fc for stock panel
    
    Change-Id: Ib0f6340ac7fea08abc637a1c3391a58cc4f1dfb6
    
    VolumeDialogImpl: Reschedule the timeout on clicks on captions
    
    Change-Id: Ie98813a91396f3c3a84aeac8a609071c5cc315f6
    
    VolumePanel: Fix glitchy ringer and notif bar when switching modes
    
    Co-Authored-By: Andrew Fluck <andrew@aospa.co>
    Co-Authored-By: rituj <ritujbeniwal@gmail.com>
    Co-Authored-By: Jyotiraditya <jyotiraditya@aospa.co>
    Co-Authored-By: Akash Srivastava <akashsrivastava@aospa.co>
    Co-Authored-By: 00day0 <therandomuser11@gmail.com>
    Co-authored-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: Arian <arian.kulmer@web.de>
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: althafvly <althafvly@gmail.com>
    Change-Id: I585c7366cb99c0c3baf699a5a553e77da895d195

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
@@ -550,6 +550,7 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
         updateRingerModeExternalW(mRingerModeObservers.mRingerMode.getValue());
         updateZenModeW();
         updateZenConfig();
+        updateLinkNotificationConfigW();
         updateEffectsSuppressorW(mNoMan.getEffectsSuppressor());
         mCallbacks.onStateChanged(mState);
     }
