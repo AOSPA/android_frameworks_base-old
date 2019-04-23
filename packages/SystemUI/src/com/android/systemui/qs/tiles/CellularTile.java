538f9cc450aabd98bc3d522c0b3fc0ac57f11299
eray orçunus <erayorcunus@gmail.com>
2019-04-23 16:46:35 +0300

    SystemUI: Enable and fix QS detail view, adapt layout to 10
    * Re-enabling dual-target(detail view) was easy as pie, but layout was from Oreo
      and DND tile was broken.
    
    * I've applied QS Customize layout to details view to match it with Pie look.
    
    * DND showDetail function was overriden to show new EnableZenModeDialog.
      New dialog's implementation were taken from old detail view, so I've disabled
      the new one, added the only new thing (priority-but-alarm-disabled warning text)
      to old one and enabled it.
    
    * DND tile will still respect "Ask duration every time" setting and ask duration to
      the user, but this time with detail view.
    
    * Squashes in the follow commits:
    
    Author: Chengzhi.Hou <hcz017@gmail.com>
    Date:   Fri May 11 12:26:56 2018 +0800
    
        SystemUI: Enable dualTarget on CellularTile
    
        * To show DataUsageDetailView
    
        Change-Id: If09b4ac51bb6df01b8ea9f59bccf123de65da8cf
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Jun 23 13:18:27 2018 +0200
    
        SystemUI: CellularTile: Don't call showDetail() when device is locked
    
        * We recently enabled dualTarget for CellularTile which allowed us
          to toggle mobile data without unlocking device. Moving showDetail
          call to postQSRunnableDismissingKeyguard when the device is locked
          fixes this issue.
    
        Change-Id: I16dc766166a33a309a2fc945e81fa15b5d194196
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Wed Jun 27 18:28:22 2018 +0200
    
        SystemUI: Fix Data Usage tile to match Settings
    
        Change-Id: Ieff3504baf3d13c77d06fcde87d5ac56d2586194
    
    Author: eray orçunus <erayorcunus@gmail.com>
    Date:   Fri Apr 26 00:15:30 2019 +0300
    
        SystemUI: Fix several layout bugs
    
        * QSDetail and QSCustomize panels' clippers starts after the black
          header, but the ripple animation coords were wrong.
    
        * QSDetail and QSCustomize use a transparent view to let QS header
          be seen. Header's height changes with orientation, but QSDetail
          and QSCustomize do nothing about it.
          As a result, there were gaps or blocked views.
    
        Change-Id: I5cefad14b519be99adcf5c913e96385d45b8fb17
        Signed-off-by: eray orçunus <erayorcunus@gmail.com>
    
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Wed Nov 6 15:01:32 2019 -0600
    
        SystemUI: Convert tiles to use UnlockMethodCache#canSkipBouncer
    
         * This updates the API following commit aaba8ef2.
    
        Change-Id: I4932a3e023a40e362e81fd74f2921e9cb472ebf3
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Fri Aug 23 15:03:32 2019 +0200
    
        SystemUI: Use matching data usage size formatting between QS and Settings
    
        * Fixes : https://gitlab.com/LineageOS/issues/android/issues/1102
    
        Change-Id: Ie80e19de758f599b1fdedb6d991cbdd86e4f2a47
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun Feb 2 14:23:29 2020 +0100
    
        SystemUI: Pass correct NetworkTemplate to CellularTile detail view
    
        Change-Id: Ib41a4086e10ccba71909370549d3714815684b2f
    
    Author: Arian <arian.kulmer@web.de>
    Date:   Tue Mar 10 19:04:17 2020 +0100
    
        CellularTile: Don't block detail view on locked screen
    
        Change-Id: Iac83d348602ca742188f4bd3420dbaaf931f1ac9
    
    Change-Id: I5ecb419fbf699be33494d0a9cc215914d90004bd

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -38,6 +38,7 @@ import android.widget.Switch;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settingslib.net.DataUsageController;
+import com.android.settingslib.net.DataUsageUtils;
 import com.android.systemui.Prefs;
 import com.android.systemui.R;
 import com.android.systemui.plugins.ActivityStarter;
@@ -144,6 +145,9 @@ public class CellularTile extends QSTileImpl<SignalState> {
 
     @Override
     protected void handleSecondaryClick() {
+        if (getState().state == Tile.STATE_UNAVAILABLE) {
+            return;
+        }
         if (mDataController.isMobileDataSupported()) {
             showDetail(true);
         } else {
@@ -165,6 +169,7 @@ public class CellularTile extends QSTileImpl<SignalState> {
         }
 
         final Resources r = mContext.getResources();
+        state.dualTarget = true;
         state.label = r.getString(R.string.mobile_data);
         boolean mobileDataEnabled = mDataController.isMobileDataSupported()
                 && mDataController.isMobileDataEnabled();
@@ -346,7 +351,9 @@ public class CellularTile extends QSTileImpl<SignalState> {
             final DataUsageDetailView v = (DataUsageDetailView) (convertView != null
                     ? convertView
                     : LayoutInflater.from(mContext).inflate(R.layout.data_usage, parent, false));
-            final DataUsageController.DataUsageInfo info = mDataController.getDataUsageInfo();
+            final DataUsageController.DataUsageInfo info = mDataController.getDataUsageInfo(
+                    DataUsageUtils.getMobileTemplate(mContext,
+                            SubscriptionManager.getDefaultDataSubscriptionId()));
             if (info == null) return v;
             v.bind(info);
             v.findViewById(R.id.roaming_text).setVisibility(mSignalCallback.mInfo.roaming
