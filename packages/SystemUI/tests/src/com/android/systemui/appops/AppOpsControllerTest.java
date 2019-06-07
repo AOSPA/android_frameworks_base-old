557c86f60edf77448578abdd4bf368b8d2276c43
Fabian Kozynski <kozynski@google.com>
2019-06-07 13:07:27 -0400

    Add cache to AppOpsController
    Reduces the Binder calls from AppOpsControllerImpl to
    PackageManager#getPermissionFlags by caching calls for 10s (moltmann@
    recommended this expiration time).
    
    Also, modify AppOpsControllerImpl#notifySuscribers to use the bg Thread.
    
    Currently calls to getPermissionFlags only happen in two functions:
    * AppOpsControllerImpl#notifySuscribers
    * AppOpsControllerImpl#getActiveAppOpsForUser
    
    First one in only called from bg thread and second one is called from a
    bg thread from PrivacyItemController.
    
    In a future CL, this calling will be enforced at the entry point
    (getActiveAppOpsForUser)
    
    Test: atest PermissionFlagsCacheTest AppOpsControllerTest
    Bug: 134687592
    
    [PA Note: This is a partial revert of
    "DO NOT MERGE Revert Privacy Indicators to P"]
    
    Change-Id: I6fa9e2d865bc295f6325935e2df574b8b758214a

--- packages/SystemUI/tests/src/com/android/systemui/appops/AppOpsControllerTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/appops/AppOpsControllerTest.java
@@ -67,6 +67,8 @@ public class AppOpsControllerTest extends SysuiTestCase {
     private AppOpsController.Callback mCallback;
     @Mock
     private AppOpsControllerImpl.H mMockHandler;
+    @Mock
+    private PermissionFlagsCache mFlagsCache;
 
     private AppOpsControllerImpl mController;
     private TestableLooper mTestableLooper;
@@ -81,16 +83,16 @@ public class AppOpsControllerTest extends SysuiTestCase {
         // All permissions of TEST_UID and TEST_UID_OTHER are user sensitive. None of
         // TEST_UID_NON_USER_SENSITIVE are user sensitive.
         getContext().setMockPackageManager(mPackageManager);
-        when(mPackageManager.getPermissionFlags(anyString(), anyString(),
+        when(mFlagsCache.getPermissionFlags(anyString(), anyString(),
                 eq(UserHandle.getUserHandleForUid(TEST_UID)))).thenReturn(
                 PackageManager.FLAG_PERMISSION_USER_SENSITIVE_WHEN_GRANTED);
-        when(mPackageManager.getPermissionFlags(anyString(), anyString(),
+        when(mFlagsCache.getPermissionFlags(anyString(), anyString(),
                 eq(UserHandle.getUserHandleForUid(TEST_UID_OTHER)))).thenReturn(
                 PackageManager.FLAG_PERMISSION_USER_SENSITIVE_WHEN_GRANTED);
-        when(mPackageManager.getPermissionFlags(anyString(), anyString(),
+        when(mFlagsCache.getPermissionFlags(anyString(), anyString(),
                 eq(UserHandle.getUserHandleForUid(TEST_UID_NON_USER_SENSITIVE)))).thenReturn(0);
 
-        mController = new AppOpsControllerImpl(mContext, mTestableLooper.getLooper());
+        mController = new AppOpsControllerImpl(mContext, mTestableLooper.getLooper(), mFlagsCache);
     }
 
     @Test
