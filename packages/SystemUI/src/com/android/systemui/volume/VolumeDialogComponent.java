3a954b3b3b1cdcc20d5de410e5418aa9450308bf
Chris Crump <chris@cypheros.co>
2019-06-08 07:53:10 -0400

    SystemUI: Implement alert slider UI
    Ported from OxygenOS and reworked for our alert slider
    implementation. We target AudioManager instead of Zen,
    icons are also the same as aosp and the dialog uses the
    material theme as well as support for our themes.
    
    To use, the alert slider config must be enabled. By default,
    the dialog shows on the left side. To move it to the right
    side, set the location config to 1.
    
    Change-Id: Ie1954a44cc5242c95a731abd7404379ea638fe70

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogComponent.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogComponent.java
@@ -30,6 +30,8 @@ import android.view.WindowManager.LayoutParams;
 import com.android.settingslib.applications.InterestingConfigChanges;
 import com.android.systemui.Dependency;
 import com.android.systemui.SystemUI;
+import com.android.systemui.aospa.TriStateUiController;
+import com.android.systemui.aospa.TriStateUiControllerImpl;
 import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.PluginDependencyProvider;
@@ -46,7 +48,7 @@ import java.io.PrintWriter;
  * Implementation of VolumeComponent backed by the new volume dialog.
  */
 public class VolumeDialogComponent implements VolumeComponent, TunerService.Tunable,
-        VolumeDialogControllerImpl.UserActivityListener{
+        VolumeDialogControllerImpl.UserActivityListener, TriStateUiController.UserActivityListener {
 
     public static final String VOLUME_DOWN_SILENT = "sysui_volume_down_silent";
     public static final String VOLUME_UP_SILENT = "sysui_volume_up_silent";
@@ -59,6 +61,7 @@ public class VolumeDialogComponent implements VolumeComponent, TunerService.Tuna
     private final SystemUI mSysui;
     private final Context mContext;
     private final VolumeDialogControllerImpl mController;
+    private TriStateUiControllerImpl mTriStateController;
     private final InterestingConfigChanges mConfigChanges = new InterestingConfigChanges(
             ActivityInfo.CONFIG_FONT_SCALE | ActivityInfo.CONFIG_LOCALE
             | ActivityInfo.CONFIG_ASSETS_PATHS);
@@ -75,6 +78,8 @@ public class VolumeDialogComponent implements VolumeComponent, TunerService.Tuna
         mContext = context;
         mController = (VolumeDialogControllerImpl) Dependency.get(VolumeDialogController.class);
         mController.setUserActivityListener(this);
+        boolean hasAlertSlider = mContext.getResources().
+                getBoolean(com.android.internal.R.bool.config_hasAlertSlider);
         // Allow plugins to reference the VolumeDialogController.
         Dependency.get(PluginDependencyProvider.class)
                 .allowPluginDependency(VolumeDialogController.class);
@@ -88,6 +93,13 @@ public class VolumeDialogComponent implements VolumeComponent, TunerService.Tuna
                     }
                     mDialog = dialog;
                     mDialog.init(LayoutParams.TYPE_VOLUME_OVERLAY, mVolumeDialogCallback);
+                    if (hasAlertSlider) {
+                        if (mTriStateController != null) {
+                            mTriStateController.destroy();
+                        }
+                        mTriStateController = new TriStateUiControllerImpl(mContext);
+                        mTriStateController.init(LayoutParams.TYPE_VOLUME_OVERLAY, this);
+                    }
                 }).build();
         applyConfiguration();
         Dependency.get(TunerService.class).addTunable(this, VOLUME_DOWN_SILENT, VOLUME_UP_SILENT,
@@ -186,6 +198,11 @@ public class VolumeDialogComponent implements VolumeComponent, TunerService.Tuna
     private void startSettings(Intent intent) {
         Dependency.get(ActivityStarter.class).startActivity(intent,
                 true /* onlyProvisioned */, true /* dismissShade */);
+    }
+
+    @Override
+    public void onTriStateUserActivity() {
+        onUserActivity();
     }
 
     private final VolumeDialogImpl.Callback mVolumeDialogCallback = new VolumeDialogImpl.Callback() {
