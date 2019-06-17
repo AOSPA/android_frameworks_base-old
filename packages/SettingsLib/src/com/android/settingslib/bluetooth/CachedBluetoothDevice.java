46d9ad7e72c7c48ae44113469bb866ec5a05c923
davidln <davidln@google.com>
2019-06-17 14:48:11 -0700

    Concurrent collections for Bluetooth callbacks.
    This allows callback classes to remove themselves or add additional
    callbacks in response to state change dispatches.
    
    Bug: 129060225
    Bug: 144357642
    
    Test: build and deploy, pair multiple devices, switch users
    
    Change-Id: I8eed81bbc9c12321ec41b2491d006764e2e483d6
    Merged-In: I8eed81bbc9c12321ec41b2491d006764e2e483d6

--- packages/SettingsLib/src/com/android/settingslib/bluetooth/CachedBluetoothDevice.java
+++ packages/SettingsLib/src/com/android/settingslib/bluetooth/CachedBluetoothDevice.java
@@ -40,6 +40,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * CachedBluetoothDevice represents a remote Bluetooth device. It contains
@@ -77,7 +78,7 @@ public class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> 
 
     boolean mJustDiscovered;
 
-    private final Collection<Callback> mCallbacks = new ArrayList<>();
+    private final Collection<Callback> mCallbacks = new CopyOnWriteArrayList<>();
 
     public int mTwspBatteryState;
     public int mTwspBatteryLevel;
@@ -730,22 +731,16 @@ public class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> 
     }
 
     public void registerCallback(Callback callback) {
-        synchronized (mCallbacks) {
-            mCallbacks.add(callback);
-        }
+        mCallbacks.add(callback);
     }
 
     public void unregisterCallback(Callback callback) {
-        synchronized (mCallbacks) {
-            mCallbacks.remove(callback);
-        }
+        mCallbacks.remove(callback);
     }
 
     void dispatchAttributesChanged() {
-        synchronized (mCallbacks) {
-            for (Callback callback : mCallbacks) {
-                callback.onDeviceAttributesChanged();
-            }
+        for (Callback callback : mCallbacks) {
+            callback.onDeviceAttributesChanged();
         }
     }
 
