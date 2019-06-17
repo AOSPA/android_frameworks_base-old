b99320dd465cdeab9edb8f58b8bc585eefb68b96
Julia Reynolds <juliacr@google.com>
2019-06-17 15:34:26 -0400

    Allow the down volume key to silence notifs
    Additionally preserve existing behavior by passing
    the keypress on.
    
    Fixes: 130025623
    Test: manual
    Change-Id: I00f83275000369c1ab03bae63eb0a4b6170c4b53
    (cherry picked from commit deb2d0b743e14871b964b30ad791ac5ff0cde016)

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2418,6 +2418,13 @@ public class NotificationManagerService extends SystemService {
         }
 
         @Override
+        public void silenceNotificationSound() {
+            checkCallerIsSystem();
+
+            mNotificationDelegate.clearEffects();
+        }
+
+        @Override
         public void setNotificationsEnabledForPackage(String pkg, int uid, boolean enabled) {
             enforceSystemOrSystemUI("setNotificationsEnabledForPackage");
 
