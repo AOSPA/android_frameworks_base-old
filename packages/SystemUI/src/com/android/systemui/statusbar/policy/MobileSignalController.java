182bd001f12a2bb42c398640e83973f5ef808343
Mohd Faraz <mohd.faraz.abc@gmail.com>
2019-07-07 17:57:32 +0530

    SystemUI: Added New icons from IMS This commit is the squash of the following commits:
    	SystemUI: Added New icons from IMS [1/3]
    
    	SystemUI: Added New icons from IMS [2/3]
    
    	SystemUI: Added New icons from IMS [3/3]
    
    Change-Id: I3061dd801bea3eb24c7f7ca1caae4cab76f7de48
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -386,10 +386,11 @@ public class MobileSignalController extends SignalController<
         int voiceNetTye = getVoiceNetworkType();
         if ( (mCurrentState.voiceCapable || mCurrentState.videoCapable)
                 &&  mCurrentState.imsRegistered ) {
-            resId = R.drawable.ic_volte;
-        }else if ( (mDataNetType == TelephonyManager.NETWORK_TYPE_LTE
-                        || mDataNetType == TelephonyManager.NETWORK_TYPE_LTE_CA)
-                    && voiceNetTye  == TelephonyManager.NETWORK_TYPE_UNKNOWN) {
+            resId = R.drawable.ims_state;
+        }else if ( mDataNetType == TelephonyManager.NETWORK_TYPE_LTE
+                    || mDataNetType == TelephonyManager.NETWORK_TYPE_LTE_CA
+                    || voiceNetTye  == TelephonyManager.NETWORK_TYPE_LTE
+                    || voiceNetTye  == TelephonyManager.NETWORK_TYPE_LTE_CA) {
             resId = R.drawable.ic_volte_no_voice;
         }
         return resId;
