c7e76fbcf4e9733321d588c210134a515f72a646
Lyn Han <lynhan@google.com>
2019-07-09 15:27:28 -0700

    Secure => global bubble settings pt 1
    Follow up to http://ag/8222583, which enables bubbles globally instead
    of at the per-user level
    - Remove user-specific tracking
    - Remove Secure.NOTIFICATION_BUBBLES from backup
    - Replace references to Secure.NOTIFICATION_BUBBLES with
    Global.NOTIFICATION_BUBBLES
    - Update tests
    
    Fixes: 136034310
    Bug: 129158983
    Test: m RunSettingsRoboTests
    Test: atest SystemUITests
    Test: manual - work profile bubbles show up
    Change-Id: I931f8f2640ac01fdf4c30ef9f9474fde7ca2d262

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9171,7 +9171,6 @@ public final class Settings {
             ASSIST_GESTURE_WAKE_ENABLED,
             VR_DISPLAY_MODE,
             NOTIFICATION_BADGING,
-            NOTIFICATION_BUBBLES,                               // moved to global
             NOTIFICATION_DISMISS_RTL,
             QS_AUTO_ADDED_TILES,
             SCREENSAVER_ENABLED,
