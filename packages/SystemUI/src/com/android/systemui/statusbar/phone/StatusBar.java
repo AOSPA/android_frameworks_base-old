e2a9785771683467e1a522affbf5768473823135
Lucas Dupin <dupin@google.com>
2019-07-22 16:47:34 -0700

    Isolate DPM and SIM PIN states from UX
    User should still be able to swipe up to retry or still see
    the bypass lock screen when face auth is disabled or sim
    card is locked. Having different UX on those cases is
    confusing.
    
    Bug: 138125943
    Change-Id: Iece4d28e7a988af846c9e8aa9eb0fff5a1d74249

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2411,6 +2411,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             mLightBarController.dump(fd, pw, args);
         }
 
+        if (mUnlockMethodCache != null) {
+            mUnlockMethodCache.dump(pw);
+        }
+
         if (mKeyguardBypassController != null) {
             mKeyguardBypassController.dump(pw);
         }
