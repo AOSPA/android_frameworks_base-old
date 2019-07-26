ed87c925eb9db45f083b4cb502edb0c28c698a8d
Kevin Chyn <kchyn@google.com>
2019-07-26 12:41:33 -0700

    Do not use canAuthenticate() to check if user can reset lockout
    canAuthenticate() is meant for apps so it will reflect the
    FACE_UNLOCK_APP_ENABLED setting. If disabled, resetLockout will not be
    run for that user.
    
    Fixes: 138269776
    
    Test: Disable unlocking for apps, get locked out, enter password.
          Lockout is reset now.
    
    Change-Id: I5d7bf3abbac7c8982595d5733464a887ab5184ab

--- services/core/java/com/android/server/locksettings/LockSettingsService.java
+++ services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -424,7 +424,7 @@ public class LockSettingsService extends ILockSettings.Stub {
 
         public boolean hasEnrolledBiometrics(int userId) {
             BiometricManager bm = mContext.getSystemService(BiometricManager.class);
-            return bm.canAuthenticate(userId) == BiometricManager.BIOMETRIC_SUCCESS;
+            return bm.hasEnrolledBiometrics(userId);
         }
 
         public int binderGetCallingUid() {
