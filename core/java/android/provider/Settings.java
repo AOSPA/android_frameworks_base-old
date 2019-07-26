4410358f441f3fbb49213c7820be501052a7b4dd
ezio84 <brabus84@gmail.com>
2019-07-26 12:45:22 +0300

    SystemUI: Less boring heads up option [1/2]
    Q: don't skip the new Bubbles (the circular popping up notifications
    like the Dialer one when a call is active and you press the home button)
    Reference: https://github.com/ezio84/abc_frameworks_base/commit/c3d6f7d386286a4e55a3e0644782827705db7168#diff-c38ee79f64d8dc57e7757863d741be46
    
    Allow to show heads up only for dialer and messaging
    app when they are not in foreground
    
    neobuddy89:
    * Added clock app too in whitelist.
    * Adapted and squashed for A12.
    * Slightly optimized apps check
    
    Stallix:
    * Adapted logging for A13.
    
    ghostrider-reborn:
    * Cleanup and rewrite logic
    * Consider notification category and importance
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Iec0a628359a219d6525f82a3d6b35212e70101d8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6009,6 +6009,12 @@ public final class Settings {
         public static final String POCKET_JUDGE = "pocket_judge";
 
         /**
+         * Whether to show heads up only for dialer and sms apps
+         * @hide
+         */
+        public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
