f4b05908e5a68fad1915fd9eb093b68cd40438ae
Felipe Leme <felipeal@google.com>
2019-08-05 16:00:12 -0700

    Fixes usage of WallpaperManager to make it optional.
    As the main javadoc states:
    "An app can check whether wallpapers are supported for the current user,
    by calling {@link #isWallpaperSupported()}, and whether setting of
    wallpapers is allowed, by calling {@link #isSetWallpaperAllowed()}."
    
    Test: manual verification on automotive
    Test: atest CtsAppTestCases:android.app.cts.WallpaperManagerTest # on automotive
    
    Bug: 138939803
    Bug: 132111956
    
    Change-Id: I88946f92ff54e556d289c41c28e22da3a4b8c8b8
    Merged-In: I88946f92ff54e556d289c41c28e22da3a4b8c8b8
    (cherry picked from commit 34a861add16eed6ef2f487b3d40017d5ad70c2a6)

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1509,6 +1509,8 @@ public final class SystemServer {
                 traceBeginAndSlog("StartWallpaperManagerService");
                 mSystemServiceManager.startService(WALLPAPER_SERVICE_CLASS);
                 traceEnd();
+            } else {
+                Slog.i(TAG, "Wallpaper service disabled by config");
             }
 
             traceBeginAndSlog("StartAudioService");
