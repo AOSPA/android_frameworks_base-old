0e22d71be1e545d9b3cc8e725aba98d8549d36c7
Selim Cinek <cinek@google.com>
2019-08-19 11:57:11 -0700

    DO NOT MERGE: Fixed an issue where the notification icons wouldn't show while docked
    Since we're using the pulsing state for docking, the icons would now
    become clipped while pulsing.
    
    Fixes: 139096431
    Test: dock, observe notification icons showing
    Change-Id: If251e6b18c03b2824b4d3ea4dab82d4a403565f1
    Merged-In: I8f7bd7a6a0562942ed3e12f28705043722d177e8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -3172,6 +3172,10 @@ public class NotificationPanelView extends PanelView implements
         mAnimateNextPositionUpdate = true;
     }
 
+    public void setPulseReason(int reason) {
+        mNotificationStackScroller.setPulseReason(reason);
+    }
+
     /**
      * Panel and QS expansion callbacks.
      */
