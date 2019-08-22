fca49e61d7f088dd82d1d5b2fceb89dc010cb871
Joshua Tsuji <tsuji@google.com>
2019-08-22 14:57:50 -0400

    Protect against NaN in flyout collapses.
    This can happen if getWidth() returns 0, likely the result of a race condition. Since
    setFlyoutCollapsed is called continuously as a flyout is dragged, we can just wait
    until getWidth() > 0.
    
    Bug: 139816141
    Test: atest SystemUITests
    Change-Id: I2e7b8521a2930564f71919f4f4708c791a9b3545

--- packages/SystemUI/src/com/android/systemui/bubbles/BubbleStackView.java
+++ packages/SystemUI/src/com/android/systemui/bubbles/BubbleStackView.java
@@ -1085,6 +1085,12 @@ public class BubbleStackView extends FrameLayout {
     }
 
     void onFlyoutDragged(float deltaX) {
+        // This shouldn't happen, but if it does, just wait until the flyout lays out. This method
+        // is continually called.
+        if (mFlyout.getWidth() <= 0) {
+            return;
+        }
+
         final boolean onLeft = mStackAnimationController.isStackOnLeftSide();
         mFlyoutDragDeltaX = deltaX;
 
@@ -1103,7 +1109,6 @@ public class BubbleStackView extends FrameLayout {
             // after it has already become the dot.
             final boolean overscrollingLeft =
                     (onLeft && collapsePercent > 1f) || (!onLeft && collapsePercent < 0f);
-
             overscrollTranslation =
                     (overscrollingPastDot ? collapsePercent - 1f : collapsePercent * -1)
                             * (overscrollingLeft ? -1 : 1)
