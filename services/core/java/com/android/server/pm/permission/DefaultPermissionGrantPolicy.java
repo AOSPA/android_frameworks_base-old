5eb849c4ee4c763fec303033f954b45e57301f5a
Akash <akashniki@gmail.com>
2019-08-28 17:03:45 +0000

    Grant storage permission to Google's Markup
    This fixes ForceClose of Google Markup when using it along with Longshot.
    
    Change-Id: Ic82a369c85220b1dde8038b42a027528025fbf63

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -744,6 +744,9 @@ public final class DefaultPermissionGrantPolicy {
             grantPermissionsToSystemPackage(systemCaptionsServicePackageName, userId,
                     MICROPHONE_PERMISSIONS);
         }
+
+        // Google Markup
+        grantSystemFixedPermissionsToSystemPackage("com.google.android.markup", userId, STORAGE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(String category, int userId) {
