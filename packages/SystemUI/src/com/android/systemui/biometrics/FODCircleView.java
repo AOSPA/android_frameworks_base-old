553af37548515b0afd65a1503fceef437741d350
TheScarastic <warabhishek@gmail.com>
2019-09-10 11:07:39 +0530

    FODCircleView: Add Support for custom FP pressed icon
    Change-Id: If357b8eb0da17b1ea97a2ab5ca880d465c33adc9

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -302,6 +302,7 @@ public class FODCircleView extends ImageView {
         dispatchPress();
 
         setImageDrawable(null);
+        mPressedView.setImageResource(R.drawable.fod_icon_pressed);
         invalidate();
     }
 
