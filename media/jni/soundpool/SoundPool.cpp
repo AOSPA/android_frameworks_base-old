63961fb9b47dededfebc940be3a5f88724fe4737
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2019-09-27 02:16:37 +0000

    Sounpool: bring back chrono and thread.
    As we used AOSP solution for this, they need both of the includes.
    
    Change-Id: I90133f154f26c73b4b79e27f16efc793e1d295da
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>

--- media/jni/soundpool/SoundPool.cpp
+++ media/jni/soundpool/SoundPool.cpp
@@ -17,12 +17,13 @@
 //#define LOG_NDEBUG 0
 #define LOG_TAG "SoundPool"
 
+#include <chrono>
 #include <inttypes.h>
+#include <thread>
 
 #include <utils/Log.h>
 
 #define USE_SHARED_MEM_BUFFER
-
 #include <media/AudioTrack.h>
 #include "SoundPool.h"
 #include "SoundPoolThread.h"
