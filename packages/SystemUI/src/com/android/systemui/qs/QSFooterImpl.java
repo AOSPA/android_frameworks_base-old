899cd9ea55319fca486c93ec604ba11d16181b32
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2019-09-28 14:52:55 +0200

    SystemUI: Don't show build version in QS footer
    Change-Id: I012b70d5b1ad247f7ee34b545c043a6d4d556475

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -165,15 +165,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     private void setBuildText() {
         TextView v = findViewById(R.id.build);
         if (v == null) return;
-        if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
-            v.setText(mContext.getString(
-                    com.android.internal.R.string.bugreport_status,
-                    Build.VERSION.RELEASE,
-                    Build.ID));
-            v.setVisibility(View.VISIBLE);
-        } else {
-            v.setVisibility(View.GONE);
-        }
+        v.setVisibility(View.GONE);
     }
 
     private void updateAnimator(int width) {
