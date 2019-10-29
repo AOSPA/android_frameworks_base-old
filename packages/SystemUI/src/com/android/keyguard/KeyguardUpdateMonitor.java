c4cfedd845ecf1714d308a5519389c7a1ec3d5fa
Carlo Savignano <carlosavignano@aospa.co>
2019-10-29 16:43:22 +0100

    [SQUASH]: Add Pocket Mode
    pocket: introduce pocket judge
    
    ZeNiXxX
    * Bring to Android 10
    
    DennySPb: adapt to 11, 12
    
    Change-Id: I33153b451f5d38d83e226be0f76bda1ed022dd48
    
    policy: introduce pocket lock
    
    * Block touch screen and keys inputs when device is in pocket.
    * Safe-door to disable pocket lock within long press power button
      if sensors do not behave correctly after PocketManager.isDeviceInPocket()
      returns true.
    * Window uses TYPE_SYSTEM_ERROR flag with ensure high
      priority over the lockscreen and other high priority windows.
    * Do not illuminate buttons when pocket lock is showing.
    * Disable fingerprint events and gestures when in pocket
    * Be friendly towards Keypress Boost
    
    Contributors:
    Carlo Savignano <carlosavignano@aospa.co>
    Chris Lahaye <dev@chrislahaye.com>
    Anas Karbila <anaskarbila@aospa.co>
    Alex Naidis <alex.naidis@linux.com>
    Park Ju Hyung <qkrwngud825@gmail.com>
    
    Ticket: NOUGAT-15
    
    ZeNiXxX
    * Fix  booting issues
    * Fix  NPEs
    * Bring to Android 10
    
    DennySPb: adapt for 11, 12
    
    Change-Id: I34108fa38f28f15b0b99c8ebb6551dcc4dabad14
    
    pocket: Add hardware acceleration and properly maintain SYSTEM_UI flags
    
    Change-Id: I585598242beec34bd92c5df5836ab395b36cea05
    
    PocketService: Adjust light sensor rate to 400ms
    
    We already adjusted the proximity sensor rate to
    400ms, so adjust the rate for the light sensor
    accordingly and create a tunable.
    
    Change-Id: Ie6dda80df8cdc677eb6cdb873a5ce1e888572fe7
    
    pocket: introduce pocket bridge
    
    This service communicates pocket state to the pocket judge kernel driver.
    It maintains the pocket state by binding to the pocket service.
    
    Ticket: NOUGAT-49
    
    Change-Id: Iee77f767f23d3f77a0d1d871f9305f3b3c6d6630
    
    pocket: Adjust sleep timeout for pocket lock view to 10 secs
    
     - Fix the case where user sets a high value for sleep timeout in display settings
       and the pocket view is also displayed for that much duration.
    
    Change-Id: If6c4daa1fd98992141f22db70e0e86d5d990f789
    
    pocket: Fix pocket lock view for display cutouts
    
     - Use LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES layout
       flag to extend pocket lock view for display cutout mode.
    
    Change-Id: I21e4e47cf999b3a0f3d6ebf9165ba9f5596ece0e
    
    Pocket lock improvements
    
    * Fix auto hide not working correctly
    * Restore systemui visibility when hidden
    
    ZeNiXxX
    * remove faceunlock logic and adapt for android 10
    
    Change-Id: I61b38798d01775b252af88abb01dd2423b3a7638
    
    pocket: Use MD2 lock drawable
    
    Change-Id: Ifb89a68505a9f9b401a2474b5179ea65e9c0e3ce
    
    pocket: Reduce sleep timeout for pocket lock
    
    *if the proximity value changes within 10s, pocketlock exits to lockscreen. Inorder to avoid that reduce timeout to 3s
    
    Change-Id: Iee08801534bebacc2fc62f0bc6e3a0712ce12712
    
    Pocket lock: Add config_pocketModeSupported overlay
    
    Change-Id: I6768c9f8cdd40b2a62df3bd9ddfa04022d69ce10
    
    PocketLock: fix flickering on animations
    
    Change-Id: I4b5499042d0e9ae8f73ecf5c32030d146f640362
    
    PocketJudge: allow register vendor pocket sensor
    
    Some devices (ie OnePlus) have own pocket sensor. This change allow to use
    native pocket sensor instead of proximity and light.
    
    Sensor name must be specified in device tree overlay.
    In this case PocketService will try to use vendor sensor first.
    If sensor wasn't found, fallback sensors (proximity and light) will be in use.
    
    Example for OnePlus sensor:
    <string name="config_pocketJudgeVendorSensorName">oneplus.sensor.pocket</string>
    
    Change-Id: Ibe423478cfd9d49e0831e2df0af793178f62c0e0
    
    Fix an edge case in KeyguardUpdateMonitor pocket callback
    
    Change-Id: If872c365ab22bd744c610b136b442ff161c65522
    
    base: Redo Pocket mode view like OOS
    
    Change-Id: I037f5120315581cade60b6c11a64b07cc47ba376
    
    SystemUI: Improve pocket mode layout
    
    Change-Id: I590bc187a5e205102b129fbb9e3625f51b532afd
    
    Change-Id: I33153b451f5d38d83e226be0f76bda1ed022dd48
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -115,6 +115,8 @@ import android.os.SystemClock;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.pocket.IPocketCallback;
+import android.pocket.PocketManager;
 import android.provider.Settings;
 import android.service.dreams.IDreamManager;
 import android.telephony.CarrierConfigManager;
@@ -229,6 +231,9 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
     private static final int MSG_TIME_FORMAT_UPDATE = 344;
     private static final int MSG_REQUIRE_NFC_UNLOCK = 345;
     private static final int MSG_KEYGUARD_DISMISS_ANIMATION_FINISHED = 346;
+
+    // Additional messages should be 600+
+    private static final int MSG_POCKET_STATE_CHANGED = 600;
 
     /** Biometric authentication state: Not listening. */
     private static final int BIOMETRIC_STATE_STOPPED = 0;
@@ -423,6 +428,27 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
     }
 
     private final Handler mHandler;
+
+    private PocketManager mPocketManager;
+    private boolean mIsDeviceInPocket;
+    private final IPocketCallback mPocketCallback = new IPocketCallback.Stub() {
+        @Override
+        public void onStateChanged(boolean isDeviceInPocket, int reason) {
+            boolean wasInPocket = mIsDeviceInPocket;
+            if (reason == PocketManager.REASON_SENSOR) {
+                mIsDeviceInPocket = isDeviceInPocket;
+            } else {
+                mIsDeviceInPocket = false;
+            }
+            if (wasInPocket != mIsDeviceInPocket) {
+                mHandler.sendEmptyMessage(MSG_POCKET_STATE_CHANGED);
+            }
+        }
+    };
+
+    public boolean isPocketLockVisible(){
+        return mPocketManager.isPocketLockVisible();
+    }
 
     private final IBiometricEnabledOnKeyguardCallback mBiometricEnabledCallback =
             new IBiometricEnabledOnKeyguardCallback.Stub() {
@@ -2188,6 +2214,10 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                     case MSG_KEYGUARD_DISMISS_ANIMATION_FINISHED:
                         handleKeyguardDismissAnimationFinished();
                         break;
+                    case MSG_POCKET_STATE_CHANGED:
+                        updateBiometricListeningState(BIOMETRIC_ACTION_UPDATE,
+                                FACE_AUTH_UPDATED_FP_AUTHENTICATED);
+                        break;
                     default:
                         super.handleMessage(msg);
                         break;
@@ -2246,6 +2276,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         mTrustManager.registerTrustListener(this);
 
         setStrongAuthTracker(mStrongAuthTracker);
+
+        mPocketManager = (PocketManager) context.getSystemService(Context.POCKET_SERVICE);
+        if (mPocketManager != null) {
+            mPocketManager.addCallback(mPocketCallback);
+        }
 
         if (mFpm != null) {
             mFingerprintSensorProperties = mFpm.getSensorPropertiesInternal();
@@ -2680,7 +2715,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         BiometricAuthenticated face = mUserFaceAuthenticated.get(getCurrentUser());
         return mAssistantVisible && mKeyguardOccluded
                 && !(face != null && face.mAuthenticated)
-                && !mUserHasTrust.get(getCurrentUser(), false);
+                && !mUserHasTrust.get(getCurrentUser(), false) && !mIsDeviceInPocket;
     }
 
     private boolean shouldTriggerActiveUnlockForAssistant() {
@@ -2746,7 +2781,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         boolean shouldListen = shouldListenKeyguardState && shouldListenUserState
                 && shouldListenBouncerState && shouldListenUdfpsState
                 && shouldListenSideFpsState
-                && !isFingerprintLockedOut();
+                && !isFingerprintLockedOut() && !mIsDeviceInPocket;
         logListenerModelData(
                 new KeyguardFingerprintListenModel(
                     System.currentTimeMillis(),
