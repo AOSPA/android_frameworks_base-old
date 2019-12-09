8fc593db09d4af50ae4131a210a1247ed0ec09b2
Patrick Baumann <patb@google.com>
2019-12-09 14:22:32 -0800

    DO NOT MERGE: Adds artificial package handler latency
    This change introduces a means of introducing an artificial long task to
    the package handler to help reproduce timing issues related to it.
    
    Bug: 141413692
    Test: atest PackageManagerTest
    Change-Id: I61ddee1fe8b94f5803d981a77babb4bb19e31662

--- core/java/android/app/ActivityManagerInternal.java
+++ core/java/android/app/ActivityManagerInternal.java
@@ -347,4 +347,7 @@ public abstract class ActivityManagerInternal {
 
     // Starts a process as empty.
     public abstract int startActivityAsUserEmpty(Bundle options);
+
+    /** Returns true if the given UID is registered as an active instrumentation. */
+    public abstract boolean isActiveInstrumentation(int uid);
 }
--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -209,6 +209,7 @@ import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.ConditionVariable;
 import android.os.Debug;
 import android.os.Environment;
 import android.os.FileUtils;
@@ -980,6 +981,8 @@ public class PackageManagerService extends IPackageManager.Stub
     private File mCacheDir;
 
     private Future<?> mPrepareAppDataFuture;
+
+    private final ConditionVariable mBlockDeleteOnUserRemoveForTest = new ConditionVariable(true);
 
     private static class IFVerificationParams {
         PackageParser.Package pkg;
@@ -23892,8 +23895,13 @@ public class PackageManagerService extends IPackageManager.Stub
                     Slog.i(TAG, "  Removing package " + packageName);
                 }
                 //end run
-                mHandler.post(() -> deletePackageX(packageName, PackageManager.VERSION_CODE_HIGHEST,
-                        userHandle, 0));
+                mHandler.post(() -> {
+                    if (!mBlockDeleteOnUserRemoveForTest.block(30000 /* 30 seconds*/)) {
+                        mBlockDeleteOnUserRemoveForTest.open();
+                    }
+                    deletePackageX(packageName, PackageManager.VERSION_CODE_HIGHEST,
+                            userHandle, 0);
+                });
             }
         }
     }
@@ -25254,6 +25262,16 @@ public class PackageManagerService extends IPackageManager.Stub
             } catch (Exception e) {
                 Slog.wtf(TAG, e);
             }
+        }
+
+        @Override
+        public void notifyingOnNextUserRemovalForTest() {
+            mBlockDeleteOnUserRemoveForTest.close();
+        }
+
+        @Override
+        public void userRemovedForTest() {
+            mBlockDeleteOnUserRemoveForTest.open();
         }
     }
 
