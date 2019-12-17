2cd97953ec61288267f2cdb4be64c19f169ea865
Lucas Dupin <dupin@google.com>
2019-12-17 13:14:53 -0800

    Fix lock screen wallpaper
    LockscreenWallpaper was set to null on NotificationMediaManager because
    StatusBar#mWallpaperSupported was not initialized yet.
    
    Fixes: 146252300
    Test: manual
    Change-Id: I4c5151ad6b40d0f0996fdbb825745729cc003425
    Merged-In: I4c5151ad6b40d0f0996fdbb825745729cc003425

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -710,6 +710,8 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         mKeyguardManager = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
         mFalsingManager = Dependency.get(FalsingManager.class);
+        mWallpaperSupported =
+                mContext.getSystemService(WallpaperManager.class).isWallpaperSupported();
 
         // Connect in to the status bar manager service
         mCommandQueue = getComponent(CommandQueue.class);
@@ -723,9 +725,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
 
         createAndAddWindows(result);
-
-        mWallpaperSupported =
-                mContext.getSystemService(WallpaperManager.class).isWallpaperSupported();
 
         if (mWallpaperSupported) {
             // Make sure we always have the most current wallpaper info.
