19ff2b2aec456cf05eaa8b62b34800483827f4b7
XiNGRZ <chenxingyu92@gmail.com>
2020-01-15 01:37:33 +0800

    FODCircleView: Add #20000000 bg
    * This lets the users set custom FOD png and not have it glitched out
    
    Change-Id: I320cfbe40c9ec24a2c65e403556cca5559ea2772

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -99,6 +99,8 @@ public class FODCircleView extends ImageView {
     private int mDreamingOffsetY;
 
     private int mCurrentBrightness;
+    private int mColor;
+    private int mColorBackground;
 
     private boolean mIsBouncer;
     private boolean mIsDreaming;
@@ -174,6 +176,8 @@ public class FODCircleView extends ImageView {
     public FODCircleView(Context context) {
         super(context);
 
+        setScaleType(ScaleType.CENTER);
+
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
         if (daemon == null) {
             throw new RuntimeException("Unable to get IFingerprintInscreen");
@@ -190,8 +194,13 @@ public class FODCircleView extends ImageView {
 
         Resources res = context.getResources();
 
-        mPaintFingerprint.setColor(res.getColor(R.color.config_fodColor));
+        mColor = res.getColor(R.color.config_fodColor);
+        mPaintFingerprint.setColor(mColor);
         mPaintFingerprint.setAntiAlias(true);
+
+        mColorBackground = res.getColor(R.color.config_fodColorBackground);
+        mPaintFingerprintBackground.setColor(mColorBackground);
+        mPaintFingerprintBackground.setAntiAlias(true);
 
         mDimIcon = res.getBoolean(R.bool.config_fodIconDim);
         mSettingsObserver = new SettingsObserver(context);
