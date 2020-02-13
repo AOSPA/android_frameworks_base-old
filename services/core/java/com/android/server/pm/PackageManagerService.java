e32fcfbb70737f011a4ead6c824d6f68dddbae92
Bhargav Upperla <bhargavuln@codeaurora.org>
2020-02-12 16:03:14 -0800

    Add support for UxPerformance to receive context information.
    Change-Id: Ic1840863e1d8efe9eff3e1481db199ea8ee5972b
    CRs-Fixed: 2656217

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -20428,6 +20428,7 @@ public class PackageManagerService extends IPackageManager.Stub
         }, overlayFilter);
 
         mModuleInfoProvider.systemReady();
+        new BoostFramework(mContext, true);
 
         // Installer service might attempt to install some packages that have been staged for
         // installation on reboot. Make sure this is the last component to be call since the
