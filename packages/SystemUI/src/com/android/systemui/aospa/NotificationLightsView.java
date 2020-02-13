f17de6142ac4cc585defdf589f4d0e328402ac27
maxwen <max.weninger@gmail.com>
2020-02-13 02:47:09 +0100

    SystemUI: support using notification color for pulse light
    optional use the color information of the notification
    as color for notification pulse light
    
    @brituj
    * Update color when a new notification arrives while pulsing
    * Fix row == null when on lockscreen even though the notification is clearable
    * other code improvements
    
    Change-Id: Icf7c24f8c695f29485123c2c941b08bbdabbbbdf
    Signed-off-by: rituj <ritujbeniwal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/aospa/NotificationLightsView.java
+++ packages/SystemUI/src/com/android/systemui/aospa/NotificationLightsView.java
@@ -20,17 +20,12 @@ package com.android.systemui.aospa;
 import android.animation.ValueAnimator;
 import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.content.Context;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
 import com.android.settingslib.Utils;
@@ -39,10 +34,26 @@ import com.android.systemui.R;
 public class NotificationLightsView extends RelativeLayout {
     private static final boolean DEBUG = false;
     private static final String TAG = "NotificationLightsView";
-    private View mNotificationAnimView;
+
     private ValueAnimator mLightAnimator;
-    private boolean mPulsing;
-    private boolean mNotification;
+    private ImageView mLeftView;
+    private ImageView mRightView;
+    private AnimatorUpdateListener mAnimatorUpdateListener = new AnimatorUpdateListener() {
+        public void onAnimationUpdate(ValueAnimator animation) {
+            if (DEBUG) Log.d(TAG, "onAnimationUpdate");
+            float progress = ((Float) animation.getAnimatedValue()).floatValue();
+            mLeftView.setScaleY(progress);
+            mRightView.setScaleY(progress);
+            float alpha = 1.0f;
+            if (progress <= 0.3f) {
+                alpha = progress / 0.3f;
+            } else if (progress >= 1.0f) {
+                alpha = 2.0f - progress;
+            }
+            mLeftView.setAlpha(alpha);
+            mRightView.setAlpha(alpha);
+        }
+    };
 
     public NotificationLightsView(Context context) {
         this(context, null);
@@ -59,70 +70,49 @@ public class NotificationLightsView extends RelativeLayout {
     public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         if (DEBUG) Log.d(TAG, "new");
-    }
-
-    private Runnable mLightUpdate = new Runnable() {
-        @Override
-        public void run() {
-            if (DEBUG) Log.d(TAG, "run");
-            animateNotification(mNotification);
-        }
-    };
-
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-        if (DEBUG) Log.d(TAG, "draw");
-        //post(mLightUpdate);
-    }
-
-    public void setPulsing(boolean pulsing) {
-        if (mPulsing == pulsing) {
-            return;
-        }
-        mPulsing = pulsing;
-    }
-
-    public void animateNotification(boolean mNotification) {
-        int defaultColor = Color.parseColor("#3980FF");
-        boolean useAccent = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACCENT,
-                0, UserHandle.USER_CURRENT) != 0;
-        int color = useAccent ?
-                Utils.getColorAccentDefaultColor(getContext()) : defaultColor;
-        StringBuilder sb = new StringBuilder();
-        sb.append("animateNotification color ");
-        sb.append(Integer.toHexString(color));
-        if (DEBUG) Log.d(TAG, sb.toString());
-        ImageView leftView = (ImageView) findViewById(R.id.notification_animation_left);
-        ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
-        leftView.setColorFilter(color);
-        rightView.setColorFilter(color);
         mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
         mLightAnimator.setDuration(2000);
-        //Infinite animation only on Always On Notifications
-        if (mNotification) {
-            mLightAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        }
         mLightAnimator.setRepeatMode(ValueAnimator.RESTART);
-        mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
-            public void onAnimationUpdate(ValueAnimator animation) {
-                if (DEBUG) Log.d(TAG, "onAnimationUpdate");
-                float progress = ((Float) animation.getAnimatedValue()).floatValue();
-                leftView.setScaleY(progress);
-                rightView.setScaleY(progress);
-                float alpha = 1.0f;
-                if (progress <= 0.3f) {
-                    alpha = progress / 0.3f;
-                } else if (progress >= 1.0f) {
-                    alpha = 2.0f - progress;
-                }
-                leftView.setAlpha(alpha);
-                rightView.setAlpha(alpha);
-            }
-        });
-        if (DEBUG) Log.d(TAG, "start");
-        mLightAnimator.start();
+    }
+
+    public void animateNotification() {
+        animateNotificationWithColor(getNotificationLightsColor());
+    }
+
+    public int getNotificationLightsColor() {
+        int color = getDefaultNotificationLightsColor();
+        boolean useAccent = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.AMBIENT_NOTIFICATION_LIGHT_AUTOMATIC, 1, UserHandle.USER_CURRENT) == 0;
+        if (useAccent) {
+            color = Utils.getColorAccentDefaultColor(getContext());
+        }
+        return color;
+    }
+
+    public int getDefaultNotificationLightsColor() {
+        return getResources().getInteger(com.android.internal.R.integer.config_AmbientPulseLightColor);
+    }
+
+    public void endAnimation() {
+        mLightAnimator.end();
+        mLightAnimator.removeAllUpdateListeners();
+    }
+
+    public void animateNotificationWithColor(int color) {
+        if (mLeftView == null) {
+            mLeftView = (ImageView) findViewById(R.id.notification_animation_left);
+        }
+        if (mRightView == null) {
+            mRightView = (ImageView) findViewById(R.id.notification_animation_right);
+        }
+        mLeftView.setColorFilter(color);
+        mRightView.setColorFilter(color);
+        if (!mLightAnimator.isRunning()) {
+            if (DEBUG) Log.d(TAG, "start");
+            mLightAnimator.setRepeatCount(ValueAnimator.INFINITE);
+            mLightAnimator.addUpdateListener(mAnimatorUpdateListener);
+            mLightAnimator.start();
+        }
     }
 
 }
