68c929892bf251aa60c4e33c622ae24737186271
maxwen <max.weninger@gmail.com>
2020-02-13 02:47:09 +0100

    SystemUI: support using notification color for pulse light
    optional use the color information of the notification
    as color for notification pulse light
    
    @brituj
    * Update color when a new notification arrives while pulsing
    * Fix row == null when on lockscreen even though the notification is clearable
    * other code improvements
    
    Change-Id: Icf7c24f8c695f29485123c2c941b08bbdabbbbdf
    Signed-off-by: rituj <ritujbeniwal@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -997,16 +997,16 @@ public final class PowerManagerService extends SystemService
                 Settings.System.GESTURE_DRAW_ARROW_RIGHT),
                 false, mSettingsObserver, UserHandle.USER_ALL);
         resolver.registerContentObserver(Settings.System.getUriFor(
-                        Settings.System.GESTURE_ONE_FINGER_SWIPE_UP),
+                Settings.System.GESTURE_ONE_FINGER_SWIPE_UP),
                 false, mSettingsObserver, UserHandle.USER_ALL);
         resolver.registerContentObserver(Settings.System.getUriFor(
-                        Settings.System.GESTURE_ONE_FINGER_SWIPE_RIGHT),
+                Settings.System.GESTURE_ONE_FINGER_SWIPE_RIGHT),
                 false, mSettingsObserver, UserHandle.USER_ALL);
         resolver.registerContentObserver(Settings.System.getUriFor(
-                        Settings.System.GESTURE_ONE_FINGER_SWIPE_UP),
+                Settings.System.GESTURE_ONE_FINGER_SWIPE_UP),
                 false, mSettingsObserver, UserHandle.USER_ALL);
         resolver.registerContentObserver(Settings.System.getUriFor(
-                        Settings.System.GESTURE_ONE_FINGER_SWIPE_RIGHT),
+                Settings.System.GESTURE_ONE_FINGER_SWIPE_RIGHT),
                 false, mSettingsObserver, UserHandle.USER_ALL);
         resolver.registerContentObserver(Settings.System.getUriFor(
                 Settings.System.GESTURE_ONE_FINGER_SWIPE_DOWN),
@@ -1020,12 +1020,6 @@ public final class PowerManagerService extends SystemService
         resolver.registerContentObserver(Settings.Global.getUriFor(
                 Settings.Global.DEVICE_DEMO_MODE),
                 false, mSettingsObserver, UserHandle.USER_SYSTEM);
-        resolver.registerContentObserver(Settings.System.getUriFor(
-                Settings.System.AMBIENT_NOTIFICATION_LIGHT),
-                false, mSettingsObserver, UserHandle.USER_ALL);
-        resolver.registerContentObserver(Settings.System.getUriFor(
-                Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED),
-                false, mSettingsObserver, UserHandle.USER_ALL);
 
         IVrManager vrManager = IVrManager.Stub.asInterface(getBinderService(Context.VR_SERVICE));
         if (vrManager != null) {
@@ -1157,22 +1151,6 @@ public final class PowerManagerService extends SystemService
             mGesturesEnabled = gesturesEnabled;
             nativeSetFeature(POWER_FEATURE_GESTURES, mGesturesEnabled ? 1 : 0);
         }
-        boolean mAmbientLights = Settings.System.getIntForUser(
-                mContext.getContentResolver(), Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED,
-                0, UserHandle.USER_CURRENT) != 0;
-        if (mAmbientLights) {
-            boolean dozeOnNotification = Settings.System.getIntForUser(resolver,
-                    Settings.System.AMBIENT_NOTIFICATION_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
-            Settings.System.putIntForUser(mContext.getContentResolver(),
-                     Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, dozeOnNotification ? 1 : 0,
-                     UserHandle.USER_CURRENT);
-        } else {
-             Settings.System.putIntForUser(mContext.getContentResolver(),
-                     Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, 0,
-                     UserHandle.USER_CURRENT);
-        }
-        // depends on AMBIENT_NOTIFICATION_LIGHT_ACTIVATED - so MUST be afterwards
-        // no need to call us again
         mAlwaysOnEnabled = mAmbientDisplayConfiguration.alwaysOnEnabled(UserHandle.USER_CURRENT);
 
         if (mSupportsDoubleTapWakeConfig) {
