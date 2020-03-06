253970d5a7852bdf9176e0f9689b63e7b3604c4a
Alexander Koskovich <zvnexus@outlook.com>
2020-03-05 21:06:06 -0500

    Alert Slider: Switch to AudioManager, kill setZenMode.
    Change-Id: I129807f32ce743d32843a818ede0d68264985cde

--- services/core/java/com/android/server/policy/AlertSliderObserver.java
+++ services/core/java/com/android/server/policy/AlertSliderObserver.java
@@ -18,6 +18,7 @@ package com.android.server.policy;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.media.AudioManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
@@ -25,7 +26,6 @@ import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.os.UEventObserver;
 import android.os.UserHandle;
-import android.os.Vibrator;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
@@ -42,18 +42,12 @@ public class AlertSliderObserver extends UEventObserver {
     private int mState;
 
     private final Context mContext;
-    private final NotificationManager mNotificationManager;
-    private final Vibrator mVibrator;
+    private final AudioManager mAudioManager;
     private final WakeLock mWakeLock;
-
-    private boolean mHasVibrator;
 
     public AlertSliderObserver(Context context) {
         mContext = context;
-        mNotificationManager
-               = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
-        mHasVibrator = mVibrator != null && mVibrator.hasVibrator();
+        mAudioManager = context.getSystemService(AudioManager.class);
         PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "AlertSliderObserver");
         init();
@@ -101,7 +95,7 @@ public class AlertSliderObserver extends UEventObserver {
     }
 
     protected void update() {
-        // Acquire wakelock when slider state changes
+        // Acquire wakelock when slider state changes.
         mWakeLock.acquire();
         mHandler.sendEmptyMessageDelayed(mState, 100);
     }
@@ -110,46 +104,27 @@ public class AlertSliderObserver extends UEventObserver {
         @Override
         public void handleMessage(Message msg) {
             final boolean inverted = isOrderInverted();
-            final int silentMode = getSilentMode();
             switch (mState) {
                 case 1:
-                    setZenMode(inverted ? Settings.Global.ZEN_MODE_OFF : silentMode);
+                    mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
                     break;
                 case 2:
-                    setZenMode(Settings.Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS);
+                    mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
                     break;
                 case 3:
-                    setZenMode(inverted ? silentMode : Settings.Global.ZEN_MODE_OFF);
+                    mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
                     break;
+            }
+            if (mWakeLock.isHeld()) {
+                mWakeLock.release();
             }
         }
     };
-
-    private void setZenMode(int zenMode) {
-        if (mHasVibrator) {
-            mVibrator.vibrate(50);
-        }
-        mNotificationManager.setZenMode(zenMode, null, TAG);
-        // Release wakelock when zen mode is set
-        if (mWakeLock.isHeld()) {
-            mWakeLock.release();
-        }
-    }
 
     // Check if ordered has been set to inverted.
     private boolean isOrderInverted() {
         return Settings.System.getIntForUser(
                     mContext.getContentResolver(), Settings.System.ALERT_SLIDER_ORDER, 0,
                     UserHandle.USER_CURRENT) != 0;
-    }
-
-    // Get silent mode for silent state:
-    // - ALARMS ONLY (Default)
-    // - TOTAL SILENCE
-    private int getSilentMode() {
-        int silentMode = Settings.System.getIntForUser(
-                mContext.getContentResolver(), Settings.System.ALERT_SLIDER_SILENT_MODE, 0,
-                UserHandle.USER_CURRENT);
-        return silentMode != 0 ? Settings.Global.ZEN_MODE_NO_INTERRUPTIONS : Settings.Global.ZEN_MODE_ALARMS;
     }
 }
