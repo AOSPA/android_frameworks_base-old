d1427f97b1087dd4b57868d3f344ad087d5f4ca9
Jake Weinstein <jake@aospa.co>
2020-03-11 21:00:37 +0100

    SystemUI: Disable heads up debugging on debug builds as well.
    This can be enabled locally if we need to test something specific.
    
    Change-Id: Ia8754b81be6c87d29780855718a86e132f7302aa

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
@@ -54,7 +54,7 @@ public class NotificationInterruptionStateProvider {
 
     private static final String TAG = "InterruptionStateProvider";
     private static final boolean DEBUG = false;
-    private static final boolean DEBUG_HEADS_UP = Build.IS_DEBUGGABLE;
+    private static final boolean DEBUG_HEADS_UP = false;
     private static final boolean ENABLE_HEADS_UP = true;
     private static final String SETTING_HEADS_UP_TICKER = "ticker_gets_heads_up";
 
