23d0f21a94a13bfa91663d32fdd39c73732a5b25
Jake Weinstein <jake@aospa.co>
2020-03-14 15:47:14 +0100

    Gestures: Add support for single tap gesture [3/4]
    Change-Id: Idb0020825be24a7717457ef2eaeb1d0884243c43

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4531,6 +4531,12 @@ public final class Settings {
         public static final String GESTURE_DOUBLE_TAP = "gesture_double_tap";
 
         /**
+         * Controls whether single tap to wake is enabled.
+         * @hide
+         */
+        public static final String GESTURE_SINGLE_TAP = "gesture_single_tap";
+
+        /**
          * Controls whether draw V gesture is enabled.
          * @hide
          */
@@ -4891,6 +4897,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(GESTURES_ENABLED);
             PRIVATE_SETTINGS.add(GESTURE_DOUBLE_TAP);
+            PRIVATE_SETTINGS.add(GESTURE_SINGLE_TAP);
             PRIVATE_SETTINGS.add(GESTURE_DRAW_V);
             PRIVATE_SETTINGS.add(GESTURE_DRAW_INVERSE_V);
             PRIVATE_SETTINGS.add(GESTURE_DRAW_O);
