fa4be8ce4d74971d4f2023c956b987045afc188d
Arian <arian.kulmer@web.de>
2020-03-15 12:22:46 +0100

    PackageManagerService: Drop caches according to incremental version
    * ro.build.date is the same on all builds across one day
    * ro.build.fingerprint is overridden with the stock fingerprint and usually doesn't change
    * ro.build.version.incremental should be changing every build
    
    Change-Id: I454601f1ff1bd67b3bd67256654311424ba04e40

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -3967,7 +3967,7 @@ public class PackageManagerService extends IPackageManager.Stub
         // feature flags should cause us to invalidate any caches.
         final String cacheName = FORCE_PACKAGE_PARSED_CACHE_ENABLED ? "debug"
                 : SystemProperties.digestOf(
-                        "ro.build.fingerprint",
+                        "ro.build.version.incremental",
                         StorageManager.PROP_ISOLATED_STORAGE,
                         StorageManager.PROP_ISOLATED_STORAGE_SNAPSHOT
                 );
