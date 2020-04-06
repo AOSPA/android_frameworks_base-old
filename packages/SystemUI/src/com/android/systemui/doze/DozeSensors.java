8071ff1bcf279aa54a3654a5da571381ee704b2a
Demon000 <demonsingur@gmail.com>
2020-04-06 15:47:43 +0200

    DozeSensors: Only use proximity sensor if supported
    On msm-4.14 devices, when the proximity sensor is in use,
    the smp2p-sleepstate IRQ is fired multiple times a second,
    with each one holding a 200ms wakelock.
    This is probably a bug in the DSP firmware.
    To fix this, avoid using the proximity sensor in doze mode,
    because sleep is preferred to turning off the screen.
    
    Change-Id: I57750afd77267abdc49780f70636626d20e666ad

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -47,6 +47,7 @@ import com.android.internal.logging.UiEvent;
 import com.android.internal.logging.UiEventLogger;
 import com.android.internal.logging.UiEventLoggerImpl;
 import com.android.keyguard.KeyguardUpdateMonitor;
+import com.android.systemui.R;
 import com.android.systemui.biometrics.AuthController;
 import com.android.systemui.plugins.SensorManagerPlugin;
 import com.android.systemui.statusbar.phone.DozeParameters;
@@ -107,6 +108,7 @@ public class DozeSensors {
     @VisibleForTesting
     protected TriggerSensor[] mTriggerSensors;
     private final ProximitySensor mProximitySensor;
+    private final boolean mProxSensorSupported;
 
     // Sensor callbacks
     private final Callback mSensorCallback; // receives callbacks on registered sensor events
@@ -169,6 +171,8 @@ public class DozeSensors {
         mProximitySensor.setTag(TAG);
         mSelectivelyRegisterProxSensors = dozeParameters.getSelectivelyRegisterSensorsUsingProx();
         mListeningProxSensors = !mSelectivelyRegisterProxSensors;
+        mProxSensorSupported = mContext.getResources().getBoolean(
+                R.bool.doze_proximity_sensor_supported);
         mScreenOffUdfpsEnabled =
                 config.screenOffUdfpsEnabled(KeyguardUpdateMonitor.getCurrentUser());
         mDevicePostureController = devicePostureController;
@@ -259,6 +263,7 @@ public class DozeSensors {
                         false /* ignoresSetting */,
                         false /* requiresProx */),
         };
+        if (!mProxSensorSupported) return;
         setProxListening(false);  // Don't immediately start listening when we register.
         mProximitySensor.register(
                 proximityEvent -> {
@@ -472,6 +477,7 @@ public class DozeSensors {
         for (TriggerSensor s : mTriggerSensors) {
             idpw.println("Sensor: " + s.toString());
         }
+        if (!mProxSensorSupported) return;
         idpw.println("ProxSensor: " + mProximitySensor.toString());
     }
 
@@ -479,7 +485,7 @@ public class DozeSensors {
      * @return true if prox is currently near, false if far or null if unknown.
      */
     public Boolean isProximityCurrentlyNear() {
-        return mProximitySensor.isNear();
+        return mProxSensorSupported ? mProximitySensor.isNear() : null;
     }
 
     @VisibleForTesting
