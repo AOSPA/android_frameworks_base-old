a429b00e91e6f16a6021407ea79237dcaf55f4cc
Ethan Halsall <ethan.halsall@gmail.com>
2020-04-09 23:35:54 +0100

    SytemUI: Do not use new lockscreen layout for bypass
    bypassEnabled will always evaluate to false now.
    Introduce bypassEnabledBiometric which is only used to tell the biometric service whether we can bypass lockscreen without swipe, dependant on Settings.Secure.FACE_UNLOCK_DISMISSES_KEYGUARD.
    
    This enables full lockscreen layout when "Skip lockscreen" setting is enabled, while also fixing status bar glitches.
    
    Change-Id: Ibdbf16df18543382219a75ce1aaf4e25f3fb4ddb
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BiometricUnlockController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BiometricUnlockController.java
@@ -585,7 +585,7 @@ public class BiometricUnlockController extends KeyguardUpdateMonitorCallback imp
         boolean unlockingAllowed =
                 mUpdateMonitor.isUnlockingWithBiometricAllowed(isStrongBiometric);
         boolean deviceDreaming = mUpdateMonitor.isDreaming();
-        boolean bypass = mKeyguardBypassController.getBypassEnabled()
+        boolean bypass = mKeyguardBypassController.getBypassEnabledBiometric()
                 || mAuthController.isUdfpsFingerDown();
         if (!mUpdateMonitor.isDeviceInteractive()) {
             if (!mKeyguardViewController.isShowing()) {
