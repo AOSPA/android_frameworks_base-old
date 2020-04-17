4d7f4b9e02010d3cb272c25964c28b0a78878a7f
Chris Crump <chris@cypheros.co>
2020-04-17 03:22:12 -0400

    SystemUI: Move screen record countdown timer to clock view
    Change-Id: I10e4896a421def93b9c871642e532e3e5ef6d0fb

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -91,8 +91,7 @@ public class PhoneStatusBarPolicy
                 DeviceProvisionedListener,
                 KeyguardMonitor.Callback,
                 PrivacyItemController.Callback,
-                LocationController.LocationChangeCallback,
-                RecordingController.RecordingStateListener {
+                LocationController.LocationChangeCallback {
     private static final String TAG = "PhoneStatusBarPolicy";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
 
@@ -113,7 +112,6 @@ public class PhoneStatusBarPolicy
     private final String mSlotMicrophone;
     private final String mSlotCamera;
     private final String mSlotSensorsOff;
-    private final String mSlotScreenRecord;
 
     private final Context mContext;
     private final Handler mHandler = new Handler();
@@ -185,7 +183,6 @@ public class PhoneStatusBarPolicy
         mSlotMicrophone = context.getString(com.android.internal.R.string.status_bar_microphone);
         mSlotCamera = context.getString(com.android.internal.R.string.status_bar_camera);
         mSlotSensorsOff = context.getString(com.android.internal.R.string.status_bar_sensors_off);
-        mSlotScreenRecord = context.getString(R.string.status_bar_screen_record);
 
         // listen for broadcasts
         IntentFilter filter = new IntentFilter();
@@ -263,10 +260,6 @@ public class PhoneStatusBarPolicy
         mIconController.setIconVisibility(mSlotSensorsOff,
                 mSensorPrivacyController.isSensorPrivacyEnabled());
 
-        // screen record
-        mIconController.setIcon(mSlotScreenRecord, R.drawable.stat_sys_screen_record, (CharSequence) null);
-        mIconController.setIconVisibility(mSlotScreenRecord, false);
-
         mRotationLockController.addCallback(this);
         mBluetooth.addCallback(this);
         mProvisionedController.addCallback(this);
@@ -279,7 +272,6 @@ public class PhoneStatusBarPolicy
         mPrivacyItemController.addCallback(this);
         mSensorPrivacyController.addCallback(mSensorPrivacyListener);
         mLocationController.addCallback(this);
-        mRecordingController.addCallback((RecordingController.RecordingStateListener) this);
 
         SysUiServiceProvider.getComponent(mContext, CommandQueue.class).addCallback(this);
     }
@@ -803,44 +795,5 @@ public class PhoneStatusBarPolicy
             mIconController.setIcon(mSlotHotspot, R.drawable.stat_sys_hotspot,
                 mContext.getString(R.string.accessibility_status_bar_hotspot));
         }
-    }
-
-    @Override
-    public void onCountdown(long millisUntilFinished) {
-        if (DEBUG) Log.d(TAG, "screenrecord: countdown " + millisUntilFinished);
-        int level = (int) Math.floorDiv(millisUntilFinished + 500, 1000);
-        int icon = R.drawable.stat_sys_screen_record;
-        if (level == 1) {
-            icon = R.drawable.stat_sys_screen_record_1;
-        } else if (level == 2) {
-            icon = R.drawable.stat_sys_screen_record_2;
-        } else if (level == 3) {
-            icon = R.drawable.stat_sys_screen_record_3;
-        }
-        mIconController.setIcon(mSlotScreenRecord, icon, (CharSequence) null);
-        mIconController.setIconVisibility(mSlotScreenRecord, true);
-    }
-
-    @Override
-    public void onCountdownEnd() {
-        if (DEBUG) Log.d(TAG, "screenrecord: hiding icon during countdown");
-        mHandler.post(() -> {
-            mIconController.setIconVisibility(mSlotScreenRecord, false);
-        });
-    }
-
-    @Override
-    public void onRecordingStart() {
-        if (DEBUG) Log.d(TAG, "screenrecord: showing icon");
-        mIconController.setIcon(mSlotScreenRecord, R.drawable.stat_sys_screen_record, (CharSequence) null);
-        mIconController.setIconVisibility(mSlotScreenRecord, true);
-    }
-
-    @Override
-    public void onRecordingEnd() {
-        if (DEBUG) Log.d(TAG, "screenrecord: hiding icon");
-        mHandler.post(() -> {
-            mIconController.setIconVisibility(mSlotScreenRecord, false);
-        });
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -46,6 +46,8 @@ import com.android.systemui.R;
 import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
+import com.android.systemui.screenrecord.RecordingController;
+import com.android.systemui.screenrecord.RecordingController.RecordingStateListener;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
@@ -64,7 +66,7 @@ import java.util.TimeZone;
  * Digital clock for the status bar.
  */
 public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.Callbacks,
-        DarkReceiver, ConfigurationListener {
+        DarkReceiver, ConfigurationListener, RecordingStateListener {
 
     public static final String CLOCK_SECONDS = "clock_seconds";
     private static final String CLOCK_SUPER_PARCELABLE = "clock_super_parcelable";
@@ -86,6 +88,9 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
     private SimpleDateFormat mClockFormat;
     private SimpleDateFormat mContentDescriptionFormat;
     private Locale mLocale;
+
+    private RecordingController mRecordingController;
+    private int mRecordCountdownInterval = -1;
 
     private static final int AM_PM_STYLE_NORMAL  = 0;
     private static final int AM_PM_STYLE_SMALL   = 1;
@@ -194,6 +199,9 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
             }
             mCurrentUserTracker.startTracking();
             mCurrentUserId = mCurrentUserTracker.getCurrentUserId();
+
+            mRecordingController = Dependency.get(RecordingController.class);
+            mRecordingController.addCallback(this);
         }
 
         // NOTE: It's safe to do these after registering the receiver since the receiver always runs
@@ -221,6 +229,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
                 Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(this);
             }
             mCurrentUserTracker.stopTracking();
+            Dependency.get(RecordingController.class).removeCallback(this);
         }
     }
 
@@ -281,7 +290,8 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
     final void updateClock() {
         if (mDemoMode) return;
         mCalendar.setTimeInMillis(System.currentTimeMillis());
-        setText(getSmallTime());
+        int countdownInterval = mRecordCountdownInterval;
+        setText(countdownInterval != -1 ? String.valueOf(countdownInterval) : getSmallTime());
         setContentDescription(mContentDescriptionFormat.format(mCalendar.getTime()));
     }
 
@@ -501,5 +511,27 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
             mSecondsHandler.postAtTime(this, SystemClock.uptimeMillis() / 1000 * 1000 + 1000);
         }
     };
+
+     @Override
+    public void onCountdown(long millisUntilFinished) {
+        mRecordCountdownInterval = (int) Math.floorDiv(millisUntilFinished + 500, 1000);
+        getHandler().post(() -> updateClock());
+    }
+
+    @Override
+    public void onCountdownEnd() {
+        mRecordCountdownInterval = -1;
+        getHandler().post(() -> updateClock());
+    }
+
+    @Override
+    public void onRecordingStart() {
+        // no op
+    }
+
+    @Override
+    public void onRecordingEnd() {
+        // no op
+    }
 }
 
