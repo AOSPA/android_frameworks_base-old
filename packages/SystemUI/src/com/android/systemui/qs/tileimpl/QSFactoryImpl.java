e4f7d7ccf1ce79cb61620da412c48bba244bed92
Linux4 <tim@linux4.de>
2020-04-20 17:57:43 +0200

    [lineage-18.0] SystemUI: Add powershare QS tile
    Author: Linux4 <tim@linux4.de>
    Date:   Mon Apr 20 17:57:43 2020 +0200
    
        base: SystemUI: add qs powershare tile
    
        Change-Id: Ibcf5cc3e766d971d9dca9242bf2770093d4d3099
    
    Author: Linux4 <tim@linux4.de>
    Date:   Sat May 2 12:54:49 2020 +0200
    
        base: SystemUI: fix NPE in PowerShareTile
    
        Change-Id: If69846f3e3d4f3a12c0f78f47b5adc1008888de1
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue Jul 7 13:27:12 2020 +0200
    
        SystemUI: Call setOnlyAlertOnce(true) for PowerShare notification
    
        * Otherwise we end up getting notification sound
          every 20 seconds when using it...
    
        Change-Id: Ia56b01f332860b64f0d2c532884e6bcaa2acfb17
    
    Change-Id: Ia09a674745adb433f3aaeebfeb79899906350793

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.PowerShareTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
@@ -86,6 +87,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<DcDimmingTile> mDcDimmingTileProvider;
     private final Provider<HeadsUpTile> mHeadsUpTileProvider;
     private final Provider<DataSwitchTile> mDataSwitchTileProvider;
+    private final Provider<PowerShareTile> mPowerShareTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
 
@@ -114,7 +116,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<ScreenRecordTile> screenRecordTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
             Provider<HeadsUpTile> headsUpTileProvider,
-            Provider<DataSwitchTile> dataSwitchTileProvider) {
+            Provider<DataSwitchTile> dataSwitchTileProvider,
+            Provider<PowerShareTile> powerShareTileProvider) {
         mQsHostLazy = qsHostLazy;
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
@@ -140,6 +143,7 @@ public class QSFactoryImpl implements QSFactory {
         mDcDimmingTileProvider = dcDimTileProvider;
         mHeadsUpTileProvider = headsUpTileProvider;
         mDataSwitchTileProvider = dataSwitchTileProvider;
+        mPowerShareTileProvider = powerShareTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -199,6 +203,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mHeadsUpTileProvider.get();
             case "dataswitch":
                 return mDataSwitchTileProvider.get();
+            case "powershare":
+                return mPowerShareTileProvider.get();
         }
 
         // Custom tiles
