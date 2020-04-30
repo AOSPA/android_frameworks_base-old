a574ec491f4cbfd8f8312399a5516475b395292c
rituj <ritujbeniwal@gmail.com>
2020-04-30 19:13:56 +0530

    base: Introduce Applock [1/2]
    * Built upon the initial commit by "Anass Karbila <anaskarbila@aospa.co>"
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Ida21d22035d708a359eaaa29e2c48ff6f0dd6627

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -160,6 +160,7 @@ import com.android.server.usage.UsageStatsService;
 import com.android.server.vr.VrManagerService;
 import com.android.server.webkit.WebViewUpdateService;
 import com.android.server.wm.ActivityTaskManagerService;
+import com.android.server.wm.AppLockService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
 
@@ -1098,6 +1099,10 @@ public final class SystemServer {
 
             traceBeginAndSlog("ActivityTriggerService");
             mSystemServiceManager.startService(ActivityTriggerService.class);
+            traceEnd();
+
+            traceBeginAndSlog("StartAppLockService");
+            mSystemServiceManager.startService(AppLockService.class);
             traceEnd();
 
             traceBeginAndSlog("SignedConfigService");
