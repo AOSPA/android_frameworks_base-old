d0261b177c661c92a9cdeb899030ac92fc6af4a4
rituj <ritujbeniwal@gmail.com>
2020-04-30 19:13:56 +0530

    base: Introduce Applock [1/3]
    * Includes applock 1.1 and more
    * Built upon the initial commit by "Anass Karbila <anaskarbila@aospa.co>"
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Ida21d22035d708a359eaaa29e2c48ff6f0dd6627

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4030,6 +4030,13 @@ public final class Settings {
         public static final int SCREEN_BRIGHTNESS_MODE_AUTOMATIC = 1;
 
         /**
+         * Indicates whether we should only show the app lock view when the device is woken up
+         * Or always.
+         * @hide
+         */
+        public static final String APP_LOCK_SHOW_ONLY_ON_WAKE = "app_lock_show_only_on_wake";
+
+        /**
          * Control whether to enable adaptive sleep mode.
          * @deprecated Use {@link android.provider.Settings.Secure#ADAPTIVE_SLEEP} instead.
          * @hide
