c7e5e45f1494452ac6e2d9cf178d96b544e72cf4
rituj <ritujbeniwal@gmail.com>
2020-04-30 19:13:56 +0530

    base: Introduce Applock [1/2]
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Ida21d22035d708a359eaaa29e2c48ff6f0dd6627

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -4702,6 +4702,16 @@ public abstract class Context {
     public static final String POCKET_SERVICE = "pocket";
 
     /**
+     * Use with {@link #getSystemService} to retrieve a
+     * {@link android.app.AppLockManager} for accessing and setting locked apps state.
+     *
+     * @hide
+     * @see #getSystemService
+     * @see android.app.AppLockManager
+     */
+    public static final String APPLOCK_SERVICE = "applock";
+
+    /**
      * Determine whether the given permission is allowed for a particular
      * process and user ID running in the system.
      *
