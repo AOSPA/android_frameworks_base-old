3f908747ff421f1cd69052e64f0f1ab9b46f9309
rituj <ritujbeniwal@gmail.com>
2020-05-05 16:45:34 +0530

    SystemUI: Fix Keyguard view being stretched for non-FOD devices
    * devices can overlay the default FOD view height of 140dp in their dt
    * remove the FOD space when no fingerprint is registered even on FOD devices
    * this is a partial revert of e45c288d9496568593fd7516716c753d4f0f361d
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Ifdc80c400afdc0be865910979eb82501561eaf72

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -16,6 +16,7 @@
 
 package com.android.systemui.biometrics;
 
+import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -44,6 +45,7 @@ import android.widget.ImageView;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.KeyguardSecurityModel.SecurityMode;
+import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.R;
 
 import vendor.pa.biometrics.fingerprint.inscreen.V1_0.IFingerprintInscreen;
@@ -81,6 +83,8 @@ public class FODCircleView extends ImageView implements OnTouchListener {
     private boolean mIsViewAdded;
 
     private Handler mHandler;
+
+    private LockPatternUtils mLockPatternUtils;
 
     private Timer mBurnInProtectionTimer;
 
@@ -180,10 +184,7 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         public void onKeyguardBouncerChanged(boolean isBouncer) {
             mIsBouncer = isBouncer;
             if (mUpdateMonitor.isFingerprintDetectionRunning()) {
-                final SecurityMode sec = mUpdateMonitor.getSecurityMode();
-                final boolean maybeShow = sec == SecurityMode.Pattern ||
-                        sec == SecurityMode.PIN;
-                if (maybeShow || !mIsBouncer) {
+                if (isPinOrPattern(mUpdateMonitor.getCurrentUser()) || !isBouncer) {
                     show();
                 } else {
                     hide();
@@ -264,6 +265,8 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         mUpdateMonitor.registerCallback(mMonitorCallback);
 
         mDisplayManager = context.getSystemService(DisplayManager.class);
+
+        mLockPatternUtils = new LockPatternUtils(mContext);
     }
 
     @Override
@@ -516,6 +519,20 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         } catch (IllegalArgumentException e) {
             // do nothing
         }
+    }
+
+    private boolean isPinOrPattern(int userId) {
+        int passwordQuality = mLockPatternUtils.getActivePasswordQuality(userId);
+        switch (passwordQuality) {
+            // PIN
+            case DevicePolicyManager.PASSWORD_QUALITY_NUMERIC:
+            case DevicePolicyManager.PASSWORD_QUALITY_NUMERIC_COMPLEX:
+            // Pattern
+            case DevicePolicyManager.PASSWORD_QUALITY_SOMETHING:
+                return true;
+        }
+
+        return false;
     }
 
     private class BurnInProtectionTask extends TimerTask {
