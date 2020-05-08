31fc8aff636197f1a531e053a9fb2587777e2fe4
Jake Weinstein <jake@aospa.co>
2020-05-09 00:56:24 +0200

    Revert "cgroup follow for procs in the same cgroup.procs"
    Needed for "Start process of next activity with top priority in advance"
    
    This reverts commit ed86c9073a0db72c9090aba3ff338cb1b7f5a35a.
    
    Change-Id: Ic97dbabf9cba14031d53ac6768ca0d66f06fbe35

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -36,7 +36,6 @@ import static android.os.Process.THREAD_GROUP_RESTRICTED;
 import static android.os.Process.THREAD_GROUP_TOP_APP;
 import static android.os.Process.THREAD_PRIORITY_DISPLAY;
 import static android.os.Process.setProcessGroup;
-import static android.os.Process.setCgroupProcsProcessGroup;
 import static android.os.Process.setThreadPriority;
 import static android.os.Process.setThreadScheduler;
 
@@ -171,9 +170,6 @@ public final class OomAdjuster {
     int mBServiceAppThreshold = 5;
     // Enable B-service aging propagation on memory pressure.
     boolean mEnableBServicePropagation = false;
-    // Process in same process Group keep in same cgroup
-    boolean mEnableProcessGroupCgroupFollow = false;
-    boolean mProcessGroupCgroupFollowDex2oatOnly = false;
 
     public static BoostFramework mPerf = new BoostFramework();
 
@@ -190,8 +186,6 @@ public final class OomAdjuster {
             mMinBServiceAgingTime = Integer.valueOf(mPerf.perfGetProp("ro.vendor.qti.sys.fw.bservice_age", "5000"));
             mBServiceAppThreshold = Integer.valueOf(mPerf.perfGetProp("ro.vendor.qti.sys.fw.bservice_limit", "5"));
             mEnableBServicePropagation = Boolean.parseBoolean(mPerf.perfGetProp("ro.vendor.qti.sys.fw.bservice_enable", "false"));
-            mEnableProcessGroupCgroupFollow = Boolean.parseBoolean(mPerf.perfGetProp("ro.vendor.qti.cgroup_follow.enable", "false"));
-            mProcessGroupCgroupFollowDex2oatOnly = Boolean.parseBoolean(mPerf.perfGetProp("ro.vendor.qti.cgroup_follow.dex2oat_only", "false"));
         }
 
         // The process group is usually critical to the response time of foreground app, so the
@@ -205,12 +199,7 @@ public final class OomAdjuster {
             final int pid = msg.arg1;
             final int group = msg.arg2;
             try {
-                if (mEnableProcessGroupCgroupFollow) {
-                    final int uid = ((Integer)msg.obj).intValue();
-                    setCgroupProcsProcessGroup(uid, pid, group, mProcessGroupCgroupFollowDex2oatOnly);
-                } else {
-                    setProcessGroup(pid, group);
-                }
+                setProcessGroup(pid, group);
             } catch (Exception e) {
                 if (DEBUG_ALL) {
                     Slog.w(TAG, "Failed setting process group of " + pid + " to " + group, e);
@@ -1827,7 +1816,7 @@ public final class OomAdjuster {
                         break;
                 }
                 mProcessGroupHandler.sendMessage(mProcessGroupHandler.obtainMessage(
-                        0 /* unused */, app.pid, processGroup, Integer.valueOf(app.info.uid)));
+                        0 /* unused */, app.pid, processGroup));
                 try {
                     if (curSchedGroup == ProcessList.SCHED_GROUP_TOP_APP) {
                         // do nothing if we already switched to RT
