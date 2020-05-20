02acde3371b2cff2dc347d9e7b05f0e9a1c2d232
rituj <ritujbeniwal@gmail.com>
2020-05-20 17:38:49 +0530

    FODCircleView: Reorder function calls and boost brightness via Display Manager always
    * onTouchEvent is enough to detect FOD event
    * boost brightness via DisplayManager irrespective of mShouldBoostBrightness
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Id9cf4fd5e502662e32b5eff1b524818f85b63ae7

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -46,7 +46,6 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.R;
 
 import vendor.pa.biometrics.fingerprint.inscreen.V1_0.IFingerprintInscreen;
-import vendor.pa.biometrics.fingerprint.inscreen.V1_0.IFingerprintInscreenCallback;
 
 import java.util.NoSuchElementException;
 import java.util.Timer;
@@ -58,7 +57,6 @@ public class FODCircleView extends ImageView {
     private final int mSize;
     private final int mDreamingMaxOffset;
     private final int mNavigationBarSize;
-    private final boolean mShouldBoostBrightness;
     private final Paint mPaintFingerprint = new Paint();
     private final WindowManager.LayoutParams mParams = new WindowManager.LayoutParams();
     private final WindowManager mWindowManager;
@@ -75,25 +73,13 @@ public class FODCircleView extends ImageView {
     private boolean mIsCircleShowing;
 
     private float mCurrentDimAmount = 0.0f;
+    private int mCurBrightness = -1;
 
     private Handler mHandler;
 
     private LockPatternUtils mLockPatternUtils;
 
     private Timer mBurnInProtectionTimer;
-
-    private IFingerprintInscreenCallback mFingerprintInscreenCallback =
-            new IFingerprintInscreenCallback.Stub() {
-        @Override
-        public void onFingerDown() {
-            mHandler.post(() -> showCircle());
-        }
-
-        @Override
-        public void onFingerUp() {
-            mHandler.post(() -> hideCircle());
-        }
-    };
 
     private KeyguardUpdateMonitor mUpdateMonitor;
 
@@ -114,7 +100,6 @@ public class FODCircleView extends ImageView {
         @Override
         public void onKeyguardBouncerChanged(boolean isBouncer) {
             mIsBouncer = isBouncer;
-
             if (mUpdateMonitor.isFingerprintDetectionRunning()) {
                 if (isPinOrPattern(mUpdateMonitor.getCurrentUser()) || !isBouncer) {
                     show();
@@ -161,7 +146,6 @@ public class FODCircleView extends ImageView {
         }
 
         try {
-            mShouldBoostBrightness = daemon.shouldBoostBrightness();
             mPositionX = daemon.getPositionX();
             mPositionY = daemon.getPositionY();
             mSize = daemon.getSize();
@@ -176,6 +160,7 @@ public class FODCircleView extends ImageView {
 
         mDisplayManager = context.getSystemService(DisplayManager.class);
         mWindowManager = context.getSystemService(WindowManager.class);
+        mLockPatternUtils = new LockPatternUtils(context);
 
         mNavigationBarSize = res.getDimensionPixelSize(R.dimen.navigation_bar_size);
 
@@ -202,8 +187,6 @@ public class FODCircleView extends ImageView {
 
         mUpdateMonitor = KeyguardUpdateMonitor.getInstance(context);
         mUpdateMonitor.registerCallback(mMonitorCallback);
-
-        mLockPatternUtils = new LockPatternUtils(context);
 
         getViewTreeObserver().addOnGlobalLayoutListener(() -> {
             float drawingDimAmount = mParams.dimAmount;
@@ -255,7 +238,6 @@ public class FODCircleView extends ImageView {
             try {
                 mFingerprintInscreenDaemon = IFingerprintInscreen.getService();
                 if (mFingerprintInscreenDaemon != null) {
-                    mFingerprintInscreenDaemon.setCallback(mFingerprintInscreenCallback);
                     mFingerprintInscreenDaemon.asBinder().linkToDeath((cookie) -> {
                         mFingerprintInscreenDaemon = null;
                     }, 0);
@@ -268,6 +250,7 @@ public class FODCircleView extends ImageView {
     }
 
     public void dispatchPress() {
+        mDisplayManager.setTemporaryBrightness(255);
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
         try {
             daemon.onPress();
@@ -318,10 +301,10 @@ public class FODCircleView extends ImageView {
     public void hideCircle() {
         mIsCircleShowing = false;
 
+        dispatchRelease();
+
         setImageResource(R.drawable.fod_icon_default);
         invalidate();
-
-        dispatchRelease();
 
         setDim(false);
         updateAlpha();
@@ -401,28 +384,23 @@ public class FODCircleView extends ImageView {
     }
 
     private void setDim(boolean dim) {
+        mCurBrightness = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.SCREEN_BRIGHTNESS, 100);
         if (dim) {
-            int curBrightness = Settings.System.getInt(getContext().getContentResolver(),
-                    Settings.System.SCREEN_BRIGHTNESS, 100);
             int dimAmount = 0;
 
             IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
             try {
-                dimAmount = daemon.getDimAmount(curBrightness);
+                dimAmount = daemon.getDimAmount(mCurBrightness);
             } catch (RemoteException e) {
                 // do nothing
             }
 
-            if (mShouldBoostBrightness) {
-                mDisplayManager.setTemporaryBrightness(255);
-            }
-
             mParams.dimAmount = dimAmount / 255.0f;
         } else {
-            mDisplayManager.setTemporaryBrightness(-1);
+            mDisplayManager.setTemporaryBrightness(mCurBrightness);
             mParams.dimAmount = 0.0f;
         }
-
         mWindowManager.updateViewLayout(this, mParams);
     }
 
