f915d1784e023813fec6d2e59340148e3a2aa289
ezio84 <brabus84@gmail.com>
2020-05-30 15:27:04 +0200

    Battery icon: fix requestLayout() calls spam when in lockscreen
    if percentage text is set outside the icon
    
    KeyguardStatusBarView.updateVisibilities calls setForceShowPercent,
    ending up calling updatePercentText
    updatePercentText method gets called in many cases in BatteryMeterView
    Actually i'm really busy so as a quick fix let's just skip all useless
    mBatteryPercentView.setText calls
    
    Change-Id: I8a8640a06d5f8ec1faeb126225c697ed8d898003

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -386,7 +386,7 @@ public class BatteryMeterView extends LinearLayout implements
             if (mShowPercentMode == MODE_ESTIMATE && !mCharging) {
                 mBatteryController.getEstimatedTimeRemainingString((String estimate) -> {
                     if (estimate != null) {
-                        mBatteryPercentView.setText(estimate);
+                        batteryPercentViewSetText(estimate);
                         setContentDescription(getContext().getString(
                                 R.string.accessibility_battery_level_with_estimate,
                                 mLevel, estimate));
@@ -410,7 +410,7 @@ public class BatteryMeterView extends LinearLayout implements
         String bolt = "\u26A1\uFE0E";
         CharSequence mChargeIndicator = mCharging && (mBatteryStyle == BATTERY_STYLE_HIDDEN)
                 ? (bolt + " ") : "";
-        mBatteryPercentView.setText(mChargeIndicator +
+        batteryPercentViewSetText(mChargeIndicator +
                 NumberFormat.getPercentInstance().format(mLevel / 100f));
         setContentDescription(
                 getContext().getString(mCharging ? R.string.accessibility_battery_level_charging
@@ -452,6 +452,13 @@ public class BatteryMeterView extends LinearLayout implements
             }
         }
         updatePercentText();
+    }
+
+    private void batteryPercentViewSetText(CharSequence text) {
+        CharSequence currentText = mBatteryPercentView.getText();
+        if (!currentText.toString().equals(text.toString())) {
+            mBatteryPercentView.setText(text);
+        }
     }
 
     @Override
