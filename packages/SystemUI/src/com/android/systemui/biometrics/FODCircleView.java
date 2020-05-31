802fa423d6e7456c75574bef060e7324f16529b4
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-06-01 01:31:22 +0300

    FODCircleView: Apply color filter to FOD background
    Change-Id: I5fbc23a9c8ce7c7446695c016e9659e0cd5d7214
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -29,6 +29,8 @@ import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
@@ -308,6 +310,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
     @Override
     protected void onDraw(Canvas canvas) {
         if (!mIsCircleShowing) {
+            mPaintFingerprintBackground.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
             canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprintBackground);
         }
         super.onDraw(canvas);
