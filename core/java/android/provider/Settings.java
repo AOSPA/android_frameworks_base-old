69f354a610ab9dcdd882d5bbc728a94180d1dc15
Mesquita <mickaelmendes50@gmail.com>
2020-07-22 16:19:15 +0000

    SystemUI: add FPS Info Overlay & Tile
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Co-authored-by: SKULSHADY <anushekprasal@gmail.com>
    Change-Id: I8a9afeec8a92af52b650314341cdd3887e025991

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14543,6 +14543,11 @@ public final class Settings {
         public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_FPS_OVERLAY = "show_fps_overlay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
