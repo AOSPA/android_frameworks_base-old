7ab5c75c95a623916f8447839b85ec6c4b90f2d6
rituj <ritujbeniwal@gmail.com>
2020-07-25 15:18:03 +0530

    base: Introduce automated DC Dimming
    * Schedule based on sunset/sunrise
    * Automate based on the average brightness
    * Automate based on both of the above options
    * Add QS Tile to control DC Dimming
    * Disable the service on devices with no DC dimming support
    * Set config_deviceDcDimmingSysfsNode to point to the relevant sysfs kernel node
    
    Co-authored-by: Andrew Fluck <andrew@aospa.co>
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Iaebf6624a8311722de3443c0ff5b033381749d20

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -5164,6 +5164,15 @@ public abstract class Context {
     public static final String DREAM_SERVICE = "dream";
 
     /**
+     * {@link android.hardware.display.DcDimManager} for accessing and setting locked apps state.
+     *
+     * @see #getSystemService
+     * @see android.hardware.display.DcDimmingManager
+     * @hide
+     */
+    public static final String DC_DIM_SERVICE = "dc_dim_service";
+
+    /**
      * Determine whether the given permission is allowed for a particular
      * process and user ID running in the system.
      *
