65acd846e0ce6307fb51d6ccc86f6ee8eb4d5b44
Dave Mankoff <mankoff@google.com>
2020-08-03 16:02:08 -0400

    [android11-d1-release] Alert ProxCheck listeners directly when timing out.
    Before this change, we were relying on the ProximitySensor class
    to circle back and tell the ProximityCheck class about null results.
    When the ProximitySensor class changed, this broke ProxCheck. The
    ProxCheck class now directly alerts its listeners when it times out.
    
    A test has been added to cover this case now.
    
    Fixes: 162205584
    Test: atest SystemUiTests && Manual
    Change-Id: Ie48589a54daae3e29defde9ed2300f3c7e2b88ae

--- packages/SystemUI/src/com/android/systemui/util/sensors/ProximitySensor.java
+++ packages/SystemUI/src/com/android/systemui/util/sensors/ProximitySensor.java
@@ -268,7 +268,7 @@ public class ProximitySensor {
         @Override
         public void run() {
             unregister();
-            mSensor.alertListeners();
+            onProximityEvent(null);
         }
 
         /**
