cdabb6c7145a2bc639507b9ef8e53ff64ae39ee4
rituj <ritujbeniwal@gmail.com>
2020-09-09 13:12:14 +0530

    SystemUI: Only listen for Quick pulldown gesture when the statusbar is collapsed
    * Fixes notification views not scrolling when this gesture is enabled
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Ifcb88baa83feb52efcc0c49a57739fe39921b592

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2122,8 +2122,8 @@ public class NotificationPanelView extends PanelView implements
 
         final float w = getMeasuredWidth();
         float region = (w * (1.f/4.f)); // TODO overlay region fraction?
-        final boolean showQsOverride = (isLayoutRtl() ? (x < region) : (w - region < x))
-            && !mKeyguardShowing && SettingConfirmationHelper.get(
+        final boolean showQsOverride = isFullyCollapsed() && (isLayoutRtl() ? (x < region)
+            : (w - region < x)) && !mKeyguardShowing && SettingConfirmationHelper.get(
                     getContext().getContentResolver(),
                     Settings.Secure.QUICK_SETTINGS_QUICK_PULL_DOWN,
                     notSetFallback);
