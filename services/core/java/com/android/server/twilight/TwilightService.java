9fc0afbe11afc635514230a3d31bdec506de63d1
Mesquita <mickaelmendes50@gmail.com>
2020-09-13 16:27:30 -0300

    TwilightService: Fix typo
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Change-Id: Ie595cf43ea22c9e91946b8d2f0b47d3e67c65b8b

--- services/core/java/com/android/server/twilight/TwilightService.java
+++ services/core/java/com/android/server/twilight/TwilightService.java
@@ -279,7 +279,7 @@ public final class TwilightService extends SystemService
      */
     private static TwilightState calculateTwilightState(Location location, long timeMillis) {
         if (location == null) {
-            return getManualTwilightState();
+            return getManualTwilightState(timeMillis);
         }
 
         final CalendarAstronomer ca = new CalendarAstronomer(
@@ -309,7 +309,7 @@ public final class TwilightService extends SystemService
         return new TwilightState(sunriseTimeMillis, sunsetTimeMillis);
     }
 
-    private static TwilightState getManualTwilightState() {
+    private static TwilightState getManualTwilightState(long timeMillis) {
         final Calendar sunrise = Calendar.getInstance();
         sunrise.setTimeInMillis(timeMillis);
         sunrise.set(Calendar.HOUR_OF_DAY, 5);
