ae3b33df81fab8fd5dbbc4ebe1934fd1b81dce5b
Christian Oder <myself5@carbonrom.org>
2020-09-15 10:41:02 -0300

    base: Introduce full circle battery style
    * Design based on "PA Battery" entirely rewritten in Kotlin with design adjustments
    
    @mickaelmendes50 : override the old icon
    
    Co-authored-by: TheStrix <parthbhatia98@gmail.com>
    Change-Id: Ic1b74a07d645c0ccedf7015303755f6fe7a3a923

--- packages/SettingsLib/src/com/android/settingslib/graph/CircleBatteryDrawable.kt
+++ packages/SettingsLib/src/com/android/settingslib/graph/CircleBatteryDrawable.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
  * Copyright (C) 2019 The LineageOS Project
- * Copyright (C) 2020 Paranoid Android
+ * Copyright (C) 2020 CarbonROM
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,11 @@
  */
 package com.android.settingslib.graph
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.*
 import android.graphics.drawable.Drawable
 import android.util.TypedValue
@@ -26,34 +30,27 @@ import com.android.settingslib.Utils
 import kotlin.math.min
 
 class CircleBatteryDrawable(private val context: Context, frameColor: Int) : Drawable() {
-    private val backPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = frameColor
-        isDither = true
-    }
-    private val frontPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply { isDither = true }
-    private val chargingPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply { style = Paint.Style.FILL }
-    private val plusPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = Utils.getColorStateListDefaultColor(context, R.color.batterymeter_plus_color)
-    }
-    private val powerSavePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = plusPaint.color
-        style = Paint.Style.STROKE
-    }
+    private val criticalLevel: Int
+    private val warningString: String
+    private val framePaint: Paint
+    private val batteryPaint: Paint
+    private val textPaint: Paint
+    private val powerSavePaint: Paint
     private val colors: IntArray
     private val padding = Rect()
     private val frame = RectF()
 
-    private var chargeColor =
-        Utils.getColorStateListDefaultColor(context, R.color.meter_consumed_color)
+    private var chargeColor: Int
     private var iconTint = Color.WHITE
-    private var intrinsicWidth = context.resources.getDimensionPixelSize(R.dimen.battery_width)
-    private var intrinsicHeight = context.resources.getDimensionPixelSize(R.dimen.battery_height)
+    private var intrinsicWidth: Int
+    private var intrinsicHeight: Int
     private var height = 0
     private var width = 0
+    private var chargingAnimator: ValueAnimator? = null
+    private var batteryAlpha: Int
 
     // Dual tone implies that battery level is a clipped overlay over top of the whole shape
-    private val dualTone =
-        context.resources.getBoolean(com.android.internal.R.bool.config_batterymeterDualTone)
+    private var dualTone = false
 
     override fun getIntrinsicHeight() = intrinsicHeight
 
@@ -61,11 +58,25 @@ class CircleBatteryDrawable(private val context: Context, frameColor: Int) : Dra
 
     var charging = false
         set(value) {
+            val previousCharging = charging
+            field = value
+            if (value) {
+                if (!previousCharging) {
+                    startChargingAnimation(ValueAnimator.INFINITE)
+                }
+            } else {
+                cancelChargingAnimation()
+                postInvalidate()
+            }
+        }
+
+    var powerSaveEnabled = false
+        set(value) {
             field = value
             postInvalidate()
         }
 
-    var powerSaveEnabled = false
+    var showPercent = false
         set(value) {
             field = value
             postInvalidate()
@@ -76,6 +87,39 @@ class CircleBatteryDrawable(private val context: Context, frameColor: Int) : Dra
             field = value
             postInvalidate()
         }
+
+    private fun startChargingAnimation(repeat: Int) {
+        val alpha = batteryPaint.alpha
+        chargingAnimator = ValueAnimator.ofInt(alpha, 0, alpha)
+        chargingAnimator?.addUpdateListener {
+            batteryAlpha = it.animatedValue as Int
+            postInvalidate()
+        }
+
+        chargingAnimator?.addListener(object: AnimatorListenerAdapter() {
+            override fun onAnimationCancel(animation: Animator) {
+                super.onAnimationCancel(animation)
+                batteryAlpha = alpha
+                postInvalidate()
+                onAnimationEnd(animation)
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                chargingAnimator = null
+            }
+        })
+        chargingAnimator?.repeatCount = repeat
+        chargingAnimator?.duration = 4000
+        chargingAnimator?.startDelay = 500
+        chargingAnimator?.start()
+    }
+
+    private fun cancelChargingAnimation() {
+        if (chargingAnimator != null) {
+            chargingAnimator?.cancel()
+        }
+    }
 
     // an approximation of View.postInvalidate()
     private fun postInvalidate() {
@@ -94,6 +138,7 @@ class CircleBatteryDrawable(private val context: Context, frameColor: Int) : Dra
         width = bounds.right - padding.right - (bounds.left + padding.left)
         intrinsicHeight = res.getDimensionPixelSize(R.dimen.battery_height)
         intrinsicWidth = res.getDimensionPixelSize(R.dimen.battery_height)
+        textPaint.textSize = height * 0.7f
     }
 
     override fun getPadding(padding: Rect): Boolean {
@@ -136,70 +181,137 @@ class CircleBatteryDrawable(private val context: Context, frameColor: Int) : Dra
 
     fun setColors(fgColor: Int, bgColor: Int, singleToneColor: Int) {
         val fillColor = if (dualTone) fgColor else singleToneColor
+
         iconTint = fillColor
-        backPaint.color = bgColor
+        framePaint.color = bgColor
         chargeColor = fillColor
+
         invalidateSelf()
     }
 
     override fun draw(c: Canvas) {
         if (batteryLevel == -1) return
+
+        val strokeWidth = powerSavePaint.strokeWidth
         val circleSize = min(width, height)
-        val strokeWidth = circleSize / 6.5f
-        backPaint.strokeWidth = strokeWidth
-        backPaint.style = Paint.Style.STROKE
-        frontPaint.strokeWidth = strokeWidth
-        frontPaint.style = Paint.Style.STROKE
-        powerSavePaint.strokeWidth = strokeWidth
+        var circleRadius = (circleSize / 2f)
+        var drawFrac = batteryLevel / 100f
+
+        if (!charging && powerSaveEnabled) {
+            circleRadius -= strokeWidth / 2.0f
+        }
+
+        framePaint.strokeWidth = 0f
+        framePaint.style = Paint.Style.FILL_AND_STROKE
+        batteryPaint.strokeWidth = 0f
+        batteryPaint.style = Paint.Style.FILL_AND_STROKE
         frame[
                 strokeWidth / 2.0f + padding.left, strokeWidth / 2.0f,
                 circleSize - strokeWidth / 2.0f + padding.left
         ] = circleSize - strokeWidth / 2.0f
-        frontPaint.color = batteryColorForLevel(batteryLevel)
-        chargingPaint.color = batteryColorForLevel(batteryLevel)
-        if (charging) {
-            val cf = (frame.right - frame.left) / 4.0f
-            c.drawCircle(frame.centerX(), frame.centerY(), cf, chargingPaint)
-        }
-        c.drawArc(frame, 270f, 360f, false, backPaint)
-        if (batteryLevel > 0) {
-            if (!charging && powerSaveEnabled) {
-                c.drawArc(frame, 270f, 3.6f * batteryLevel, false, powerSavePaint)
+        // set the battery charging color
+        batteryPaint.color = batteryColorForLevel(batteryLevel)
+        if (chargingAnimator != null) {
+            if (batteryLevel == 100) {
+                cancelChargingAnimation()
             } else {
-                c.drawArc(frame, 270f, 3.6f * batteryLevel, false, frontPaint)
+                batteryPaint.alpha = batteryAlpha
             }
         }
+
+        if (batteryLevel <= criticalLevel) {
+            drawFrac = 0f
+        }
+
+        // draw outer circle first
+        c.drawCircle(frame.centerX(), frame.centerY(), circleRadius, framePaint)
+
+        c.save()
+        // compute percentage text
+        if (batteryLevel != 100 && showPercent) {
+            val textHeight = -textPaint.fontMetrics.ascent
+            val pctText =
+                if (batteryLevel > criticalLevel)
+                    batteryLevel.toString()
+                else
+                    warningString
+            val pctY = (height + textHeight) * 0.45f
+            textPaint.color = batteryColorForLevel(batteryLevel)
+            c.drawText(pctText, frame.centerX(), pctY, textPaint)
+            var textPath = Path()
+            textPaint.getTextPath(pctText, 0, pctText.length, frame.centerX(),
+                pctY, textPath)
+            c.clipOutPath(textPath)
+        }
+
+        // draw colored circle representing charge level
+        if (drawFrac != 0f) {
+            c.drawCircle(frame.centerX(), frame.centerY(), circleRadius
+                * drawFrac, batteryPaint)
+        }
+
+        if (!charging && powerSaveEnabled) {
+            c.drawCircle(frame.centerX(), frame.centerY(), circleRadius,
+                powerSavePaint)
+        }
+        c.restore()
     }
 
     // Some stuff required by Drawable.
     override fun setAlpha(alpha: Int) {}
 
     override fun setColorFilter(colorFilter: ColorFilter?) {
-        backPaint.colorFilter = colorFilter
-        frontPaint.colorFilter = colorFilter
-        chargingPaint.colorFilter = colorFilter
-        plusPaint.colorFilter = colorFilter
+        framePaint.colorFilter = colorFilter
+        batteryPaint.colorFilter = colorFilter
+        textPaint.colorFilter = colorFilter
     }
 
     override fun getOpacity() = PixelFormat.UNKNOWN
 
     init {
         val res = context.resources
-        val colorLevels = res.obtainTypedArray(R.array.batterymeter_color_levels)
-        val colorValues = res.obtainTypedArray(R.array.batterymeter_color_values)
-        colors = IntArray(2 * colorLevels.length())
-        for (i in 0 until colorLevels.length()) {
-            colors[2 * i] = colorLevels.getInt(i, 0)
-            if (colorValues.getType(i) == TypedValue.TYPE_ATTRIBUTE) {
+        val color_levels = res.obtainTypedArray(R.array.batterymeter_color_levels)
+        val color_values = res.obtainTypedArray(R.array.batterymeter_color_values)
+        colors = IntArray(2 * color_levels.length())
+        for (i in 0 until color_levels.length()) {
+            colors[2 * i] = color_levels.getInt(i, 0)
+            if (color_values.getType(i) == TypedValue.TYPE_ATTRIBUTE) {
                 colors[2 * i + 1] = Utils.getColorAttrDefaultColor(
                     context,
-                    colorValues.getThemeAttributeId(i, 0)
+                    color_values.getThemeAttributeId(i, 0)
                 )
             } else {
-                colors[2 * i + 1] = colorValues.getColor(i, 0)
+                colors[2 * i + 1] = color_values.getColor(i, 0)
             }
         }
-        colorLevels.recycle()
-        colorValues.recycle()
+        color_levels.recycle()
+        color_values.recycle()
+        warningString = res.getString(R.string.battery_meter_very_low_overlay_symbol)
+        criticalLevel = res.getInteger(
+            com.android.internal.R.integer.config_criticalBatteryWarningLevel
+        )
+        framePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        framePaint.color = frameColor
+        framePaint.isDither = true
+        batteryPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        batteryPaint.isDither = true
+        batteryAlpha = batteryPaint.alpha
+        textPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        textPaint.typeface = Typeface.create("sans-serif-condensed", Typeface.BOLD)
+        textPaint.textAlign = Paint.Align.CENTER
+        textPaint.strokeWidth = 2f
+        textPaint.style = Paint.Style.STROKE
+        chargeColor = Utils.getColorStateListDefaultColor(context, R.color.meter_consumed_color)
+        powerSavePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        powerSavePaint.color = Utils.getColorStateListDefaultColor(
+            context,
+            R.color.batterymeter_plus_color
+        )
+        powerSavePaint.style = Paint.Style.STROKE
+        powerSavePaint.strokeWidth = 3f
+        intrinsicWidth = res.getDimensionPixelSize(R.dimen.battery_width)
+        intrinsicHeight = res.getDimensionPixelSize(R.dimen.battery_height)
+
+        dualTone = res.getBoolean(com.android.internal.R.bool.config_batterymeterDualTone)
     }
 }
