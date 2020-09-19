c3d4431a724f08f13c7bd42cd2baeff58628a5ae
Danny Lin <danny@kdrag0n.dev>
2020-09-19 00:52:35 -0700

    overlays: Build 2-button navigation bar overlay
    There's no reason not to build this and give users the choice to use it
    if it's still supported.
    
    Change-Id: I2650930814d2999d1758706908b4c827aeb8b261

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -51,6 +51,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconShapeTaperedRectOverlay \
 	IconShapeTeardropOverlay \
 	IconShapeVesselOverlay \
+	NavigationBarMode2ButtonOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
 	NavigationBarModeGesturalOverlayNarrowBack \
