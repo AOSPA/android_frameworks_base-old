f6ee3058107554ec4848052626693660ea652420
Evan Anderson <evananderson@aospa.co>
2020-09-22 20:59:54 -0500

    core: Remove @SystemApi annotation for VOLUME_HUSH_CYCLE.
      This addition should not be annotated with @SystemApi because it
      creates an addition that fails the API test in user builds
    
    Change-Id: I9f3e88eeae1c341bd431128db255c9214a852b15
    (cherry picked from commit 5780cbc037c06b0c4208715d0da01862dae423f6)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9922,7 +9922,6 @@ public final class Settings {
         @SystemApi
         public static final int VOLUME_HUSH_MUTE = 2;
         /** @hide */
-        @SystemApi
         public static final int VOLUME_HUSH_CYCLE = 3;
 
         /**
