f4af0f6ea226f7907f127eff7c631065de24eae3
Chenyang Zhong <zhongcy95@gmail.com>
2020-09-27 21:17:32 +0700

    SystemUI: Set correct animation duration for linear brightness slider
    The log scale slider uses GAMMA_SPACE_MAX as the slider max value, which
    is usually 4 times the max value of linear brightness slider (1023 to
    255). Slider animation duration depends on the slider max, so use the
    proper value for each case.
    
    Change-Id: Ia150840a5498a94572417e6565b5ec792af619dc

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -88,6 +88,7 @@ public class BrightnessController implements ToggleSlider.Listener {
     private boolean mListening;
     private boolean mExternalChange;
     private boolean mControlValueInitialized;
+    private int mLinearSliderMax;
 
     private ValueAnimator mSliderAnimator;
 
@@ -479,7 +480,8 @@ public class BrightnessController implements ToggleSlider.Listener {
             mControl.setMax(GAMMA_SPACE_MAX);
             animateSliderTo(sliderVal);
         } else {
-            mControl.setMax(max - min);
+            mLinearSliderMax = max - min;
+            mControl.setMax(mLinearSliderMax);
             animateSliderTo(val - min);
         }
     }
@@ -500,7 +502,7 @@ public class BrightnessController implements ToggleSlider.Listener {
             mExternalChange = false;
         });
         final long animationDuration = SLIDER_ANIMATION_DURATION * Math.abs(
-                mControl.getValue() - target) / GAMMA_SPACE_MAX;
+                mControl.getValue() - target) / (mLinearBrightnessSlider ? mLinearSliderMax : GAMMA_SPACE_MAX);
         mSliderAnimator.setDuration(animationDuration);
         mSliderAnimator.start();
     }
