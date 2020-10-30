a7bc0913184dd3797e058f7281232dfe503a5495
Jyotiraditya <jyotiraditya@aospa.co>
2020-10-30 11:30:59 -0300

    [1/2] SystemUI: Introduce Adaptive Playback
    Automatically pause media when muted. Resume if unmuted within a minute.
    
    Co-Authored-By: Stylogey <stylogey@gmail.com>
    Change-Id: I5d37478b3739309dcaae4eb6a0e4aac6c87f120f
    Signed-off-by: Jyotiraditya <jyotiraditya@aospa.co>

--- services/core/java/com/android/server/media/MediaSessionService.java
+++ services/core/java/com/android/server/media/MediaSessionService.java
@@ -138,6 +138,7 @@ public class MediaSessionService extends SystemService implements Monitor {
     private ContentResolver mContentResolver;
     private SettingsObserver mSettingsObserver;
     private boolean mHasFeatureLeanback;
+    private boolean mAdaptivePlayback;
 
     // The FullUserRecord of the current users. (i.e. The foreground user that isn't a profile)
     // It's always not null after the MediaSessionService is started.
@@ -195,6 +196,9 @@ public class MediaSessionService extends SystemService implements Monitor {
         mSettingsObserver.observe();
         mHasFeatureLeanback = mContext.getPackageManager().hasSystemFeature(
                 PackageManager.FEATURE_LEANBACK);
+        mAdaptivePlayback = Settings.System.getIntForUser(mContentResolver,
+                Settings.System.ADAPTIVE_PLAYBACK_ENABLED,
+                0, UserHandle.USER_CURRENT) == 1;
 
         updateUser();
 
@@ -1091,10 +1095,16 @@ public class MediaSessionService extends SystemService implements Monitor {
         private void observe() {
             mContentResolver.registerContentObserver(mSecureSettingsUri,
                     false, this, USER_ALL);
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.ADAPTIVE_PLAYBACK_ENABLED), false, this,
+                    UserHandle.USER_ALL);
         }
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
+            mAdaptivePlayback = Settings.System.getIntForUser(mContentResolver,
+                            Settings.System.ADAPTIVE_PLAYBACK_ENABLED,
+                            0, UserHandle.USER_CURRENT) == 1;
             updateActiveSessionListeners();
         }
     }
@@ -2045,7 +2055,7 @@ public class MediaSessionService extends SystemService implements Monitor {
                             + ". flags=" + flags + ", preferSuggestedStream="
                             + preferSuggestedStream + ", session=" + session);
                 }
-                if ((flags & AudioManager.FLAG_ACTIVE_MEDIA_ONLY) != 0
+                if ((flags & AudioManager.FLAG_ACTIVE_MEDIA_ONLY) != 0 && !mAdaptivePlayback
                         && !AudioSystem.isStreamActive(AudioManager.STREAM_MUSIC, 0)) {
                     if (DEBUG_KEY_EVENT) {
                         Log.d(TAG, "Nothing is playing on the music stream. Skipping volume event,"
