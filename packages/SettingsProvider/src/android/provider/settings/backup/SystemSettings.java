c0fb4d306d7a3a66df16c965e050dd717b42a2a3
kldoc <fred.berbigier@gmail.com>
2020-11-05 11:21:50 -0300

    SystemUI: add Pulse and Ambient notification bars [1/2]
    [maxwen] SystemUI: support using notification color for pulse light
    
    optional use the color information of the notification
    as color for notification pulse light
    
    @brituj
    * Update color when a new notification arrives while pulsing
    * Fix row == null when on lockscreen even though the notification is clearable
    * other code improvements
    
    @mickaelmendes50
    * Adapt to Android 11
    
    -------------------------------------------------------------------
    [SQUASH]
    Add Pulse and Ambient Display bars for notifications
    
    Change-Id: Ic5fdbdf05ef3f836e0c42ecef30e791a1f7eeb6e
    
    SystemUI: trigger pulse light only for notifications
    
    store the doze trigger reason in a setting to check later
    if it has been triggered for a notification or any other reason
    
    Change-Id: Iecee97b04e5e943c766fb4794d89e036ded65b3a
    
    SystemUI: Pulse light accent color option
    
    Change-Id: If402e96d7549aff7f758236e6f54a6034d08f9c2
    
    SystemUI: use ROWS_HIGH_PRIORITY for active notification pulse check
    
    only one of those could make pulse happen anyway
    
    Change-Id: I002344f321bdfed770f1099d7d33edc35671d997
    
    SystemUI: replace pulse light drawable with our own vector
    --------------------------------------------------------------------
    
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: I95756d9886b2f4120d5ece72418850b100fe7394

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -83,6 +83,8 @@ public class SystemSettings {
         Settings.System.HAPTIC_FEEDBACK_INTENSITY,
         Settings.System.DISPLAY_COLOR_MODE,
         Settings.System.ALARM_ALERT,
+        Settings.System.AMBIENT_NOTIFICATION_LIGHT,
+        Settings.System.AMBIENT_NOTIFICATION_LIGHT_AUTOMATIC,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
     };
 }
