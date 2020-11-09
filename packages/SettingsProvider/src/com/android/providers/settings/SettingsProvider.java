a08b8d48e0b69f71f4d5c4cd146622d266658722
Kra1o5 <kra1o5x@gmail.com>
2020-11-09 15:57:18 +0100

    base: Introduce Immersive Navigation [1/3]
    Based in Hide gesture navbar from Syberia project
    
    [ghostrider-reborn: updated for android 12]
    
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: Víctor Santoja <victorsantoja@gmail.com>
    Change-Id: I871918779f012d6da5bcc9515fc5ff26a880ed67
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -239,6 +239,8 @@ public class SettingsProvider extends ContentProvider {
     private static final Set<String> OVERLAY_ALLOWED_SYSTEM_INSTANT_APP_SETTINGS = new ArraySet<>();
     private static final Set<String> OVERLAY_ALLOWED_SECURE_INSTANT_APP_SETTINGS = new ArraySet<>();
 
+    private static final String NAV_MODE_IMMERSIVE_OVERLAY = "co.aospa.overlay.systemui.immnav.gestural";
+
     static {
         for (String name : Resources.getSystem().getStringArray(
                 com.android.internal.R.array.config_allowedGlobalInstantAppSettings)) {
@@ -5605,11 +5607,17 @@ public class SettingsProvider extends ContentProvider {
             try {
                 final IOverlayManager om = IOverlayManager.Stub.asInterface(
                         ServiceManager.getService(Context.OVERLAY_SERVICE));
-                final OverlayInfo info = om.getOverlayInfo(NAV_BAR_MODE_GESTURAL_OVERLAY, userId);
+                final Setting immNavSetting = secureSettings.getSettingLocked(
+                        Settings.Secure.IMMERSIVE_NAVIGATION);
+                final boolean immNavEnabled = !immNavSetting.isNull() &&
+                        immNavSetting.getValue().equals("1");
+                final OverlayInfo info = om.getOverlayInfo(immNavEnabled ?
+                        NAV_MODE_IMMERSIVE_OVERLAY : NAV_BAR_MODE_GESTURAL_OVERLAY, userId);
                 if (info != null && !info.isEnabled()) {
                     final int curInset = getContext().getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.config_backGestureInset);
-                    om.setEnabledExclusiveInCategory(NAV_BAR_MODE_GESTURAL_OVERLAY, userId);
+                    om.setEnabledExclusiveInCategory(immNavEnabled ? NAV_MODE_IMMERSIVE_OVERLAY :
+                        NAV_BAR_MODE_GESTURAL_OVERLAY, userId);
                     final int defInset = getContext().getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.config_backGestureInset);
 
