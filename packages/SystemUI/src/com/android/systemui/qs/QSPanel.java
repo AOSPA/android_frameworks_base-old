681a85c7ceaf8ab130149df113854162df300bc6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-14 01:21:41 +0530

    SystemUI: Fixes for dynamic QS panel rows
     * Fixes https://github.com/AOSPA/android_frameworks_base/commit/ddcb8e67b841e4d5db4c7e294d3efa6ea670ca7e
    
     * Fix the following issues:
     
       1) On changing any SystemUI overlay such as accent or icon pack when media player is
          inactive, QS panel resets back to 3x2 size and stays there.
    
       2) Similarly when media player is active and theme is altered, QS panel gets stuck
          at 3x3 with the media player at bottom, making it look ugly.
    
    Change-Id: I21beeddbc700af0eaa88085ce4566a296be219d8

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -214,10 +214,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     }
 
     protected void onMediaVisibilityChanged(Boolean visible) {
-        switchTileLayout();
-        if (getTileLayout() != null) {
-            getTileLayout().setMinRows(visible ? 2 : 3);
-        }
+        switchTileLayout(true);
         if (mMediaVisibilityChangedListener != null) {
             mMediaVisibilityChangedListener.accept(visible);
         }
@@ -519,7 +516,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
         if (newConfig.orientation != mLastOrientation) {
             mLastOrientation = newConfig.orientation;
-            switchTileLayout();
+            switchTileLayout(true);
         }
     }
 
@@ -572,7 +569,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             if (mHost != null) setTiles(mHost.getTiles());
             newLayout.setListening(mListening);
             if (needsDynamicRowsAndColumns()) {
-                newLayout.setMinRows(horizontal ? 2 : 1);
+                boolean isLandscape = getResources().getConfiguration().orientation
+                            == Configuration.ORIENTATION_LANDSCAPE;
+                newLayout.setMinRows(horizontal ? 2 : (mMediaHost.getVisible() ? 2 : (isLandscape ? 1 : 3)));
                 // Let's use 3 columns to match the current layout
                 newLayout.setMaxColumns(horizontal ? 3 : TileLayout.NO_MAX_COLUMNS);
             }
