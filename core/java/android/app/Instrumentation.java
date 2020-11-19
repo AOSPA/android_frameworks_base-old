dc2940c18ce0acbc5a90f22361d1bb7a6a4783d3
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-19 20:46:32 -0300

    base: Introduce PixelPropsUtils
    That will spoof build fingerprints on some g00gle apps
    
    * Also will enable some cool stuffs like:
      - Dynamic navbar on GBoard
      - SafetyHub
    
    and others.
    
    Thanks to @kdrag0n for the original idea at https://github.com/ProtonAOSP/android_frameworks_base/commit/5a54bfd846c7a26ba4820a361a6fec779edf8c5a
    
    A12 Edition - also includes:
    PixelPropsUtils: Use Pixel 6 props
    PixelPropsUtils: Rewrite logic
    
    - Unlocks new smartspace in Pixel Launcher,
      monet in gapps and more
    
    Squashed:
    
    commit a99982c18c7124a8baa2ea85573afb11ba09fa5b
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Thu Dec 23 21:33:19 2021 +0530
    
        PixelPropsUtils: Use redfin props for some apps
    
        Based on https://github.com/PixelExperience/frameworks_base/commit/700a05483c244fa39cf732b7c19f75bf81ad24a1
    
        Co-authored-by: jhenrique09 <jhsv09@gmail.com>
        Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    commit 400155f3ca62bca66092a17ea47b781fbe414cf6
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Sun Dec 19 21:02:28 2021 +0530
    
        PixelPropsUtils: Update raven fp from december release
    
    commit 780c1b64fd28c46cfd6248e51c019dd5f64ba8c5
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Sat Nov 27 16:53:23 2021 +0530
    
        PixelPropsUtils: Avoid spoofing props for gcam and mods
    
    commit 73ce877f8cdec22c3c8ebe9180483b554daa0b6e
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Wed Nov 3 00:17:01 2021 +0530
    
        PixelPropsUtils: Update raven fp from november release
    
    commit 305df6dc539dd71597a9bbb49ecdcf70af3b9c56
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Fri Oct 29 16:49:03 2021 +0530
    
        PixelPropsUtils: Switch to raven release-keys fingerprint
    
    commit 276f9ce9771b3f95db386b026789b20221948fda
    Author: jhenrique09 <jhsv09@gmail.com>
    Date:   Sat Oct 16 12:18:09 2021 -0300
    
        PixelPropsUtils: Set userdebug as Build.TYPE for GMS
    
        Fixes force close on google apps
    
        10-15 07:44:52.294  8041  8252 E AndroidRuntime: FATAL EXCEPTION: DG
        10-15 07:44:52.294  8041  8252 E AndroidRuntime: Process: com.google.android.youtube, PID: 8041
        10-15 07:44:52.294  8041  8252 E AndroidRuntime: java.lang.SecurityException: GoogleCertificatesRslt: Package signed with debug key (go/gsrlt)
    
        [ghostrider-reborn]
        - Use propsToKeep
        - Move setting GMS-specific props out of for loop
    
        Change-Id: Idb11a2f032ebe6d162363b0a644ad7ab6d37393a
        Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    commit dcdac09df7bae72253021957f15e2ee06c1a128f
    Author: Ratoriku <a1063021545@gmail.com>
    Date:   Sat Oct 23 18:01:56 2021 +0800
    
        PixelPropsUtils: Add chrome stable into extra packages list
    
        Signed-off-by: Ratoriku <a1063021545@gmail.com>
        Change-Id: I09413e803287f00fbb56924edf6c73f9286d9cc7
    
    commit 36541ff8a52c23c06f5316a221f2f011df4d7236
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Thu Oct 28 00:33:25 2021 +0530
    
        PixelPropsUtils: Drop raven fingerprint & spoof build type
    
        Fixes google sign-in in various apps
        Thanks to Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
    
    commit 3875adf4828c46e30c08ddfd50cc88e00be141a9
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Sat Oct 23 00:28:53 2021 +0530
    
        PixelPropsUtils: Use walleye fp for GMS
    
         * required for passing safetynet
    
    Change-Id: I1078e7402833fec77edb751070c5144d08c85b6c
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -56,6 +56,7 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
+import com.android.internal.util.PixelPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1177,6 +1178,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
     
@@ -1194,6 +1196,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
 
