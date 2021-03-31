4b20f295914e4c6a46c98f0d924d260081b0227d
James Mattis <jmattis@google.com>
2021-03-31 13:57:52 -0700

    Calling linkToDeath on binder in NRI constructor
    Any NRI constructor that can possibly have a binder should call
    linkToDeath on that binder if present. Not doing so can result in a
    no such element exception when that NRI is removed.
    
    Bug: 184155022
    Test: atest FrameworksNetTests
    atest FrameworksNetIntegrationTests
    atest CtsNetTestCasesLatestSdk
    
    Change-Id: I90d594e43474483c554d0d315ff7abb6f678e093
    (cherry picked from commit 088550048864aa836e9411acc28ae8f8fed28a03)

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -5586,12 +5586,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
             incrementRequestCountOrThrow(this);
             mCallbackFlags = callbackFlags;
             mCallingAttributionTag = callingAttributionTag;
-
-            try {
-                mBinder.linkToDeath(this, 0);
-            } catch (RemoteException e) {
-                binderDied();
-            }
+            linkDeathRecipient();
         }
 
         NetworkRequestInfo(@NonNull final NetworkRequestInfo nri,
@@ -5629,6 +5624,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
             incrementRequestCountOrThrow(this);
             mCallbackFlags = nri.mCallbackFlags;
             mCallingAttributionTag = nri.mCallingAttributionTag;
+            linkDeathRecipient();
         }
 
         NetworkRequestInfo(int asUid, @NonNull final NetworkRequest r) {
@@ -5657,8 +5653,18 @@ public class ConnectivityService extends IConnectivityManager.Stub
             return Collections.unmodifiableList(tempRequests);
         }
 
+        void linkDeathRecipient() {
+            if (null != mBinder) {
+                try {
+                    mBinder.linkToDeath(this, 0);
+                } catch (RemoteException e) {
+                    binderDied();
+                }
+            }
+        }
+
         void unlinkDeathRecipient() {
-            if (mBinder != null) {
+            if (null != mBinder) {
                 mBinder.unlinkToDeath(this, 0);
             }
         }
