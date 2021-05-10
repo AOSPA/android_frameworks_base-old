40c1cc37f82255f2923d917a7babf40d0387f07a
Rituj Beniwal <ritujbeniwal@gmail.com>
2021-05-10 13:04:00 +0530

    SystemUI: Move brightness slider to the bottom
    * Move the complete implementation to QS Container
    * Make it accessible on quick QS panel
    * Add auto-brightness button
    
    Using changes from the following commits:
    
    SystemUI: move brightness to bottom and show in quick qs
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I5738e9c632407f31fb3c3c7726758b24b27a753c
    
    SystemUI: OxygenOS 11 QS layout
    
    also shrink qs media player layout a bit to save some space
    
    imjyotiraditya for brightness slider icons stuff
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Change-Id: I0b9ae8e2326ab9b246ea7fa414bc6c70c0c6a084
    Signed-off-by: Rituj Beniwal <ritujbeniwal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessDialog.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessDialog.java
@@ -58,8 +58,8 @@ public class BrightnessDialog extends Activity {
                 R.layout.quick_settings_brightness_dialog, null);
         setContentView(v);
 
-        final ToggleSliderView slider = findViewById(R.id.brightness_slider);
-        mBrightnessController = new BrightnessController(this, slider, mBroadcastDispatcher);
+        mBrightnessController = new BrightnessController(this, findViewById(R.id.brightness_icon),
+                findViewById(R.id.brightness_slider), mBroadcastDispatcher);
     }
 
     @Override
