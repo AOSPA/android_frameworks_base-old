7b0e811b614184bccfde2971bfe87afe81f71d0d
Rituj Beniwal <ritujbeniwal@gmail.com>
2021-06-08 13:09:17 +0530

    base: Add ringer vibration patterns from OOS [1/3]
    Signed-off-by: Rituj Beniwal <ritujbeniwal@gmail.com>
    Change-Id: I8e57aa96f8c1f3edd7db04188aa2acdfe7507ea5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5699,6 +5699,17 @@ public final class Settings {
         public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
 
         /**
+         * Which Vibration Pattern to use
+         * 0: dzzz-dzzz
+         * 1: dzzz-da
+         * 2: mm-mm-mm
+         * 3: da-da-dzzz
+         * 4: da-dzzz-da
+         * @hide
+         */
+        public static final String RINGTONE_VIBRATION_PATTERN = "ringtone_vibration_pattern";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
