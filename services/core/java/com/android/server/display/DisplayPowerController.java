463fa4ae849bfafd139398cd4030a6ec3c8cad67
Michael Wright <michaelwr@google.com>
2021-07-07 15:04:59 +0100

    Use exact brightnesses values for comparison.
    Using BrightnessSynchronizer introduced an epsilon value in the
    comparison. This meant it was possible for brightness changes to never
    reach min or max if they were made in sufficiently small steps. Given:
    
    1) The need to reach both min and max brightnesses.
    
    2) The increased resolution of brightness controls on devices.
    
    3) The desire to make the framework as independent as possible of
    brightness resolution.
    
    we no longer allow for an epsilon delta.
    
    Fixes: 191935286
    Test: manual
    
    Change-Id: Ie4872fa81aa6f23799cd6b4dfdbfd2a61ec6ce7a

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -1393,9 +1393,8 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
             final float currentBrightness = mPowerState.getScreenBrightness();
             final float currentSdrBrightness = mPowerState.getSdrScreenBrightness();
             if (isValidBrightnessValue(animateValue)
-                    && (!BrightnessSynchronizer.floatEquals(animateValue, currentBrightness)
-                    || !BrightnessSynchronizer.floatEquals(
-                            sdrAnimateValue, currentSdrBrightness))) {
+                    && (animateValue != currentBrightness
+                    || sdrAnimateValue != currentSdrBrightness)) {
                 if (initialRampSkip || hasBrightnessBuckets
                         || wasOrWillBeInVr || !isDisplayContentVisible || brightnessIsTemporary) {
                     animateScreenBrightness(animateValue, sdrAnimateValue,
@@ -1741,11 +1740,10 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 mHbmController.getCurrentBrightnessMin(), mHbmController.getCurrentBrightnessMax());
     }
 
-    // Checks whether the brightness is within the valid brightness range, not including the off or
-    // invalid states.
-    private boolean isValidBrightnessValue(float brightnessState) {
-        return brightnessState >= PowerManager.BRIGHTNESS_MIN
-                && brightnessState <= PowerManager.BRIGHTNESS_MAX;
+    // Checks whether the brightness is within the valid brightness range, not including off.
+    private boolean isValidBrightnessValue(float brightness) {
+        return brightness >= PowerManager.BRIGHTNESS_MIN
+                && brightness <= PowerManager.BRIGHTNESS_MAX;
     }
 
     private void animateScreenBrightness(float target, float sdrTarget, float rate) {
@@ -2075,6 +2073,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     }
 
     private void putScreenBrightnessSetting(float brightnessValue, boolean updateCurrent) {
+        if (!isValidBrightnessValue(brightnessValue)) {
+            return;
+        }
         if (updateCurrent) {
             setCurrentScreenBrightness(brightnessValue);
         }
@@ -2124,8 +2125,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 || mPendingScreenBrightnessSetting < 0.0f)) {
             return treatAsIfUserChanged;
         }
-        if (BrightnessSynchronizer.floatEquals(
-                mCurrentScreenBrightnessSetting, mPendingScreenBrightnessSetting)) {
+        if (mCurrentScreenBrightnessSetting == mPendingScreenBrightnessSetting) {
             mPendingScreenBrightnessSetting = PowerManager.BRIGHTNESS_INVALID_FLOAT;
             mTemporaryScreenBrightness = PowerManager.BRIGHTNESS_INVALID_FLOAT;
             return treatAsIfUserChanged;
