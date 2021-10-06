aa5339988e63d42a902d185bff39353c0bf73193
Danny Lin <danny@kdrag0n.dev>
2021-10-05 21:00:57 -0700

    [DNM] SystemUI: Use colorkt-based dynamic theme engine for Monet
    Dynamic wallpaper-based theming ("Monet") is a prominent aspect of
    Android 12's Material You design system. Implement the system-wide
    wallpaper-based theming using my colorkt-based [1] dynamic theme engine [2],
    which many users prefer over Google's Pixel implementation in empirical
    tests as it typically keeps colors closer to the wallpaper and doesn't
    suffer from CAM16's blue-purple hue shift issues. It also provides
    better perceived contrast than Google's implementation in most cases
    because it uses ZCAM's modern lightness correlate as compared to CIELAB
    L* [3].
    
    NB: Unlike Google's implementation, we overlay the framework's default
    modulated surface colors for performance (no need to modulate colors
    when inflating views at runtime) and consistency (reduced dependency on
    framework-side color code, which may not respect user preferences).
    
    Demo screenshots: https://twitter.com/kdrag0n/status/1445584174790832134
    
    [1] https://github.com/kdrag0n/colorkt
    [2] https://github.com/kdrag0n/android12-extensions
    [3] https://twitter.com/MyndexResearch/status/1435491919702888448
    
    Change-Id: I48cbbacc32b2a8095eb07729aca1734f162d2391

---
+++ packages/SystemUI/src/org/protonaosp/systemui/theme/CustomThemeOverlayController.kt
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2021 The Proton AOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.protonaosp.systemui.theme
+
+import android.annotation.ColorInt
+import android.app.WallpaperColors
+import android.app.WallpaperManager
+import android.content.Context
+import android.content.om.FabricatedOverlay
+import android.os.Handler
+import android.os.UserManager
+import android.util.TypedValue
+import com.android.systemui.broadcast.BroadcastDispatcher
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.dagger.qualifiers.Background
+import com.android.systemui.dagger.qualifiers.Main
+import com.android.systemui.dump.DumpManager
+import com.android.systemui.keyguard.WakefulnessLifecycle
+import com.android.systemui.settings.UserTracker
+import com.android.systemui.statusbar.FeatureFlags
+import com.android.systemui.statusbar.policy.DeviceProvisionedController
+import com.android.systemui.theme.ThemeOverlayApplier
+import com.android.systemui.theme.ThemeOverlayController
+import com.android.systemui.util.settings.SecureSettings
+import dev.kdrag0n.colorkt.Color
+import dev.kdrag0n.colorkt.cam.Zcam
+import dev.kdrag0n.colorkt.conversion.ConversionGraph.convert
+import dev.kdrag0n.colorkt.data.Illuminants
+import dev.kdrag0n.colorkt.rgb.Srgb
+import dev.kdrag0n.colorkt.tristimulus.CieXyzAbs.Companion.toAbs
+import dev.kdrag0n.colorkt.ucs.lab.CieLab
+import dev.kdrag0n.monet.theme.DynamicColorScheme
+import dev.kdrag0n.monet.theme.MaterialYouTargets
+import java.util.concurrent.Executor
+import javax.inject.Inject
+
+@SysUISingleton
+class CustomThemeOverlayController @Inject constructor(
+    private val context: Context,
+    broadcastDispatcher: BroadcastDispatcher,
+    @Background bgHandler: Handler,
+    @Main mainExecutor: Executor,
+    @Background bgExecutor: Executor,
+    themeOverlayApplier: ThemeOverlayApplier,
+    secureSettings: SecureSettings,
+    wallpaperManager: WallpaperManager,
+    userManager: UserManager,
+    deviceProvisionedController: DeviceProvisionedController,
+    userTracker: UserTracker,
+    dumpManager: DumpManager,
+    featureFlags: FeatureFlags,
+    wakefulnessLifecycle: WakefulnessLifecycle,
+) : ThemeOverlayController(
+    context,
+    broadcastDispatcher,
+    bgHandler,
+    mainExecutor,
+    bgExecutor,
+    themeOverlayApplier,
+    secureSettings,
+    wallpaperManager,
+    userManager,
+    deviceProvisionedController,
+    userTracker,
+    dumpManager,
+    featureFlags,
+    wakefulnessLifecycle,
+) {
+    private val cond = Zcam.ViewingConditions(
+        surroundFactor = Zcam.ViewingConditions.SURROUND_AVERAGE,
+        // sRGB
+        adaptingLuminance = 0.4 * SRGB_WHITE_LUMINANCE,
+        // Gray world
+        backgroundLuminance = CieLab(
+            L = 50.0,
+            a = 0.0,
+            b = 0.0,
+        ).toXyz().y * SRGB_WHITE_LUMINANCE,
+        referenceWhite = Illuminants.D65.toAbs(SRGB_WHITE_LUMINANCE),
+    )
+    private val targets = MaterialYouTargets(
+        chromaFactor = 1.0,
+        useLinearLightness = false,
+        cond = cond,
+    )
+
+    // Seed colors
+    override fun getNeutralColor(colors: WallpaperColors) = colors.primaryColor.toArgb()
+    override fun getAccentColor(colors: WallpaperColors) = getNeutralColor(colors)
+
+    override fun getOverlay(primaryColor: Int, type: Int): FabricatedOverlay {
+        // Generate color scheme
+        val colorScheme = DynamicColorScheme(
+            targets = targets,
+            seedColor = Srgb(primaryColor),
+            chromaFactor = 1.0,
+            cond = cond,
+            accurateShades = true,
+        )
+
+        val (groupKey, colorsList) = when (type) {
+            ACCENT -> "accent" to colorScheme.accentColors
+            NEUTRAL -> "neutral" to colorScheme.neutralColors
+            else -> error("Unknown type $type")
+        }
+
+        return FabricatedOverlay.Builder(context.packageName, groupKey, "android").run {
+            colorsList.withIndex().forEach { listEntry ->
+                val group = "$groupKey${listEntry.index + 1}"
+
+                listEntry.value.forEach { (shade, color) ->
+                    val colorSrgb = color.convert<Srgb>()
+                    setColor("system_${group}_$shade", colorSrgb)
+                }
+            }
+
+            // Override special modulated surface colors for performance and consistency
+            if (type == NEUTRAL) {
+                // surface light = neutral1 20 (L* 98)
+                colorsList[0][20]?.let { setColor("surface_light", it) }
+
+                // surface highlight dark = neutral1 650 (L* 35)
+                colorsList[0][650]?.let { setColor("surface_highlight_dark", it) }
+            }
+
+            build()
+        }
+    }
+
+    companion object {
+        private const val TAG = "CustomThemeOverlayController"
+
+        private const val SRGB_WHITE_LUMINANCE = 200.0 // cd/m^2
+
+        private fun FabricatedOverlay.Builder.setColor(name: String, @ColorInt color: Int) =
+            setResourceValue("android:color/$name", TypedValue.TYPE_INT_COLOR_ARGB8, color)
+
+        private fun FabricatedOverlay.Builder.setColor(name: String, color: Color): FabricatedOverlay.Builder {
+            val rgb = color.convert<Srgb>().toRgb8()
+            val argb = rgb or (0xff shl 24)
+            return setColor(name, argb)
+        }
+    }
+}
