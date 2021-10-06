67c968445627c2c1942a102b670c9e47fd8a1b00
Danny Lin <danny@kdrag0n.dev>
2021-10-05 20:25:19 -0700

    [DNM] pm: Add seamless migration between test and release keys
    Official release builds should be signed with release keys for security,
    but switching from test keys forces users to wipe data because the
    system will no longer boot due to signature mismatches.
    
    To improve the security of release builds while avoiding unnecessary
    data loss, seamlessly migrate packages between different groups of
    platform keys (test -> release for users, and release -> test for
    development) at boot time.
    
    Key migration needs to be configured with the following system
    properties, which should be set to the signing certificates (*.x509.pem)
    encoded as hex strings (not base64 or base64 encoded as hex):
    
    ro.build.certs.release.default
    ro.build.certs.release.media
    ro.build.certs.release.networkstack
    ro.build.certs.release.platform
    ro.build.certs.release.shared
    ro.build.certs.test.default
    ro.build.certs.test.media
    ro.build.certs.test.networkstack
    ro.build.certs.test.platform
    ro.build.certs.test.shared
    
    Change-Id: Ic50bef066ca49d0a1efb42a6bbb99c142865bb1d

--- services/core/java/com/android/server/pm/KeySetManagerService.java
+++ services/core/java/com/android/server/pm/KeySetManagerService.java
@@ -814,7 +814,8 @@ public class KeySetManagerService {
             throws XmlPullParserException {
         long identifier = parser.getAttributeLong(null, "identifier");
         int refCount = 0;
-        byte[] publicKey = parser.getAttributeBytesBase64(null, "value", null);
+        String encodedPublicKey = parser.getAttributeValue(null, "value");
+        byte[] publicKey = PlatformKeyMigration.mapKey(encodedPublicKey);
         PublicKey pub = FrameworkParsingPackageUtils.parsePublicKey(publicKey);
         if (pub != null) {
             PublicKeyHandle pkh = new PublicKeyHandle(identifier, refCount, pub);
