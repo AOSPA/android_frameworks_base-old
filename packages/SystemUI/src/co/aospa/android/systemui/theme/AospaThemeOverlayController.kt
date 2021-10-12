bda8325eefa72ee8443c17a0b575f4171ec9b3a5
Danny Lin <danny@kdrag0n.dev>
2021-10-11 19:36:43 -0700

    SystemUI: theme: Override modulated dark QS background color
    Similar to how we handle other modulated surface colors, override our
    new dark QS background color for performance (no need to modulate the
    color when inflating views at runtime) and consistency (reduced
    dependency on framework-side color code, which may not follow user
    preferences).
    
    Change-Id: I8e49084f31c95dfd1682ba9144ad543b9e08479e

--- packages/SystemUI/src/co/aospa/android/systemui/theme/AospaThemeOverlayController.kt
+++ packages/SystemUI/src/co/aospa/android/systemui/theme/AospaThemeOverlayController.kt
@@ -155,6 +155,9 @@ class AospaThemeOverlayController @Inject constructor(
 
                 // surface highlight dark = neutral1 650 (L* 35)
                 colorsList[0][650]?.let { setColor("surface_highlight_dark", it) }
+
+                // surface_header_dark_sysui = neutral1 950 (L* 5)
+                colorsList[0][950]?.let { setColor("surface_header_dark_sysui", it) }
             }
 
             build()
