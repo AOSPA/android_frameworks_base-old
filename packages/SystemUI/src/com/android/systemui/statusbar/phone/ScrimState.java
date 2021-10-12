5da463d89ac02bd7c39ca37f36cdda226d474e79
Danny Lin <danny@kdrag0n.dev>
2021-10-11 19:24:58 -0700

    SystemUI: Follow light/dark theme in quick settings
    Android 12's dual-tone style where the quick settings panel is always
    dark makes the light theme look like a second-class citizen. Pure black
    also looks out-of-place next to QS tiles and the notification panel
    because dynamic themes don't affect it at all.
    
    Revert to the ~Beta 1 style where quick settings used the same theme as
    the notification shade.
    
    - colorAccentPrimary has been replaced with colorAccent for contrast in
      light mode, because colorAccentPrimary is system_accent1_100 (dark
      accent color)
    - Footer chips have been converted to surfaces (similar to QS tiles and
      notifications), which makes more sense with the new style
    - The QS background is now the same as the notification shade background
      in both light and dark modes
    
    Demo screenshots (with dual-tone commit):
    https://twitter.com/kdrag0n/status/1445922541218922496
    
    [kdrag0n: ported to 12L]
    [neobuddy89: adapted for 14QPR1]
    
    Saalim:
    - Use colorAccents in shade values to match systme theme design change
    
    Change-Id: I3d45b72f0f119e100505409d178ab8f698993881
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
@@ -82,7 +82,7 @@ public enum ScrimState {
             mBehindAlpha = mClipQsScrim ? 1 : mScrimBehindAlphaKeyguard;
             mNotifAlpha = mClipQsScrim ? mScrimBehindAlphaKeyguard : 0;
             if (mClipQsScrim) {
-                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.BLACK);
+                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.TRANSPARENT);
             }
         }
     },
@@ -122,7 +122,7 @@ public enum ScrimState {
         @Override
         public void prepare(ScrimState previousState) {
             mBehindAlpha = mClipQsScrim ? 1 : mDefaultScrimAlpha;
-            mBehindTint = mClipQsScrim ? Color.BLACK : mSurfaceColor;
+            mBehindTint = mSurfaceColor;
             mNotifAlpha = mClipQsScrim ? mDefaultScrimAlpha : 0;
             mNotifTint = Color.TRANSPARENT;
             mFrontAlpha = 0f;
@@ -154,10 +154,10 @@ public enum ScrimState {
             mBehindAlpha = mClipQsScrim ? 1 : mDefaultScrimAlpha;
             mNotifAlpha = 1f;
             mFrontAlpha = 0f;
-            mBehindTint = mClipQsScrim ? Color.TRANSPARENT : Color.BLACK;
+            mBehindTint = Color.TRANSPARENT;
 
             if (mClipQsScrim) {
-                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.BLACK);
+                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.TRANSPARENT);
             }
         }
     },
@@ -259,22 +259,22 @@ public enum ScrimState {
                     && !fromAod;
 
             mFrontTint = Color.TRANSPARENT;
-            mBehindTint = Color.BLACK;
+            mBehindTint = Color.TRANSPARENT;
             mBlankScreen = false;
 
             if (mDisplayRequiresBlanking && previousState == ScrimState.AOD) {
                 // Set all scrims black, before they fade transparent.
                 updateScrimColor(mScrimInFront, 1f /* alpha */, Color.BLACK /* tint */);
-                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.BLACK /* tint */);
+                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.TRANSPARENT /* tint */);
 
                 // Scrims should still be black at the end of the transition.
                 mFrontTint = Color.BLACK;
-                mBehindTint = Color.BLACK;
+                mBehindTint = Color.TRANSPARENT;
                 mBlankScreen = true;
             }
 
             if (mClipQsScrim) {
-                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.BLACK);
+                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.TRANSPARENT);
             }
         }
     },
