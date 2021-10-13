feaff956ecbef0c9f465806047c87b0e0a4cbb2d
Matt Mower <mowerm@gmail.com>
2021-10-13 09:22:28 +0000

    SystemUI: Implement lockscreen quick unlock.
    [BigBrother1984 <carlosavignano@aospa.co>]:
        * PasswordTextView QuickUnlock listener functions and method
    
    [mydongistiny <jaysonedson@gmail.com>]:
        * Forward port to Android 8 / 10
    
    [SagarMakhar <sagarmakhar@gmail.com>]:
        * Forward port to Android 12
    
    [Hikari-no-Tenshi <kyryljan.serhij@gmail.com>]:
        * Make Quick Unlock compatible with long PIN/Password (Idea from OOS)
        * Use AsyncTask for PIN/Password check.
        * Restore call on main thread check.
    
    [Hernán Castañón <herna@paranoidandroid.co>]:
        * Make it independent of toggle (enabled by default)
    
    [Jyotiraditya Panda <jyotiraditya@aospa.co>]:
        * Code cleanup and format
        * Hide Ok button in favour of quick unlock
        * Move backspace button to right
    
    [ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>]:
        * Simplify and refactor to use verifyPasswordAndUnlock()
        * Handle case where password length is not set
        * Hide backspace button when PIN is empty
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: BigBrother1984 <carlosavignano@aospa.co>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Idbad25c473e059b0016730eac1ff1f23fb7e701a

--- packages/SystemUI/src/com/android/keyguard/KeyguardPinViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPinViewController.java
@@ -16,17 +16,23 @@
 
 package com.android.keyguard;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.android.internal.util.LatencyTracker;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardSecurityModel.SecurityMode;
 import com.android.systemui.R;
 import com.android.systemui.classifier.FalsingCollector;
+import com.android.keyguard.PasswordTextView.QuickUnlockListener;
 
 public class KeyguardPinViewController
         extends KeyguardPinBasedInputViewController<KeyguardPINView> {
     private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
+    private final LockPatternUtils mLockPatternUtils;
+    private final View mDeleteButton;
 
     protected KeyguardPinViewController(KeyguardPINView view,
             KeyguardUpdateMonitor keyguardUpdateMonitor,
@@ -40,11 +46,63 @@ public class KeyguardPinViewController
                 messageAreaControllerFactory, latencyTracker, liftToActivateListener,
                 emergencyButtonController, falsingCollector);
         mKeyguardUpdateMonitor = keyguardUpdateMonitor;
+        mLockPatternUtils = lockPatternUtils;
+        mDeleteButton = mView.findViewById(R.id.delete_button);
     }
 
     @Override
     protected void onViewAttached() {
         super.onViewAttached();
+
+        int passwordLength = mLockPatternUtils.getPinPasswordLength(
+                KeyguardUpdateMonitor.getCurrentUser());
+
+        mPasswordEntry.setQuickUnlockListener(new QuickUnlockListener() {
+            public void onValidateQuickUnlock(String password) {
+                if (password != null) {
+                    int length = password.length();
+                    if (length > 0) {
+                        showDeleteButton(true, true);
+                    }
+                    if (length == passwordLength) {
+                        verifyPasswordAndUnlock();
+                    }
+                }
+            }
+        });
+
+        if (mDeleteButton != null) {
+            mDeleteButton.setOnClickListener(v -> {
+                // check for time-based lockouts
+                if (mPasswordEntry.isEnabled()) {
+                    mPasswordEntry.deleteLastChar();
+                    if (mPasswordEntry.getText().isEmpty()) {
+                        showDeleteButton(false, true);
+                    }
+                }
+            });
+            mDeleteButton.setOnLongClickListener(v -> {
+                // check for time-based lockouts
+                if (mPasswordEntry.isEnabled()) {
+                    mView.resetPasswordText(true /* animate */, true /* announce */);
+                    showDeleteButton(false, true);
+                }
+                mView.doHapticKeyClick();
+                return true;
+            });
+            showDeleteButton(false, false);
+        }
+
+        View okButton = mView.findViewById(R.id.key_enter);
+        if (okButton != null) {
+            /* show okButton only if password length is unset
+               because quick unlock won't work */
+            if (passwordLength != -1) {
+                ((LinearLayout) mView.findViewById(R.id.row4)).setLayoutDirection(
+                        View.LAYOUT_DIRECTION_RTL);
+                okButton.setVisibility(View.INVISIBLE);
+            }
+        }
 
         View cancelBtn = mView.findViewById(R.id.cancel_button);
         if (cancelBtn != null) {
@@ -65,11 +123,37 @@ public class KeyguardPinViewController
     void resetState() {
         super.resetState();
         mMessageAreaController.setMessage("");
+        showDeleteButton(false, false);
     }
 
     @Override
     public boolean startDisappearAnimation(Runnable finishRunnable) {
         return mView.startDisappearAnimation(
                 mKeyguardUpdateMonitor.needsSlowUnlockTransition(), finishRunnable);
+    }
+
+    private void showDeleteButton(boolean show, boolean animate) {
+        int visibility = show ? View.VISIBLE : View.INVISIBLE;
+        if (mDeleteButton != null && mDeleteButton.getVisibility() != visibility) {
+            if (animate) {
+                mDeleteButton.setAlpha(show ? 0.0f : 1.0f);
+                mDeleteButton.animate()
+                    .alpha(show ? 1.0f : 0.0f)
+                    .setDuration(show ? 250 : 450)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            if (show) mDeleteButton.setVisibility(visibility);
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            if (!show) mDeleteButton.setVisibility(visibility);
+                        }
+                    });
+            } else {
+                mDeleteButton.setVisibility(visibility);
+            }
+        }
     }
 }
