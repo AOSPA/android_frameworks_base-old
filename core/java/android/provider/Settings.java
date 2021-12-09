279c4afd894fe953e7ed6754b7ebfe3965602965
jhonboy121 <jhonboy121@users.noreply.github.com>
2021-12-09 17:48:31 +0000

    SystemUI: Allow customizing the length of gesture navbar
    Change-Id: I9b4b81f90f56ad6f448975b4158bed8cf2b9354c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5325,6 +5325,13 @@ public final class Settings {
        public static final String GESTURE_TWO_FINGER_SWIPE = "gesture_two_finger_swipe";
 
         /**
+         * Position of gesture bar length slider.
+         * @hide
+         */
+        public static final String GESTURE_NAVBAR_LENGTH = "gesture_navbar_length";
+
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
