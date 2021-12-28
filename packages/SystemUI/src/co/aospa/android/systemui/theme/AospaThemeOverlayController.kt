efd95a835a4dd0e59f950a879906c38270cba265
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:57:38 +0530

    SystemUI: Update monet according to new sysui stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I6ef16b8afedac661d06b4b7d8467cac21428d3e3

--- packages/SystemUI/src/co/aospa/android/systemui/theme/AospaThemeOverlayController.kt
+++ packages/SystemUI/src/co/aospa/android/systemui/theme/AospaThemeOverlayController.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.protonaosp.systemui.theme
+package co.aospa.android.systemui.theme
 
 import android.annotation.ColorInt
 import android.app.WallpaperColors
@@ -53,7 +53,7 @@ import kotlin.math.log10
 import kotlin.math.pow
 
 @SysUISingleton
-class CustomThemeOverlayController @Inject constructor(
+class AospaThemeOverlayController @Inject constructor(
     private val context: Context,
     broadcastDispatcher: BroadcastDispatcher,
     @Background bgHandler: Handler,
@@ -162,7 +162,7 @@ class CustomThemeOverlayController @Inject constructor(
     }
 
     companion object {
-        private const val TAG = "CustomThemeOverlayController"
+        private const val TAG = "AospaThemeOverlayController"
 
         private const val PREF_PREFIX = "monet_engine"
         private const val PREF_COLOR_OVERRIDE = "${PREF_PREFIX}_color_override"
