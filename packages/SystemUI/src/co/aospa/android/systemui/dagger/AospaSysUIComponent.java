60081f431fe18775d9bcb5ed51c0680ad4966ef5
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial AOSPA SystemUI stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I67f36f20cc01dfc82572f2241287c3e739592306

---
+++ packages/SystemUI/src/co/aospa/android/systemui/dagger/AospaSysUIComponent.java
@@ -0,0 +1,25 @@
+package co.aospa.android.systemui.dagger;
+
+import com.android.systemui.dagger.DefaultComponentBinder;
+import com.android.systemui.dagger.DependencyProvider;
+import com.android.systemui.dagger.SysUISingleton;
+import com.android.systemui.dagger.SystemUIBinder;
+import com.android.systemui.dagger.SysUIComponent;
+import com.android.systemui.dagger.SystemUIModule;
+
+import dagger.Subcomponent;
+
+@SysUISingleton
+@Subcomponent(modules = {
+        DefaultComponentBinder.class,
+        DependencyProvider.class,
+        SystemUIBinder.class,
+        SystemUIModule.class,
+        AospaSystemUIModule.class})
+public interface AospaSysUIComponent extends SysUIComponent {
+    @SysUISingleton
+    @Subcomponent.Builder
+    interface Builder extends SysUIComponent.Builder {
+        AospaSysUIComponent build();
+    }
+}
