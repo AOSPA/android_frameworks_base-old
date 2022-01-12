4b188d715bb58c0d9faa6caa7174787e22e28dc1
SagarMakhar <sagarmakhar@gmail.com>
2022-01-12 18:32:54 +0000

    SystemUI: Add support for black theme [1/2]
    Squashed:
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Sat Apr 16 10:38:36 2022 +0530
    
        Fixes and improvements for Black Theme
    
        * Add to SettingsObserver for observing toggle behavior.
        * Apply only with dark theme - looks awful on light mode.
        * Add configuration listener to reapply theme when dark mode changes.
    
        Change-Id: Ib114fa5772c3db74b46b7656aeb0eabdf6d4de4d
    
    Author: Michael Bestas <mkbestas@gmail.com>
    Date:   Wed Aug 24 21:19:57 2022 +0300
    
        Don't skip theme application if it's already current
    
        This fixes black theme and font/icon shape customizations.
    
        Change-Id: I8c3d3b4634e1ac60d28e4d5ef20bf9c2015e2e18
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: Ib165e7ba0288eaad7a1e3c1109638acdb66fa05c
    Signed-off-by: rk134 <rahul@aospa.co>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11503,6 +11503,12 @@ public final class Settings {
                 "com.android.server.display.HBM_SETTING_KEY";
 
         /**
+         * Whether to turn on black theme
+         * @hide
+         */
+        public static final String SYSTEM_BLACK_THEME = "system_black_theme";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
