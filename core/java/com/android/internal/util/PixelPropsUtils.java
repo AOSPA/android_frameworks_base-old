77c032e5383cbf1d744e9250cbd2084788a53e22
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-01-29 23:05:49 +0530

    PixelPropsUtils: Allow disabling netflix spoofing
    Set persist.pixelpropsutils.spoof_netflix to false to
    opt out of netflix spoofing if, say, the device supports
    Dolby Vision or has horrible (tinted) HDR.
    
    Change-Id: I0a62bd64d92d71401f862c4edc99077dc8241c5b

--- core/java/com/android/internal/util/PixelPropsUtils.java
+++ core/java/com/android/internal/util/PixelPropsUtils.java
@@ -17,6 +17,7 @@
 package com.android.internal.util;
 
 import android.os.Build;
+import android.os.SystemProperties;
 import android.util.Log;
 
 import java.util.Arrays;
@@ -32,6 +33,7 @@ public class PixelPropsUtils {
 
     private static volatile boolean sIsGms = false;
     public static final String PACKAGE_GMS = "com.google.android.gms";
+    public static final String PACKAGE_NETFLIX = "com.netflix.mediaclient";
 
     private static final Map<String, Object> propsToChange;
     private static final Map<String, Object> propsToChangeP6;
@@ -39,8 +41,8 @@ public class PixelPropsUtils {
     private static final Map<String, Object> propsToChangeP1;
     private static final Map<String, ArrayList<String>> propsToKeep;
     private static final String[] extraPackagesToChange = {
-        "com.android.chrome"
-        "com.netflix.mediaclient"
+        "com.android.chrome",
+        PACKAGE_NETFLIX
     };
     private static final String[] packagesToChangeP5 = {
         "com.google.android.tts",
@@ -92,6 +94,11 @@ public class PixelPropsUtils {
         }
         if (packageName.equals(PACKAGE_GMS)) {
             sIsGms = true;
+        }
+        if (packageName.equals(PACKAGE_NETFLIX) && !SystemProperties.getBoolean(
+                "persist.pixelpropsutils.spoof_netflix", true)) {
+            if (DEBUG) Log.d(TAG, "Netflix spoofing disabled by system prop");
+            return;
         }
         if ((packageName.startsWith("com.google.") && !Arrays.asList(packagesToKeep).contains(packageName))
                 || Arrays.asList(extraPackagesToChange).contains(packageName)) {
