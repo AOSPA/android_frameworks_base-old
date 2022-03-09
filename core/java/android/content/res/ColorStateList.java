02fbdd540c53784b22ffb017ca9ef6e342d37d37
Kunmun <kunmun.devroms@gmail.com>
2022-03-09 19:43:39 +0530

    SystemUI: Drop colorkt-based dynamic theme engine for Monet
    This reverts commit de85bd963e5b161a732e4706f08e1d456fe2156b.
    
    • We are switching to AOSP Monet
    
    Change-Id: I060e5dadeea7b848218382f1f4cff020aea591b4

--- core/java/android/content/res/ColorStateList.java
+++ core/java/android/content/res/ColorStateList.java
@@ -34,10 +34,8 @@ import android.util.StateSet;
 import android.util.Xml;
 
 import com.android.internal.R;
-import com.android.internal.graphics.color.CieLab;
-import com.android.internal.graphics.color.CieXyzAbs;
-import com.android.internal.graphics.color.Zcam;
-import com.android.internal.graphics.color.ZcamGamut;
+import com.android.internal.graphics.ColorUtils;
+import com.android.internal.graphics.cam.Cam;
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.GrowingArrayUtils;
 
@@ -558,9 +556,8 @@ public class ColorStateList extends ComplexColor implements Parcelable {
         final int alpha = MathUtils.constrain((int) (baseAlpha * alphaMod + 0.5f), 0, 255);
 
         if (validLStar) {
-            final Zcam baseZcam = new Zcam(new CieXyzAbs(baseColor));
-            baseZcam.lightness = CieLab.lToZcamJz(lStar);
-            baseColor = ZcamGamut.clipToRgb8(baseZcam);
+            final Cam baseCam = ColorUtils.colorToCAM(baseColor);
+            baseColor = ColorUtils.CAMToColor(baseCam.getHue(), baseCam.getChroma(), lStar);
         }
 
         return (baseColor & 0xFFFFFF) | (alpha << 24);
