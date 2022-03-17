352761450c64aba54569ebec79f87820be9109b6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-03-17 18:02:25 +0530

    SystemUI: Add tuner switch for statusbar wifi standard
    Revised for refactored icon (ef83a59583b23f2fc4dcb79974ac3b77e9607959).
    
    Change-Id: I1bc727745f351fafcdc3f5a284cebba2314182ab
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarWifiView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarWifiView.java
@@ -198,7 +198,7 @@ public class StatusBarWifiView extends BaseStatusBarWifiView implements DarkRece
         if (mState.resId != state.resId && state.resId >= 0) {
             mWifiIcon.setImageDrawable(mContext.getDrawable(state.resId));
         }
-        setWifiStandard();
+        setWifiStandard(state);
         mIn.setVisibility(state.activityIn ? View.VISIBLE : View.GONE);
         mOut.setVisibility(state.activityOut ? View.VISIBLE : View.GONE);
         mInoutContainer.setVisibility(
@@ -207,7 +207,8 @@ public class StatusBarWifiView extends BaseStatusBarWifiView implements DarkRece
         mSignalSpacer.setVisibility(state.signalSpacerVisible ? View.VISIBLE : View.GONE);
 
         boolean needsLayout = state.activityIn != mState.activityIn
-                ||state.activityOut != mState.activityOut;
+                || state.activityOut != mState.activityOut
+                || state.wifiStandard != mState.wifiStandard;
 
         if (mState.visible != state.visible) {
             needsLayout |= true;
@@ -223,7 +224,7 @@ public class StatusBarWifiView extends BaseStatusBarWifiView implements DarkRece
         if (mState.resId >= 0) {
             mWifiIcon.setImageDrawable(mContext.getDrawable(mState.resId));
         }
-        setWifiStandard();
+        setWifiStandard(mState);
         mIn.setVisibility(mState.activityIn ? View.VISIBLE : View.GONE);
         mOut.setVisibility(mState.activityOut ? View.VISIBLE : View.GONE);
         mInoutContainer.setVisibility(
@@ -233,21 +234,24 @@ public class StatusBarWifiView extends BaseStatusBarWifiView implements DarkRece
         setVisibility(mState.visible ? View.VISIBLE : View.GONE);
     }
 
-    private void setWifiStandard() {
-        final boolean showNetworkStandard = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_show_network_standard);
-        if (!showNetworkStandard) {
-            mWifiStandard.setVisibility(View.GONE);
-            return;
+    private void setWifiStandard(WifiIconState state) {
+        int resid = 0;
+        switch (state.wifiStandard) {
+            case 4:
+                resid = R.drawable.ic_wifi_standard_4;
+                break;
+            case 5:
+                resid = R.drawable.ic_wifi_standard_5;
+                break;
+            case 6:
+                resid = R.drawable.ic_wifi_standard_6;
+                break;
         }
-        int wifiStandard = mState.wifiStandard;
-        if (wifiStandard >= 4) {
-            int identifier = getResources().getIdentifier("ic_wifi_standard_" + wifiStandard,
-                    "drawable", getContext().getPackageName());
-            if (identifier > 0) {
-                mWifiStandard.setVisibility(View.VISIBLE);
-                mWifiStandard.setImageDrawable(mContext.getDrawable(identifier));
-            }
+        if (resid > 0) {
+            mWifiStandard.setVisibility(View.VISIBLE);
+            mWifiStandard.setImageDrawable(mContext.getDrawable(resid));
+        } else {
+            mWifiStandard.setVisibility(View.GONE);
         }
     }
 
