ccb5a10eba216da8dc9bac2fa657a5b5bc86966a
Ido Ben-Hur <idoybh2@gmail.com>
2022-03-19 21:34:20 +0200

    SystemUI: Nuke oriented navbar handle
    AKA fake navbar
    AGAIN!!! For the 3rd time. This ain't working dear google.
    Not even getting better with their attempts.
    I gave up hope on this, I don't see it ever coming back.
    Adapted (lazily) to A13
    
    Includes but not limited to these reverts (much code changed to make it work):
    
    Revert "Prevent secondary home handle from handling touches"
    
    This reverts commit d05ad989682b658b84bfb8989df7f36c0966a191.
    
    Revert "Sample region under secondary home handle"
    
    This reverts commit d2fe92fbe4b715617e8778655f5c0d04c2c9c353.
    
    Revert "Add ID to secondaryOrientationHandle for tests"
    
    This reverts commit 83308950ee97f99fb3f4bd0b9ebefafc23013bb9.
    
    Revert "Show secondary home handle in immersive apps"
    
    This reverts commit 9c84ea1ff0d93354b6eb378957b2e2f869537664.
    
    Revert "Draw a fake home handle during QuickSwitch"
    
    This reverts commit 63e92bc00bdfef5cb5f8a8e086fc54e7e6a8d864.
    
    Revert "Draw a fake home handle during QuickSwitch"
    
    This reverts commit 63e92bc00bdfef5cb5f8a8e086fc54e7e6a8d864.
    
    Change-Id: I0fcc8cc18438fda9b9ebbf5fe83b09cbd94f6c33
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -1119,8 +1119,6 @@ public class NavigationBarView extends FrameLayout {
 
         pw.println("    mScreenOn: " + mScreenOn);
 
-
-        dumpButton(pw, "back", getBackButton());
         dumpButton(pw, "home", getHomeButton());
         dumpButton(pw, "handle", getHomeHandle());
         dumpButton(pw, "rcnt", getRecentsButton());
