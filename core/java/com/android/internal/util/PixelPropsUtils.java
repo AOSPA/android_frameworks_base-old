7e85d4af98e1222ae6af5c5accd06d3c38206b0d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-04-08 03:32:59 +0530

    PixelPropsUtils: Update fingerprints to March 2022
    Change-Id: I96ad177a9fa70a8f0aa3ac465ba99ecb9453e759

--- core/java/com/android/internal/util/PixelPropsUtils.java
+++ core/java/com/android/internal/util/PixelPropsUtils.java
@@ -71,12 +71,12 @@ public class PixelPropsUtils {
         propsToChangeP5.put("DEVICE", "redfin");
         propsToChangeP5.put("PRODUCT", "redfin");
         propsToChangeP5.put("MODEL", "Pixel 5");
-        propsToChangeP5.put("FINGERPRINT", "google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys");
+        propsToChangeP5.put("FINGERPRINT", "google/redfin/redfin:12/SP2A.220305.012/8177914:user/release-keys");
         propsToChangeP6 = new HashMap<>();
         propsToChangeP6.put("DEVICE", "raven");
         propsToChangeP6.put("PRODUCT", "raven");
         propsToChangeP6.put("MODEL", "Pixel 6 Pro");
-        propsToChangeP6.put("FINGERPRINT", "google/raven/raven:12/SQ1D.220205.004/8151327:user/release-keys");
+        propsToChangeP6.put("FINGERPRINT", "google/raven/raven:12/SP2A.220305.013.A3/8229987:user/release-keys");
         propsToChangeP1 = new HashMap<>();
         propsToChangeP1.put("DEVICE", "marlin");
         propsToChangeP1.put("PRODUCT", "marlin");
