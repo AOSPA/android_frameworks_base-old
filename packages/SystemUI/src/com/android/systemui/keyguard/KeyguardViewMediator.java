89c61adff6b426f3c0fa9175c2da5c6f5a12905c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-04-09 17:22:46 +0530

    Revert "SystemUI: Invoke keyguard exit animation"
    We will disable remote keyguard animation via prop instead.
    
    This reverts commit eb2209acc5c6e58fd90f76e78f6bc8294629fe1d.
    
    Change-Id: I976b48264803d0d072ef0286b0a5cb27d3f1ef80

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2606,8 +2606,9 @@ public class KeyguardViewMediator extends SystemUI implements Dumpable,
             if (callback != null) {
                 if (mWakeAndUnlocking) {
                     mDrawnCallback = callback;
+                } else {
+                    notifyDrawn(callback);
                 }
-                notifyDrawn(callback);
             }
         }
         Trace.endSection();
