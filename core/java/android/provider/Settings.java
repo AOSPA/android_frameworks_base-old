a042bd280988bc9061c8eeff78eb885054b77c29
jhenrique09 <jhsv09@gmail.com>
2022-04-09 00:43:29 +0000

    base: Add TETHERING_ALLOW_VPN_UPSTREAMS setting
    Change-Id: I8b388dbb356567de25457bb98ae8c64cd0ccb6a1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11376,6 +11376,16 @@ public final class Settings {
         public static final String DOZE_PICK_UP_GESTURE_AMBIENT = "doze_pick_up_gesture_ambient";
 
         /**
+         * Whether tethering is allowed to use VPN upstreams
+         * 0 = false, 1 = true
+         * @hide
+         */
+        @Readable
+        @SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
+        @SuppressLint("NoSettingsProvider")
+        public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
