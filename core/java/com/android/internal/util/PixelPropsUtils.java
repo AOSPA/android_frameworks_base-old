8930e6563ee515d2aef9346c204aa24539967379
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-04-11 23:22:41 +0900

    PixelPropsUtils: switch gms spoof to redfin as well
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I3602d87ab9773e2eeca98baf81d5a9fb646770d9

--- core/java/com/android/internal/util/PixelPropsUtils.java
+++ core/java/com/android/internal/util/PixelPropsUtils.java
@@ -34,15 +34,11 @@ public class PixelPropsUtils {
     public static final String PACKAGE_GMS = "com.google.android.gms";
 
     private static final Map<String, Object> propsToChange;
-    private static final Map<String, Object> propsToChangeP6;
     private static final Map<String, Object> propsToChangeP5;
     private static final Map<String, Object> propsToChangeP1;
     private static final Map<String, ArrayList<String>> propsToKeep;
     private static final String[] extraPackagesToChange = {
         "com.android.chrome"
-    };
-    private static final String[] packagesToChangeP6 = {
-        "com.google.android.gms"
     };
     private static final String[] packagesToChangeP1 = {
          "com.google.android.apps.photos"
@@ -72,11 +68,6 @@ public class PixelPropsUtils {
         propsToChangeP5.put("PRODUCT", "redfin");
         propsToChangeP5.put("MODEL", "Pixel 5");
         propsToChangeP5.put("FINGERPRINT", "google/redfin/redfin:12/SP2A.220405.003/8210211:user/release-keys");
-        propsToChangeP6 = new HashMap<>();
-        propsToChangeP6.put("DEVICE", "raven");
-        propsToChangeP6.put("PRODUCT", "raven");
-        propsToChangeP6.put("MODEL", "Pixel 6 Pro");
-        propsToChangeP6.put("FINGERPRINT", "google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys");
         propsToChangeP1 = new HashMap<>();
         propsToChangeP1.put("DEVICE", "marlin");
         propsToChangeP1.put("PRODUCT", "marlin");
@@ -94,9 +85,7 @@ public class PixelPropsUtils {
         if ((packageName.startsWith("com.google.") && !Arrays.asList(packagesToKeep).contains(packageName))
                 || Arrays.asList(extraPackagesToChange).contains(packageName)) {
             if (DEBUG) Log.d(TAG, "Defining props for: " + packageName);
-            if (Arrays.asList(packagesToChangeP6).contains(packageName)) {
-                propsToChange.putAll(propsToChangeP6);
-            } else if (Arrays.asList(packagesToChangeP1).contains(packageName)) {
+            if (Arrays.asList(packagesToChangeP1).contains(packageName)) {
                 propsToChange.putAll(propsToChangeP1);
             } else {
                 propsToChange.putAll(propsToChangeP5);
