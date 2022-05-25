c0fe0a6a39339bca16829ada391567ebf0a58d65
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-25 14:18:24 +0530

    SystemUI: QSAnimator: Hide secondary label in QQS
    Fixes secondary label being stuck while collapsing
    QS panel from non-first page.
    
    Change-Id: I39d3607599950e315d93920f1e6083cd14a2d5eb

--- packages/SystemUI/src/com/android/systemui/qs/QSAnimator.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSAnimator.java
@@ -422,6 +422,11 @@ public class QSAnimator implements QSHost.Callback, PagedTileLayout.PageListener
                     mAllViews.add(tileView.getSecondaryLabel());
                 }
 
+                QSTileView quickTileView = mQuickQSPanelController.getTileView(tile);
+                if (quickTileView != null) {
+                    quickTileView.getSecondaryLabel().setAlpha(0f);
+                }
+
                 mAllViews.add(tileView);
                 count++;
             }
