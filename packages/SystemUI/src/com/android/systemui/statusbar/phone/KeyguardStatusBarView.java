60ff95ede7e6d002d3c7c6f0ce6d6190be5321a6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-26 18:39:27 +0530

    SystemUI: Re-inflate keyguard statusbar on theme change
    Fixes lockscreen statusbar icons not refreshing on changing icon pack.
    Inspired by https://github.com/AOSPA/android_frameworks_base/commit/20dc3a97ffbce00f26ff2d96f027eefb826877ee
    
    Change-Id: I1d004c5a54fad5231dfe04e1d134a28293993d6d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -242,11 +242,15 @@ public class KeyguardStatusBarView extends RelativeLayout {
     WindowInsets updateWindowInsets(
             WindowInsets insets,
             StatusBarContentInsetsProvider insetsProvider) {
+        updateWindowInsets(insetsProvider);
+        return super.onApplyWindowInsets(insets);
+    }
+
+    private void updateWindowInsets(StatusBarContentInsetsProvider insetsProvider) {
         mLayoutState = LAYOUT_NONE;
         if (updateLayoutConsideringCutout(insetsProvider)) {
             requestLayout();
         }
-        return super.onApplyWindowInsets(insets);
     }
 
     private boolean updateLayoutConsideringCutout(StatusBarContentInsetsProvider insetsProvider) {
@@ -412,9 +416,11 @@ public class KeyguardStatusBarView extends RelativeLayout {
     }
 
     /** Should only be called from {@link KeyguardStatusBarViewController}. */
-    void onThemeChanged(StatusBarIconController.TintedIconManager iconManager) {
+    void onThemeChanged(StatusBarIconController.TintedIconManager iconManager,
+            StatusBarContentInsetsProvider insetsProvider) {
         mBatteryView.setColorsFromContext(mContext);
         updateIconsAndTextColors(iconManager);
+        updateWindowInsets(insetsProvider);
     }
 
     /** Should only be called from {@link KeyguardStatusBarViewController}. */
