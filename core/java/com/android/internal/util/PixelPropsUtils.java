239d60e0aaa255ae471c583935e5e7ab6c355bb1
Yuan Si <do4suki@gmail.com>
2022-06-11 01:15:12 +0800

    PixelPropsUtils: Update build fingerprint to June release
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: I96472b5f92096fa32df28a01a784f14cf8adc820

--- core/java/com/android/internal/util/PixelPropsUtils.java
+++ core/java/com/android/internal/util/PixelPropsUtils.java
@@ -68,7 +68,7 @@ public class PixelPropsUtils {
         propsToChangeP5.put("DEVICE", "redfin");
         propsToChangeP5.put("PRODUCT", "redfin");
         propsToChangeP5.put("MODEL", "Pixel 5");
-        propsToChangeP5.put("FINGERPRINT", "google/redfin/redfin:12/SP2A.220505.002/8353555:user/release-keys");
+        propsToChangeP5.put("FINGERPRINT", "google/redfin/redfin:12/SQ3A.220605.009.A1/8643238:user/release-keys");
         propsToChangeP1 = new HashMap<>();
         propsToChangeP1.put("DEVICE", "marlin");
         propsToChangeP1.put("PRODUCT", "marlin");
