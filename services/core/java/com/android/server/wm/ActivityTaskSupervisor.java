e256ae9069e9ea56ab7c888489e055af4bc86e80
LibXZR <i@xzr.moe>
2022-07-11 20:51:12 +0800

    StrictStandby: Offload force stop onto lock-free context
    Fixes potential dead lock when plenty of tasks get removed at the
    same time.
    
    Change-Id: Ib26dc55bfb3b86b29f25e25887406c7859f66e34
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
+++ services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
@@ -212,6 +212,7 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
     private static final int REPORT_PIP_MODE_CHANGED_MSG = FIRST_SUPERVISOR_TASK_MSG + 15;
     private static final int START_HOME_MSG = FIRST_SUPERVISOR_TASK_MSG + 16;
     private static final int TOP_RESUMED_STATE_LOSS_TIMEOUT_MSG = FIRST_SUPERVISOR_TASK_MSG + 17;
+    private static final int STRICT_STANDBY_KILL_MSG = FIRST_SUPERVISOR_TASK_MSG + 18;
 
     // Used to indicate that windows of activities should be preserved during the resize.
     static final boolean PRESERVE_WINDOWS = true;
@@ -1721,15 +1722,7 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
         if (getAppOpsManager().checkOpNoThrow(
                 AppOpsManager.OP_RUN_ANY_IN_BACKGROUND,
                 task.effectiveUid, pkg) != AppOpsManager.MODE_ALLOWED) {
-            try {
-                ActivityManager.getService().forceStopPackage(pkg, task.mUserId);
-                return;
-            } catch (RemoteException e) {
-                Slog.e(TAG, "Force stop failed, falling back to old path...");
-            } finally {
-                mService.mAppStandbyInternal.restrictApp(
-                        pkg, task.mUserId, REASON_MAIN_FORCED_BY_USER, 0);
-            }
+            mHandler.sendMessage(mHandler.obtainMessage(STRICT_STANDBY_KILL_MSG, task));
         }
 
         ArrayList<Object> procsToKill = new ArrayList<>();
@@ -2595,6 +2588,17 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
                         mService.mAmInternal.killProcess(processName, uid,
                                 "restartActivityProcessTimeout");
                     }
+                } break;
+                case STRICT_STANDBY_KILL_MSG: {
+                    Task task = (Task) msg.obj;
+                    String pkg = task.getBaseIntent().getComponent().getPackageName();
+                    try {
+                        ActivityManager.getService().forceStopPackage(pkg, task.mUserId);
+                    } catch (RemoteException e) {
+                        Slog.e(TAG, "Strict standby force stop failed...");
+                    }
+                    mService.mAppStandbyInternal.restrictApp(
+                            pkg, task.mUserId, REASON_MAIN_FORCED_BY_USER, 0);
                 } break;
             }
         }
