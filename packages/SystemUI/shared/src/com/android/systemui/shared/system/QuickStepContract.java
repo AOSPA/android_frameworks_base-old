db19ebd899b5aa1261300c8696464969b4dfc520
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-27 12:22:55 +0530

    SystemUI: Set launcher activity name to ParanoidLauncher
    This should perhaps be an overlay.
    
    Change-Id: Iaf272b461ffa36873f9baef82aca099068e5146a

--- packages/SystemUI/shared/src/com/android/systemui/shared/system/QuickStepContract.java
+++ packages/SystemUI/shared/src/com/android/systemui/shared/system/QuickStepContract.java
@@ -38,7 +38,7 @@ import java.util.StringJoiner;
 public class QuickStepContract {
     // Fully qualified name of the Launcher activity.
     public static final String LAUNCHER_ACTIVITY_CLASS_NAME =
-            "com.google.android.apps.nexuslauncher.NexusLauncherActivity";
+            "co.aospa.launcher.ParanoidLauncher";
 
     public static final String KEY_EXTRA_SYSUI_PROXY = "extra_sysui_proxy";
     public static final String KEY_EXTRA_WINDOW_CORNER_RADIUS = "extra_window_corner_radius";
