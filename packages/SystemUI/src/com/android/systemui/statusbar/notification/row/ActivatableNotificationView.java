59c71be516ed9e6067cb02854a824f91bfbde13d
DillerOFire <niktofe1@gmail.com>
2022-07-27 15:28:16 +1000

    SystemUI: Switch notification background to monet on heads up
    Change-Id: If1822acc3ea604444f2083d7fadec06ffb8eec19

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ActivatableNotificationView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ActivatableNotificationView.java
@@ -168,7 +168,7 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
      * be useful in a configuration change.
      */
     protected void initBackground() {
-        mBackgroundNormal.setCustomBackground(R.drawable.notification_material_bg);
+        setTranslucentBackground(true);
     }
 
     protected boolean hideBackground() {
@@ -179,6 +179,13 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
         mBackgroundNormal.setVisibility(hideBackground() ? INVISIBLE : VISIBLE);
     }
 
+    public void setTranslucentBackground(boolean translucent) {
+        if (translucent) {
+            mBackgroundNormal.setCustomBackground(R.drawable.notification_material_bg);
+        } else {
+            mBackgroundNormal.setCustomBackground(R.drawable.notification_material_bg_monet);
+        }
+    }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
