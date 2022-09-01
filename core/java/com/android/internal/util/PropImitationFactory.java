7c5e09a24442ab84803e2455bcde552cc3508b69
Omkar Chandorkar <gotenksIN@aospa.co>
2022-09-01 17:18:58 +0530

    Introduce PropImitationFactory
    - A minimal implementation to pass CTS and Play Integrity while
      loading correct ARCore profiles if existing
    
    - Loosely inspired by PixelPropUtils from
      https://github.com/PixelExperience/frameworks_base/blob/twelve/core/java/com/android/internal/util/custom/PixelPropsUtils.java
    
    - Thanks Danny for original idea at
      https://github.com/ProtonAOSP/android_frameworks_base/blob/sc-qpr3/core/java/com/android/internal/gmscompat/AttestationHooks.java
    
    - Thanks Dyneteve for fixing Play Integrity API at
      https://github.com/hentaiOS/platform_frameworks_base/commit/c9a086f9b635bf4e3e61fbd501ade464f8f5b713
    
    Change-Id: I6937258c8297aa909fcdb3b40195cbd6dc0f869b
    Co-authored-by: Chris Crawford <chriscrawford893@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

---
+++ core/java/com/android/internal/util/PropImitationFactory.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2022 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util;
+
+import android.app.Application;
+import android.content.res.Resources;
+import android.os.Build;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+import com.android.internal.R;
+
+public class PropImitationFactory {
+
+    private static final String TAG = "PropImitationFactory";
+    private static final boolean DEBUG = false;
+
+    private static final String pixel_fp =
+            Resources.getSystem().getString(com.android.internal.R.string.pixel_fingerprint);
+
+    private static final String stock_fp =
+            Resources.getSystem().getString(com.android.internal.R.string.stock_fingerprint);
+
+    private static final String PACKAGE_ARCORE = "com.google.ar.core";
+    private static final String PACKAGE_FINSKY = "com.android.vending";
+    private static final String PACKAGE_GMS = "com.google.android.gms";
+
+    private static volatile boolean sIsGms = false;
+    private static volatile boolean sIsFinsky = false;
+
+    public void setProps(Application app) {
+        final String packageName = app.getPackageName();
+        final String processName = app.getProcessName();
+
+        if (packageName == null) {
+            return;
+        }
+
+        if (!pixel_fp.isEmpty() && packageName.equals(PACKAGE_GMS) &&
+                processName.equals(PACKAGE_GMS + ".unstable")) {
+            if (DEBUG) Log.d(TAG, "Defining fingerprint for: " + packageName);
+            setPropValue("FINGERPRINT", pixel_fp);
+            sIsGms = true;
+        }
+
+        if (!pixel_fp.isEmpty() && PACKAGE_FINSKY.equals(app.getPackageName())) {
+            if (DEBUG) Log.d(TAG, "Defining fingerprint for: " + packageName);
+            setPropValue("FINGERPRINT", pixel_fp);
+            sIsFinsky = true;
+        }
+    }
+
+    private void setPropValue(String key, Object value){
+        try {
+            if (DEBUG) Log.d(TAG, "Defining prop " + key + " to " + value.toString());
+            Field field = Build.class.getDeclaredField(key);
+            field.setAccessible(true);
+            field.set(null, value);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to set prop " + key, e);
+        }
+    }
+
+    private static boolean isCallerSafetyNet() {
+        return Arrays.stream(Thread.currentThread().getStackTrace())
+                .anyMatch(elem -> elem.getClassName().contains("DroidGuard"));
+    }
+
+    public static void onEngineGetCertificateChain() {
+        // Check stack for SafetyNet
+        if (sIsGms && isCallerSafetyNet()) {
+            throw new UnsupportedOperationException();
+        }
+
+        // Check stack for PlayIntegrity
+        if (sIsFinsky) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
