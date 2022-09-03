57808483ca90101daa8e04e40261d130774aafcb
rituj <ritujbeniwal@gmail.com>
2022-09-03 02:51:42 -0300

    [SQUASH] base: Add option to cycle through ringer modes [1/2]
    * Settings->System->Gestures->Prevent ringing
    
    core: Remove @SystemApi annotation for VOLUME_HUSH_CYCLE.
    
      This addition should not be annotated with @SystemApi because it
      creates an addition that fails the API test in user builds
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Co-authored-by: Evan Anderson <evananderson@aospa.co>
    Change-Id: I334bd165e970665ac5300d387a0ea6745748d3ad

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10944,7 +10944,7 @@ public final class Settings {
 
         /**
          * What behavior should be invoked when the volume hush gesture is triggered
-         * One of VOLUME_HUSH_OFF, VOLUME_HUSH_VIBRATE, VOLUME_HUSH_MUTE.
+         * One of VOLUME_HUSH_OFF, VOLUME_HUSH_VIBRATE, VOLUME_HUSH_MUTE, VOLUME_HUSH_CYCLE.
          *
          * @hide
          */
@@ -10961,6 +10961,8 @@ public final class Settings {
         /** @hide */
         @SystemApi
         public static final int VOLUME_HUSH_MUTE = 2;
+        /** @hide */
+        public static final int VOLUME_HUSH_CYCLE = 3;
 
         /**
          * The number of times (integer) the user has manually enabled battery saver.
