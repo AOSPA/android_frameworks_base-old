2a81d886440eb13342b4777e2f1e148482e5fcd3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-04 18:19:00 +0530

    SystemUI: Switch to separate drawables for wifi standard
    This enables us to use wifi icon from icon pack,
    along with wifi standard.
    
    Inspired by https://github.com/Project-Kaleidoscope/android_frameworks_base/commit/1302dda146674906783030ff2846b3c17bcf50ad.
    
    Change-Id: Icc35df904b8f72cd2ac8ccc1d275e48d423909b0

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/WifiSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/WifiSignalController.java
@@ -197,7 +197,8 @@ public class WifiSignalController extends SignalController<WifiState, IconGroup>
                 mCurrentState.enabled, statusIcon, qsIcon,
                 ssidPresent && mCurrentState.activityIn,
                 ssidPresent && mCurrentState.activityOut,
-                wifiDesc, mCurrentState.isTransient, mCurrentState.statusLabel
+                wifiDesc, mCurrentState.isTransient, mCurrentState.statusLabel,
+                mCurrentState.wifiStandard
         );
         callback.setWifiIndicators(wifiIndicators);
     }
@@ -259,21 +260,6 @@ public class WifiSignalController extends SignalController<WifiState, IconGroup>
         return getCurrentIconIdForCarrierWifi();
     }
 
-
-    private void updateIconGroup() {
-	if (mCurrentState.wifiStandard == ScanResult.WIFI_STANDARD_11N) {
-            mCurrentState.iconGroup = mWifi4IconGroup;
-        } else if (mCurrentState.wifiStandard == ScanResult.WIFI_STANDARD_11AC) {
-            mCurrentState.iconGroup = mWifi5IconGroup;
-        } else if (mCurrentState.wifiStandard == ScanResult.WIFI_STANDARD_11AX) {
-            mCurrentState.iconGroup = mWifi6IconGroup;
-        } else if (mCurrentState.wifiStandard == ScanResult.WIFI_STANDARD_11BE) {
-            mCurrentState.iconGroup = mWifi7IconGroup;
-        } else {
-            mCurrentState.iconGroup = mDefaultWifiIconGroup;
-        }
-
-    }
     /**
      * Fetches wifi initial state replacing the initial sticky broadcast.
      */
@@ -327,7 +313,8 @@ public class WifiSignalController extends SignalController<WifiState, IconGroup>
         mCurrentState.isCarrierMerged = mWifiTracker.isCarrierMerged;
         mCurrentState.subId = mWifiTracker.subId;
         mCurrentState.wifiStandard = mWifiTracker.wifiStandard;
-        updateIconGroup();
+        mCurrentState.iconGroup = mDefaultWifiIconGroup;
+
     }
 
     boolean isCarrierMergedWifi(int subId) {
