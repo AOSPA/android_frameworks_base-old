1b7f8e3e6d7129bbda3e530a794061bd33b9ecb0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-18 17:20:32 +0530

    SystemUI: Improve statusbar mobile type icons padding
    The new "Silk design" mobile icons introduced in [1]
    have 2dp of spacing inside it on each sides. This is
    quite unnecessary and looks ugly as the mobile layout
    already provides 2.5dp of padding.
    
    For the sake of consistency, remove this spacing using
    insets and rely only on padding. Add space only for
    the second SIM's mobile data type icon.
    
    While we're at it, use 4G icon instead of LTE by default
    to reduce space usage.
    
    [1]: 084c13c8216f6a899cd3eda04fc1d7acff3d1248 ("Update RAT icons to match Silk design")
    
    Change-Id: I4d703e37983ee45da2219936156028d5d40f31a0

--- packages/SettingsLib/src/com/android/settingslib/mobile/MobileMappings.java
+++ packages/SettingsLib/src/com/android/settingslib/mobile/MobileMappings.java
@@ -213,7 +213,7 @@ public class MobileMappings {
         public boolean showAtLeast3G = false;
         public boolean show4gFor3g = false;
         public boolean alwaysShowCdmaRssi = false;
-        public boolean show4gForLte = false;
+        public boolean show4gForLte = true;
         public boolean show4glteForLte = false;
         public boolean hideLtePlus = false;
         public boolean hspaDataDistinguishable;
@@ -249,8 +249,6 @@ public class MobileMappings {
             if (b != null) {
                 config.alwaysShowDataRatIcon = b.getBoolean(
                         CarrierConfigManager.KEY_ALWAYS_SHOW_DATA_RAT_ICON_BOOL);
-                config.show4gForLte = b.getBoolean(
-                        CarrierConfigManager.KEY_SHOW_4G_FOR_LTE_DATA_ICON_BOOL);
                 config.show4glteForLte = b.getBoolean(
                         CarrierConfigManager.KEY_SHOW_4GLTE_FOR_LTE_DATA_ICON_BOOL);
                 config.show4gFor3g = b.getBoolean(
