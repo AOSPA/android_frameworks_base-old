3628b287cc903de58225c4f39c69ac91d0da66f4
timocapa <timo.capa@gmail.com>
2022-09-29 00:43:57 +0200

    Revert "udfps: Add support for udfps on aod without dedicated sensor"
    This reverts commit fa8becd8b99b9e889a97da89ff3200190b2da154.
    
    Change-Id: Id5d33a0c2a90a0d009028a8f54f2c6aa4efca5ac

--- services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintAuthenticationClient.java
+++ services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintAuthenticationClient.java
@@ -142,7 +142,7 @@ class FingerprintAuthenticationClient extends AuthenticationClient<AidlSession> 
     public void onAcquired(@FingerprintAcquired int acquiredInfo, int vendorCode) {
         // For UDFPS, notify SysUI with acquiredInfo, so that the illumination can be turned off
         // for most ACQUIRED messages. See BiometricFingerprintConstants#FingerprintAcquired
-        mSensorOverlays.ifUdfps(controller -> controller.onAcquired(getSensorId(), acquiredInfo, vendorCode));
+        mSensorOverlays.ifUdfps(controller -> controller.onAcquired(getSensorId(), acquiredInfo));
         super.onAcquired(acquiredInfo, vendorCode);
     }
 
