bca9beeb86a2944205e5a7f79937192d43536056
althafvly <althafvly@gmail.com>
2022-09-28 21:06:43 +0530

    SystemUI: QSCustomizer: Fix QS tile reset text color
    - https://gitlab.com/LineageOS/issues/android/-/issues/4873
    
    Change-Id: I49a599f10f68c0c576ec863b919936a3b171f2c6

--- packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -20,6 +20,9 @@ import android.animation.Animator.AnimatorListener;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -75,8 +78,11 @@ public class QSCustomizer extends LinearLayout {
         toolbar.setNavigationIcon(
                 getResources().getDrawable(value.resourceId, mContext.getTheme()));
 
-        toolbar.getMenu().add(Menu.NONE, MENU_RESET, 0, com.android.internal.R.string.reset)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        SpannableString resetText = new SpannableString(
+                mContext.getString(com.android.internal.R.string.reset));
+        resetText.setSpan(new ForegroundColorSpan(isNightMode() ?
+                Color.WHITE : Color.BLACK), 0, resetText.length(), 0);
+        toolbar.getMenu().add(Menu.NONE, MENU_RESET, 0, resetText);
         toolbar.setTitle(R.string.qs_edit);
         mRecyclerView = findViewById(android.R.id.list);
         mTransparentView = findViewById(R.id.customizer_transparent_view);
@@ -85,6 +91,11 @@ public class QSCustomizer extends LinearLayout {
         mRecyclerView.setItemAnimator(animator);
 
         updateTransparentViewHeight();
+    }
+
+    private boolean isNightMode() {
+        return (mContext.getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
     }
 
     void updateResources() {
