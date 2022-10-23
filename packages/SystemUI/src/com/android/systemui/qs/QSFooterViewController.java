4329c73534f039bf7b0530d976a718a444813c56
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-24 00:22:38 +0530

    SystemUI: Show QS data usage suffix only when necessary
    Hide the suffix if we have internet tile in the first
    QS page, since it already shows the current network.
    Also show/hide the suffix upon clicking on usage text.
    
    While we're at it, add 4dp of start padding.
    
    Change-Id: I70087ec222502118bfeae85ec2ed3fd66b168f8a

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterViewController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterViewController.java
@@ -16,6 +16,8 @@
 
 package com.android.systemui.qs;
 
+import static android.provider.Settings.Secure.QS_TILES;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.ClipData;
@@ -37,17 +39,20 @@ import com.android.systemui.qs.dagger.QSScope;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.connectivity.NetworkController;
 import com.android.systemui.statusbar.connectivity.SignalCallback;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.ViewController;
 
 import com.android.settingslib.wifi.WifiStatusTracker;
 
+import java.util.Arrays;
 import javax.inject.Inject;
 
 /**
  * Controller for {@link QSFooterView}.
  */
 @QSScope
-public class QSFooterViewController extends ViewController<QSFooterView> implements QSFooter {
+public class QSFooterViewController extends ViewController<QSFooterView>
+        implements QSFooter, TunerService.Tunable {
 
     private final UserTracker mUserTracker;
     private final QSPanelController mQsPanelController;
@@ -58,6 +63,9 @@ public class QSFooterViewController extends ViewController<QSFooterView> impleme
     private final WifiStatusTracker mWifiTracker;
     private final NetworkController mNetworkController;
     private final Context mContext;
+    private final TunerService mTunerService;
+
+    private static final String INTERNET_TILE = "internet";
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -81,7 +89,8 @@ public class QSFooterViewController extends ViewController<QSFooterView> impleme
             ActivityStarter activityStarter,
             QSPanelController qsPanelController,
             NetworkController networkController,
-            Context context) {
+            Context context,
+            TunerService tunerService) {
         super(view);
         mUserTracker = userTracker;
         mQsPanelController = qsPanelController;
@@ -89,6 +98,7 @@ public class QSFooterViewController extends ViewController<QSFooterView> impleme
         mActivityStarter = activityStarter;
         mNetworkController = networkController;
         mContext = context;
+        mTunerService = tunerService;
         mPageIndicator = mView.findViewById(R.id.footer_page_indicator);
         mEditButton = mView.findViewById(android.R.id.edit);
         mWifiTracker = new WifiStatusTracker(context, context.getSystemService(WifiManager.class),
@@ -116,12 +126,29 @@ public class QSFooterViewController extends ViewController<QSFooterView> impleme
         mWifiTracker.setListening(true);
         onWifiStatusUpdated();
         mNetworkController.addCallback(mSignalCallback);
+        mTunerService.addTunable(this, QS_TILES);
     }
 
     @Override
     protected void onViewDetached() {
         mContext.unregisterReceiver(mReceiver);
         mNetworkController.removeCallback(mSignalCallback);
+        mTunerService.removeTunable(this);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        if (key.equals(QS_TILES)) {
+            if (TextUtils.isEmpty(newValue)) {
+                newValue = mContext.getString(R.string.quick_settings_tiles_default);
+            }
+            int rows = mContext.getResources().getInteger(R.integer.quick_settings_max_rows);
+            int cols = mContext.getResources().getInteger(R.integer.quick_settings_num_columns);
+            // Don't show the suffix if we have internet tile in the first page.
+            mView.setShowSuffix(!Arrays.stream(newValue.split(","))
+                                       .limit(rows * cols)
+                                       .anyMatch(INTERNET_TILE::equals));
+         }
     }
 
     @Override
