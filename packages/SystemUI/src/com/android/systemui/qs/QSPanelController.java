27ab3a79b7d0f914bd10184badd5b2b4cfc36cf6
Andrew Fluck <andrew@aospa.co>
2022-12-15 16:08:56 +0100

    SystemUI: Animate auto brightness toggle
    Also enforce restriction for toggle
    
    Change-Id: Ia80713220e47948a3cdf244f7bf8f69ce4421c08

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelController.java
@@ -105,8 +105,7 @@ public class QSPanelController extends QSPanelControllerBase<QSPanel> {
         mBrightnessSliderController = brightnessSliderFactory.create(getContext(), mView);
         mView.setBrightnessView(mBrightnessSliderController.getRootView());
 
-        mBrightnessController = brightnessControllerFactory.create(
-                mBrightnessSliderController.getIconView(), mBrightnessSliderController);
+        mBrightnessController = brightnessControllerFactory.create(mBrightnessSliderController);
         mBrightnessMirrorHandler = new BrightnessMirrorHandler(mBrightnessController);
         mStatusBarKeyguardViewManager = statusBarKeyguardViewManager;
     }
