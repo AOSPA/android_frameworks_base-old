aac7ea5b91995bdc1977b300cf28fe9ab257a435
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-21 22:18:14 +0530

    Revert^2 "DisplayPowerController: Fix mismerge in proximity check"
    This reverts commit 9f45a8bd3de72bfca8adaefdd08e9223235a05ce.
    
    Change-Id: Icaddae955da11e718427220eb5f15837f76b1afb

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -2469,7 +2469,6 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
 
     private void ignoreProximitySensorUntilChangedInternal() {
         if (!mIgnoreProximityUntilChanged
-                && mPowerRequest.useProximitySensor
                 && mProximity == PROXIMITY_POSITIVE) {
             // Only ignore if it is still reporting positive (near)
             mIgnoreProximityUntilChanged = true;
