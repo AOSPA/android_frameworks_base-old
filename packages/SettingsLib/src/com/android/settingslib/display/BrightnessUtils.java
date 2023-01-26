ea9f6762cec01763b474b862d5a903278fe087e3
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-26 08:35:32 +0800

    BrightnessUtils: Allow maintainer to set desired brightness curve impl
    Change-Id: I7e8f508a45d9d4583141c7dddd57661a5bd0a30c

--- packages/SettingsLib/src/com/android/settingslib/display/BrightnessUtils.java
+++ packages/SettingsLib/src/com/android/settingslib/display/BrightnessUtils.java
@@ -16,14 +16,18 @@
 
 package com.android.settingslib.display;
 
+import android.os.SystemProperties;
 import android.util.MathUtils;
 
 import com.android.internal.display.BrightnessSynchronizer;
 
 public class BrightnessUtils {
 
+    public static final boolean sysUseLowGamma = Boolean.parseBoolean(
+        SystemProperties.get("persist.sys.brightness.low.gamma", "false"));
+
     public static final int GAMMA_SPACE_MIN = 0;
-    public static final int GAMMA_SPACE_MAX = 255;
+    public static final int GAMMA_SPACE_MAX = sysUseLowGamma ? 255 : 65535;
 
     // Hybrid Log Gamma constant values
     private static final float R = 0.5f;
@@ -77,8 +81,20 @@ public class BrightnessUtils {
      * @return The corresponding setting value.
      */
     public static final float convertGammaToLinearFloat(int val, float min, float max) {
-        return MathUtils.constrain(BrightnessSynchronizer.brightnessIntToFloat(val),
-                         min, max);
+        final float normalizedVal = MathUtils.norm(GAMMA_SPACE_MIN, GAMMA_SPACE_MAX, val);
+        final float ret;
+        if (normalizedVal <= R) {
+            ret = MathUtils.sq(normalizedVal / R);
+        } else {
+            ret = MathUtils.exp((normalizedVal - C) / A) + B;
+        }
+
+        // HLG is normalized to the range [0, 12], ensure that value is within that range,
+        // it shouldn't be out of bounds.
+        final float normalizedRet = MathUtils.constrain(ret, 0, 12);
+
+        return sysUseLowGamma ? MathUtils.constrain(BrightnessSynchronizer.brightnessIntToFloat(val),
+                         min, max) : MathUtils.lerp(min, max, normalizedRet / 12);
     }
 
     /**
@@ -116,7 +132,16 @@ public class BrightnessUtils {
      * @return The corresponding slider value
      */
     public static final int convertLinearToGammaFloat(float val, float min, float max) {
-        return BrightnessSynchronizer.brightnessFloatToInt(
-                       MathUtils.constrain(val, min, max));
+        // For some reason, HLG normalizes to the range [0, 12] rather than [0, 1]
+        final float normalizedVal = MathUtils.norm(min, max, val) * 12;
+        final float ret;
+        if (normalizedVal <= 1f) {
+            ret = MathUtils.sqrt(normalizedVal) * R;
+        } else {
+            ret = A * MathUtils.log(normalizedVal - B) + C;
+        }
+
+        return sysUseLowGamma ? BrightnessSynchronizer.brightnessFloatToInt(
+                       MathUtils.constrain(val, min, max)) : Math.round(MathUtils.lerp(GAMMA_SPACE_MIN, GAMMA_SPACE_MAX, ret));
     }
 }
