d106f38d602367c05e5d580f795b10598be238cb
Jake Weinstein <jake@aospa.co>
2023-02-04 13:45:06 +0900

    SystemUI: Remove combined status bar signal icons
    Qualcomm did not remove this correctly in their
    QPR1 merge.
    
    This partially applies Revert^2 "Remove support for COMBINED_SIGNAL_ICONS"
    but not completely because VoLTE and VoWiFi icons depend on
    some of the code from that feature.
    
    Change-Id: I4fc845b2b9b61d4de08ae78eaf334be9a4a628d2
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalControllerFactory.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalControllerFactory.kt
@@ -22,7 +22,6 @@ import android.telephony.TelephonyManager
 import com.android.settingslib.mobile.MobileMappings
 import com.android.settingslib.mobile.MobileStatusTracker
 import com.android.systemui.dagger.SysUISingleton
-import com.android.systemui.flags.FeatureFlags
 import com.android.systemui.util.CarrierConfigTracker
 import javax.inject.Inject
 
@@ -34,7 +33,6 @@ internal class MobileSignalControllerFactory @Inject constructor(
     val context: Context,
     val callbackHandler: CallbackHandler,
     val carrierConfigTracker: CarrierConfigTracker,
-    val featureFlags: FeatureFlags,
 ) {
     fun createMobileSignalController(
         config: MobileMappings.Config,
@@ -63,7 +61,6 @@ internal class MobileSignalControllerFactory @Inject constructor(
             receiverLooper,
             carrierConfigTracker,
             mobileTrackerFactory,
-            featureFlags,
         )
     }
 }
