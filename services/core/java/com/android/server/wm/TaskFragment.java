7c62b5edac1dbdcaf761763573e6ae0343e4152b
pioush.kumar-IN009423 <pioush.kumar@oppo.com>
2023-02-27 11:01:00 +0530

    Pause is getting scheduled 2 times
    Problem: {
        1. open sony liv app.
        2. Play any video.
        3. Press home. Observe app is going to PIP
        4. Now in logs we can see. am_pause_activity 2 times.
        5. erforming pause of activity that is not resumed: {com.jio.jioplay.tv/com.jio.jioplay.tv.activities.HomeActivity}
           02-21 23:47:01.705 18593 18593 E ActivityThread: java.lang.RuntimeException: Performing pause of activity that is not resumed: {com.jio.jioplay.tv/com.jio.jioplay.tv.activities.HomeActivity}
           02-21 23:47:01.705 18593 18593 E ActivityThread: 	at android.app.ActivityThread.performPauseActivity(ActivityThread.java:5342)
           02-21 23:47:01.705 18593 18593 E ActivityThread: 	at android.app.ActivityThread.handlePauseActivity(ActivityThread.java:5302)
           02-21 23:47:01.705 18593 18593 E ActivityThread: 	at android.app.servertransaction.PauseActivityItem.execute(PauseActivityItem.java:53)
    }
    
    Solution: {
        1. Need to define a variable to signify is pauseScheduled. If it is scheduled then do not schedule again for PIP.
    }
    
    Bug: 271360298
    
    Signed-off-by: pioush.kumar-IN009423 <pioush.kumar@oppo.com>
    Change-Id: I033acec4e290875652196506f3c998d9f8c68f7f
    Signed-off-by: pioush.kumar-IN009423 <pioush.kumar@oppo.com>

--- services/core/java/com/android/server/wm/TaskFragment.java
+++ services/core/java/com/android/server/wm/TaskFragment.java
@@ -1651,6 +1651,7 @@ class TaskFragment extends WindowContainer<WindowContainer> {
 
         if (prev.attachedToProcess()) {
             if (shouldAutoPip) {
+                prev.mPauseSchedulePendingForPip = true;
                 boolean didAutoPip = mAtmService.enterPictureInPictureMode(
                         prev, prev.pictureInPictureArgs, false /* fromClient */);
                 ProtoLog.d(WM_DEBUG_STATES, "Auto-PIP allowed, entering PIP mode "
@@ -1714,6 +1715,7 @@ class TaskFragment extends WindowContainer<WindowContainer> {
             boolean pauseImmediately, boolean autoEnteringPip, String reason) {
         ProtoLog.v(WM_DEBUG_STATES, "Enqueueing pending pause: %s", prev);
         try {
+            prev.mPauseSchedulePendingForPip = false;
             EventLogTags.writeWmPauseActivity(prev.mUserId, System.identityHashCode(prev),
                     prev.shortComponentName, "userLeaving=" + userLeaving, reason);
 
