39dcdb1c62ab08033328ad6deb166f4bd73c9ea1
Swarn Singh <quic_swarnkum@quicinc.com>
2023-03-09 14:24:11 +0530

    Add correct copyright marking in Wifi Icon files
    Add correct copyright marking in files required to add changes to
    display Wi-Fi standard in signal icons for Wi-Fi 7 APs.
    
    Change-Id: Ic12b4ba64939cef1b2db09eb99bd9f6f759ebd14
    CRs-Fixed: 3429217

--- packages/SettingsLib/src/com/android/settingslib/Utils.java
+++ packages/SettingsLib/src/com/android/settingslib/Utils.java
@@ -1,3 +1,9 @@
+/*
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
 package com.android.settingslib;
 
 import static android.app.admin.DevicePolicyResources.Strings.Settings.WORK_PROFILE_USER_LABEL;
