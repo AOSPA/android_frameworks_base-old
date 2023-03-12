c96ae29050feff664c5fd4cec998f781678b9301
junklu <quic_junklu@quicinc.com>
2023-03-12 15:19:53 +0800

    SystemUI: Handle the SIM_STATE_NOT_READY state
    The SIM_STATE_NOT_READY state is ignored when the multi-sim
    configuration is dynamically changed between DSDS and SSSS.
    It will lead to sim pin view reappear after entering the correct pin.
    
    In this case, handle SIM_STATE_NOT_READY state when it is received.
    
    Change-Id: I95ec7d07b64c0fc7eec9056b41af94f748d94929
    CRs-Fixed: 3428634

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1916,6 +1916,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                 state = TelephonyManager.SIM_STATE_READY;
             } else if (Intent.SIM_STATE_LOADED.equals(stateExtra)) {
                 state = TelephonyManager.SIM_STATE_LOADED;
+            } else if (Intent.SIM_STATE_NOT_READY.equals(stateExtra)) {
+                state = TelephonyManager.SIM_STATE_NOT_READY;
             } else {
                 state = TelephonyManager.SIM_STATE_UNKNOWN;
             }
@@ -3390,6 +3392,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         mLogger.logSimState(subId, slotId, state);
 
         boolean becameAbsent = false;
+        boolean becameNotReady = false;
         if (!SubscriptionManager.isValidSubscriptionId(subId)) {
             mLogger.w("invalid subId in handleSimStateChange()");
             /* Only handle No SIM(ABSENT) and Card Error(CARD_IO_ERROR) due to
@@ -3408,6 +3411,13 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                 }
             } else if (state == TelephonyManager.SIM_STATE_CARD_IO_ERROR) {
                 updateTelephonyCapable(true);
+            } else if (state == TelephonyManager.SIM_STATE_NOT_READY) {
+                becameNotReady = true;
+                for (SimData data : mSimDatas.values()) {
+                    if (data.slotId == slotId) {
+                        data.simState = TelephonyManager.SIM_STATE_NOT_READY;
+                    }
+                }
             } else {
                 return;
             }
@@ -3425,7 +3435,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
             data.subId = subId;
             data.slotId = slotId;
         }
-        if ((changed || becameAbsent) && state != TelephonyManager.SIM_STATE_UNKNOWN) {
+        if ((changed || becameAbsent || becameNotReady)
+                && state != TelephonyManager.SIM_STATE_UNKNOWN) {
             for (int i = 0; i < mCallbacks.size(); i++) {
                 KeyguardUpdateMonitorCallback cb = mCallbacks.get(i).get();
                 if (cb != null) {
