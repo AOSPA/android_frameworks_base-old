399240c72d18313b8f311d6d4b42c9043bbf7e66
Chenming Huang <quic_chenhuan@quicinc.com>
2023-05-26 13:49:41 +0800

    wifi(statusbar): Disable new wifi icon to display generation info
    New wifi icon only shows wifi signal strength without generation
    info.
    
    To retain wifi generation info, disable new wifi icon.
    
    CRs-Fixed: 3506308
    Change-Id: I037ddb8863acc4b2f5e07fed132cf807d466fe92

--- packages/SystemUI/src/com/android/systemui/flags/Flags.kt
+++ packages/SystemUI/src/com/android/systemui/flags/Flags.kt
@@ -319,7 +319,7 @@ object Flags {
     val NEW_STATUS_BAR_MOBILE_ICONS = releasedFlag(606, "new_status_bar_mobile_icons")
 
     // TODO(b/256614210): Tracking Bug
-    val NEW_STATUS_BAR_WIFI_ICON = releasedFlag(607, "new_status_bar_wifi_icon")
+    val NEW_STATUS_BAR_WIFI_ICON = unreleasedFlag(607, "new_status_bar_wifi_icon")
 
     // TODO(b/256614751): Tracking Bug
     val NEW_STATUS_BAR_MOBILE_ICONS_BACKEND =
