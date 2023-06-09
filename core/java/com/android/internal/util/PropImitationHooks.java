c9a53f78581458332072399d8fb526804bdd3a6b
Timo Dorfner <timo.capa@gmail.com>
2023-06-09 22:02:04 +0000

    Revert "PIHooks: Use certified properties for Snapchat"
    This reverts commit 9ce20b1ce9dbbefec896d1eb68bc2486be8532fa.
    
    Reason for revert: doesn't seem to actually work
    
    Change-Id: Ie12885cde4e1e015f6f76a684b64a0ac8d67d82a

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -39,16 +39,10 @@ public class PropImitationHooks {
     private static final String sStockFp =
             Resources.getSystem().getString(R.string.config_stockFingerprint);
 
-    // Use stock fingerprint for ARCore to load correct profile
     private static final String PACKAGE_ARCORE = "com.google.ar.core";
-
-    // Use certified properties for GMS to pass SafetyNet / Play Integrity
     private static final String PACKAGE_FINSKY = "com.android.vending";
     private static final String PACKAGE_GMS = "com.google.android.gms";
     private static final String PROCESS_GMS_UNSTABLE = PACKAGE_GMS + ".unstable";
-
-    // Use certified properties for Snapchat to prevent send/receive delays
-    private static final String PACKAGE_SNAPCHAT = "com.snapchat.android";
 
     private static volatile boolean sIsGms = false;
     private static volatile boolean sIsFinsky = false;
@@ -65,11 +59,11 @@ public class PropImitationHooks {
         sIsGms = packageName.equals(PACKAGE_GMS) && processName.equals(PROCESS_GMS_UNSTABLE);
         sIsFinsky = packageName.equals(PACKAGE_FINSKY);
 
-        /* Set certified properties for GMSCore / Snapchat
-         * Set stock fingerprint for ARCore
+        /* Set Certified Properties for GMSCore
+         * Set Stock Fingerprint for ARCore
          */
-        if (sCertifiedProps.length == 4 && (sIsGms || packageName.equals(PACKAGE_SNAPCHAT))) {
-            dlog("Spoofing build for: " + packageName);
+        if (sCertifiedProps.length == 4 && sIsGms) {
+            dlog("Spoofing build for GMS");
             setPropValue("DEVICE", sCertifiedProps[0]);
             setPropValue("PRODUCT", sCertifiedProps[1]);
             setPropValue("MODEL", sCertifiedProps[2]);
