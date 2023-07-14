45bc87615e141a10ba87a54786e2ebfdc450cf05
Mengjun Leng <quic_mengjunl@quicinc.com>
2023-07-14 10:23:21 +0530

    Correct the copyright markings on internet dialog
    Change-Id: I8d119ae15f6baa8ca2d152a82a972d607a4b8fd4
    CRs-Fixed: 3503654

--- packages/SystemUI/tests/src/com/android/systemui/qs/tiles/dialog/InternetDialogTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/qs/tiles/dialog/InternetDialogTest.java
@@ -1,3 +1,8 @@
+/**
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
 package com.android.systemui.qs.tiles.dialog;
 
 import static com.android.systemui.qs.tiles.dialog.InternetDialogController.MAX_WIFI_ENTRY_COUNT;
