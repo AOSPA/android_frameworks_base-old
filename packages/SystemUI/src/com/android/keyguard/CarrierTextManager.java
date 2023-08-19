98bb63ca02138bc2e58d2113b0b748cda68c62f3
HELLBOY017 <abhaygill017@gmail.com>
2023-08-19 10:13:08 +0530

    SystemUI: Fix carrier name network type text on QS
    * Fix 4G showing on IWLAN mode even when 5G is being used
    * Use TelephonyDisplayInfo for 5G NSA network type so that 5G icon shows on NSA mode
    
    Change-Id: Ide9090e29c13920c36a8c9c3efde85660825a9e2

--- packages/SystemUI/src/com/android/keyguard/CarrierTextManager.java
+++ packages/SystemUI/src/com/android/keyguard/CarrierTextManager.java
@@ -25,10 +25,13 @@ import android.net.wifi.WifiManager;
 import android.telephony.ServiceState;
 import android.telephony.SubscriptionInfo;
 import android.telephony.TelephonyCallback.ActiveDataSubscriptionIdListener;
+import android.telephony.TelephonyCallback;
+import android.telephony.TelephonyDisplayInfo;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
@@ -37,8 +40,6 @@ import com.android.systemui.R;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
-import com.android.systemui.statusbar.policy.FiveGServiceClient;
-import com.android.systemui.statusbar.policy.FiveGServiceClient.FiveGServiceState;
 import com.android.systemui.telephony.TelephonyListenerManager;
 import com.android.systemui.util.CarrierNameCustomization;
 
@@ -91,9 +92,9 @@ public class CarrierTextManager {
                     if (callback != null) callback.startedGoingToSleep();
                 }
             };
-    private FiveGServiceClient mFiveGServiceClient;
     private CarrierNameCustomization mCarrierNameCustomization;
     private boolean mShowCustomizeName;
+    private TelephonyDisplayInfo mTelephonyDisplayInfo;
     @VisibleForTesting
     protected final KeyguardUpdateMonitorCallback mCallback = new KeyguardUpdateMonitorCallback() {
         @Override
@@ -142,6 +143,17 @@ public class CarrierTextManager {
             }
         }
     };
+
+    private class CarrierTextManagerTelephonyCallback extends TelephonyCallback implements
+            TelephonyCallback.DisplayInfoListener {
+        @Override
+        public void onDisplayInfoChanged(@NonNull TelephonyDisplayInfo displayInfo) {
+            mTelephonyDisplayInfo = displayInfo;
+            updateCarrierText();
+        }
+    }
+
+    private final CarrierTextManagerTelephonyCallback mTelephonyCallback = new CarrierTextManagerTelephonyCallback();
 
     /**
      * The status of this lock screen. Primarily used for widgets on LockScreen.
@@ -275,6 +287,7 @@ public class CarrierTextManager {
                     mWakefulnessLifecycle.addObserver(mWakefulnessObserver);
                 });
                 mTelephonyListenerManager.addActiveDataSubscriptionIdListener(mPhoneStateListener);
+		mTelephonyManager.registerTelephonyCallback(mMainExecutor, mTelephonyCallback);
             } else {
                 // Don't listen and clear out the text when the device isn't a phone.
                 mMainExecutor.execute(() -> callback.updateCarrierInfo(
@@ -288,6 +301,7 @@ public class CarrierTextManager {
                 mWakefulnessLifecycle.removeObserver(mWakefulnessObserver);
             });
             mTelephonyListenerManager.removeActiveDataSubscriptionIdListener(mPhoneStateListener);
+	    mTelephonyManager.unregisterTelephonyCallback(mTelephonyCallback);
         }
     }
 
@@ -770,11 +784,6 @@ public class CarrierTextManager {
         int networkType = getNetworkType(sub.getSubscriptionId());
         String networkClass = networkTypeToString(networkType);
 
-        String fiveGNetworkClass = get5GNetworkClass(sub, networkType);
-        if ( fiveGNetworkClass != null ) {
-            networkClass = fiveGNetworkClass;
-        }
-
         if (!TextUtils.isEmpty(originCarrierName)) {
             String[] names = originCarrierName.toString().split(mSeparator.toString(), 2);
             for (int j = 0; j < names.length; j++) {
@@ -826,7 +835,7 @@ public class CarrierTextManager {
         if (ss != null && (ss.getDataRegState() == ServiceState.STATE_IN_SERVICE
                 || ss.getVoiceRegState() == ServiceState.STATE_IN_SERVICE)) {
             networkType = ss.getDataNetworkType();
-            if (networkType == TelephonyManager.NETWORK_TYPE_UNKNOWN) {
+            if (networkType == TelephonyManager.NETWORK_TYPE_IWLAN || networkType == TelephonyManager.NETWORK_TYPE_UNKNOWN) {
                 networkType = ss.getVoiceNetworkType();
             }
         }
@@ -834,49 +843,27 @@ public class CarrierTextManager {
     }
 
     private String networkTypeToString(int networkType) {
+        final int overrideNetworkType = mTelephonyDisplayInfo == null
+                ? TelephonyDisplayInfo.OVERRIDE_NETWORK_TYPE_NONE
+                : mTelephonyDisplayInfo.getOverrideNetworkType();
+        final boolean is5GNsa = networkType == TelephonyManager.NETWORK_TYPE_LTE 
+                || networkType == TelephonyManager.NETWORK_TYPE_LTE_CA && 
+                (overrideNetworkType == TelephonyDisplayInfo.OVERRIDE_NETWORK_TYPE_NR_ADVANCED
+                || overrideNetworkType == TelephonyDisplayInfo.OVERRIDE_NETWORK_TYPE_NR_NSA);
         int classId = com.android.systemui.R.string.config_rat_unknown;
         long mask = TelephonyManager.getBitMaskForNetworkType(networkType);
         if ((mask & TelephonyManager.NETWORK_CLASS_BITMASK_2G) != 0) {
           classId = com.android.systemui.R.string.config_rat_2g;
         } else if ((mask & TelephonyManager.NETWORK_CLASS_BITMASK_3G) != 0) {
           classId = com.android.systemui.R.string.config_rat_3g;
-        } else if ((mask & TelephonyManager.NETWORK_CLASS_BITMASK_4G) != 0) {
+	      } else if (is5GNsa) {
+          return mContext.getResources().getString(R.string.data_connection_5g);
+        } else if (networkType == TelephonyManager.NETWORK_TYPE_LTE 
+                      || networkType == TelephonyManager.NETWORK_TYPE_LTE_CA) {
           classId = com.android.systemui.R.string.config_rat_4g;
+        } else if (networkType == TelephonyManager.NETWORK_TYPE_NR) {
+          return mContext.getResources().getString(R.string.data_connection_5g);
         }
-        return getContext().getResources().getString(classId);
-    }
-
-
-    private String get5GNetworkClass(SubscriptionInfo sub, int networkType) {
-        if ( networkType == TelephonyManager.NETWORK_TYPE_NR ) {
-            return mContext.getResources().getString(R.string.data_connection_5g);
-        }
-
-        int slotIndex = sub.getSimSlotIndex();
-        int subId = sub.getSubscriptionId();
-
-        if ( mFiveGServiceClient == null ) {
-            mFiveGServiceClient = FiveGServiceClient.getInstance(mContext);
-            mFiveGServiceClient.registerCallback(mCallback);
-        }
-        FiveGServiceState fiveGServiceState =
-                mFiveGServiceClient.getCurrentServiceState(slotIndex);
-        if ( fiveGServiceState.isNrIconTypeValid() && isDataRegisteredOnLte(subId)) {
-            return mContext.getResources().getString(R.string.data_connection_5g);
-        }
-
-        return null;
-    }
-
-    private boolean isDataRegisteredOnLte(int subId) {
-        TelephonyManager telephonyManager = (TelephonyManager)
-                mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        int dataType = telephonyManager.getDataNetworkType(subId);
-        if (  dataType == TelephonyManager.NETWORK_TYPE_LTE ||
-                dataType == TelephonyManager.NETWORK_TYPE_LTE_CA) {
-            return true;
-        }else{
-            return false;
-        }
+	return getContext().getResources().getString(classId);
     }
 }
