4d5073cd2a7e427c8bdee0df8b109dcf753451a1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-30 08:30:35 +0530

    PocketLock: Make usage of light sensor optional
    Some devices with under-display light sensor or other issues might
    want to disable this.
    
    Change-Id: I4aed6f080dd562bf3f23ee87d266bc99e039b1fb

--- core/java/android/pocket/PocketConstants.java
+++ core/java/android/pocket/PocketConstants.java
@@ -1,5 +1,7 @@
 package android.pocket;
 
+import android.content.res.Resources;
+
 /**
  * This class contains global pocket setup constants.
  * @author Carlo Savignano
@@ -19,7 +21,8 @@ public class PocketConstants {
     /**
      * Whether to use light sensor to evaluate pocket state.
      */
-    public static final boolean ENABLE_LIGHT_JUDGE = true;
+    public static final boolean ENABLE_LIGHT_JUDGE = Resources.getSystem().getBoolean(
+        com.android.internal.R.bool.config_pocketModeLightSensorSupported);
 
 
 }
