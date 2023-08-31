b8390ba2b2afd4b8343ff9bd0ca64f34c108dae7
Michael W <baddaemon87@gmail.com>
2023-08-31 18:23:58 +0330

    ChargingControl: No need to log a crash when not supported
    Instead of relying on catching the exception later on, check if
    ChargingControl is actually supported before checking if a specific
    mode is
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: Icaf8d333842c9847bea00bedb4b302b7acd49bb3

--- services/core/java/com/android/server/lineage/health/ChargingControlController.java
+++ services/core/java/com/android/server/lineage/health/ChargingControlController.java
@@ -254,7 +254,7 @@ public class ChargingControlController extends LineageHealthFeature {
 
     public boolean isChargingModeSupported(int mode) {
         try {
-            return (mChargingControl.getSupportedMode() & mode) != 0;
+            return isSupported() && (mChargingControl.getSupportedMode() & mode) != 0;
         } catch (RemoteException e) {
             throw new RuntimeException(e);
         }
