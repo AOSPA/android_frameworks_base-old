201d25c3f0777bbb9122b18ee42595ea46d37f7c
LuK1337 <priv.luk@gmail.com>
2023-08-31 18:27:53 +0330

    ChargingControl: Use ServiceManager.waitForDeclaredService()
    It might take a while until health HAL is up, thus let's just wait for
    it.
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: Ie929a25a2a774aedb4559662e9ad0a47f1b154e6

--- services/core/java/com/android/server/lineage/health/ChargingControlController.java
+++ services/core/java/com/android/server/lineage/health/ChargingControlController.java
@@ -142,7 +142,8 @@ public class ChargingControlController extends LineageHealthFeature {
 
         mContentResolver = mContext.getContentResolver();
         mChargingControl = IChargingControl.Stub.asInterface(
-                ServiceManager.getService(IChargingControl.DESCRIPTOR + "/default"));
+                ServiceManager.waitForDeclaredService(
+                        IChargingControl.DESCRIPTOR + "/default"));
 
         if (mChargingControl == null) {
             Log.i(TAG, "Lineage Health HAL not found");
