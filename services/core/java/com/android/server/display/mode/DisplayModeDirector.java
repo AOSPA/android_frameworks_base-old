9614cbf354c99e7cec6f5800b76b887af3848092
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-05 17:03:03 +0530

    display: Consider peak refresh rate in BrightnessObserver
    If the peak refresh rate set by the user is lower than the low/high
    zone refresh set in the device overlay, it switches to 60Hz. This is
    not ideal, as for example, the user may have set the peak refresh rate
    to 90Hz (while the overlay defines 120Hz as the refresh rate in the low
    brightness zone), but at low brightness the display enters 60Hz instead
    of staying at 90Hz, leading to poor UX.
    
    Use the minimum of the user-set peak brightness and the predefined
    refresh rate of the brightness zone to avoid this behaviour.
    
    Change-Id: Ia69fd5e8221bedb05b968d1eedebb4823ad4a9b7

--- services/core/java/com/android/server/display/mode/DisplayModeDirector.java
+++ services/core/java/com/android/server/display/mode/DisplayModeDirector.java
@@ -1645,6 +1645,7 @@ public class DisplayModeDirector {
         @Nullable
         private SparseArray<RefreshRateRange> mHighZoneRefreshRateForThermals;
         private int mRefreshRateInHighZone;
+        private int mPeakRefreshRate;
 
         @GuardedBy("mLock")
         private @Temperature.ThrottlingStatus int mThermalStatus = Temperature.THROTTLING_NONE;
@@ -1883,10 +1884,16 @@ public class DisplayModeDirector {
         @VisibleForTesting
         public void onRefreshRateSettingChangedLocked(float min, float max) {
             boolean changeable = (max - min > 1f && max > 60f);
-            if (mRefreshRateChangeable != changeable) {
+            int peakRefreshRate = Math.round(Math.max(min, max));
+            if (mRefreshRateChangeable != changeable || mPeakRefreshRate != peakRefreshRate) {
                 mRefreshRateChangeable = changeable;
-                updateSensorStatus();
-                if (!changeable) {
+                mPeakRefreshRate = peakRefreshRate;
+                if (changeable) {
+                    synchronized (mLock) {
+                        onBrightnessChangedLocked();
+                    }
+                } else {
+                    updateSensorStatus();
                     // Revoke previous vote from BrightnessObserver
                     mVotesStorage.updateGlobalVote(Vote.PRIORITY_FLICKER_REFRESH_RATE, null);
                     mVotesStorage.updateGlobalVote(Vote.PRIORITY_FLICKER_REFRESH_RATE_SWITCH, null);
@@ -2215,10 +2222,11 @@ public class DisplayModeDirector {
                 return;
             }
 
+            int refreshRateInLowZone = Math.min(mPeakRefreshRate, mRefreshRateInLowZone);
             boolean insideLowZone = hasValidLowZone() && isInsideLowZone(mBrightness, mAmbientLux);
             if (insideLowZone) {
                 refreshRateVote =
-                        Vote.forPhysicalRefreshRates(mRefreshRateInLowZone, mRefreshRateInLowZone);
+                        Vote.forPhysicalRefreshRates(refreshRateInLowZone, refreshRateInLowZone);
                 if (mLowZoneRefreshRateForThermals != null) {
                     RefreshRateRange range = SkinThermalStatusObserver
                             .findBestMatchingRefreshRateRange(mThermalStatus,
@@ -2231,12 +2239,12 @@ public class DisplayModeDirector {
                 refreshRateSwitchingVote = Vote.forDisableRefreshRateSwitching();
             }
 
+            int refreshRateInHighZone = Math.min(mPeakRefreshRate, mRefreshRateInHighZone);
             boolean insideHighZone = hasValidHighZone()
                     && isInsideHighZone(mBrightness, mAmbientLux);
             if (insideHighZone) {
                 refreshRateVote =
-                        Vote.forPhysicalRefreshRates(mRefreshRateInHighZone,
-                                mRefreshRateInHighZone);
+                        Vote.forPhysicalRefreshRates(refreshRateInHighZone, refreshRateInHighZone);
                 if (mHighZoneRefreshRateForThermals != null) {
                     RefreshRateRange range = SkinThermalStatusObserver
                             .findBestMatchingRefreshRateRange(mThermalStatus,
