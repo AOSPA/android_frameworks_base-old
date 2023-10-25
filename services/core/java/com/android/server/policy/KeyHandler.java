9aa1d9674aed4c633cf81a2ca567d8cf35b37087
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-10-25 21:37:45 +0530

    KeyHandler: Specify reason when waking up the device
    Without this, it defaults to WAKE_REASON_UNKNOWN which isn't included
    in config_face_auth_wake_up_triggers and fails to trigger face unlock...
    
    Change-Id: If355d06438a0b4bd2d15ab270d5d293b9b06eb52

--- services/core/java/com/android/server/policy/KeyHandler.java
+++ services/core/java/com/android/server/policy/KeyHandler.java
@@ -554,12 +554,12 @@ public class KeyHandler {
                 if (mPowerManager.isInteractive()) {
                     return;
                 }
-                mPowerManager.wakeUp(SystemClock.uptimeMillis());
+                wakeUp();
                 handled = true;
                 vibrate = false;
                 break;
             case DIALER:
-                mPowerManager.wakeUp(SystemClock.uptimeMillis());
+                wakeUp();
                 if (isKeyguardShowing()) {
                     dismissKeyguard();
                 }
@@ -599,6 +599,10 @@ public class KeyHandler {
         }
 
         doHapticFeedback(handled, vibrate);
+    }
+
+    private void wakeUp() {
+        mPowerManager.wakeUp(SystemClock.uptimeMillis(), PowerManager.WAKE_REASON_GESTURE, TAG);
     }
 
     private void acquireGestureWakeLock() {
