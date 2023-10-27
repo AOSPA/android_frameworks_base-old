05198025c742c03b36c019ef81d614dc1d4fc881
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-10-27 07:08:57 +0530

    SystemUI: Disable wakeup animations when unlocking with fingerprint
    If the ripple animation is disabled it makes sense to disable these too.
    
    Change-Id: I44542796acdb1dfa2fd7626b0a69386da499da2c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -3160,6 +3160,11 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
         mScrimController.setExpansionAffectsAlpha(!unlocking);
 
+        boolean authRippleEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.AUTH_RIPPLE_ENABLED, 1, UserHandle.USER_CURRENT) == 1;
+        mScrimController.setLaunchingAffordanceWithPreview(!authRippleEnabled
+                && mBiometricUnlockController.isWakeAndUnlock());
+
         if (mAlternateBouncerInteractor.isVisibleState()) {
             if ((!isOccluded() || mShadeSurface.isPanelExpanded())
                     && (mState == StatusBarState.SHADE || mState == StatusBarState.SHADE_LOCKED
