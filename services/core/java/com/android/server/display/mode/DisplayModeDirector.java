33e2a6ede05576b5062f678dad9e8fb0a6ebf6c6
cjh1249131356 <cjh1249131356@gmail.com>
2023-11-21 22:24:08 +0900

    DisplayModeDirector: Allow to enforce user selected resolution
    * On some devices with QHD supported but FHD+ default set in dtb, getDesiredDisplayModeSpecs() always vote for default display mode's width/height,
      which causes it selects a display mode with FHD+ resolution while user actually using QHD resolution. This makes device randomly jump to another
      resolution in some apps (e.g. Google dialer).
    
      This patch hooks primarySummary.width/height to user selected resolution beforing filtering out desired display mode.
      It's a bit hack, but as seen in reversed code, Oplus just did similar thing.
    
      To enable this hack, set config_forceToUseSelectedResolution to true.
    
    Change-Id: Ie611409be6633c2179ae01ce21b31e5940cfd7eb
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- services/core/java/com/android/server/display/mode/DisplayModeDirector.java
+++ services/core/java/com/android/server/display/mode/DisplayModeDirector.java
@@ -50,6 +50,7 @@ import android.provider.DeviceConfig;
 import android.provider.DeviceConfigInterface;
 import android.provider.Settings;
 import android.sysprop.SurfaceFlingerProperties;
+import android.text.TextUtils;
 import android.util.IndentingPrintWriter;
 import android.util.Pair;
 import android.util.Slog;
@@ -93,6 +94,8 @@ public class DisplayModeDirector {
     private static final String TAG = "DisplayModeDirector";
     private boolean mLoggingEnabled;
 
+    private static final String RESOLUTION_METRIC_SETTING_KEY = "user_selected_resolution";
+
     private static final int MSG_REFRESH_RATE_RANGE_CHANGED = 1;
     private static final int MSG_LOW_BRIGHTNESS_THRESHOLDS_CHANGED = 2;
     private static final int MSG_DEFAULT_PEAK_REFRESH_RATE_CHANGED = 3;
@@ -112,6 +115,7 @@ public class DisplayModeDirector {
 
     private final AppRequestObserver mAppRequestObserver;
     private final SettingsObserver mSettingsObserver;
+    private final ResolutionSettingsObserver mResolutionSettingsObserver;
     private final DisplayObserver mDisplayObserver;
     private final UdfpsObserver mUdfpsObserver;
     private final SensorObserver mSensorObserver;
@@ -119,6 +123,7 @@ public class DisplayModeDirector {
     private final SkinThermalStatusObserver mSkinThermalStatusObserver;
     private final DeviceConfigInterface mDeviceConfig;
     private final DeviceConfigDisplaySettings mDeviceConfigDisplaySettings;
+    private final boolean mForceSelectedResolution;
 
     @GuardedBy("mLock")
     @Nullable
@@ -160,6 +165,9 @@ public class DisplayModeDirector {
         mDeviceConfig = injector.getDeviceConfig();
         mDeviceConfigDisplaySettings = new DeviceConfigDisplaySettings();
         mSettingsObserver = new SettingsObserver(context, handler);
+        mForceSelectedResolution = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_forceToUseSelectedResolution);
+        mResolutionSettingsObserver = new ResolutionSettingsObserver(context, handler);
         mBrightnessObserver = new BrightnessObserver(context, handler, injector);
         mDefaultDisplayDeviceConfig = null;
         mUdfpsObserver = new UdfpsObserver();
@@ -182,6 +190,9 @@ public class DisplayModeDirector {
      */
     public void start(SensorManager sensorManager) {
         mSettingsObserver.observe();
+        if (mForceSelectedResolution) {
+            mResolutionSettingsObserver.observe();
+        }
         mDisplayObserver.observe();
         mBrightnessObserver.observe(sensorManager);
         mSensorObserver.observe();
@@ -407,6 +418,15 @@ public class DisplayModeDirector {
                         || primarySummary.width == Vote.INVALID_SIZE) {
                     primarySummary.width = defaultMode.getPhysicalWidth();
                     primarySummary.height = defaultMode.getPhysicalHeight();
+                }
+
+                if (mForceSelectedResolution) {
+                    int width = mResolutionSettingsObserver.getWidth();
+                    int height = mResolutionSettingsObserver.getHeight();
+                    if (width > 0 && height > 0) {
+                        primarySummary.width = width;
+                        primarySummary.height = height;
+                    }
                 }
 
                 availableModes = filterModes(modes, primarySummary);
@@ -1266,6 +1286,61 @@ public class DisplayModeDirector {
             pw.println("  SettingsObserver");
             pw.println("    mDefaultRefreshRate: " + mDefaultRefreshRate);
             pw.println("    mDefaultPeakRefreshRate: " + mDefaultPeakRefreshRate);
+        }
+    }
+
+    final class ResolutionSettingsObserver extends ContentObserver {
+        private final Uri mUserSelectedResolutionUri =
+                Settings.System.getUriFor(RESOLUTION_METRIC_SETTING_KEY);
+
+        private final Context mContext;
+
+        private int mWidth;
+        private int mHeight;
+
+        ResolutionSettingsObserver(@NonNull Context context, @NonNull Handler handler) {
+            super(handler);
+            mContext = context;
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri, int userId) {
+            synchronized (mLock) {
+                if (mUserSelectedResolutionUri.equals(uri)) {
+                    updateResolutionSettingsLocked();
+                }
+            }
+        }
+
+        public void observe() {
+            final ContentResolver cr = mContext.getContentResolver();
+            cr.registerContentObserver(mUserSelectedResolutionUri, false /*notifyDescendants*/,
+                    this, UserHandle.USER_SYSTEM);
+
+            synchronized (mLock) {
+                updateResolutionSettingsLocked();
+            }
+        }
+
+        public int getWidth() {
+            return mWidth;
+        }
+
+        public int getHeight() {
+            return mHeight;
+        }
+
+        private void updateResolutionSettingsLocked() {
+            final ContentResolver cr = mContext.getContentResolver();
+            final String resolution = Settings.System.getString(cr, RESOLUTION_METRIC_SETTING_KEY);
+            if (!TextUtils.isEmpty(resolution) && resolution.contains("x")) {
+                final String[] splited = resolution.split("x");
+                mWidth = Integer.parseInt(splited[0]);
+                mHeight = Integer.parseInt(splited[1]);
+            } else {
+                mWidth = -1;
+                mHeight = -1;
+            }
         }
     }
 
