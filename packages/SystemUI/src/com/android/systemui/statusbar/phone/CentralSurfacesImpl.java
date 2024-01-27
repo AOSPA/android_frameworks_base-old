06c67692fb3aea54a3d0ddf8dd1990e59a6a0f8f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-27 08:02:33 +0530

    SystemUI: Cancel keyguard unlock animation when expanding panel
    Prevent notification panel from having transparent background
    when expanded immediately after unlocking.
    
    Change-Id: I3edd9fd1be79d8eba829730cef6aac24b8740309

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -592,6 +592,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     protected boolean mDozing;
     private boolean mIsFullscreen;
 
+    private float mLastExpansionFraction = 0f;
+
     boolean mCloseQsBeforeScreenOff;
 
     private final NotificationMediaManager mMediaManager;
@@ -1465,6 +1467,18 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
      * keyguard.
      */
     private void dispatchPanelExpansionForKeyguardDismiss(float fraction, boolean trackingTouch) {
+        // If we're expanding the panel right after dismissing keyguard, the unlock animation
+        // might still be running and the panel background will remain transparent until it's
+        // completed (~0.5s). To prevent this, cancel the unlock animation as soon as we start
+        // expanding.
+        if (mLastExpansionFraction == 0f && fraction > 0f && !isKeyguardShowing()
+                && mKeyguardViewMediator.isAnimatingBetweenKeyguardAndSurfaceBehind()) {
+            Log.i(TAG, "cancelling kg exit anim, panel expanding fraction=" + fraction
+                    + " mLastExpansionFraction=" + mLastExpansionFraction);
+            mKeyguardViewMediator.cancelKeyguardExitAnimation();
+            return;
+        }
+
         // Things that mean we're not swiping to dismiss the keyguard, and should ignore this
         // expansion:
         // - Keyguard isn't even visible.
@@ -1514,6 +1528,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                 getShadeViewController().updateSystemUiStateFlags();
             }
         }
+
+        mLastExpansionFraction = fraction;
     }
 
     @VisibleForTesting
