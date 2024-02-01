2c4fc9da7289a649a8578de9f04bb2dd9b45ef0b
Vaibhav Raut <quic_vraut@quicinc.com>
2024-02-01 15:21:38 +0530

    Revert "Dynamically enable multi-peripheral mode"
    This reverts commit f58eb1d14fc386e1634bbe18ec1c7f953fa590eb.
    
    CRs-Fixed: 3721555
    Change-Id: I47d135ddcc6cc7bac1bae4461690b7c3b82bda20

--- services/usb/java/com/android/server/usb/UsbAlsaManager.java
+++ services/usb/java/com/android/server/usb/UsbAlsaManager.java
@@ -38,7 +38,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import android.os.SystemProperties;
 
 /**
  * UsbAlsaManager manages USB audio and MIDI devices.
@@ -49,7 +48,7 @@ public final class UsbAlsaManager {
 
     // Flag to turn on/off multi-peripheral select mode
     // Set to true to have single-device-only mode
-    private static boolean mIsSingleMode = true;
+    private static final boolean mIsSingleMode = true;
 
     private static final String ALSA_DIRECTORY = "/dev/snd/";
 
@@ -136,10 +135,6 @@ public final class UsbAlsaManager {
     private synchronized void selectAlsaDevice(UsbAlsaDevice alsaDevice) {
         if (DEBUG) {
             Slog.d(TAG, "selectAlsaDevice() " + alsaDevice);
-        }
-
-        if ("true".equals(SystemProperties.get("vendor.audio.gaming.enabled", "false"))) {
-            mIsSingleMode = false;
         }
 
         // This must be where an existing USB audio device is deselected.... (I think)
