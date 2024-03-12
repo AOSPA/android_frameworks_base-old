223d4709b71c2f180863694644eef606c246f9e2
COSMIC <rayavanindra@gmail.com>
2024-03-12 13:06:47 +0530

    SystemUI: Partially revert 718d6d6
    - Optimize split notification layout for regular screen
    
    Change-Id: I41a0e4cfa2a8b6902acb901a4f187f703154259b
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SystemUI/src/com/android/systemui/scene/ui/view/WindowRootView.kt
+++ packages/SystemUI/src/com/android/systemui/scene/ui/view/WindowRootView.kt
@@ -101,15 +101,11 @@ open class WindowRootView(
             if (child.layoutParams is LayoutParams) {
                 val layoutParams = child.layoutParams as LayoutParams
                 if (
-                    (layoutParams.rightMargin != rightInset ||
-                        layoutParams.leftMargin != leftInset)
+                    !layoutParams.ignoreRightInset &&
+                        (layoutParams.rightMargin != rightInset ||
+                            layoutParams.leftMargin != leftInset)
                 ) {
-                    val leftMargin = if (layoutParams.ignoreLeftInset) 0 else leftInset
-                    val rightMargin = if (
-                        !layoutParams.ignoreRightInset ||
-                            (layoutParams.ignoreLeftInset && layoutParams.rightMargin == 0)
-                    ) rightInset else 0
-                    layoutParams.updateMargins(left = leftMargin, right = rightMargin)
+                    layoutParams.updateMargins(left = leftInset, right = rightInset)
                     child.requestLayout()
                 }
             }
@@ -138,7 +134,6 @@ open class WindowRootView(
 
     private class LayoutParams : FrameLayout.LayoutParams {
         var ignoreRightInset = false
-        var ignoreLeftInset = false
 
         constructor(
             width: Int,
@@ -161,11 +156,6 @@ open class WindowRootView(
             ignoreRightInset =
                 obtainedAttributes.getBoolean(
                     R.styleable.StatusBarWindowView_Layout_ignoreRightInset,
-                    false
-                )
-            ignoreLeftInset =
-                obtainedAttributes.getBoolean(
-                    R.styleable.StatusBarWindowView_Layout_ignoreLeftInset,
                     false
                 )
             obtainedAttributes.recycle()
