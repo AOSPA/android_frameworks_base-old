39293bc51848ea804fde94cbcb449ef728e849ce
ali-nasiri-80 <alinasiri576@gmail.com>
2024-05-23 21:21:44 +0000

    base: Add Flashlight blink on incoming calls [/3]
    Change-Id: I77a5aa5770cfd7846c4a458c491576cc4e78e42c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6052,6 +6052,28 @@ public final class Settings {
         @Readable
         public static final String NOTIFICATION_VIBRATION_PATTERN = "notification_vibration_pattern";
 
+        /** Whether to blink flashlight for incoming calls
+         * 0 = Disabled (Default)
+         * 1 = Blink flashlight only in Ringer mode
+         * 2 = Blink flashlight only when ringer is not audible
+         * 3 = Blink flahslight only when entirely silent
+         * 4 = Blink flashlight always regardless of ringer mode
+         * @hide
+         */
+        public static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
+
+        /**
+         * Whether flashlight_on_call ignores DND (Zen Mode)
+         * @hide
+         */
+        public static final String FLASHLIGHT_ON_CALL_IGNORE_DND = "flashlight_on_call_ignore_dnd";
+
+        /**
+         * Rate in Hz in which to blink flashlight_on_call
+         * @hide
+         */
+        public static final String FLASHLIGHT_ON_CALL_RATE = "flashlight_on_call_rate";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
