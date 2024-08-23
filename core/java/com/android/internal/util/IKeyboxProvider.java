cbf92bcb911ce8ab7c995df444b4d76f7fdb8140
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-08-24 01:04:33 +0530

    DNM: WIP: Initial implementation of KeyboxImitationHooks
    Change-Id: Id0e093cbbd095792c8925cdb48ae631bf86118ad
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

---
+++ core/java/com/android/internal/util/IKeyboxProvider.java
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Paranoid Android
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.util;
+
+public interface IKeyboxProvider {
+
+    String getEcPrivateKey();
+
+    String getRsaPrivateKey();
+
+    String[] getEcCertificateChain();
+
+    String[] getRsaCertificateChain();
+}
