14e76642f90e6afb6ebc93e694fe1471ffd7dcf6
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-08-24 01:04:33 +0530

    DNM: WIP: Initial implementation of KeyboxImitationHooks
    Change-Id: Id0e093cbbd095792c8925cdb48ae631bf86118ad

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -1,19 +1,8 @@
 /*
- * Copyright (C) 2022 Paranoid Android
- *           (C) 2023 ArrowOS
- *           (C) 2023 The LibreMobileOS Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: 2024 Paranoid Android
+ * SPDX-FileCopyrightText: 2023 ArrowOS
+ * SPDX-FileCopyrightText: 2023 The LibreMobileOS Foundation
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.android.internal.util;
@@ -24,8 +13,8 @@ import android.app.TaskStackListener;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.res.Resources;
-import android.os.Build;
 import android.os.Binder;
+import android.os.Build;
 import android.os.Process;
 import android.os.SystemProperties;
 import android.text.TextUtils;
@@ -34,7 +23,6 @@ import android.util.Log;
 import com.android.internal.R;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
 import java.util.Set;
 
 public class PropImitationHooks {
@@ -207,19 +195,6 @@ public class PropImitationHooks {
             return false;
         }
         return gmsUid == callingUid;
-    }
-
-    private static boolean isCallerSafetyNet() {
-        return sIsGms && Arrays.stream(Thread.currentThread().getStackTrace())
-                .anyMatch(elem -> elem.getClassName().contains("DroidGuard"));
-    }
-
-    public static void onEngineGetCertificateChain() {
-        // Check stack for SafetyNet or Play Integrity
-        if (isCallerSafetyNet() || sIsFinsky) {
-            dlog("Blocked key attestation sIsGms=" + sIsGms + " sIsFinsky=" + sIsFinsky);
-            throw new UnsupportedOperationException();
-        }
     }
 
     public static boolean hasSystemFeature(String name, boolean has) {
